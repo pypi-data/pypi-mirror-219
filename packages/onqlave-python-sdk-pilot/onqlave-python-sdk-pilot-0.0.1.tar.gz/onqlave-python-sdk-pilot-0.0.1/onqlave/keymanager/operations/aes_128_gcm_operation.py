d05cf610e207b36b9dc52219f2fbfea28b1cbc9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:22 +0000

    Add package onqlave-python-sdk-pilot/0.0.1/onqlave-python-sdk-pilot-0.0.1.tar.gz

---
+++ packages/onqlave-python-sdk-pilot/onqlave-python-sdk-pilot-0.0.1.tar.gz/onqlave-python-sdk-pilot-0.0.1/onqlave/keymanager/operations/aes_128_gcm_operation.py
@@ -0,0 +1,25 @@
+from ctypes import c_uint32
+from keymanager.onqlave_types.types import KeyFactory,KeyFormat
+
+AESGCMKeyVersion = 0
+
+class AesGcmKeyFormat:
+    def __init__(self, key_size: c_uint32, version: c_uint32) -> None:
+        self._key_size = key_size
+        self._key_version = version
+    
+    def size(self) -> c_uint32:
+        return self._key_size
+
+class AesGcmKeyOperation:
+    def __init__(self,key_factory: KeyFactory) -> None:
+        self._factory = key_factory
+        self._format = AesGcmKeyFormat(key_size=16,version=AESGCMKeyVersion) # need to change this
+
+    def get_format(self) -> KeyFormat:
+        return self._format
+    
+    def get_factory(self) -> KeyFactory:
+        return self._factory
+
+    
