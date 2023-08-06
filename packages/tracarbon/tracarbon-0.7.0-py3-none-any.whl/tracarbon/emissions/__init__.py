ec61c5d45773d759c689f6f6940aa644cdd47edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:36 +0000

    Add package tracarbon/0.7.0/tracarbon-0.7.0-py3-none-any.whl

---
+++ packages/tracarbon/tracarbon-0.7.0-py3-none-any.whl/tracarbon/emissions/__init__.py
@@ -0,0 +1,5 @@
+from tracarbon.emissions.carbon_emissions import (
+    CarbonEmission,
+    CarbonUsage,
+    CarbonUsageUnit,
+)
---
+++ packages/tracarbon/tracarbon-0.7.0-py3-none-any.whl/tracarbon/exporters/stdout.py
@@ -0,0 +1,32 @@
+from loguru import logger
+
+from tracarbon.exporters.exporter import Exporter, MetricGenerator
+
+
+class StdoutExporter(Exporter):
+    """
+    Print the metrics to Stdout.
+    """
+
+    async def launch(self, metric_generator: MetricGenerator) -> None:
+        """
+        Launch the Stdout exporter with the metrics.
+
+        :param metric_generator: the metric generator
+        """
+        async for metric in metric_generator.generate():
+            metric_value = await metric.value()
+            if metric_value:
+                logger.info(
+                    f"Metric name[{metric.format_name(metric_prefix_name=self.metric_prefix_name)}], value[{metric_value}], tags{metric.format_tags()}"
+                )
+                self.add_metric_to_report(metric=metric, value=metric_value)
+
+    @classmethod
+    def get_name(cls) -> str:
+        """
+        Get the name of the exporter.
+
+        :return: the Exporter's name
+        """
+        return "Stdout"
