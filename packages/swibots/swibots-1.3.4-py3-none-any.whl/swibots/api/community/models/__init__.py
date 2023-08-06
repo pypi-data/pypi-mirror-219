86cb454b07bca2fd99067d572ccdaf6ab74b3903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:50 +0000

    Add package swibots/1.3.4/swibots-1.3.4-py3-none-any.whl

---
+++ packages/swibots/swibots-1.3.4-py3-none-any.whl/swibots/api/community/models/__init__.py
@@ -0,0 +1,9 @@
+from .channel import Channel
+from .community import Community
+from .group import Group
+from .role import Role
+from .rolepermission import RolePermission
+from .rolemember import RoleMember
+from .baninfo import BanInfo
+
+__all__ = ["Channel", "Community", "Group", "Role", "RolePermission", "RoleMember", "BanInfo"]
