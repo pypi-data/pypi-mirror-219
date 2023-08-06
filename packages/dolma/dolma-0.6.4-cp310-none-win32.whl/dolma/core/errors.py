6821487a55b33d559fe12df74a40c013a6bf9b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:28 +0000

    Add package dolma/0.6.4/dolma-0.6.4-cp310-none-win32.whl

---
+++ packages/dolma/dolma-0.6.4-cp310-none-win32.whl/dolma/core/errors.py
@@ -0,0 +1,18 @@
+class DolmaError(Exception):
+    """Base class for all errors"""
+
+
+class DolmaFatalError(DolmaError):
+    """Fatal error. Abort the entire process"""
+
+
+class DolmaShardError(DolmaError):
+    """Fail the shard and continue"""
+
+
+class DolmaRetryableFailure(DolmaError):
+    """Retry if a shard throws this error"""
+
+
+class DolmaRustPipelineError(DolmaError):
+    """Error raised by the rust pipeline"""
