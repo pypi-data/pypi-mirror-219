2c4c6eb5c27b416c8a6f0ac0055397c70c419af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:25 +0000

    Add package factool/0.0.1/factool-0.0.1-py2.py3-none-any.whl

---
+++ packages/factool/factool-0.0.1-py2.py3-none-any.whl/factool/knowledge_qa/tool.py
@@ -0,0 +1,11 @@
+from factool.knowledge_qa.google_serper import GoogleSerperAPIWrapper
+
+class google_search():
+    def __init__(self, snippet_cnt):
+        self.serper = GoogleSerperAPIWrapper(snippet_cnt=snippet_cnt)
+
+    async def run(self, queries):
+        return await self.serper.run(queries)
+
+if __name__ == "__main__":
+    pass
