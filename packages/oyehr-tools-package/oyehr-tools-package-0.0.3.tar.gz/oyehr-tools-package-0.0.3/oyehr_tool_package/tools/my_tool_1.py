bc89c2e769790e0d02e16ba33e5a041cc48eae46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:42 +0000

    Add package oyehr-tools-package/0.0.3/oyehr-tools-package-0.0.3.tar.gz

---
+++ packages/oyehr-tools-package/oyehr-tools-package-0.0.3.tar.gz/oyehr-tools-package-0.0.3/oyehr_tool_package/tools/my_tool_1.py
@@ -0,0 +1,15 @@
+from promptflow import tool
+from promptflow.connections import CustomConnection
+from promptflow.core.tools_manager import register_builtin_method
+
+
+@tool
+def my_tool(connection: CustomConnection, input_text: str) -> str:
+    # Replace with your tool code.
+    # Usually connection contains configs to connect to an API.
+    # Use CustomConnection is a dict. You can use it like: connection.api_key, connection.api_base
+    # Not all tools need a connection. You can remove it if you don't need it.
+    return "Hello " + input_text
+
+
+register_builtin_method(my_tool)
---
+++ packages/oyehr-tools-package/oyehr-tools-package-0.0.3.tar.gz/oyehr-tools-package-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="oyehr-tools-package",
+    version="0.0.3",
+    description="This is my tools package",
+    packages=find_packages(),
+    entry_points={
+        "package_tools": ["oyehr_tools = oyehr_tool_package.tools.utils:list_package_tools"],
+    },
+    include_package_data=True,   # This line tells setuptools to include files from MANIFEST.in
+)
