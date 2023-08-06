e011d00f7b4ae381c8ac5bdb5d9b4103e0ca9856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:49 +0000

    Add package Arl-Gauss-Binom/0.1/Arl_Gauss_Binom-0.1.tar.gz

---
+++ packages/Arl-Gauss-Binom/Arl_Gauss_Binom-0.1.tar.gz/Arl_Gauss_Binom-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Arl_Gauss_Binom',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Arl_Gauss_Binom'],
+      zip_safe=False)
