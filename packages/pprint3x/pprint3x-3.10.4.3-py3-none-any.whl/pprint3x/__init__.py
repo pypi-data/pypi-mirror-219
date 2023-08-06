69c11375a8e7247cbb9d33981c6f53345116f039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:06 +0000

    Add package pprint3x/3.10.4.3/pprint3x-3.10.4.3-py3-none-any.whl

---
+++ packages/pprint3x/pprint3x-3.10.4.3-py3-none-any.whl/pprint3x/__init__.py
@@ -0,0 +1,11 @@
+from .pprint3x import *
+
+from . import __init__conf__
+
+__title__ = __init__conf__.title
+__version__ = __init__conf__.version
+__name__ = __init__conf__.name
+__url__ = __init__conf__.url
+__author__ = __init__conf__.author
+__author_email__ = __init__conf__.author_email
+__shell_command__ = __init__conf__.shell_command
