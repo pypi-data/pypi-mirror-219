527810a302b1f15b8cdf8643e3ba67697699376f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:14 +0000

    Add package pdm-bump/0.7.2/pdm_bump-0.7.2-py3-none-any.whl

---
+++ packages/pdm-bump/pdm_bump-0.7.2-py3-none-any.whl/pdm_bump/vcs/__init__.py
@@ -0,0 +1,40 @@
+#
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021-2023 Carsten Igel.
+#
+# This file is part of pdm-bump
+# (see https://github.com/carstencodes/pdm-bump).
+#
+# This file is published using the MIT license.
+# Refer to LICENSE for more information
+#
+
+""""""
+
+
+from ..core.loader import loader as _loader
+from .core import (
+    DefaultVcsProvider,
+    VcsProvider,
+    VcsProviderAggregator,
+    VcsProviderFactory,
+    VcsProviderRegistry,
+    vcs_provider,
+    vcs_providers,
+)
+
+# Justification: Add items to registry
+_loader.load_modules_of_package(
+    __file__, __name__, "__init__", "core", "mixins"
+)
+
+__all__ = (
+    "vcs_provider",
+    "vcs_providers",
+    "VcsProvider",
+    "VcsProviderAggregator",
+    "VcsProviderFactory",
+    "VcsProviderRegistry",
+    "DefaultVcsProvider",
+)
