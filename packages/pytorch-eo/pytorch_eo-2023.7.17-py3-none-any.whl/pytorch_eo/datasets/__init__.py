b6a656809ef743188ed3a4ad53e3d3b4c0e4a786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:50 +0000

    Add package pytorch-eo/2023.7.17/pytorch_eo-2023.7.17-py3-none-any.whl

---
+++ packages/pytorch-eo/pytorch_eo-2023.7.17-py3-none-any.whl/pytorch_eo/datasets/__init__.py
@@ -0,0 +1,8 @@
+from .RGBImageDataset import RGBImageDataset
+from .ConcatDataset import ConcatDataset
+from .SensorImageDataset import SensorImageDataset
+from .CategoricalImageDataset import CategoricalImageDataset
+from .SingleBandImageDataset import SingleBandImageDataset
+
+from .eurosat import EuroSATRGB, EuroSAT
+from .ucmerced import UCMercedLandUse
