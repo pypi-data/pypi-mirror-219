a1a43a9c665f155bec065bfb3691ee7f7bbc77e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:14 +0000

    Add package nnx/0.0.8/nnx-0.0.8.tar.gz

---
+++ packages/nnx/nnx-0.0.8.tar.gz/nnx-0.0.8/nnx/__init__.py
@@ -0,0 +1,71 @@
+__version__ = "0.0.0"
+
+
+from .containers import (
+    BatchStat,
+    Cache,
+    Container,
+    ContainerMetadata,
+    Intermediate,
+    Node,
+    Param,
+    Static,
+    Variable,
+    with_metadata,
+)
+from .contextlib import Context, context
+from .dataclasses import (
+    dataclass,
+    field,
+    node_field,
+    param_field,
+    static_field,
+    var_field,
+)
+from .errors import TraceContextError
+from .helpers import Dict, Sequence, TrainState
+from .module import Module, ModuleDef, Pure, PureModule
+from .nn import initializers
+from .nn.activations import (
+    celu,
+    elu,
+    gelu,
+    glu,
+    hard_sigmoid,
+    hard_silu,
+    hard_swish,
+    hard_tanh,
+    leaky_relu,
+    log_sigmoid,
+    log_softmax,
+    logsumexp,
+    normalize,
+    one_hot,
+    relu,
+    relu6,
+    selu,
+    sigmoid,
+    silu,
+    soft_sign,
+    softmax,
+    softplus,
+    standardize,
+    swish,
+    tanh,
+)
+from .nn.linear import Conv, Embed, Linear
+from .nn.normalization import BatchNorm, LayerNorm
+from .nn.stochastic import Dropout
+from .nodes import is_node, register_node_type
+from .partitioning import All, Not, buffers
+from .pytreelib import Pytree, TreeNode
+from .spmd import (
+    PARTITION_NAME,
+    get_partition_spec,
+    logical_axis_rules,
+    logical_to_mesh,
+    with_logical_constraint,
+    with_logical_partitioning,
+)
+from .state import State
+from .transforms import Remat, Scan, grad, jit, remat, scan
