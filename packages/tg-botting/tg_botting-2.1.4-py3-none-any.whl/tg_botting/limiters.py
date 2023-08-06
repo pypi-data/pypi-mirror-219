33b9a9d2e6c484c93f0aead5f2f13d17917510a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:37 +0000

    Add package tg-botting/2.1.4/tg_botting-2.1.4-py3-none-any.whl

---
+++ packages/tg-botting/tg_botting-2.1.4-py3-none-any.whl/tg_botting/limiters.py
@@ -0,0 +1,25 @@
+from tg_botting import Command
+
+def check(predicate):
+
+    def decorator(func):
+        if isinstance(func, Command):
+            func.checks.append(predicate)
+        else:
+            if not hasattr(func, '__commands_checks__'):
+                func.__commands_checks__ = []
+
+            func.__commands_checks__.append(predicate)
+
+        return func
+    return decorator
+
+
+def in_user_list(*ids):
+    ids = list(map(int, ids))
+
+    def predicate(ctx):
+        if ctx.message.from_id in ids:
+            return True
+
+    return check(predicate)
