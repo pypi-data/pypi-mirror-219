202caf4cccbc348648c0848260c6947717f77653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:20 +0000

    Add package scikit-rt/0.5.13/scikit_rt-0.5.13-py3-none-any.whl

---
+++ packages/scikit-rt/scikit_rt-0.5.13-py3-none-any.whl/skrt/__init__.py
@@ -0,0 +1,19 @@
+import pkg_resources
+import warnings
+
+from skrt.better_viewer import BetterViewer
+from skrt.better_viewer.options import set_viewer_options
+from skrt.core import Defaults
+from skrt.image import Image
+from skrt.dose import Dose
+from skrt.registration import Registration
+from skrt.simulation import SyntheticImage
+from skrt.structures import ROI, StructureSet
+from skrt.patient import Patient, Study
+
+# Suppress warning from pydicom.
+warnings.filterwarnings("ignore", message="Invalid value for VR UI",
+        module="pydicom")
+
+# Assign version number.
+__version__ = pkg_resources.get_distribution("scikit-rt").version
