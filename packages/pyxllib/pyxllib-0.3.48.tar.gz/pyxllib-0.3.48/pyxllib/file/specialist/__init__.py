ea254be5d199535807338bcdcbfbed60b789b487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:40 +0000

    Add package pyxllib/0.3.48/pyxllib-0.3.48.tar.gz

---
+++ packages/pyxllib/pyxllib-0.3.48.tar.gz/pyxllib-0.3.48/pyxllib/file/specialist/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author : 陈坤泽
+# @Email  : 877362867@qq.com
+# @Date   : 2021/06/06 17:46
+
+
+from pyxllib.file.specialist.filelib import *
+from pyxllib.file.specialist.dirlib import *
+from pyxllib.file.specialist.download import *
