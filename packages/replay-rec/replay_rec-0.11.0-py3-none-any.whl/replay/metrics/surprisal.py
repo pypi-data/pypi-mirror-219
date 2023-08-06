04342ca5acda26df278f1e5f9479101728e7b40d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:16 +0000

    Add package replay-rec/0.11.0/replay_rec-0.11.0-py3-none-any.whl

---
+++ packages/replay-rec/replay_rec-0.11.0-py3-none-any.whl/replay/metrics/surprisal.py
@@ -0,0 +1,128 @@
+from functools import partial
+from typing import Optional
+
+import numpy as np
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as sf
+from pyspark.sql import types as st
+
+from replay.constants import AnyDataFrame
+from replay.metrics.base_metric import (
+    fill_na_with_empty_array,
+    RecOnlyMetric,
+    sorter,
+)
+from replay.utils import convert2spark, get_top_k_recs
+
+
+# pylint: disable=too-few-public-methods
+class Surprisal(RecOnlyMetric):
+    """
+    Measures how many surprising rare items are present in recommendations.
+
+    .. math::
+        \\textit{Self-Information}(j)= -\log_2 \\frac {u_j}{N}
+
+    :math:`u_j` -- number of users that interacted with item :math:`j`.
+    Cold items are treated as if they were rated by 1 user.
+    That is, if they appear in recommendations it will be completely unexpected.
+
+    Metric is normalized.
+
+    Surprisal for item :math:`j` is
+
+    .. math::
+        Surprisal(j)= \\frac {\\textit{Self-Information}(j)}{log_2 N}
+
+    Recommendation list surprisal is the average surprisal of items in it.
+
+    .. math::
+        Surprisal@K(i) = \\frac {\sum_{j=1}^{K}Surprisal(j)} {K}
+
+    Final metric is averaged by users.
+
+    .. math::
+        Surprisal@K = \\frac {\sum_{i=1}^{N}Surprisal@K(i)}{N}
+    """
+
+    _scala_udf_name = "getSurprisalMetricValue"
+
+    def __init__(
+        self, log: AnyDataFrame,
+        use_scala_udf: bool = False
+    ):  # pylint: disable=super-init-not-called
+        """
+        Here we calculate self-information for each item
+
+        :param log: historical data
+        """
+        self._use_scala_udf = use_scala_udf
+        self.log = convert2spark(log)
+        n_users = self.log.select("user_idx").distinct().count()  # type: ignore
+        self.item_weights = self.log.groupby("item_idx").agg(
+            (
+                sf.log2(n_users / sf.countDistinct("user_idx"))  # type: ignore
+                / np.log2(n_users)
+            ).alias("rec_weight")
+        )
+
+    @staticmethod
+    def _get_metric_value_by_user(k, *args):
+        weigths = args[0]
+        return sum(weigths[:k]) / k
+
+    def _get_enriched_recommendations(
+        self,
+        recommendations: DataFrame,
+        ground_truth: DataFrame,
+        max_k: int,
+        ground_truth_users: Optional[AnyDataFrame] = None,
+    ) -> DataFrame:
+        recommendations = convert2spark(recommendations)
+        ground_truth_users = convert2spark(ground_truth_users)
+        recommendations = get_top_k_recs(recommendations, max_k)
+        sort_udf = sf.udf(
+            partial(sorter, extra_position=2),
+            returnType=st.StructType(
+                [
+                    st.StructField(
+                        "pred",
+                        st.ArrayType(
+                            self.item_weights.schema["item_idx"].dataType
+                        ),
+                    ),
+                    st.StructField(
+                        "rec_weight",
+                        st.ArrayType(
+                            self.item_weights.schema["rec_weight"].dataType
+                        ),
+                    ),
+                ],
+            ),
+        )
+        recommendations = (
+            recommendations.join(self.item_weights, on="item_idx", how="left")
+            .fillna(1.0)
+            .groupby("user_idx")
+            .agg(
+                sf.collect_list(
+                    sf.struct("relevance", "item_idx", "rec_weight")
+                ).alias("rel_id_weight")
+            )
+            .withColumn("pred_rec_weight", sort_udf(sf.col("rel_id_weight")))
+            .select(
+                "user_idx",
+                sf.col("pred_rec_weight.rec_weight").alias("rec_weight"),
+            )
+        )
+
+        if ground_truth_users is not None:
+            recommendations = fill_na_with_empty_array(
+                recommendations.join(
+                    ground_truth_users, on="user_idx", how="right"
+                ),
+                "rec_weight",
+                self.item_weights.schema["rec_weight"].dataType,
+            )
+
+        return recommendations
---
+++ packages/replay-rec/replay_rec-0.11.0-py3-none-any.whl/replay/models/word2vec.py
@@ -0,0 +1,227 @@
+from typing import Optional
+
+from pyspark.ml.feature import Word2Vec
+from pyspark.sql import DataFrame
+from pyspark.sql import functions as sf
+from pyspark.sql import types as st
+from pyspark.ml.stat import Summarizer
+
+from replay.models.base_rec import Recommender, ItemVectorModel
+from replay.utils import vector_dot, multiply_scala_udf, join_with_col_renaming
+
+
+# pylint: disable=too-many-instance-attributes
+class Word2VecRec(Recommender, ItemVectorModel):
+    """
+    Trains word2vec model where items ar treated as words and users as sentences.
+    """
+
+    idf: DataFrame
+    vectors: DataFrame
+
+    can_predict_cold_users = True
+    _search_space = {
+        "rank": {"type": "int", "args": [50, 300]},
+        "window_size": {"type": "int", "args": [1, 100]},
+        "use_idf": {"type": "categorical", "args": [True, False]},
+    }
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        rank: int = 100,
+        min_count: int = 5,
+        step_size: int = 0.025,
+        max_iter: int = 1,
+        window_size: int = 1,
+        use_idf: bool = False,
+        seed: Optional[int] = None,
+        num_partitions: Optional[int] = None,
+    ):
+        """
+        :param rank: embedding size
+        :param min_count: the minimum number of times a token must
+            appear to be included in the word2vec model's vocabulary
+        :param step_size: step size to be used for each iteration of optimization
+        :param max_iter: max number of iterations
+        :param window_size: window size
+        :param use_idf: flag to use inverse document frequency
+        :param seed: random seed
+        """
+
+        self.rank = rank
+        self.window_size = window_size
+        self.use_idf = use_idf
+        self.min_count = min_count
+        self.step_size = step_size
+        self.max_iter = max_iter
+        self._seed = seed
+        self._num_partitions = num_partitions
+
+    @property
+    def _init_args(self):
+        return {
+            "rank": self.rank,
+            "window_size": self.window_size,
+            "use_idf": self.use_idf,
+            "min_count": self.min_count,
+            "step_size": self.step_size,
+            "max_iter": self.max_iter,
+            "seed": self._seed,
+        }
+
+    def _fit(
+        self,
+        log: DataFrame,
+        user_features: Optional[DataFrame] = None,
+        item_features: Optional[DataFrame] = None,
+    ) -> None:
+        self.idf = (
+            log.groupBy("item_idx")
+            .agg(sf.countDistinct("user_idx").alias("count"))
+            .withColumn(
+                "idf",
+                sf.log(sf.lit(self.users_count) / sf.col("count"))
+                if self.use_idf
+                else sf.lit(1.0),
+            )
+            .select("item_idx", "idf")
+        )
+        self.idf.cache().count()
+
+        log_by_users = (
+            log.groupBy("user_idx")
+            .agg(
+                sf.collect_list(sf.struct("timestamp", "item_idx")).alias(
+                    "ts_item_idx"
+                )
+            )
+            .withColumn("ts_item_idx", sf.array_sort("ts_item_idx"))
+            .withColumn(
+                "items",
+                sf.col("ts_item_idx.item_idx").cast(
+                    st.ArrayType(st.StringType())
+                ),
+            )
+            .drop("ts_item_idx")
+        )
+
+        self.logger.debug("Model training")
+
+        if self._num_partitions is None:
+            self._num_partitions = log_by_users.rdd.getNumPartitions()
+
+        word_2_vec = Word2Vec(
+            vectorSize=self.rank,
+            minCount=self.min_count,
+            numPartitions=self._num_partitions,
+            stepSize=self.step_size,
+            maxIter=self.max_iter,
+            inputCol="items",
+            outputCol="w2v_vector",
+            windowSize=self.window_size,
+            seed=self._seed,
+        )
+        self.vectors = (
+            word_2_vec.fit(log_by_users)
+            .getVectors()
+            .select(sf.col("word").cast("int").alias("item"), "vector")
+        )
+        self.vectors.cache().count()
+
+    def _clear_cache(self):
+        if hasattr(self, "idf") and hasattr(self, "vectors"):
+            self.idf.unpersist()
+            self.vectors.unpersist()
+
+    @property
+    def _dataframes(self):
+        return {"idf": self.idf, "vectors": self.vectors}
+
+    def _get_user_vectors(
+        self,
+        users: DataFrame,
+        log: DataFrame,
+    ) -> DataFrame:
+        """
+        :param users: user ids, dataframe ``[user_idx]``
+        :param log: interaction dataframe
+            ``[user_idx, item_idx, timestamp, relevance]``
+        :return: user embeddings dataframe
+            ``[user_idx, user_vector]``
+        """
+        res = join_with_col_renaming(
+            log, users, on_col_name="user_idx", how="inner"
+        )
+        res = join_with_col_renaming(
+            res, self.idf, on_col_name="item_idx", how="inner"
+        )
+        res = res.join(
+            self.vectors.hint("broadcast"),
+            how="inner",
+            on=sf.col("item_idx") == sf.col("item"),
+        ).drop("item")
+        return (
+            res.groupby("user_idx")
+            .agg(
+                Summarizer.mean(
+                    multiply_scala_udf(sf.col("idf"), sf.col("vector"))
+                ).alias("user_vector")
+            )
+            .select("user_idx", "user_vector")
+        )
+
+    def _predict_pairs_inner(
+        self,
+        pairs: DataFrame,
+        log: DataFrame,
+    ) -> DataFrame:
+        if log is None:
+            raise ValueError(
+                f"log is not provided, {self} predict requires log."
+            )
+
+        user_vectors = self._get_user_vectors(
+            pairs.select("user_idx").distinct(), log
+        )
+        pairs_with_vectors = join_with_col_renaming(
+            pairs, user_vectors, on_col_name="user_idx", how="inner"
+        )
+        pairs_with_vectors = pairs_with_vectors.join(
+            self.vectors, on=sf.col("item_idx") == sf.col("item"), how="inner"
+        ).drop("item")
+        return pairs_with_vectors.select(
+            "user_idx",
+            sf.col("item_idx"),
+            (
+                vector_dot(sf.col("vector"), sf.col("user_vector"))
+                + sf.lit(self.rank)
+            ).alias("relevance"),
+        )
+
+    # pylint: disable=too-many-arguments
+    def _predict(
+        self,
+        log: DataFrame,
+        k: int,
+        users: DataFrame,
+        items: DataFrame,
+        user_features: Optional[DataFrame] = None,
+        item_features: Optional[DataFrame] = None,
+        filter_seen_items: bool = True,
+    ) -> DataFrame:
+        return self._predict_pairs_inner(users.crossJoin(items), log)
+
+    def _predict_pairs(
+        self,
+        pairs: DataFrame,
+        log: Optional[DataFrame] = None,
+        user_features: Optional[DataFrame] = None,
+        item_features: Optional[DataFrame] = None,
+    ) -> DataFrame:
+        return self._predict_pairs_inner(pairs, log)
+
+    def _get_item_vectors(self):
+        return self.vectors.withColumnRenamed(
+            "vector", "item_vector"
+        ).withColumnRenamed("item", "item_idx")
