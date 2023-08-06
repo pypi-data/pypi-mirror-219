99311c19e349c196ecd8ab9bc0edf13a11216e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:49 +0000

    Add package psychopy-whisper/0.0.1/psychopy_whisper-0.0.1-py3-none-any.whl

---
+++ packages/psychopy-whisper/psychopy_whisper-0.0.1-py3-none-any.whl/tests/test_sound_transcription_whisper.py
@@ -0,0 +1,11 @@
+import psychopy_whisper as pw
+
+
+def test_sound_transcription():
+    """Audio transcription test using OpenAI Whisper.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    test_sound_transcription()
