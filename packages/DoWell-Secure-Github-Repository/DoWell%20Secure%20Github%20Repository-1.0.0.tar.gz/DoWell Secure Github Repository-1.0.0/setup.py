863b5a13df5bacdc8ae6c417985011cc585feaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:03 +0000

    Add package DoWell-Secure-Github-Repository/1.0.0/DoWell%20Secure%20Github%20Repository-1.0.0.tar.gz

---
+++ packages/DoWell-Secure-Github-Repository/DoWell%20Secure%20Github%20Repository-1.0.0.tar.gz/DoWell Secure Github Repository-1.0.0/setup.py
@@ -0,0 +1,24 @@
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name="DoWell Secure Github Repository",
+    version="1.0.0",
+    description="Dowell secure github repository from DoWell",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='DoWell UX Living Lab',
+    license="Apache License 2.0",
+    packages=["doWellSecureGithubRepository"],
+    include_package_data=True,
+    install_requires=["requests"]
+)
