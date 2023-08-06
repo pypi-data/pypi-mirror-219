8fc3845f0e3eb07829056a63db97b241d36c4008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:18 +0000

    Add package banking-system/1.0.1/banking_system-1.0.1-py3-none-any.whl

---
+++ packages/banking-system/banking_system-1.0.1-py3-none-any.whl/banking_system/account.py
@@ -0,0 +1,16 @@
+class Account:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            raise ValueError("Insufficient balance")
+
+    def get_balance(self):
+        return self.balance
