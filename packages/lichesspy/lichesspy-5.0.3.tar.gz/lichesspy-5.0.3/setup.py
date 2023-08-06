9c3325f57da622b5fdd437519768467a10bce038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:50 +0000

    Add package lichesspy/5.0.3/lichesspy-5.0.3.tar.gz

---
+++ packages/lichesspy/lichesspy-5.0.3.tar.gz/lichesspy-5.0.3/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+
+def def_requirements():
+    """Check PIP Requirements"""
+    with open("requirements.txt") as file_content:
+        pip_lines = file_content.read().splitlines()
+    return pip_lines
+
+
+def def_readme():
+    """Check Readme Markdown"""
+    readme = ""
+    with open("README.md") as file_content:
+        readme = file_content.read()
+    return readme
+
+
+setuptools.setup(
+    name="lichesspy",
+    version="5.0.3",
+    author="Liffecs",
+    author_email="Liffecs@gmail.com",
+    description="Python wrapper for lichess",
+    long_description=def_readme(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/Liffecs/lichesspy",
+    packages=["lichesspy"],
+    package_data={"lichesspy": ["VERSION"]},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=def_requirements(),
+)
