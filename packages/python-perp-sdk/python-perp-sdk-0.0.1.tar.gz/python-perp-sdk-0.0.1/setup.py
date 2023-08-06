542792d76e99a7a5fa4eb55911f67ae5d992ac0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:25 +0000

    Add package python-perp-sdk/0.0.1/python-perp-sdk-0.0.1.tar.gz

---
+++ packages/python-perp-sdk/python-perp-sdk-0.0.1.tar.gz/python-perp-sdk-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-perp-sdk',
+    version='0.0.1',
+    url='https://github.com/yourusername/my-python-sdk',
+    author='Author Name',
+    author_email='author@gmail.com',
+    description='Perp python sdk',
+    packages=find_packages(),    
+    install_requires=[],
+)
