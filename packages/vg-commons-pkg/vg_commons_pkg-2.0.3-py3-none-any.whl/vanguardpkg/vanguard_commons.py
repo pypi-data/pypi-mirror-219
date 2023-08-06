564479dd7b97dfb2203fc753ae2394a942def37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:29 +0000

    Add package vg-commons-pkg/2.0.3/vg_commons_pkg-2.0.3-py3-none-any.whl

---
+++ packages/vg-commons-pkg/vg_commons_pkg-2.0.3-py3-none-any.whl/vanguardpkg/vanguard_commons.py
@@ -0,0 +1,21 @@
+class VanguardParent:
+    def __init__(self):
+        pass
+
+    def hello1_vanguard(self):
+        return "hello from vanguard commons ..."
+
+    def hello2_vanguard(self):
+        return "hello 2 from vanguard commons ..."
+
+    def hello(self):
+        return "hello my package"
+
+    def _add(self, a, b):
+        return a + b
+
+    def _subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
