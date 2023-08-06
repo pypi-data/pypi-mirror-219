07e0184c8f213cd8a85fa219ae75e0fb5ab224c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:15 +0000

    Add package PyGraphica/0.0.8/PyGraphica-0.0.8.tar.gz

---
+++ packages/PyGraphica/PyGraphica-0.0.8.tar.gz/PyGraphica-0.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name='PyGraphica',
+    version='0.0.8',
+    license='MIT',
+    author="Luke Campbell",
+    author_email='LukeCampbell5853@gmail.com',
+    description="PyGraphica is an easy-to-learn GUI module designed for Python, built on the Python bindings (pysdl2) for SDL-2.",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'':['*.ttf']},
+    url='https://github.com/LukeCampbell5853/PyGraphica',
+    keywords='gui',
+    install_requires=[
+          'pysdl2',
+          'pysdl2-dll',
+          'pillow'
+      ],
+)
