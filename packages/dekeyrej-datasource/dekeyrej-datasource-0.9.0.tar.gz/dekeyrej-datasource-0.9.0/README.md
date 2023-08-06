ccba018c64f3f6f6e0db0d7b686fcf28d31bb280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:20 +0000

    Add package dekeyrej-datasource/0.9.0/dekeyrej-datasource-0.9.0.tar.gz

---
+++ packages/dekeyrej-datasource/dekeyrej-datasource-0.9.0.tar.gz/dekeyrej-datasource-0.9.0/README.md
@@ -0,0 +1,5 @@
+Example abstraction for multiple database engines
+Also implements a specific schema for storing just the latest record of a record type
+
+python setup.py pytest
+python setup.py bdist_wheel -d ../wheels
---
+++ packages/dekeyrej-datasource/dekeyrej-datasource-0.9.0.tar.gz/dekeyrej-datasource-0.9.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup()
+# setup(
+#     name='datasourcelib',
+#     packages=find_packages(include=['datasourcelib']),
+#     version='0.1.0',
+#     description='Datasource library',
+#     author='J.DeKeyrel',
+#     license='MIT',
+#     install_requires=['arrow','pymongo','psycopg2-binary'],
+#     setup_requires=['pytest-runner'],
+#     tests_require=['pytest'],
+#     test_suite='tests',
+# )
