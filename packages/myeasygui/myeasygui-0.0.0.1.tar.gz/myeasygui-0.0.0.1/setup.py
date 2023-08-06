5144a5d8fb8356fa8636b16eaac470e88b685474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:25 +0000

    Add package myeasygui/0.0.0.1/myeasygui-0.0.0.1.tar.gz

---
+++ packages/myeasygui/myeasygui-0.0.0.1.tar.gz/myeasygui-0.0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='myeasygui',
+    version='0.0.0.1',
+    packages=['mygui', 'mygui.boxes'],
+    url='',
+    license='',
+    author='Yvan',
+    author_email='yvan_chen2022@163.com',
+    description=''
+)
