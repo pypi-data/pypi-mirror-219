d88b791d7b0fe3b7b6eb9b9da3008f1d3cd21832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:22 +0000

    Add package population-trend/5.3.0/population_trend-5.3.0.tar.gz

---
+++ packages/population-trend/population_trend-5.3.0.tar.gz/population_trend-5.3.0/population_trend/__init__.py
@@ -0,0 +1,8 @@
+"""A template Python module"""
+
+__version__ = "5.3.0"
+from .cli import *  # noqa
+from .filter_data import *  # noqa
+from .population_growth_model import *  # noqa
+from .calculate_growth_rates import *  # noqa
+from bootstrapping_tools import *  # noqa
