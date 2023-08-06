9b5dca0d53efe116ecb945b96e2da9433a76e2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:02 +0000

    Add package scoamp/0.4.0a0/scoamp-0.4.0a0-py3-none-any.whl

---
+++ packages/scoamp/scoamp-0.4.0a0-py3-none-any.whl/scoamp/utils/logger.py
@@ -0,0 +1,14 @@
+import logging
+from rich.logging import RichHandler
+
+#LOG_FORMAT = '%(asctime)s -- %(levelname)s: %(message)s'
+LOG_FORMAT = '%(message)s'
+
+logging.basicConfig(format=LOG_FORMAT, datefmt="[%X]", handlers=[RichHandler(rich_tracebacks=True)])
+
+def get_logger(name: str=None, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    return logger
+
+default_logger = get_logger()
