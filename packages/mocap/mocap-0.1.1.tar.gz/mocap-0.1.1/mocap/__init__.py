aedd950cc27f3db57fd7c602cb14e892fcc6b722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:40 +0000

    Add package mocap/0.1.1/mocap-0.1.1.tar.gz

---
+++ packages/mocap/mocap-0.1.1.tar.gz/mocap-0.1.1/mocap/__init__.py
@@ -0,0 +1,11 @@
+from .ArucoDevice import ArucoDevice, ShareAruco
+from .ArucoBot import ArucoBot
+from .ArucoHelper import ArucoHelper, ENV, ARUCO
+from .timestamp import time_str
+
+
+
+__author__ = 'UCLA LEMUR'
+__credits__ = 'The Laboratory for Embedded Machines and Ubiquitous Robots'
+__url__ = 'https://github.com/rems-ucla/mocap'
+__description__ = 'Aruco tag based motion capture system'
---
+++ packages/mocap/mocap-0.1.1.tar.gz/mocap-0.1.1/mocap/_static_version.py
@@ -0,0 +1,2 @@
+# This file has been created by setup.py.
+version = 'unknown+g132bc1c.dirty'
