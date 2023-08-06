c00c0f91a9737b5477999946bab237e54f844512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:24 +0000

    Add package oarepo-ui/5.0.26/oarepo_ui-5.0.26-py2.py3-none-any.whl

---
+++ packages/oarepo-ui/oarepo_ui-5.0.26-py2.py3-none-any.whl/oarepo_ui/theme/assets/semantic-ui/js/oarepo_ui/forms/contexts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const FormConfigContext = React.createContext()
+
+export const useFormConfig = () => {
+    const context = React.useContext(FormConfigContext)
+    if (!context) {
+        throw new Error('useFormConfig must be used inside FormConfigContext.Provider')
+    }
+}
+
+export const FormConfigProvider = ({children, value}) => {
+    return <FormConfigContext.Provider value={value}>{children}</FormConfigContext.Provider>
+}
