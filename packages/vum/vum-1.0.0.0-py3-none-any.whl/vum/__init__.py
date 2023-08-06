40591719d97b1fa1faa5e510b66e42d8ee5e463c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:49 +0000

    Add package vum/1.0.0.0/vum-1.0.0.0-py3-none-any.whl

---
+++ packages/vum/vum-1.0.0.0-py3-none-any.whl/vum/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "1.0.0.0"
+__author__ = "Jon Kristian Dahl"
+
+"""
+Version legend:
+a.b.c.d
+
+a: major release
+b: new functionality added
+c: new feature to existing functionality
+d: bug fixes
+"""
+
+from .vum import *
