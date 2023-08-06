ad6bebd9efbc788183a1ce157327d6d6c74051d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:20 +0000

    Add package evesso/0.1.3/evesso-0.1.3-py3-none-any.whl

---
+++ packages/evesso/evesso-0.1.3-py3-none-any.whl/evesso/const.py
@@ -0,0 +1,8 @@
+HEADERS = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'login.eveonline.com'
+}
+AUTH_URL = 'https://login.eveonline.com/v2/oauth/authorize/'
+TOKEN_URL = 'https://login.eveonline.com/v2/oauth/token'
+DEFAULT_CALLBACK_URL = 'http://localhost/'
+DEFAULT_JWT_PATH = 'jwt.json'
