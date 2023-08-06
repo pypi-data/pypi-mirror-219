617109b58a92f5cdd66d74f21b7a0a0059638c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:47 +0000

    Add package council-ai/0.0.4/council_ai-0.0.4.tar.gz

---
+++ packages/council-ai/council_ai-0.0.4.tar.gz/council_ai-0.0.4/council/controllers/basic_controller.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from .controller_base import ControllerBase
+from council.core import AgentContext, Chain, Budget
+from council.core.execution_context import ScoredAgentMessage
+from council.core.execution_unit import ExecutionUnit
+
+
+class BasicController(ControllerBase):
+    """a basic controller that requests all chains to be executed and returns all results"""
+
+    def get_plan(self, context: AgentContext, chains: List[Chain], budget: Budget) -> List[ExecutionUnit]:
+        return [ExecutionUnit(chain, budget) for chain in chains]
+
+    def select_responses(self, context: AgentContext) -> List[ScoredAgentMessage]:
+        return context.evaluationHistory[-1]
