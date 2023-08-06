283259767ec51a38ba4bf2dac0875b9648162492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:03 +0000

    Add package fastapi-serve/0.0.2.dev30/fastapi-serve-0.0.2.dev30.tar.gz

---
+++ packages/fastapi-serve/fastapi-serve-0.0.2.dev30.tar.gz/fastapi-serve-0.0.2.dev30/fastapi_serve/__init__.py
@@ -0,0 +1,18 @@
+def _ignore_warnings():
+    import logging
+    import warnings
+
+    logging.captureWarnings(True)
+    warnings.filterwarnings(
+        "ignore",
+        category=DeprecationWarning,
+        message="Deprecated call to `pkg_resources.declare_namespace('google')`.",
+    )
+
+
+__version__ = '0.0.2.dev30'
+
+
+_ignore_warnings()
+
+from .utils import JinaAPIKeyHeader, JinaAuthDependency, JinaAuthMiddleware
