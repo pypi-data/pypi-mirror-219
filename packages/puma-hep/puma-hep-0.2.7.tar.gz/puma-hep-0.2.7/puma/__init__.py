793a0d217a30872f2e998a84fd6e11a683ff74f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:09 +0000

    Add package puma-hep/0.2.7/puma-hep-0.2.7.tar.gz

---
+++ packages/puma-hep/puma-hep-0.2.7.tar.gz/puma-hep-0.2.7/puma/__init__.py
@@ -0,0 +1,13 @@
+"""puma framework - Plotting UMami Api."""
+
+# flake8: noqa
+
+__version__ = "0.2.7"
+
+from puma.histogram import Histogram, HistogramPlot
+from puma.line_plot_2d import Line2D, Line2DPlot
+from puma.pie import PiePlot
+from puma.plot_base import PlotBase, PlotLineObject, PlotObject
+from puma.roc import Roc, RocPlot
+from puma.var_vs_eff import VarVsEff, VarVsEffPlot
+from puma.var_vs_var import VarVsVar, VarVsVarPlot
