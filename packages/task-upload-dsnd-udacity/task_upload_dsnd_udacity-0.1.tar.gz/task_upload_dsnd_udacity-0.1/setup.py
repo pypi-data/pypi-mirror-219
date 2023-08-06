240b5de478aa1da5628f72f978ca8687384674a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:00 +0000

    Add package task-upload-dsnd-udacity/0.1/task_upload_dsnd_udacity-0.1.tar.gz

---
+++ packages/task-upload-dsnd-udacity/task_upload_dsnd_udacity-0.1.tar.gz/task_upload_dsnd_udacity-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='task_upload_dsnd_udacity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['task_upload_dsnd_udacity'],
+      zip_safe=False)
