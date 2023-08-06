850e3ec0f45068d5137afdabdf674567598eac47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:47 +0000

    Add package assess-acceptability-judgments/0.0.7/assess_acceptability_judgments-0.0.7.tar.gz

---
+++ packages/assess-acceptability-judgments/assess_acceptability_judgments-0.0.7.tar.gz/assess_acceptability_judgments-0.0.7/assess_acceptability_judgments/link_grammar.py
@@ -0,0 +1,10 @@
+# from AESLF.aeslf.fluency_score_interface import LanguageModelFluencyScoreInterface
+#
+# # https://github.com/opencog/link-grammar/blob/master/bindings/python/linkgrammar.py
+# # https://github.com/opencog/link-grammar/tree/master/docker/docker-python
+# # Ou en service Java https://github.com/opencog/link-grammar/blob/master/docker/docker-parser/Dockerfile
+#
+#
+# class LinkGrammar(LanguageModelFluencyScoreInterface):
+#     def compute_score(self, sentence: str) -> float:
+#         parsed_sentence = ""
