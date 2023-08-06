f4fc01cb275373424b3a12cf065a0a538dd5ccac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:05 +0000

    Add package astroquery/0.4.7.dev8738/astroquery-0.4.7.dev8738-py3-none-any.whl

---
+++ packages/astroquery/astroquery-0.4.7.dev8738-py3-none-any.whl/astroquery/irsa_dust/__init__.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+
+import warnings
+
+warnings.warn("the ``irsa_dust`` module has been moved to "
+              "astroquery.ipac.irsa.irsa_dust, "
+              "please update your imports.", DeprecationWarning, stacklevel=2)
+
+from astroquery.ipac.irsa.irsa_dust import IrsaDust, IrsaDustClass, Conf, conf
+
+
+__all__ = ['IrsaDust', 'IrsaDustClass', 'Conf', 'conf']
