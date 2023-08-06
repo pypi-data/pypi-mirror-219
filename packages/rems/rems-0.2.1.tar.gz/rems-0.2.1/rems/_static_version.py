51e7f53affb7876a6772cb6038f5e8ce2498798c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:35 +0000

    Add package rems/0.2.1/rems-0.2.1.tar.gz

---
+++ packages/rems/rems-0.2.1.tar.gz/rems-0.2.1/rems/_static_version.py
@@ -0,0 +1,2 @@
+# This file has been created by setup.py.
+version = 'unknown+g03e1139.dirty'
---
+++ packages/rems/rems-0.2.1.tar.gz/rems-0.2.1/rems/sim_handler/ray/ProcessActor.py
@@ -0,0 +1,25 @@
+import ray
+from rems.sim_handler.job_background import JobHandler as JobHandler
+from rems.sim_handler.ray.RobotRayWrapper import RobotRayWrapper
+
+
+@ray.remote
+class ProcessActor:
+    def __init__(self, process, *args, **kwargs):
+        for a in args:
+            if isinstance(a, RobotRayWrapper):
+                a._reset_attr()
+        self.process_system = process(*args)
+        self.jHandler = JobHandler()
+
+    def init(self, *args, **kwargs):
+        self.process_system.init()
+
+    def process(self, t):
+        rets = self.process_system.process(t)
+        self.jHandler.find_job(rets)
+        self.jHandler.execute()
+
+    def get_inpt(self):
+        return self.process_system.get_inpt()
+
