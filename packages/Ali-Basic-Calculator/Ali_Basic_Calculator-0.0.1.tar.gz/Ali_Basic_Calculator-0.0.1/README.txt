aa3a0a54f1e0e3672ebba371a4e95354a6cbce6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:52 +0000

    Add package Ali-Basic-Calculator/0.0.1/Ali_Basic_Calculator-0.0.1.tar.gz

---
+++ packages/Ali-Basic-Calculator/Ali_Basic_Calculator-0.0.1.tar.gz/Ali_Basic_Calculator-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is very simple calculator that takes tow numbers and either add, subtract, multiply, divide or the_root them.
---
+++ packages/Ali-Basic-Calculator/Ali_Basic_Calculator-0.0.1.tar.gz/Ali_Basic_Calculator-0.0.1/aliasicalculator/__init__.py
@@ -0,0 +1,18 @@
+class Calculator ():
+    import math
+    
+    def Add(self,first_num, second_num):
+        return first_num + second_num
+    
+    def Subtract(self,first_num, second_num):
+        return first_num - second_num
+    
+    def Divide(self,first_num, second_num):
+        return first_num / second_num
+    
+    def Multiply(self,first_num, second_num):
+        return first_num * second_num
+    
+    def The_Root(self,first_num):
+        x = math.sqrt(first_num)
+        return x
