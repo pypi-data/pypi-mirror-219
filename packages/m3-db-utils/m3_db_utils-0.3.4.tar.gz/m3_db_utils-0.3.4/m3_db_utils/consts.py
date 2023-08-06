5864e81c032964e6197f65e78f0656ec8964fe5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:13 +0000

    Add package m3-db-utils/0.3.4/m3_db_utils-0.3.4.tar.gz

---
+++ packages/m3-db-utils/m3_db_utils-0.3.4.tar.gz/m3_db_utils-0.3.4/m3_db_utils/consts.py
@@ -0,0 +1,9 @@
+KEY_FIELD_NAME = 'key'
+ORDER_NUMBER_FIELD_NAME = 'order_number'
+
+ATTRIBUTE_NOT_FOUND = 'attribute not found'
+
+DEFAULT_ORDER_NUMBER = 100_000
+
+LOG_QUERY = 'log_query'
+UNNAMED = 'unnamed'
