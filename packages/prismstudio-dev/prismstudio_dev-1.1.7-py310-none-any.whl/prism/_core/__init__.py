17f2d9171172b1263f86646f2750eb7ee7dd9aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:50 +0000

    Add package prismstudio-dev/1.1.7/prismstudio_dev-1.1.7-py310-none-any.whl

---
+++ packages/prismstudio-dev/prismstudio_dev-1.1.7-py310-none-any.whl/prism/_core/__init__.py
@@ -0,0 +1,13 @@
+from ._req_builder import *
+from ._req_builder import preference
+from ._data import (
+    financial,
+    estimate,
+    market,
+    precalculated,
+    securitymaster,
+    index,
+    industry,
+    event,
+)
+from ._fn import max, min, std, mean, var, sum
