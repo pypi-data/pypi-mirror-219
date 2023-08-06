640c6b845546150d1ef6c54ccddfe8720d2d9db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:40 +0000

    Add package sigmoid-alpha/0.1/sigmoid_alpha-0.1.tar.gz

---
+++ packages/sigmoid-alpha/sigmoid_alpha-0.1.tar.gz/sigmoid_alpha-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name = 'sigmoid_alpha',
+      version = '0.1',
+      author = 'sigmoid_capital',
+      author_email = 'sigmoid_capital@outlook.com',
+      description = 'find alpha signal',
+      packages = find_packages(),
+      install_requires = [])
