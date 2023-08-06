e4f7913fba52733579b727512d742c76d657823a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:33 +0000

    Add package django-postgresql-reconnect/1.0.4/django_postgresql_reconnect-1.0.4-py3-none-any.whl

---
+++ packages/django-postgresql-reconnect/django_postgresql_reconnect-1.0.4-py3-none-any.whl/django_postgresql_reconnect/backend/base.py
@@ -0,0 +1,44 @@
+#  Copyright © 2022 Ingram Micro Inc. All rights reserved.
+
+import logging
+
+from django.utils.asyncio import async_unsafe
+from django.core.exceptions import ImproperlyConfigured
+from django.db.backends.postgresql.base import DatabaseWrapper as PgDatabaseWrapper
+
+try:
+    from psycopg2 import InterfaceError
+    from psycopg2.extensions import STATUS_IN_TRANSACTION
+except ImportError as e:
+    raise ImproperlyConfigured("Error loading psycopg2 module: %s" % e)
+
+
+logger = logging.getLogger('django.db.backend')
+
+
+class DatabaseWrapper(PgDatabaseWrapper):
+
+    @async_unsafe
+    def create_cursor(self, name=None):
+        try:
+            return super().create_cursor(name)
+        except InterfaceError:
+            if self.should_reconnect():
+                self.reconnect()
+                return super().create_cursor(name)
+            raise
+
+    def reconnect_enabled(self):
+        return bool(self.settings_dict.get('RECONNECT'))
+
+    def should_reconnect(self):
+        if self.connection is None:
+            return False
+        is_in_transaction = self.connection.status == STATUS_IN_TRANSACTION
+        return self.reconnect_enabled() and (not is_in_transaction)
+
+    def reconnect(self):
+        logger.warning('Reconnect to the database "%s"', self.alias, exc_info=True)
+        self.close()
+        self.connection = None
+        self.connect()
