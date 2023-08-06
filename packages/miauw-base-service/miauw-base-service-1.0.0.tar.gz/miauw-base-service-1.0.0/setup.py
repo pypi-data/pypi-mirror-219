bd619498d9772bc3ecfe11b43dbc48e23f874e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:42 +0000

    Add package miauw-base-service/1.0.0/miauw-base-service-1.0.0.tar.gz

---
+++ packages/miauw-base-service/miauw-base-service-1.0.0.tar.gz/miauw-base-service-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("Readme.md") as f:
+    content = f.read()
+
+
+setup(
+    name="miauw-base-service",
+    version="1.0.0",
+    author="cheetahbyte",
+    author_email="contact@cheetahbyte.dev",
+    description="base service & worker class for miauw-social",
+    long_description=content,
+    long_description_content_type = "text/markdown",
+    packages=find_packages(),
+    py_modules="base_service",
+)
