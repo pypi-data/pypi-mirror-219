39e010af8af5ad064b4cd62061db6d079b43a135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:47 +0000

    Add package python-search/0.24.3/python_search-0.24.3.tar.gz

---
+++ packages/python-search/python_search-0.24.3.tar.gz/python_search-0.24.3/python_search/environment.py
@@ -0,0 +1,15 @@
+import os
+
+
+def is_mac():
+    import platform
+
+    return platform.system() == "Darwin"
+
+
+def is_archlinux():
+    return 0 == os.system("uname -r | grep -i ARCH")
+
+
+def is_linux():
+    return 0 == os.system("cat /etc/*release | grep -i  linux")
