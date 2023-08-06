7914959008233b7601a70e7e728fcb7957e78f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:13 +0000

    Add package leadtools/22.0.0.8/leadtools-22.0.0.8-py3-none-macosx_10_9_x86_64.whl

---
+++ packages/leadtools/leadtools-22.0.0.8-py3-none-macosx_10_9_x86_64.whl/leadtools/bin/Xcode/Frameworks/macOS/Leadtools.Dicom.framework/Headers/LTDicomEncapsulatedDocumentType.h
@@ -0,0 +1,14 @@
+// *************************************************************
+// Copyright (c) 1991-2023 LEAD Technologies, Inc.
+// All Rights Reserved.
+// *************************************************************
+//
+//  LTDicomEncapsulatedDocumentType.h
+//  Leadtools.Dicom
+//
+
+typedef NS_ENUM(NSInteger, LTDicomEncapsulatedDocumentType) {
+	LTDicomEncapsulatedDocumentTypeUnknown = 0,
+	LTDicomEncapsulatedDocumentTypePdf     = 1,
+	LTDicomEncapsulatedDocumentTypeCda     = 2,
+};
