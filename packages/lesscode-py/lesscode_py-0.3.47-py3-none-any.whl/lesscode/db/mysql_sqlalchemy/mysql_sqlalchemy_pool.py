0603e3cb92d66df03ce743543e544eec84ff23ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:26 +0000

    Add package lesscode-py/0.3.47/lesscode_py-0.3.47-py3-none-any.whl

---
+++ packages/lesscode-py/lesscode_py-0.3.47-py3-none-any.whl/lesscode/db/mysql_sqlalchemy/mysql_sqlalchemy_pool.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import importlib
+
+from tornado.options import options
+
+from lesscode.db.base_connection_pool import BaseConnectionPool
+
+
+class MysqlSqlAlchemyPool(BaseConnectionPool):
+    """
+    mysql 数据库链接创建类
+    """
+
+    def sync_create_pool(self):
+        mysql_url = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(
+            self.conn_info.user, self.conn_info.password, self.conn_info.host, self.conn_info.port,
+            self.conn_info.db_name)
+        try:
+            tornado_sqlalchemy = importlib.import_module("tornado_sqlalchemy")
+        except ImportError:
+            raise Exception(f"tornado-sqlalchemy is not exist,run:pip install tornado-sqlalchemy==0.7.0")
+        db = tornado_sqlalchemy.SQLAlchemy(mysql_url, engine_options={"echo": True, "pool_recycle": 3600})
+        options.db = db
+        return db
