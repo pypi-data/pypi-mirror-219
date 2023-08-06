8a1774de249fa174accf3985b11735989609bb56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:08 +0000

    Add package versioned/0.5.2/versioned-0.5.2.tar.gz

---
+++ packages/versioned/versioned-0.5.2.tar.gz/versioned-0.5.2/versioned/api.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+Public API.
+"""
+
+from . import exc
+from .constants import DYNAMODB_TABLE_NAME
+from .constants import S3_PREFIX
+from .constants import LATEST_VERSION
+from .constants import VERSION_ZFILL
+from .constants import METADATA_KEY_ARTIFACT_SHA256
+from .s3_and_dynamodb_backend import Artifact
+from .s3_and_dynamodb_backend import Alias
+from .s3_and_dynamodb_backend import Repository
+from . import s3_and_dynamodb_backend
+from . import s3_only_backend
