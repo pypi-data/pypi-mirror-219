dfce69aa7bc1ec8e43cc5d2094fe7b8a0137e49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:05 +0000

    Add package mess-serverVAG/0.0.1/mess_serverVAG-0.0.1.tar.gz

---
+++ packages/mess-serverVAG/mess_serverVAG-0.0.1.tar.gz/mess_serverVAG-0.0.1/common/descriptors.py
@@ -0,0 +1,33 @@
+import ipaddress
+import logging
+import sys
+
+logger = logging.getLogger("server")
+
+
+class Port:
+    def __set__(self, instance, value):
+        if not 1023 < value < 65536:
+            logger.critical("В качастве порта заданно некорректное значение")
+            sys.exit(1)
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
+
+
+class IpAddress:
+    def __set__(self, instance, value):
+        try:
+            ipaddress.ip_address(value)
+            correct_ip = True
+        except ValueError:
+            correct_ip = False
+        if not correct_ip and value != '':
+            logger.critical(
+                f"Попытка запуска сервера с указанием некорректного ip")
+            sys.exit(1)
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
