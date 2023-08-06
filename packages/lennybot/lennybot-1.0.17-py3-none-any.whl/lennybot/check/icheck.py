61bae7dc4243c353b4b304e24af149d2a077f5b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:02 +0000

    Add package lennybot/1.0.17/lennybot-1.0.17-py3-none-any.whl

---
+++ packages/lennybot/lennybot-1.0.17-py3-none-any.whl/lennybot/check/icheck.py
@@ -0,0 +1,15 @@
+class ICheck:
+    @property
+    def application(self) -> str:
+        pass
+
+    @property
+    def source_version(self) -> str:
+        pass
+
+    @property
+    def target_version(self) -> str:
+        pass
+
+    def check(self) -> bool:
+        pass
