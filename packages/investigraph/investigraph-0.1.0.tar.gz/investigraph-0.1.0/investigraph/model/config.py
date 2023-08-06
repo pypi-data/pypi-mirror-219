b6979f9b5948adb17da07d6e319035731e578328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:03 +0000

    Add package investigraph/0.1.0/investigraph-0.1.0.tar.gz

---
+++ packages/investigraph/investigraph-0.1.0.tar.gz/investigraph-0.1.0/investigraph/model/config.py
@@ -0,0 +1,96 @@
+import os
+from functools import cache
+from pathlib import Path
+from typing import Any
+
+import yaml
+from nomenklatura.dataset.catalog import DataCatalog
+from nomenklatura.dataset.dataset import Dataset
+from nomenklatura.util import PathLike
+from pydantic import BaseModel
+from smart_open import open
+
+from investigraph.exceptions import ImproperlyConfigured
+from investigraph.logging import get_logger
+from investigraph.settings import DATASETS_BLOCK
+from investigraph.util import is_module
+
+from .block import get_block
+from .stage import ExtractStage, LoadStage, TransformStage
+
+log = get_logger(__name__)
+
+
+def abs(base: Path, path: str) -> str:
+    return os.path.normpath(str(Path(base / path).absolute()))
+
+
+class Config(BaseModel):
+    dataset: str
+    base_path: Path
+    metadata: dict[str, Any]
+
+    extract: ExtractStage
+    transform: TransformStage | None = TransformStage()
+    load: LoadStage | None = LoadStage()
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def __init__(self, **data):
+        # ensure absolute file paths for local sources
+        super().__init__(**data)
+        for source in self.extract.sources:
+            if source.is_local and source.uri.startswith("."):
+                source.uri = (self.base_path / source.uri).absolute()
+
+    @classmethod
+    def from_path(cls, fp: PathLike) -> "Config":
+        base_path = Path(fp).parent.absolute()
+        catalog = DataCatalog(Dataset, {})
+        with open(fp) as fh:
+            data = yaml.safe_load(fh)
+        data["title"] = data.get("title", data["name"].title())
+        dataset: Dataset = catalog.make_dataset(data)
+
+        config = {
+            "dataset": dataset.name,
+            "base_path": base_path,
+            "metadata": dataset.to_dict(),
+        }
+
+        for key in cls.__fields__:
+            if key not in config and key in data:
+                config[key] = data[key]
+
+        config = cls(**config)
+
+        # custom user code
+        if not is_module(config.extract.handler):
+            config.extract.handler = abs(config.base_path, config.extract.handler)
+        if not is_module(config.transform.handler):
+            config.transform.handler = abs(config.base_path, config.transform.handler)
+        if not is_module(config.load.handler):
+            config.load.handler = abs(config.base_path, config.load.handler)
+
+        return config
+
+
+@cache
+def get_config(
+    dataset: str | None = None, block: str | None = None, path: PathLike | None = None
+) -> Config:
+    """
+    Return configuration based on block or path (path has precedence)
+    """
+    if path is not None:
+        return Config.from_path(path)
+    if dataset is not None:
+        if block is not None:
+            block = get_block(block)
+        else:
+            block = get_block(DATASETS_BLOCK)
+        log.info("Using block `%s`" % block)
+        block.load(dataset)
+        return Config.from_path(block.path / dataset / "config.yml")
+    raise ImproperlyConfigured("Specify `dataset` and `block` or `path` to config.")
---
+++ packages/investigraph/investigraph-0.1.0.tar.gz/investigraph-0.1.0/investigraph/model/source.py
@@ -0,0 +1,108 @@
+import mimetypes
+from datetime import datetime
+from typing import Literal, TypeAlias
+
+import requests
+from dateparser import parse as parse_date
+from normality import slugify
+from pantomime import normalize_mimetype, types
+from pydantic import BaseModel
+from smart_open import open, parse_uri
+
+from investigraph.exceptions import ImproperlyConfigured
+from investigraph.types import BytesGenerator, SDict
+from investigraph.util import slugified_dict
+
+
+class SourceHead(BaseModel):
+    etag: str | None = None
+    last_modified: datetime | None = None
+    content_type: str | None = None
+    content_length: int | None = None
+
+    def __init__(self, **data):
+        super().__init__(
+            last_modified=parse_date(data.pop("last_modified", "")),
+            content_type=normalize_mimetype(data.pop("content_type", None)),
+            **data,
+        )
+
+    def should_stream(self) -> bool:
+        return self.content_type in (types.CSV, types.JSON)
+
+
+class Source(BaseModel):
+    name: str
+    uri: str
+    scheme: str
+    mimetype: str | None = None
+    extract_kwargs: dict | None = {}
+    stream: bool | None = False
+    ijson_path: str | None = "item"
+
+    def __init__(self, **data):
+        data["uri"] = str(data["uri"])
+        data["name"] = data.get("name", slugify(data["uri"]))
+        data["scheme"] = data.get("scheme", parse_uri(data["uri"]).scheme)
+        if "mimetype" not in data:
+            mtype, _ = mimetypes.guess_type(data["uri"])
+            data["mimetype"] = normalize_mimetype(mtype)
+        data["stream"] = data.get("stream", data["mimetype"] == types.CSV)
+        super().__init__(**data)
+
+    @property
+    def is_http(self) -> bool:
+        return self.scheme.startswith("http")
+
+    @property
+    def is_local(self) -> bool:
+        return self.scheme.startswith("file")
+
+    def head(self) -> SourceHead:
+        if self.is_http:
+            res = requests.head(self.uri)
+            return SourceHead(**slugified_dict(res.headers))
+        raise NotImplementedError("Cannot fetch head for scheme %s" % self.scheme)
+
+    def iter_lines(self) -> BytesGenerator:
+        raise NotImplementedError
+
+    def open(self):
+        return open(self.uri)
+
+
+class HttpSourceResponse(Source):
+    response: requests.Response
+    header: SDict
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def __init__(self, **data):
+        data["mimetype"] = normalize_mimetype(data["header"]["content_type"])
+        super().__init__(**data)
+
+    @property
+    def content(self) -> bytes:
+        if self.stream:
+            raise ImproperlyConfigured("%s is a stream" % self.uri)
+        return self.response.content
+
+    def iter_lines(self) -> BytesGenerator:
+        yield from self.response.iter_lines()
+
+
+class SmartSourceResponse(Source):
+    @property
+    def content(self) -> bytes:
+        if self.stream:
+            raise ImproperlyConfigured("%s is a stream" % self.uri)
+        with open(self.uri, "rb") as fh:
+            return fh.read()
+
+    def iter_lines(self) -> BytesGenerator:
+        with open(self.uri, "rb") as fh:
+            yield from fh
+
+
+TResponse: TypeAlias = Literal[HttpSourceResponse, SmartSourceResponse]
