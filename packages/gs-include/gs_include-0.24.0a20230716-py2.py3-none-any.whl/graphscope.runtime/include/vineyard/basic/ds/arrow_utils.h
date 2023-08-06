389f83c9a4655cfebd7b16ebd0443a0ef7dc97dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:25 +0000

    Add package gs-include/0.24.0a20230716/gs_include-0.24.0a20230716-py2.py3-none-any.whl

---
+++ packages/gs-include/gs_include-0.24.0a20230716-py2.py3-none-any.whl/graphscope.runtime/include/vineyard/basic/ds/arrow_utils.h
@@ -0,0 +1,359 @@
+/** Copyright 2020-2023 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef MODULES_BASIC_DS_ARROW_UTILS_H_
+#define MODULES_BASIC_DS_ARROW_UTILS_H_
+
+#include <map>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "arrow/api.h"
+#include "arrow/io/api.h"
+
+#include "basic/ds/types.h"
+#include "common/util/arrow.h"
+#include "common/util/logging.h"
+#include "common/util/status.h"
+
+namespace vineyard {
+
+namespace arrow_types {
+using OIDT = std::string;
+using VIDT = uint64_t;
+using EIDT = uint64_t;
+}  // namespace arrow_types
+
+struct RefString;
+
+template <typename T>
+struct ConvertToArrowType {};
+
+template <typename T>
+class NumericArray;
+
+template <typename T>
+class NumericArrayBuilder;
+
+template <typename T>
+class FixedNumericArrayBuilder;
+
+#define CONVERT_TO_ARROW_TYPE(type, data_type, array_type, builder_type,       \
+                              type_value)                                      \
+  template <>                                                                  \
+  struct ConvertToArrowType<type> {                                            \
+    using DataType = data_type;                                                \
+    using ArrayType = array_type;                                              \
+    using VineyardArrayType = NumericArray<type>;                              \
+    using BuilderType = builder_type;                                          \
+    using VineyardBuilderType = NumericArrayBuilder<type>;                     \
+    using FixedVineyardBuilderType = FixedNumericArrayBuilder<type>;           \
+    static std::shared_ptr<arrow::DataType> TypeValue() { return type_value; } \
+  };
+
+template <typename T>
+using ArrowDataType = typename ConvertToArrowType<T>::DataType;
+
+template <typename T>
+using ArrowArrayType = typename ConvertToArrowType<T>::ArrayType;
+
+template <typename T>
+using ArrowVineyardArrayType =
+    typename ConvertToArrowType<T>::VineyardArrayType;
+
+template <typename T>
+using ArrowBuilderType = typename ConvertToArrowType<T>::BuilderType;
+
+template <typename T>
+using ArrowVineyardBuilderType =
+    typename ConvertToArrowType<T>::VineyardBuilderType;
+
+template <typename T>
+using FixedArrowVineyardBuilderType =
+    typename ConvertToArrowType<T>::FixedVineyardBuilderType;
+
+CONVERT_TO_ARROW_TYPE(void, arrow::NullType, arrow::NullArray,
+                      arrow::NullBuilder, arrow::null())
+CONVERT_TO_ARROW_TYPE(bool, arrow::BooleanType, arrow::BooleanArray,
+                      arrow::BooleanBuilder, arrow::boolean())
+CONVERT_TO_ARROW_TYPE(int8_t, arrow::Int8Type, arrow::Int8Array,
+                      arrow::Int8Builder, arrow::int8())
+CONVERT_TO_ARROW_TYPE(uint8_t, arrow::UInt8Type, arrow::UInt8Array,
+                      arrow::UInt8Builder, arrow::uint8())
+CONVERT_TO_ARROW_TYPE(int16_t, arrow::Int16Type, arrow::Int16Array,
+                      arrow::Int16Builder, arrow::int16())
+CONVERT_TO_ARROW_TYPE(uint16_t, arrow::UInt16Type, arrow::UInt16Array,
+                      arrow::UInt16Builder, arrow::uint16())
+CONVERT_TO_ARROW_TYPE(int32_t, arrow::Int32Type, arrow::Int32Array,
+                      arrow::Int32Builder, arrow::int32())
+CONVERT_TO_ARROW_TYPE(uint32_t, arrow::UInt32Type, arrow::UInt32Array,
+                      arrow::UInt32Builder, arrow::uint32())
+CONVERT_TO_ARROW_TYPE(int64_t, arrow::Int64Type, arrow::Int64Array,
+                      arrow::Int64Builder, arrow::int64())
+CONVERT_TO_ARROW_TYPE(uint64_t, arrow::UInt64Type, arrow::UInt64Array,
+                      arrow::UInt64Builder, arrow::uint64())
+CONVERT_TO_ARROW_TYPE(float, arrow::FloatType, arrow::FloatArray,
+                      arrow::FloatBuilder, arrow::float32())
+CONVERT_TO_ARROW_TYPE(double, arrow::DoubleType, arrow::DoubleArray,
+                      arrow::DoubleBuilder, arrow::float64())
+CONVERT_TO_ARROW_TYPE(RefString, arrow::LargeStringType,
+                      arrow::LargeStringArray, arrow::LargeStringBuilder,
+                      arrow::large_utf8())
+CONVERT_TO_ARROW_TYPE(std::string, arrow::LargeStringType,
+                      arrow::LargeStringArray, arrow::LargeStringBuilder,
+                      arrow::large_utf8())
+CONVERT_TO_ARROW_TYPE(arrow_string_view, arrow::LargeStringType,
+                      arrow::LargeStringArray, arrow::LargeStringBuilder,
+                      arrow::large_utf8())
+CONVERT_TO_ARROW_TYPE(arrow::TimestampType, arrow::TimestampType,
+                      arrow::TimestampArray, arrow::TimestampBuilder,
+                      arrow::timestamp(arrow::TimeUnit::MILLI))
+CONVERT_TO_ARROW_TYPE(arrow::Date32Type, arrow::Date32Type, arrow::Date32Array,
+                      arrow::Date32Builder, arrow::date32())
+CONVERT_TO_ARROW_TYPE(arrow::Date64Type, arrow::Date64Type, arrow::Date64Array,
+                      arrow::Date64Builder, arrow::date64())
+
+std::shared_ptr<arrow::DataType> FromAnyType(AnyType type);
+
+namespace detail {
+/**
+ * @brief Make a copy for a arrow ArrayData.
+ *
+ * arrow::ArrayData::Copy() is a shallow thus is not suitable in many
+ * cases.
+ */
+Status Copy(std::shared_ptr<arrow::ArrayData> const& array,
+            std::shared_ptr<arrow::ArrayData>& out,
+
+            bool shallow = true,
+            arrow::MemoryPool* pool = arrow::default_memory_pool());
+
+Status Copy(std::shared_ptr<arrow::Array> const& array,
+            std::shared_ptr<arrow::Array>& out, bool shallow = true,
+            arrow::MemoryPool* pool = arrow::default_memory_pool());
+
+Status Copy(std::shared_ptr<arrow::ChunkedArray> const& array,
+            std::shared_ptr<arrow::ChunkedArray>& out, bool shallow = true,
+            arrow::MemoryPool* pool = arrow::default_memory_pool());
+
+/**
+ * @brief Make a copy for a arrow recordbatch.
+ */
+Status Copy(std::shared_ptr<arrow::RecordBatch> const& batch,
+            std::shared_ptr<arrow::RecordBatch>& out, bool shallow = true,
+            arrow::MemoryPool* pool = arrow::default_memory_pool());
+
+Status Copy(std::shared_ptr<arrow::Table> const& batch,
+            std::shared_ptr<arrow::Table>& out, bool shallow = true,
+            arrow::MemoryPool* pool = arrow::default_memory_pool());
+
+}  // namespace detail
+
+struct EmptyTableBuilder {
+  static Status Build(const std::shared_ptr<arrow::Schema>& schema,
+                      std::shared_ptr<arrow::Table>& table);
+
+  static std::shared_ptr<arrow::Schema> EmptySchema();
+};
+
+/**
+ * Similar to arrow's `GetRecordBatchSize`, but considering the schema.
+ *
+ * Used for pre-allocate buffer for NewStreamWriter's `WriteRecordBatch`.
+ */
+Status GetRecordBatchStreamSize(const arrow::RecordBatch& batch, size_t* size);
+
+Status SerializeDataType(const std::shared_ptr<arrow::DataType>& type,
+                         std::shared_ptr<arrow::Buffer>* buffer);
+
+Status DeserializeDataType(const std::shared_ptr<arrow::Buffer>& buffer,
+                           std::shared_ptr<arrow::DataType>* type);
+
+Status SerializeSchema(const arrow::Schema& schema,
+                       std::shared_ptr<arrow::Buffer>* buffer);
+
+Status DeserializeSchema(const std::shared_ptr<arrow::Buffer>& buffer,
+                         std::shared_ptr<arrow::Schema>* schema);
+
+Status SerializeRecordBatch(const std::shared_ptr<arrow::RecordBatch>& batch,
+                            std::shared_ptr<arrow::Buffer>* buffer);
+
+Status DeserializeRecordBatch(const std::shared_ptr<arrow::Buffer>& buffer,
+                              std::shared_ptr<arrow::RecordBatch>* batch);
+
+Status SerializeRecordBatchesToAllocatedBuffer(
+    const std::vector<std::shared_ptr<arrow::RecordBatch>>& batches,
+    std::shared_ptr<arrow::Buffer>* buffer);
+
+Status SerializeRecordBatches(
+    const std::vector<std::shared_ptr<arrow::RecordBatch>>& batches,
+    std::shared_ptr<arrow::Buffer>* buffer);
+
+Status DeserializeRecordBatches(
+    const std::shared_ptr<arrow::Buffer>& buffer,
+    std::vector<std::shared_ptr<arrow::RecordBatch>>* batches);
+
+Status RecordBatchesToTable(
+    const std::vector<std::shared_ptr<arrow::RecordBatch>>& batches,
+    std::shared_ptr<arrow::Table>* table);
+
+Status RecordBatchesToTable(
+    const std::shared_ptr<arrow::Schema> schema,
+    const std::vector<std::shared_ptr<arrow::RecordBatch>>& batches,
+    std::shared_ptr<arrow::Table>* table);
+
+Status CombineRecordBatches(
+    const std::vector<std::shared_ptr<arrow::RecordBatch>>& batches,
+    std::shared_ptr<arrow::RecordBatch>* batch);
+
+Status TableToRecordBatches(
+    const std::shared_ptr<arrow::Table> table,
+    std::vector<std::shared_ptr<arrow::RecordBatch>>* batches);
+
+std::shared_ptr<arrow::ChunkedArray> ConcatenateChunkedArrays(
+    const std::vector<std::shared_ptr<arrow::ChunkedArray>>& arrays);
+
+std::shared_ptr<arrow::ChunkedArray> ConcatenateChunkedArrays(
+    const std::vector<std::vector<std::shared_ptr<arrow::ChunkedArray>>>&
+        arrays);
+
+Status SerializeTableToAllocatedBuffer(
+    const std::shared_ptr<arrow::Table> table,
+    std::shared_ptr<arrow::Buffer>* buffer);
+
+Status SerializeTable(const std::shared_ptr<arrow::Table> table,
+                      std::shared_ptr<arrow::Buffer>* buffer);
+
+Status DeserializeTable(const std::shared_ptr<arrow::Buffer> buffer,
+                        std::shared_ptr<arrow::Table>* table);
+
+/**
+ * @brief Concatenate multiple arrow tables into one.
+ *
+ * Like `arrow::ConcatenateTables`, but unify the column names as well.
+ */
+Status ConcatenateTables(
+    const std::vector<std::shared_ptr<arrow::Table>>& tables,
+    std::shared_ptr<arrow::Table>& table);
+
+/**
+ * @brief Concatenate multiple arrow tables into one in column wise.
+ */
+Status ConcatenateTablesColumnWise(
+    const std::vector<std::shared_ptr<arrow::Table>>& tables,
+    std::shared_ptr<arrow::Table>& table);
+
+/**
+ * @brief Add extra metadata mapping to existing recordbatch.
+ */
+std::shared_ptr<arrow::RecordBatch> AddMetadataToRecordBatch(
+    std::shared_ptr<arrow::RecordBatch> const& batch,
+    std::map<std::string, std::string> const& meta);
+
+/**
+ * @brief Add extra metadata mapping to existing recordbatch, the
+ * std::unordered_map variant.
+ */
+std::shared_ptr<arrow::RecordBatch> AddMetadataToRecordBatch(
+    std::shared_ptr<arrow::RecordBatch> const& batch,
+    std::unordered_map<std::string, std::string> const& meta);
+
+/**
+ * @brief Convert type name in string to arrow type.
+ *
+ */
+std::shared_ptr<arrow::DataType> type_name_to_arrow_type(
+    const std::string& name);
+
+std::string type_name_from_arrow_type(
+    const std::shared_ptr<arrow::DataType>& type);
+
+const void* get_arrow_array_data(std::shared_ptr<arrow::Array> const& array);
+
+Status TypeLoosen(const std::vector<std::shared_ptr<arrow::Schema>>& schemas,
+                  std::shared_ptr<arrow::Schema>& schema);
+
+Status CastStringToBigString(const std::shared_ptr<arrow::Array>& in,
+                             const std::shared_ptr<arrow::DataType>& to_type,
+                             std::shared_ptr<arrow::Array>& out);
+
+Status CastNullToOthers(const std::shared_ptr<arrow::Array>& in,
+                        const std::shared_ptr<arrow::DataType>& to_type,
+                        std::shared_ptr<arrow::Array>& out);
+
+Status GeneralCast(const std::shared_ptr<arrow::Array>& in,
+                   const std::shared_ptr<arrow::DataType>& to_type,
+                   std::shared_ptr<arrow::Array>& out);
+
+Status CastTableToSchema(const std::shared_ptr<arrow::Table>& table,
+                         const std::shared_ptr<arrow::Schema>& schema,
+                         std::shared_ptr<arrow::Table>& out);
+
+Status ConsolidateColumns(
+    const std::vector<std::shared_ptr<arrow::Array>>& columns,
+    std::shared_ptr<arrow::Array>& out);
+
+Status ConsolidateColumns(
+    const std::vector<std::shared_ptr<arrow::ChunkedArray>>& columns,
+    std::shared_ptr<arrow::ChunkedArray>& out);
+
+/**
+ * @brief Consolidate columns in an arrow table into one column
+ * (FixedSizeListArray).
+ *
+ * Note that the bitmap in the given columns will be discard.
+ *
+ * @param table
+ * @return boost::leaf::result<std::shared_ptr<arrow::Table>>
+ */
+Status ConsolidateColumns(const std::shared_ptr<arrow::Table>& table,
+                          std::vector<std::string> const& column_names,
+                          std::string const& consolidated_column_name,
+                          std::shared_ptr<arrow::Table>& out);
+
+Status ConsolidateColumns(const std::shared_ptr<arrow::Table>& table,
+                          std::shared_ptr<arrow::Table>& out);
+
+namespace arrow_shim {
+
+/**
+ * @brief Textual schema representation.
+ *
+ * @see
+ * https://github.com/apache/arrow-rs/blob/27f4762c8794ef1c5d042933562185980eb85ae5/arrow/src/datatypes/datatype.rs#L536
+ */
+Status SchemaToJSON(const std::shared_ptr<arrow::Schema>& schema, json& object);
+
+Status SchemaFromJSON(const json& object,
+                      std::shared_ptr<arrow::Schema>& schema);
+
+Status DataTypeToJSON(const std::shared_ptr<arrow::DataType>& datatype,
+                      json& object);
+
+Status DataTypeFromJSON(const json& object,
+                        std::shared_ptr<arrow::DataType>& datatype);
+
+Status FieldToJSON(const std::shared_ptr<arrow::Field>& field, json& object);
+
+Status FieldFromJSON(const json& object, std::shared_ptr<arrow::Field>& field);
+
+}  // namespace arrow_shim
+
+}  // namespace vineyard
+
+#endif  // MODULES_BASIC_DS_ARROW_UTILS_H_
---
+++ packages/gs-include/gs_include-0.24.0a20230716-py2.py3-none-any.whl/graphscope.runtime/include/vineyard/client/ds/blob.h
@@ -0,0 +1,305 @@
+/** Copyright 2020-2023 Alibaba Group Holding Limited.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef SRC_CLIENT_DS_BLOB_H_
+#define SRC_CLIENT_DS_BLOB_H_
+
+#include <cstdint>
+#include <limits>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <utility>
+
+#include "arrow/api.h"
+#include "arrow/io/api.h"
+
+#include "client/ds/i_object.h"
+#include "common/memory/payload.h"
+#include "common/util/uuid.h"
+
+namespace vineyard {
+
+class BlobWriter;
+class BufferSet;
+class Client;
+class PlasmaClient;
+class ObjectMeta;
+
+/**
+ * @brief The unit to store data payload in vineyard.
+ *
+ * When the client gets a blob from vineyard, the vineyard server maps
+ * a chunk of memory from its memory space to the client space in a
+ * zero-copy fashion.
+ */
+class Blob : public Registered<Blob> {
+ public:
+  /**
+   * @brief Get the size of the blob, i.e., the number of bytes of the data
+   * payload in the blob.
+   *
+   * Note that the size of blob is the "allocated size" of the blob, and may
+   * (usually) not be the same value of the requested size.
+   *
+   * @return The (allocated) size of the blob.
+   */
+  size_t size() const __attribute__((
+      deprecated("The method `size()` no longer report accurate requested "
+                 "memory size, use allocated_size() instead.")));
+
+  /**
+   * @brief Get the allocated size of the blob, i.e., the number of bytes of the
+   * data payload in the blob.
+   *
+   * @return The allocated size of the blob.
+   */
+  size_t allocated_size() const;
+
+  /**
+   * @brief Get the const data pointer of the data payload in the blob.
+   *
+   * @return The const data pointer.
+   */
+  const char* data() const;
+
+  /**
+   * @brief Get the arrow buffer of the blob.
+   *
+   * @return The arrow buffer which holds the data payload
+   * of the blob.
+   */
+  const std::shared_ptr<arrow::Buffer>& Buffer() const;
+
+  /**
+   * @brief Get the arrow buffer of the blob, ensure a valid shared_ptr been
+   * returned even the blob is empty (size == 0).
+   *
+   * @return The arrow buffer which holds the data payload
+   * of the blob.
+   */
+  const std::shared_ptr<arrow::Buffer> BufferOrEmpty() const;
+
+  static std::unique_ptr<Object> Create() __attribute__((used)) {
+    return std::static_pointer_cast<Object>(std::unique_ptr<Blob>{new Blob()});
+  }
+
+  /**
+   * @brief Construct the blob locally for the given object meta.
+   *
+   * @param meta The given object meta.
+   */
+  void Construct(ObjectMeta const& meta) override;
+
+  /**
+   * @brief Dump the buffer for debugging.
+   */
+  void Dump() const;
+
+  /**
+   * @brief Create an empty blob in the vineyard server.
+   *
+   * @param client The client connected to the vineyard server.
+   */
+  static std::shared_ptr<Blob> MakeEmpty(Client& client);
+
+  /**
+   * @brief Create the blob from a buffer from the client-side allocator.
+   *
+   * @param object_id The object ID of this blob.
+   * @param pointer The address of buffer in the client-side allocator.
+   * @param size The estimated size of the buffer.
+   */
+  static std::shared_ptr<Blob> FromAllocator(Client& client,
+                                             const ObjectID object_id,
+                                             const uintptr_t pointer,
+                                             const size_t size);
+
+  /**
+   * @brief Create the blob from a given buffer. If the buffer already lies in
+   * the vineyardd, it would return immediately without copying, otherwise a
+   *        blob writer will be created and the content of the buffer will be
+   *        copied into.
+   *
+   * @param pointer The address of the buffer.
+   * @param size The estimated size of the buffer.
+   */
+  static std::shared_ptr<Blob> FromPointer(Client& client,
+                                           const uintptr_t pointer,
+                                           const size_t size);
+
+ private:
+  /**
+   * The default constructor is only used in BlobWriter.
+   */
+  Blob() {
+    this->id_ = InvalidObjectID();
+    this->size_ = std::numeric_limits<size_t>::max();
+    this->buffer_ = nullptr;
+  }
+
+  const std::shared_ptr<arrow::Buffer>& BufferUnsafe() const;
+
+  size_t size_ = 0;
+  std::shared_ptr<arrow::Buffer> buffer_ = nullptr;
+
+  friend class Client;
+  friend class PlasmaClient;
+  friend class RPCClient;
+  friend class BlobWriter;
+  friend class BufferSet;
+  friend class ObjectMeta;
+};
+
+/**
+ * @brief The writer to write a blob in vineyard.
+ *
+ * The writer is initialized in the client with a local buffer and its size,
+ * and a blob in vineyard will be created when Build is invoked.
+ */
+class BlobWriter : public ObjectBuilder {
+ public:
+  /**
+   * @brief Return the object id of this blob builder. Note that before sealing
+   * the blob builder the object id cannot be used to get "Blob" objects.
+   *
+   * @return The ObjectID of the blob writer.
+   */
+  ObjectID id() const;
+
+  /**
+   * @brief Get the size of the blob, i.e., the number of bytes of the data
+   * payload in the blob.
+   *
+   * @return The size of the blob.
+   */
+  size_t size() const;
+
+  /**
+   * @brief Get the data pointer of the data payload in the blob.
+   *
+   * @return The data pointer.
+   */
+  char* data();
+
+  /**
+   * @brief Get the const data pointer of the data payload in the blob.
+   *
+   * @return The const data pointer.
+   */
+  const char* data() const;
+
+  /**
+   * @brief Get the mutable buffer of the blob.
+   *
+   * @return The mutable buffer of the blob, which can be modified
+   * to update the content in the blob.
+   */
+  const std::shared_ptr<arrow::MutableBuffer>& Buffer() const;
+
+  /**
+   * @brief Build a blob in vineyard server.
+   *
+   * @param client The client connected to the vineyard server.
+   */
+  Status Build(Client& client) override;
+
+  /**
+   * @brief Abort the blob builder.
+   *
+   * @param client Release the blob builder object if it is not sealed.
+   */
+  Status Abort(Client& client);
+
+  /**
+   * @brief Add key-value metadata for the blob.
+   *
+   * @param key The key of the metadata.
+   * @param value The value of the metadata.
+   */
+  void AddKeyValue(std::string const& key, std::string const& value);
+
+  /**
+   * @brief Add key-value metadata for the blob.
+   *
+   * @param key The key of the metadata.
+   * @param value The value of the metadata.
+   */
+  void AddKeyValue(std::string const& key, std::string&& value);
+
+  /**
+   * @brief Dump the buffer for debugging.
+   */
+  void Dump() const;
+
+ protected:
+  Status _Seal(Client& client, std::shared_ptr<Object>& object) override;
+
+ private:
+  BlobWriter(ObjectID const object_id, const Payload& payload,
+             std::shared_ptr<arrow::MutableBuffer> const& buffer)
+      : object_id_(object_id), payload_(payload), buffer_(buffer) {}
+
+  BlobWriter(ObjectID const object_id, Payload&& payload,
+             std::shared_ptr<arrow::MutableBuffer> const& buffer)
+      : object_id_(object_id), payload_(payload), buffer_(buffer) {}
+
+  ObjectID object_id_;
+  Payload payload_;
+  std::shared_ptr<arrow::MutableBuffer> buffer_;
+  // Allowing blobs have extra key-value metadata
+  std::unordered_map<std::string, std::string> metadata_;
+
+  friend class Client;
+  friend class PlasmaClient;
+  friend class RPCClient;
+};
+
+/**
+ * @brief A set of (readonly) buffers that been associated with an object and
+ * its members (recursively).
+ */
+class BufferSet {
+ public:
+  const std::set<ObjectID>& AllBufferIds() const { return buffer_ids_; }
+
+  const std::map<ObjectID, std::shared_ptr<arrow::Buffer>>& AllBuffers() const {
+    return buffers_;
+  }
+
+  Status EmplaceBuffer(ObjectID const id);
+
+  Status EmplaceBuffer(ObjectID const id,
+                       std::shared_ptr<arrow::Buffer> const& buffer);
+
+  void Extend(BufferSet const& others);
+
+  void Extend(std::shared_ptr<BufferSet> const& others);
+
+  bool Contains(ObjectID const id) const;
+
+  bool Get(ObjectID const id, std::shared_ptr<arrow::Buffer>& buffer) const;
+
+ private:
+  // blob ids to buffer mapping: local blobs (not null) + remote blobs (null).
+  std::set<ObjectID> buffer_ids_;
+  std::map<ObjectID, std::shared_ptr<arrow::Buffer>> buffers_;
+};
+
+}  // namespace vineyard
+
+#endif  // SRC_CLIENT_DS_BLOB_H_
