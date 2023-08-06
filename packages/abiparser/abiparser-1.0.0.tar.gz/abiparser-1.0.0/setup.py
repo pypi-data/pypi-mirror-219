a428847bf289e46ea8a403674f5f0abc5511e840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:23 +0000

    Add package abiparser/1.0.0/abiparser-1.0.0.tar.gz

---
+++ packages/abiparser/abiparser-1.0.0.tar.gz/abiparser-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='abiparser',
+    version='1.0.0',
+    description='A Python package to parse Ethereum contract ABI files',
+    author='August Radjoe',
+    author_email='atg271@gmail.com',
+    packages=['abiparser'],
+    install_requires=[],  # Add any dependencies here
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
---
+++ packages/abiparser/abiparser-1.0.0.tar.gz/abiparser-1.0.0/tests/test_abi_parser.py
@@ -0,0 +1,10 @@
+from abi_parser import ContractABI
+
+abi = ContractABI('tests/uniswap.sol/IUniswapV2Router.json')
+
+assert abi.name == 'IUniswapV2Router'
+assert abi.file_path == 'tests/uniswap.sol/IUniswapV2Router.json'
+assert abi.constructor is None
+assert abi.fallback is None
+assert abi.receive is None
+assert abi.get_function_by_name('swapExactTokensForTokens') is not None
