d660284d02a8bf919f224af4dd679ed367978f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:46 +0000

    Add package lfg3/0.0.3/lfg3-0.0.3.tar.gz

---
+++ packages/lfg3/lfg3-0.0.3.tar.gz/lfg3-0.0.3/lfg3/utils.py
@@ -0,0 +1,10 @@
+def ether() -> str:
+    """Summary line.
+
+    Returns the string representation of 1 ether in wei
+
+    Returns:
+        str: ether in string
+    """
+
+    return "1000000000000000000"
