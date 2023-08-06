303455d50c5fdf68baf1a15a4ba99ddfde63e44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:07 +0000

    Add package terminal-in-colors/0.1.0/terminal_in_colors-0.1.0-py3-none-any.whl

---
+++ packages/terminal-in-colors/terminal_in_colors-0.1.0-py3-none-any.whl/terminal_in_colors/__init__.py
@@ -0,0 +1,6 @@
+"""
+Classes exported by this package:
+
+- `ColorTerminal`: Class that provides methods to create formatted, colored sentences.
+- `info_colors`: Dictionary that provides information of 256 colors to class ColorTerminal.
+"""
