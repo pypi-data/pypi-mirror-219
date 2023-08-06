be5b66412b381e889804d14f3a7be896a60ab134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:31 +0000

    Add package fc2-live-dl/2.2.0/fc2_live_dl-2.2.0-py3-none-any.whl

---
+++ packages/fc2-live-dl/fc2_live_dl-2.2.0-py3-none-any.whl/fc2_live_dl/autofc2.py
@@ -0,0 +1,169 @@
+import argparse
+import asyncio
+import json
+
+import apprise
+
+from .FC2LiveDL import FC2LiveDL, CallbackEvent
+from .util import Logger
+
+
+class AutoFC2:
+    default_args = {
+        "config": "autofc2.json",
+    }
+
+    def __init__(self, args):
+        # Merge default args with user args
+        self.args = self.clone(self.default_args)
+        self.args.update(args)
+
+        self.logger = Logger("autofc2")
+        self.logger.info("starting")
+        self.last_valid_config = None
+
+        # Disable progress spinners
+        Logger.print_inline = False
+
+    def get_config(self):
+        try:
+            with open(self.args["config"], "r", encoding="utf8") as f:
+                self.last_valid_config = json.load(f)
+        except Exception as ex:
+            if self.last_valid_config is None:
+                self.logger.error("Error reading config file")
+                raise ex
+            else:
+                self.logger.warn("Warning: unable to load config, using last valid one")
+                self.logger.warn(ex)
+        return self.last_valid_config
+
+    def clone(self, obj):
+        return json.loads(json.dumps(obj))
+
+    def get_channels(self):
+        config = self.get_config()
+        return config["channels"].keys()
+
+    def get_channel_params(self, channel_id):
+        config = self.get_config()
+        params = self.clone(config["default_params"])
+        params.update(self.clone(config["channels"][channel_id]))
+        return params
+
+    def reload_channels_list(self, tasks):
+        async def noop():
+            pass
+
+        channels = self.get_channels()
+        for channel_id in channels:
+            if channel_id not in tasks:
+                tasks[channel_id] = asyncio.create_task(noop())
+
+        for channel_id in tasks.keys():
+            if channel_id not in channels:
+                tasks[channel_id].cancel()
+
+    async def config_watcher(self):
+        last_log_level = Logger.loglevel
+
+        while True:
+            await asyncio.sleep(1)
+
+            config = self.get_config()
+
+            if "autofc2" not in config:
+                continue
+
+            log_level = config["autofc2"]["log_level"]
+            if log_level == last_log_level:
+                continue
+
+            last_log_level = log_level
+
+            if log_level not in Logger.LOGLEVELS:
+                self.logger.error(f"Invalid log level {log_level}")
+                continue
+
+            Logger.loglevel = Logger.LOGLEVELS[log_level]
+            self.logger.info(f"Setting log level to {log_level}")
+
+    def handle_event(self, event):
+        try:
+            if event.type != CallbackEvent.Type.STREAM_ONLINE:
+                return
+
+            config = self.get_config()
+            finfo = FC2LiveDL.get_format_info(
+                meta=event.data,
+                params=event.instance.params,
+                sanitize=False,
+            )
+
+            if "notifications" not in config:
+                return
+
+            for cfg in config["notifications"]:
+                notifier = apprise.Apprise()
+                notifier.add(cfg["url"])
+                notifier.notify(body=cfg["message"] % finfo)
+
+        except Exception as ex:
+            self.logger.error("Error handling event")
+            self.logger.error(ex)
+            return
+
+    async def handle_channel(self, channel_id):
+        params = self.get_channel_params(channel_id)
+        async with FC2LiveDL(params, self.handle_event) as fc2:
+            await fc2.download(channel_id)
+
+    async def _main(self):
+        tasks = {}
+        sleep_task = None
+        config_task = asyncio.create_task(self.config_watcher())
+        try:
+            while True:
+                self.reload_channels_list(tasks)
+                sleep_task = asyncio.create_task(asyncio.sleep(1))
+                task_arr = [config_task, sleep_task]
+                for channel in tasks.keys():
+                    if tasks[channel].done():
+                        tasks[channel] = asyncio.create_task(
+                            self.handle_channel(channel)
+                        )
+                    task_arr.append(tasks[channel])
+
+                await asyncio.wait(task_arr, return_when=asyncio.FIRST_COMPLETED)
+        except asyncio.CancelledError:
+            self.logger.error("Interrupted")
+        finally:
+            if sleep_task is not None:
+                sleep_task.cancel()
+            for task in tasks.values():
+                task.cancel()
+
+    def main(self):
+        try:
+            asyncio.run(self._main())
+        except KeyboardInterrupt:
+            pass
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Automatically download FC2 live streams"
+    )
+    parser.add_argument(
+        "--config",
+        "-c",
+        help="config file to use",
+        default="autofc2.json",
+    )
+    args = parser.parse_args()
+
+    AutoFC2({"config": args.config}).main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/fc2-live-dl/fc2_live_dl-2.2.0-py3-none-any.whl/fc2_live_dl/util.py
@@ -0,0 +1,160 @@
+import argparse
+import asyncio
+import re
+import sys
+from datetime import datetime
+
+
+class Logger:
+    LOGLEVELS = {
+        "silent": 0,
+        "error": 1,
+        "warn": 2,
+        "info": 3,
+        "debug": 4,
+        "trace": 5,
+    }
+
+    loglevel = LOGLEVELS["info"]
+    print_inline = True
+    print_colors = True
+
+    ansi_purple = "\033[35m"
+    ansi_cyan = "\033[36m"
+    ansi_yellow = "\033[33m"
+    ansi_red = "\033[31m"
+    ansi_reset = "\033[0m"
+    ansi_delete_line = "\033[K"
+
+    def __init__(self, module):
+        self._module = module
+        self._loadspin_n = 0
+
+        if not sys.stdout.isatty():
+            self.print_inline = False
+            self.print_colors = False
+
+    def trace(self, *args, **kwargs):
+        if self.loglevel >= self.LOGLEVELS["trace"]:
+            self._print(self.ansi_purple, *args, **kwargs)
+
+    def debug(self, *args, **kwargs):
+        if self.loglevel >= self.LOGLEVELS["debug"]:
+            self._print(self.ansi_cyan, *args, **kwargs)
+
+    def info(self, *args, **kwargs):
+        if self.loglevel >= self.LOGLEVELS["info"]:
+            self._print("", *args, **kwargs)
+
+    def warn(self, *args, **kwargs):
+        if self.loglevel >= self.LOGLEVELS["warn"]:
+            self._print(self.ansi_yellow, *args, **kwargs)
+
+    def error(self, *args, **kwargs):
+        if self.loglevel >= self.LOGLEVELS["error"]:
+            self._print(self.ansi_red, *args, **kwargs)
+
+    def _spin(self):
+        chars = "⡆⠇⠋⠙⠸⢰⣠⣄"
+        self._loadspin_n = (self._loadspin_n + 1) % len(chars)
+        return chars[self._loadspin_n]
+
+    def _print(self, color, *args, inline=False, spin=False):
+        if inline and not self.print_inline:
+            return
+
+        args = list(args)
+
+        if self.print_colors:
+            args.append(self.ansi_reset)
+        else:
+            color = ""
+
+        if spin:
+            args.insert(0, self._spin())
+
+        end = self.ansi_delete_line if self.print_inline else ""
+        end = end + ("\r" if inline else "\n")
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        print(
+            "{} {}[{}]".format(timestamp, color, self._module),
+            *args,
+            end=end,
+            flush=True
+        )
+
+
+class AsyncMap:
+    def __init__(self):
+        self._map = {}
+        self._cond = asyncio.Condition()
+
+    async def put(self, key, value):
+        async with self._cond:
+            self._map[key] = value
+            self._cond.notify_all()
+
+    async def pop(self, key):
+        while True:
+            async with self._cond:
+                await self._cond.wait()
+                if key in self._map:
+                    return self._map.pop(key)
+
+
+class SmartFormatter(argparse.HelpFormatter):
+    def flatten(self, input_array):
+        result_array = []
+        for element in input_array:
+            if isinstance(element, str):
+                result_array.append(element)
+            elif isinstance(element, list):
+                result_array += self.flatten(element)
+        return result_array
+
+    def _split_lines(self, text, width):
+        if text.startswith("R|"):
+            return text[2:].splitlines()
+        elif text.startswith("A|"):
+            return self.flatten(
+                [
+                    argparse.HelpFormatter._split_lines(self, x, width)
+                    if len(x) >= width
+                    else x
+                    for x in text[2:].splitlines()
+                ]
+            )
+        return argparse.HelpFormatter._split_lines(self, text, width)
+
+
+def sanitize_filename(fname):
+    # https://stackoverflow.com/a/31976060
+    fname = str(fname)
+
+    # replace windows and linux forbidden characters
+    fname = re.sub(r"[\\/:*?\"<>|]+", "_", fname)
+
+    # remove ascii control characters
+    fname = re.sub(r"[\x00-\x1f\x7f]", "", fname)
+
+    # remove leading and trailing whitespace
+    fname = fname.strip()
+
+    # remove leading and trailing dots
+    fname = fname.strip(".")
+
+    # check windows reserved names
+    badnames = """
+        CON PRN AUX NUL
+        COM1 COM2 COM3 COM4 COM5 COM6 COM7 COM8 COM9
+        LPT1 LPT2 LPT3 LPT4 LPT5 LPT6 LPT7 LPT8 LPT9
+    """.split()
+
+    fup = fname.upper()
+    for badname in badnames:
+        if fup == badname or fup.startswith(badname + "."):
+            fname = "_" + fname
+
+    return fname
