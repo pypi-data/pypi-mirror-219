c39b353046961af381779ed211a93e454848e075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:43 +0000

    Add package easybio/0.4.3/easybio-0.4.3-py3.10.egg

---
+++ packages/easybio/easybio-0.4.3-py3.10.egg/easyBio/easyBio.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Author: Lei
+# Date: 2023-04-20
+# Description:
+ascii_snek = """\
+  /\_/\  
+ / o o \ 
+(   "   )
+"""
+
+
+def main():
+    print(ascii_snek)
+
+
+if __name__ == '__main__':
+    main()
