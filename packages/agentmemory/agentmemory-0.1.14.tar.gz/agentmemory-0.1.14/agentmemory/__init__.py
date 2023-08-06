4d24f2d74417cd47764d53a6799152bb35594769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:19 +0000

    Add package agentmemory/0.1.14/agentmemory-0.1.14.tar.gz

---
+++ packages/agentmemory/agentmemory-0.1.14.tar.gz/agentmemory-0.1.14/agentmemory/__init__.py
@@ -0,0 +1,43 @@
+"""
+agentmemory
+
+Simple agent memory, powered by chromadb
+"""
+
+__version__ = "0.1.14"
+__author__ = "Moon (https://github.com/lalalune)"
+__credits__ = "https://github.com/lalalune/agentmemory and https://www.trychroma.com/"
+
+from .main import (
+    create_memory,
+    get_memories,
+    search_memory,
+    get_memory,
+    update_memory,
+    delete_memory,
+    count_memories,
+    wipe_category,
+    wipe_all_memories,
+    set_storage_path,
+    save_memory,
+    get_chroma_client,
+    chroma_collection_to_list,
+    list_to_chroma_collection,
+)
+
+__all__ = [
+    "create_memory",
+    "get_memories",
+    "search_memory",
+    "get_memory",
+    "update_memory",
+    "delete_memory",
+    "count_memories",
+    "wipe_category",
+    "wipe_all_memories",
+    "set_storage_path",
+    "save_memory",
+    "get_chroma_client",
+    "chroma_collection_to_list",
+    "list_to_chroma_collection",
+]
