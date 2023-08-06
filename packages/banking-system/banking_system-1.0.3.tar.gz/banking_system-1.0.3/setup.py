bafbdd7e04bcb65b9f082313def2c0840a43ed67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:20 +0000

    Add package banking-system/1.0.3/banking_system-1.0.3.tar.gz

---
+++ packages/banking-system/banking_system-1.0.3.tar.gz/banking_system-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='banking_system',
+    version='1.0.3',
+    author='Kunal Kumar',
+    description='A simple banking management system package',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=['banking_system'],
+    install_requires=[],  # Add dependencies if any
+)
