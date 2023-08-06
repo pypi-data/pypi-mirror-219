b85fe0ed61241774a8811ad6e89fe4d2f106c664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:28 +0000

    Add package mage-ai/0.9.2/mage-ai-0.9.2.tar.gz

---
+++ packages/mage-ai/mage-ai-0.9.2.tar.gz/mage-ai-0.9.2/mage_ai/streaming/sinks/sink_factory.py
@@ -0,0 +1,35 @@
+from mage_ai.streaming.constants import SinkType
+from typing import Dict
+
+
+class SinkFactory:
+    @classmethod
+    def get_sink(self, config: Dict, **kwargs):
+        connector_type = config['connector_type']
+        if connector_type == SinkType.AMAZON_S3:
+            from mage_ai.streaming.sinks.amazon_s3 import AmazonS3Sink
+
+            return AmazonS3Sink(config, **kwargs)
+        elif connector_type == SinkType.DUMMY:
+            from mage_ai.streaming.sinks.dummy import DummySink
+
+            return DummySink(config, **kwargs)
+        elif connector_type == SinkType.KAFKA:
+            from mage_ai.streaming.sinks.kafka import KafkaSink
+
+            return KafkaSink(config, **kwargs)
+        elif connector_type == SinkType.KINESIS:
+            from mage_ai.streaming.sinks.kinesis import KinesisSink
+
+            return KinesisSink(config, **kwargs)
+        elif connector_type == SinkType.MONGODB:
+            from mage_ai.streaming.sinks.mongodb import MongoDbSink
+
+            return MongoDbSink(config, **kwargs)
+        elif connector_type == SinkType.OPENSEARCH:
+            from mage_ai.streaming.sinks.opensearch import OpenSearchSink
+
+            return OpenSearchSink(config, **kwargs)
+        raise Exception(
+            f'Ingesting data to {connector_type} is not supported in streaming pipelines yet.',
+        )
