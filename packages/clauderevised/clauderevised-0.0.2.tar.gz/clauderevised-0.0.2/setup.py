613345cf86d665a2aca7f9c4678492c0c39ed6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:02 +0000

    Add package clauderevised/0.0.2/clauderevised-0.0.2.tar.gz

---
+++ packages/clauderevised/clauderevised-0.0.2.tar.gz/clauderevised-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='clauderevised',
+    version='0.0.2',
+    url='https://github.com/boyueluzhipeng/claude_ai',
+    packages=['clauderevised'],
+    install_requires=[
+        'requests',
+    ],
+)
