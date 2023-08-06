6aeb00459ece571e1b63971444a480f763e4668c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:23 +0000

    Add package niche-cv/0.0.1/niche_cv-0.0.1.tar.gz

---
+++ packages/niche-cv/niche_cv-0.0.1.tar.gz/niche_cv-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="niche_cv",
+    version="0.0.1",
+    url="https://github.com/Niche-Squad/niche_cv",
+    author="James Chen",
+    author_email="niche@vt.edu",
+    description="The Computer Vision Library for Niche Squad",
+    packages=find_packages(),
+    install_requires=[],
+)
