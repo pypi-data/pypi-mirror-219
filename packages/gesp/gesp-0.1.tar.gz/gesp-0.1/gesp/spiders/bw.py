a740899dfc154ff3007bfc614d47ea1699720c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:27 +0000

    Add package gesp/0.1/gesp-0.1.tar.gz

---
+++ packages/gesp/gesp-0.1.tar.gz/gesp-0.1/gesp/spiders/bw.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import datetime
+import scrapy
+from ..src.output import output
+from ..pipelines.formatters import AZsPipeline, DatesPipeline, CourtsPipeline
+from ..pipelines.texts import TextsPipeline
+from ..pipelines.exporters import ExportAsHtmlPipeline, FingerprintExportPipeline, RawExporter
+
+class SpdrBW(scrapy.Spider):
+    name = "spider_bw"
+    base_url = "https://lrbw.juris.de/cgi-bin/laender_rechtsprechung/"
+    custom_settings = {
+        "DOWNLOAD_DELAY": 1, # minimum download delay 
+        "AUTOTHROTTLE_ENABLED": True,
+        "ITEM_PIPELINES": { 
+            AZsPipeline: 100,
+            DatesPipeline: 200,
+            CourtsPipeline: 300,
+            TextsPipeline: 400,
+            ExportAsHtmlPipeline: 500,
+            FingerprintExportPipeline: 600,
+            RawExporter : 900
+        }
+    }
+
+    def __init__(self, path, courts="", states="", fp=False, domains="", store_docId=False, postprocess=False, wait=False, **kwargs):
+        self.path = path
+        self.courts = courts
+        self.states = states
+        self.fp = fp
+        self.domains = domains
+        self.store_docId = store_docId
+        self.postprocess = postprocess
+        self.wait = wait
+        super().__init__(**kwargs)
+
+    def start_requests(self):
+        start_urls = []
+        base_url = self.base_url + "list.py?Gericht=bw&Art=en"
+        add_years = lambda url : [url + str(y) for y in reversed(range(2007, datetime.date.today().year + 1))] # Urteilsdatenbank BW startet mit dem Jahr 2007
+        if self.courts:
+            if "ag" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Amtsgerichte&Datum="))
+            if "arbg" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Arbeitsgerichte&Datum="))
+            if "fg" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Finanzgericht&Datum="))
+            if "lag" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Arbeitsgerichte&Datum="))
+            if "lg" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Landgerichte&Datum="))
+            if "lsg" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Sozialgerichte&Datum="))
+            if "olg" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Oberlandesgerichte&Datum="))
+            if "ovg" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Verwaltungsgerichte&Datum="))
+            if "sg" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Sozialgerichte&Datum="))
+            if "vg" in self.courts: start_urls.extend(add_years(base_url + "&GerichtAuswahl=Verwaltungsgerichte&Datum="))
+        else:
+            start_urls.extend(add_years(base_url + "&Datum="))
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        if not response.xpath("//p[@class='FehlerMeldung']"): #  Hinweis-Seite ohne Suchergebnisse, d.h. alle Seiten für das Jahr wurden durchgegangen
+            for doc_link in response.xpath("//a[@class='doklink']"):
+                if self.courts:
+                    # Auswahl notwendig, da ArbG & LAG == Arbeitsgerichte, SG & LSG == Sozialgerichte, VG & VGH == Verwaltungserichte
+                    # Nur wenn self.courts, um Geschwindigkeit (XPath...) bei ungefiltertem Durchgang nicht zu bremsen
+                    doc_court = doc_link.xpath("../../td[@class='EGericht']/text()").get().split()[0].lower()
+                    if not doc_court in self.courts:
+                        continue
+                    # Wenn Rechtsgebiet ausgewählt weitere Unterscheidung notwendig, da ag + lg + olg == Straf UND Zivil
+                    # ggf. Filtern nach Aktenzeichen?
+                    if "straf" in self.domains and not "zivil" in self.domains:
+                        output("filter (-s bw -d straf) not yet implemented", "warn")
+                        # Ausbauen ....
+                    elif "zivil" in self.domains and not "straf" in self.domains:
+                        output("filter (-s bw -d zivil) not yet implemented", "warn")
+                        # Ausbauen ....            
+                yield {
+                    "postprocess": self.postprocess,
+                    "wait": self.wait,
+                    "court": doc_link.xpath("../../td[@class='EGericht']/text()").get(),
+                    "date": doc_link.xpath("../../td[@class='EDatum']/text()").get(),
+                    "az": doc_link.xpath("text()").get(),
+                    "link": self.base_url + doc_link.xpath("@href").get() + "&Blank=1"
+                }
+        if response.xpath("//img[@title='nächste Seite']"):
+            yield response.follow(response.xpath("//img[@title='nächste Seite']/../@href").get(), callback=self.parse)
---
+++ packages/gesp/gesp-0.1.tar.gz/gesp-0.1/gesp/spiders/ni.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import urllib.parse
+from ..src.output import output
+from ..pipelines.formatters import AZsPipeline, DatesPipeline, CourtsPipeline
+from ..pipelines.texts import TextsPipeline
+from ..pipelines.exporters import ExportAsHtmlPipeline, FingerprintExportPipeline, RawExporter
+
+class SpdrNI(scrapy.Spider):
+    name = "spider_ni"
+    base_url = "https://www.rechtsprechung.niedersachsen.de/jportal/portal/"
+    custom_settings = {
+        "DOWNLOAD_DELAY": 2, # minimum download delay 
+        "AUTOTHROTTLE_ENABLED": False,
+        "ITEM_PIPELINES": { 
+            AZsPipeline: 100,
+            DatesPipeline: 200,
+            CourtsPipeline: 300,
+            TextsPipeline: 400,
+            ExportAsHtmlPipeline: 500,
+            FingerprintExportPipeline: 600,
+            RawExporter: 900
+        }
+    }
+
+    def __init__(self, path, courts="", states="", fp=False, domains="", store_docId=False, postprocess=False, wait=False, **kwargs):
+        self.path = path
+        self.courts = courts
+        self.states = states
+        self.fp = fp
+        self.domains = domains
+        self.store_docId = store_docId
+        self.postprocess = postprocess
+        self.wait = wait
+        self.base_url = "https://www.rechtsprechung.niedersachsen.de/jportal/portal/"
+        super().__init__(**kwargs)
+
+    def start_requests(self):
+        start_urls = [] 
+        filter_url = self.base_url + "page/bsndprod.psml?nav=ger&node=BS-ND%5B%23%5D%4000"
+        if self.courts:
+            if "ag" in self.courts:
+                start_urls.append(filter_url + "40%40Amtsgerichte%5B%23%5D")
+            if "arbg" in self.courts:
+                # Herausfiltern des lag
+                arbgs = ["Braunschweig", "Celle", "Emden", "Göttingen", "Hannover", "Lingen", "Nienburg", "Oldenburg+%28Oldenburg%29", "Osnabrück", "Verden"]
+                for arbg in arbgs:
+                    start_urls.append(filter_url + f"70%40Arbeitsgerichte%5B%23%5D%400002%40ArbG+{arbg}%5B%24%5DArbG+{arbg}%7B.%7D%5B%23%5D")
+            if "fg" in self.courts:
+                start_urls.append(filter_url + "80%40Finanzgericht%7B.%7D%5B%23%5D")
+            if "lag" in self.courts:
+                n = "Landesarbeitsgericht+Niedersachsen"
+                start_urls.append(filter_url + f"70%40Arbeitsgerichte%5B%23%5D%400001%40{n}%5B%24%5D{n}%7B.%7D%5B%23%5D")
+            if "lg" in self.courts:
+                start_urls.append(filter_url + "30%40Landgerichte%5B%23%5D")
+            if "lsg" in self.courts:
+                n = "Landessozialgericht+Niedersachsen-Bremen"
+                start_urls.append(filter_url + f"60%40Sozialgerichte%5B%23%5D%400001%40{n}%5B%24%5D{n}%7B.%7D%5B%23%5D")
+            if "olg" in self.courts:
+                start_urls.append(filter_url + "20%40Oberlandesgerichte%5B%23%5D")
+            if "ovg" in self.courts:
+                n = "Niedersächsiches+Oberverwaltungsgericht"
+                start_urls.append(filter_url + f"50%40Verwaltungsgerichte%5B%23%5D%400001%40{n}%5B%24%5D{n}%7B.%7D%5B%23%5D")
+            if "sg" in self.courts:
+                # Herausfiltern des lsg
+                sgs = ["Aurich", "Braunschweig", "Hannover", "Hildesheim", "Lüneburg", "Oldenburg+%28Oldenburg%29", "Osnabrück", "Stade"]
+                for sg in sgs:
+                    start_urls.append(filter_url + f"60%40Sozialgerichte%5B%23%5D%400002%40SG+{sg}%5B%24%5DSG+{sg}%7B.%7D%5B%23%5D")
+            if "vg" in self.courts:
+                # Herausfiltern des ovg
+                vgs = ["Braunschweig", "Göttingen", "Hannover", "Lüneburg", "Oldenburg+%28Oldenburg%29", "Osnabrück", "Stade"]
+                for vg in vgs:
+                    start_urls.append(filter_url + f"50%40Verwaltungsgerichte%5B%23%5D%400002%40Verwaltungsgericht+{vg}%5B%24%5DVerwaltungsgericht+{vg}%7B.%7D%5B%23%5D")
+        else:
+            start_urls.append(self.base_url + "page/bsndprod.psml/js_peid/FastSearch/media-type/html?form=bsIntFastSearch&sm=fs&query=")
+
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        if (response.xpath("//table")):
+            for tr in response.xpath("//tr"):
+                az = tr.xpath(".//a/text()[2]").get()
+                az = az.replace("\n", "")
+                az = az.replace(" | ", "")
+                link = self.base_url + tr.xpath(".//a/@href").get()
+                yield {
+                       "postprocess": self.postprocess,
+                       "wait": self.wait,
+                        "court": tr.xpath(".//strong[1]/text()").get(),
+                        "date": tr.xpath(".//td[1]/text()").get().replace("\n", ""),
+                        "link": link,
+                        "az": az,
+                        "docId": urllib.parse.parse_qs(urllib.parse.urlparse(link).query)['doc.id'][0]
+
+                }
+            if response.xpath("//p[@class='skipNav']/strong[2]/following-sibling::a"):
+                yield response.follow(response.xpath("//p[@class='skipNav']/strong[2]/following-sibling::a/@href").get(), callback=self.parse)
+        else:
+            output("empty search result list", "warn")
