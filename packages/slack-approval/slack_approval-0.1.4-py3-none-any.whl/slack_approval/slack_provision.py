926606fe32ed68c9fd6391390bea6b6562e280b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:26 +0000

    Add package slack-approval/0.1.4/slack_approval-0.1.4-py3-none-any.whl

---
+++ packages/slack-approval/slack_approval-0.1.4-py3-none-any.whl/slack_approval/slack_provision.py
@@ -0,0 +1,116 @@
+import json
+import os
+import logging
+from slack_sdk.signature import SignatureVerifier
+from slack_sdk import WebhookClient, WebClient, errors
+
+logger = logging.getLogger("slack_provision")
+logger.setLevel(logging.DEBUG)
+
+
+class SlackProvision:
+    def __init__(self, request, requesters_channel=None):
+        self.token = os.environ.get("SLACK_BOT_TOKEN")
+        self.data = request.get_data()
+        self.headers = request.headers
+        payload = json.loads(request.form["payload"])
+        action = payload["actions"][0]
+        self.action_id = action["action_id"]
+        self.inputs = json.loads(action["value"])
+        self.ts = self.inputs.pop("ts")
+        self.requesters_channel = self.inputs.pop("requesters_channel")
+        self.approvers_channel = self.inputs.pop("approvers_channel", None)
+        self.name = self.inputs["provision_class"]
+        self.user = " ".join(
+            [s.capitalize() for s in payload["user"]["name"].split(".")]
+        )
+        self.response_url = payload["response_url"]
+        self.exception = None
+
+    def is_valid_signature(self, signing_secret):
+        """Validates the request from the Slack integration
+        """
+        timestamp = self.headers["x-slack-request-timestamp"]
+        signature = self.headers["x-slack-signature"]
+        verifier = SignatureVerifier(signing_secret)
+        return verifier.is_valid(self.data, timestamp, signature)
+
+    def approved(self):
+        logger.info("request approved")
+
+    def rejected(self):
+        logger.info("request rejected")
+
+    def __call__(self):
+        try:
+            if self.action_id == "Approved":
+                self.approved()
+            elif self.action_id == "Rejected":
+                self.rejected()
+        except Exception as e:
+            self.exception = e
+        hide = self.inputs.get("hide")
+        if hide:
+            for field in hide:
+                self.inputs.pop(field, None)
+            self.inputs.pop("hide")
+        self.send_status_message()
+
+    def send_status_message(self):
+        blocks = [
+            {
+                "type": "header",
+                "text": {"type": "plain_text", "text": self.name, "emoji": True,},
+            },
+            {"type": "divider"},
+        ]
+        input_blocks = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*{' '.join([s.capitalize() for s in key.split('_')])}:* {value}",
+                },
+            }
+            for key, value in self.inputs.items()
+            if key != "provision_class"
+        ]
+        blocks.extend(input_blocks)
+        blocks.append({"type": "divider"})
+        blocks.append(
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"*Status: {self.action_id} by {self.user}*",
+                },
+            }
+        )
+        if self.exception:
+            blocks.append({"type": "divider"})
+            blocks.append(
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"Error while provisioning: {self.exception}",
+                    },
+                }
+            )
+        try:
+            slack_client = WebhookClient(self.response_url)
+            response = slack_client.send(text="fallback", blocks=blocks)
+            logger.info(response.status_code)
+        except errors.SlackApiError as e:
+            logger.error(e)
+        try:
+            slack_web_client = WebClient(self.token)
+            response = slack_web_client.chat_update(
+                channel=self.requesters_channel,
+                ts=self.ts,
+                text="fallback",
+                blocks=blocks,
+            )
+            logger.info(response.status_code)
+        except errors.SlackApiError as e:
+            logger.error(e)
