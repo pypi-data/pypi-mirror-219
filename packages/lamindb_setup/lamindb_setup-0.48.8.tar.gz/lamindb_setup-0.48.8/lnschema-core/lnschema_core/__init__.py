0cf58f79b25ca0bdc9e3453fe599b11d01156943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:57 +0000

    Add package lamindb_setup/0.48.8/lamindb_setup-0.48.8.tar.gz

---
+++ packages/lamindb_setup/lamindb_setup-0.48.8.tar.gz/lamindb_setup-0.48.8/lnschema-core/lnschema_core/__init__.py
@@ -0,0 +1,29 @@
+"""Data objects & lineage (`yvzi`)."""
+_schema_id = "yvzi"
+_name = "core"
+__version__ = "0.37a1"
+
+# can directly import from lamindb_setup going forward
+from lamindb_setup._check_instance_setup import (
+    check_instance_setup as _check_instance_setup,
+)
+
+_INSTANCE_SETUP = _check_instance_setup()
+
+if _INSTANCE_SETUP:
+    from . import ids, types
+    from .models import (  # type: ignore
+        ORM,
+        Dataset,
+        Feature,
+        FeatureSet,
+        File,
+        Project,
+        Run,
+        Storage,
+        Tag,
+        Transform,
+        User,
+    )
+
+    Features = FeatureSet  # backward compat
