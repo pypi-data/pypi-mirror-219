c1c0221e0911f9cd0a7f42a1be09c7214ed4b960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:40 +0000

    Add package upstox/2.0.2/upstox-2.0.2.tar.gz

---
+++ packages/upstox/upstox-2.0.2.tar.gz/upstox-2.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+  name = 'upstox',
+  packages = ['upstox_api', 'upstox_api.constants'],
+  version = '2.0.2',
+  include_package_data=True,
+  description = 'DEPRECATED - The Upstox Python SDK is in its end-of-life stage and has been deprecated. We strongly encourage you to transition to the updated package, upstox-python-sdk. This can be installed via pip install upstox-python-sdk (find further details at https://pypi.org/project/upstox-python-sdk/). The new package ensures full compatibility with Upstox API v2.0, as well as subsequent versions, and will receive ongoing updates and support.',
+  author = 'Upstox Development Team',
+  author_email = 'support@upstox.com',
+  url = 'https://github.com/upstox/upstox-python',
+  install_requires=['future', 'requests', 'websocket_client'],
+  keywords = ['upstox', 'python', 'sdk', 'trading', 'stock markets'],
+  classifiers=[
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+  ],
+)
