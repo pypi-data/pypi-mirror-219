6d4552c4f6df5c53be1fed890ad429cf759ff869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:19 +0000

    Add package leadtools/22.0.0.8/leadtools-22.0.0.8-py3-none-macosx_11_0_arm64.whl

---
+++ packages/leadtools/leadtools-22.0.0.8-py3-none-macosx_11_0_arm64.whl/leadtools/bin/Xcode/Frameworks/macOS/Leadtools.Ocr.framework/Headers/LTOcrImageSharingMode.h
@@ -0,0 +1,13 @@
+// *************************************************************
+// Copyright (c) 1991-2023 LEAD Technologies, Inc.
+// All Rights Reserved.
+// *************************************************************
+//
+//  LTOcrImageSharingMode.h
+//  Leadtools.Ocr
+//
+
+typedef NS_ENUM(NSInteger, LTOcrImageSharingMode) {
+   LTOcrImageSharingModeNone,
+   LTOcrImageSharingModeAutoDispose,
+};
