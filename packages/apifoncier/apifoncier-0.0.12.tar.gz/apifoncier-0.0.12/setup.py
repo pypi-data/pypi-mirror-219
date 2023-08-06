dd442f80bbb1f46681988ce7411b34811dd7024c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:19 +0000

    Add package apifoncier/0.0.12/apifoncier-0.0.12.tar.gz

---
+++ packages/apifoncier/apifoncier-0.0.12.tar.gz/apifoncier-0.0.12/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="apifoncier",
+    version="0.0.12",
+    description="Mobiliser les données foncières de l'api du Cerema directement avec python",
+    package_dir={"": "apifoncier"},
+    packages=find_packages(where="apifoncier"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rcadot/py.apifoncier",
+    author="Romain Cadot",
+    author_email="romain.cadot@cerema.fr",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=["utils", "config", "cartofriches", "dvf_opendata", "dv3f"],
+    keywords=["api", "foncier", "cerema", "dv3f", "friches", "artificialisation"],
+    install_requires=["pandas", "requests", "plotly", "geopandas"],
+    extras_require={"dev": ["pytest"]},
+    python_requires=">=3.9",
+)
