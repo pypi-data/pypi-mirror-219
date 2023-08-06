74ad27a682ced55379a1a303a978344a497cb321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:40 +0000

    Add package myippanel/0.0.2/myippanel-0.0.2-py3-none-any.whl

---
+++ packages/myippanel/myippanel-0.0.2-py3-none-any.whl/myippanel/errors.py
@@ -0,0 +1,32 @@
+import enum
+
+from myippanel.models import Response
+
+
+class ResponseCode(enum.Enum):
+    ErrForbidden = 403
+    ErrNotFound = 404
+    ErrUnprocessableEntity = 422
+    ErrInternalServer = 500
+
+
+class Error(Exception):
+    """
+    Error template
+    """
+
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+
+        super(Error, self).__init__(str(message))
+
+
+class HTTPError(Exception):
+    pass
+
+
+def parse_errors(response: Response):
+    if response.code != 200 or response.error_message != '' and response.error_message is not None:
+        return Error(response.code, response.error_message)
+    return
