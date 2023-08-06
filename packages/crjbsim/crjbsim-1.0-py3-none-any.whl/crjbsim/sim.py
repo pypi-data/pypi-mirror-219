28f1e4474f5445a3b1a62e2adbbe82abf0396abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:29 +0000

    Add package crjbsim/1.0/crjbsim-1.0-py3-none-any.whl

---
+++ packages/crjbsim/crjbsim-1.0-py3-none-any.whl/crjbsim/sim.py
@@ -0,0 +1,23 @@
+import random
+
+from crjbsim import des_aware_logging, time_provider
+from crjbsim.discrete_event_scheduler import DiscreteEventScheduler
+
+
+scheduler = None
+
+
+def run_sim(initial_event, post_processing=None):
+    random.seed(0)
+    des_aware_logging.setup()
+    time_provider.set_time(0)
+
+    global scheduler
+    scheduler = DiscreteEventScheduler()
+
+    scheduler.do_in(0, initial_event)
+
+    scheduler.start()
+
+    if post_processing:
+        post_processing()
