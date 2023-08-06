abaaf2a561d08ea7ece162f8391236fcefb58efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:01 +0000

    Add package llm-toys/0.0.6/llm-toys-0.0.6.tar.gz

---
+++ packages/llm-toys/llm-toys-0.0.6.tar.gz/llm-toys-0.0.6/llm_toys/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.6"
+
+
+print(
+    "\033[93mWARNING: Note that we are using the transformers and peft packages from the source directory, "
+    "not the installed package. BitsandBytes 4bit quantizaztion was only working with the "
+    "main brach of transformers and peft. Once transformers version 4.31.0 and peft version 0.4.0 is "
+    "published to pypi we will use the published version.\033[0m"
+)
