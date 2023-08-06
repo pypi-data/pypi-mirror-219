62881dec602d3ff319d95c4997ce07148e158631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:49 +0000

    Add package gfx-perp-python-sdk/0.0.6/gfx_perp_python_sdk-0.0.6.tar.gz

---
+++ packages/gfx-perp-python-sdk/gfx_perp_python_sdk-0.0.6.tar.gz/gfx_perp_python_sdk-0.0.6/gfx_perp_python_sdk/trader.py
@@ -0,0 +1,27 @@
+class Trader:
+    def __init__(self):
+        super()
+
+    def create_trader_account_ixs():
+        print('create_trader_account_ixs')
+
+    def get_open_orders():
+        print('get_open_orders')
+    
+    def init():
+        print('init_by_name')
+
+    def deposit_funds_ix():
+        print('deposit_funds_ix')
+
+    def withdraw_funds_ix():
+        print('withdraw_funds_ix')
+
+    def new_order_ix():
+        print('new_order_ix')
+    
+    def cancel_order_ix():
+        print('cancel_order_ix')
+
+    def refresh_data():
+        print('refresh_data')
