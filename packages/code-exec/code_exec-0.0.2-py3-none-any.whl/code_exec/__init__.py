d192cf632ff4219207a4951f00a577142c80dd58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:08 +0000

    Add package code-exec/0.0.2/code_exec-0.0.2-py3-none-any.whl

---
+++ packages/code-exec/code_exec-0.0.2-py3-none-any.whl/code_exec/__init__.py
@@ -0,0 +1,34 @@
+from .checker import StaticCodeChecker
+from .executor import CodeExecutor
+
+DEFAULT_UNSAFE_BUILTINS = [
+    "__import__",
+    "exec",
+    "eval",
+    "quit",
+    "exit",
+    "breakpoint",
+    "globals",
+    "locales"
+]
+
+__safe_executor = CodeExecutor(
+    static_checker=StaticCodeChecker(
+        no_import=True,
+        no_private_attr_access=True,
+        unsafe_calls=DEFAULT_UNSAFE_BUILTINS,
+    )
+)
+
+
+def safe_exec(source: str, filename=CodeExecutor.FILENAME_STRING, globals_: dict | None = None,
+              locals_: dict | None = None):
+    __safe_executor.exec(source=source, filename=filename, globals_=globals_, locals_=locals_,
+                         blacklist=DEFAULT_UNSAFE_BUILTINS)
+
+
+def safe_eval(source: str, filename=CodeExecutor.FILENAME_STRING, globals_: dict | None = None,
+              locals_: dict | None = None):
+    return __safe_executor.exec(source=source, filename=filename, globals_=globals_, locals_=locals_,
+                                blacklist=DEFAULT_UNSAFE_BUILTINS)
+
