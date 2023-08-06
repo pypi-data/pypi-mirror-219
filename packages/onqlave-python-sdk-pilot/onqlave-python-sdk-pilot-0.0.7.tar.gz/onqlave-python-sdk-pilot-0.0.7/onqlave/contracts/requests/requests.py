b9da8b88cd16eae49d3e2a05cb7ef9998f81bc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:07 +0000

    Add package onqlave-python-sdk-pilot/0.0.7/onqlave-python-sdk-pilot-0.0.7.tar.gz

---
+++ packages/onqlave-python-sdk-pilot/onqlave-python-sdk-pilot-0.0.7.tar.gz/onqlave-python-sdk-pilot-0.0.7/onqlave/contracts/requests/requests.py
@@ -0,0 +1,24 @@
+import json
+
+class OnqlaveRequest():
+    def __init__(self,body_data: dict):
+        self._json = body_data
+    
+    def get_content(self): # return []byte, error
+        """convert to JSON encoding"""
+        return json.dumps(self._json)
+    
+class EncryptionOpenRequest(OnqlaveRequest):
+    def __init__(self,body_data: dict):
+        super().__init__(body_data=body_data)
+        
+    def get_content(self):
+        return json.dumps(self._json)
+        
+
+class DecryptionOpenRequest(OnqlaveRequest):
+    def __init__(self, body_data: dict) -> None:
+        super().__init__(body_data=body_data)
+    
+    def get_content(self):
+        return json.dumps(self._json)
