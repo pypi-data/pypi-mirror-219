222ed2f75b6b648c7ba8dd743f36d64bcaee1f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:10 +0000

    Add package lichenggong/0.3.1.0/lichenggong-0.3.1.0-py3-none-any.whl

---
+++ packages/lichenggong/lichenggong-0.3.1.0-py3-none-any.whl/lichenggong/Noodows/__init__.py
@@ -0,0 +1,5 @@
+def start():
+    import sys
+    sys.path.append('.\lichenggong\\Noodows')
+    import __noodows__
+    __noodows__.__n_start__()
---
+++ packages/lichenggong/lichenggong-0.3.1.0-py3-none-any.whl/lichenggong/Program_Files/System/__thanks__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+def thanks():
+    """
+
+    # thanks
+
+    """
+    print("致谢名单：李导(真名 李成功 职责 开发)")
+    print("thanks  ：舟舟(真名 陈鸿舟 职责 测试)")
+    print("thanks  ：大黄(真名 黄佳亿 职责 灵感)")
