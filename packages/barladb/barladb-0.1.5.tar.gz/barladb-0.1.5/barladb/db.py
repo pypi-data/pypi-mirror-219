21343b36ca7d099ea079c729483a5d03f0af3d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:02 +0000

    Add package barladb/0.1.5/barladb-0.1.5.tar.gz

---
+++ packages/barladb/barladb-0.1.5.tar.gz/barladb-0.1.5/barladb/db.py
@@ -0,0 +1,73 @@
+from barladb.classes import Json
+from barladb import config
+from os import system
+import os
+try:
+    from colorama import Style, Fore, init
+except:
+    system("pip install colorama")
+    from colorama import Style, Fore, init
+init()
+data = {
+
+}
+
+#get - получить
+def get(filepath: str):
+    try:
+        data = Json.get(filepath)
+        if config.debug:
+            print("BarlaDB: " + Fore.GREEN + "Данные успешно были получены!" + Style.RESET_ALL)
+        if data == {}:
+            print("BarlaDB: " + Fore.YELLOW + "База данных пустая." + Style.RESET_ALL)
+            return
+        else:
+            return data
+    except:
+        print("BarlaDB: " + Fore.RED + f"Базы данных {filepath} не существует!" + Style.RESET_ALL)
+        return
+#save - сохранить в БД
+def save(filepath: str, data: str):
+    try:
+        Json.save(filepath, data)
+        if config.debug:
+            print("BarlaDB: " + Fore.GREEN + "Данные успешно были обновлены!" + Style.RESET_ALL)
+        if data == None:
+            print("BarlaDB: " + Fore.YELLOW + "Переменная с данными пуста." + Style.RESET_ALL)
+        else:
+            return
+    except:
+        print("BarlaDB: " + Fore.RED + f"Базы данных {filepath} не существует!" + Style.RESET_ALL)
+        return
+#create - создать БД
+def create(filename: str):
+    Json.save(filename, data)
+    if config.debug:
+        print("BarlaDB: " + Fore.GREEN + f"База данных {filepath} была успешно создана!" + Style.RESET_ALL)
+#delete - удалить БД
+def delete(filepath: str):
+    try:
+        os.remove(f"{filename}.json")
+        if config.debug:
+            print("BarlaDB: " + Fore.GREEN + f"База данных {filepath} была успешно удалена!" + Style.RESET_ALL)
+        else:
+            return data
+    except:
+        print("BarlaDB: " + Fore.RED + f"Базы данных {filepath} не существует!" + Style.RESET_ALL)
+        return
+def search(filepath, key):
+    try:
+        with open(filepath + ".json", "r") as file:
+            data = Json.get(filepath)
+            if data == {}:
+                print("BarlaDB: " + Fore.YELLOW + "База данных пустая." + Style.RESET_ALL)
+                return
+            if key in data:
+                print("BarlaDB: " + Fore.GREEN + "Найдено одно совпадение.\n" + Style.RESET_ALL + f'"{key}": {data[key]}')
+                return data[key]
+            else:
+                print("BarlaDB: " + Fore.YELLOW + "Не найдено ни одно совпадение." + Style.RESET_ALL)
+                return None
+    except:
+        print("BarlaDB: " + Fore.RED + f"Базы данных {filepath} не существует!" + Style.RESET_ALL)
+        return
