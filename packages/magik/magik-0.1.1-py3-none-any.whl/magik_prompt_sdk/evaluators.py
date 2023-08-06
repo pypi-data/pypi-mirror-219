2c36d7b879592c473a469f8a0f814ea5335865fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:25 +0000

    Add package magik/0.1.1/magik-0.1.1-py3-none-any.whl

---
+++ packages/magik/magik-0.1.1-py3-none-any.whl/magik_prompt_sdk/evaluators.py
@@ -0,0 +1,212 @@
+# Contains functions to evaluate assertions.
+import re
+import ast
+from magik_prompt_sdk.openai_helper import OpenAI
+from magik_prompt_sdk.constants import OPEN_AI_DEFAULT_MODEL
+
+
+def generate_grading_prompt(output_to_evaluate, grading_criteria):
+    return (
+        """
+    You are grading a response string according to a grading criteria given to you.
+    
+    If the gradient criteria is true, then the test_result is True. Otherwise, it is false.
+    
+    Explain the reason for the test_result in the reason field.
+    
+    Respond with a python dictionary object that looks like this:
+    {test_result: boolean; reason: string;}
+    
+    Examples:
+
+    String: Qui est le président des États-Unis?
+    Grading Criteria: Contains the french words for 'president' and 'United States'
+    {"test_result": True, "reason": "the content contains the word 'world'"}
+
+    String: Earth is the second planet from the sun.
+    Grading Criteria: If the fact is true, then the test_result is True. Otherwise, it is false.
+    {"test_result": False, "reason": "The string is factually inaccurate - Earth is actually the third planet from the sun."}`
+    """
+        + f"""
+        String: {output_to_evaluate}
+        Grading Criteria: {grading_criteria}
+    """
+    )
+
+
+# Define the evaluation functions
+def equals(
+    output_to_test,
+    expected_output,
+    case_sensitive=False,
+):
+    if case_sensitive == False:
+        output_to_test = output_to_test.lower()
+        expected_output = expected_output.lower()
+    if output_to_test == expected_output:
+        result = True
+        reason = "✅ output exactly matches expected output"
+    else:
+        result = False
+        reason = "output does not exactly match expected output"
+    return {"result": result, "reason": reason}
+
+
+def contains_all(output_to_test, keywords, case_sensitive=False):
+    if case_sensitive == False:
+        output_to_test = output_to_test.lower()
+        keywords = list(map(lambda k: k.lower(), keywords))
+    missing_keywords = []
+    for keyword in keywords:
+        if keyword not in output_to_test:
+            result = False
+            missing_keywords.append(keyword)
+    if (len(missing_keywords)) > 0:
+        result = False
+        reason = f"keywords not found in output: " + ", ".join(missing_keywords)
+    else:
+        result = True
+        reason = f"{len(keywords)}/{len(keywords)} keywords found in output"
+
+    return {"result": result, "reason": reason}
+
+
+def contains_any(output_to_test, keywords, case_sensitive=False):
+    if not case_sensitive:
+        output_to_test = output_to_test.lower()
+        keywords = list(map(lambda k: k.lower(), keywords))
+
+    found_keywords = []
+    for keyword in keywords:
+        if keyword in output_to_test:
+            found_keywords.append(keyword)
+
+    if found_keywords:
+        result = True
+        reason = f"One or more keywords were found in output: " + ", ".join(
+            found_keywords
+        )
+    else:
+        result = False
+        reason = "No keywords found in output"
+
+    return {"result": result, "reason": reason}
+
+
+def contains_none(output_to_test, keywords, case_sensitive=False):
+    if not case_sensitive:
+        output_to_test = output_to_test.lower()
+        keywords = list(map(lambda k: k.lower(), keywords))
+
+    found_keywords = []
+    for keyword in keywords:
+        if keyword in output_to_test:
+            found_keywords.append(keyword)
+
+    if found_keywords:
+        result = False
+        reason = f"One or more keywords were found in output: " + ", ".join(
+            found_keywords
+        )
+    else:
+        result = True
+        reason = "No keywords found in output"
+
+    return {"result": result, "reason": reason}
+
+
+def negate(output_to_test, eval_function, *args, **kwargs):
+    eval_result = eval_function(output_to_test, *args, **kwargs)
+    return {
+        "result": not eval_result["result"],
+        "reason": f"{eval_function.__name__} returned {eval_result['result']} with reason {eval_result['reason']}",
+    }
+
+
+def contains(output_to_test, keyword, case_sensitive=False):
+    if case_sensitive == False:
+        output_to_test = output_to_test.lower()
+        keyword = keyword.lower()
+    if keyword not in output_to_test:
+        result = False
+        reason = f"keyword not found in output: " + keyword
+    else:
+        result = True
+        reason = f"keyword {keyword} found in output"
+
+    return {"result": result, "reason": reason}
+
+
+def regex_match(output_to_test, pattern):
+    match = re.match(pattern, output_to_test)
+    if match:
+        return {"result": True, "reason": f"regex pattern {pattern} found in output"}
+    else:
+        return {
+            "result": False,
+            "reason": f"regex pattern {pattern} not found in output",
+        }
+
+
+def starts_with(output_to_test, substring, case_sensitive=False):
+    if case_sensitive == False:
+        output_to_test = output_to_test.lower()
+        substring = substring.lower()
+    result = output_to_test.startswith(substring)
+    if result == True:
+        return {"result": result, "reason": "output starts with " + substring}
+    else:
+        return {"result": result, "reason": "output does not start with " + substring}
+
+
+def ends_with(output_to_test, substring, case_sensitive=False):
+    if case_sensitive == False:
+        output_to_test = output_to_test.lower()
+        substring = substring.lower()
+    result = output_to_test.endswith(substring)
+    if result == True:
+        return {"result": result, "reason": "output ends with " + substring}
+    else:
+        return {"result": result, "reason": "output does not end with " + substring}
+
+
+def grade_using_llm(output_to_test, eval_rubric):
+    openai = OpenAI()
+    # eval_rubric is a string that contains the rubric by which to evaluate the output
+    evaluation_prompt = generate_grading_prompt(output_to_test, eval_rubric)
+    llm_response = openai.openai_chat_completion_message(
+        model=OPEN_AI_DEFAULT_MODEL, prompt=evaluation_prompt
+    )
+
+    # llm_response_dict is a dictionary that contains the response from the LLM
+    llm_response_dict = ast.literal_eval(llm_response)
+
+    # result is the true / false value from the LLM
+    result = llm_response_dict["test_result"]
+    reason = llm_response_dict["reason"]
+
+    # output the result of the evaluation
+    if result != True and result != False:
+        result = False
+        reason = "LLM response does not contain a boolean value"
+    return {"result": result, "reason": reason}
+
+
+def is_email(output_to_test):
+    return regex_match(
+        output_to_test, r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    )
+
+
+def is_phone_number(output_to_test):
+    return regex_match(output_to_test, r"^\+?1?\d{9,15}$")
+
+
+def contains_email(output_to_test):
+    return regex_match(
+        output_to_test, r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
+    )
+
+
+def contains_phone_number(output_to_test):
+    return regex_match(output_to_test, r"\+?1?\d{9,15}|\(\d{3}\)\s*\d{3}[-\s]?\d{4}")
---
+++ packages/magik/magik-0.1.1-py3-none-any.whl/magik_prompt_sdk/run.py
@@ -0,0 +1,164 @@
+import os
+import sys
+import importlib.util
+from datetime import datetime
+from magik_prompt_sdk.logger import logger
+from magik_prompt_sdk.utils import substitute_vars
+from magik_prompt_sdk.openai_helper import OpenAI
+from magik_prompt_sdk.sys_exec import read_from_file, create_file
+from magik_prompt_sdk.config import get_open_ai_default_model
+from magik_prompt_sdk.constants import TESTRUNS_DIR, TEST_DIR
+
+
+def run_tests(test_name):
+    tests = _load_tests(test_name)
+    raw_prompt = _load_prompt(test_name)
+    log_file_path = _log_file_path(test_name)
+    _run_tests_for_prompt(test_name, tests, raw_prompt, log_file_path=log_file_path)
+
+
+def _run_tests_for_prompt(test_name, tests, raw_prompt, log_file_path):
+    _log_to_file_and_console("---------------")
+    _log_to_file_and_console("TEST RESULTS")
+    _log_to_file_and_console("---------------")
+    _log_to_file_and_console("\n")
+
+    num_tests_passed = 0
+    for test in tests:
+        test_result = _run_individual_test_for_prompt(
+            test_name, test, raw_prompt, log_file_path
+        )
+        if test_result:
+            num_tests_passed += 1
+
+    num_tests_failed = len(tests) - num_tests_passed
+
+    logger.info("\n------------")
+    if num_tests_passed == len(tests):
+        logger.info("✅ All tests passed")
+        logger.info("\n\n")
+    else:
+        logger.info(
+            f"""
+✅ {num_tests_passed}/{len(tests)} tests passed
+❌ {num_tests_failed}/{len(tests)} tests failed
+        """
+        )
+
+
+def _run_individual_test_for_prompt(test_name, test, raw_prompt, log_file_path):
+    openai = OpenAI()
+    prompt = substitute_vars(raw_prompt, test["prompt_vars"])
+    model = get_open_ai_default_model()
+    prompt_response = openai.openai_chat_completion_message(model=model, prompt=prompt)
+    return _run_individual_test_for_prompt_response(
+        test_name, test, prompt, prompt_response, log_file_path=log_file_path
+    )
+
+
+def _run_individual_test_for_prompt_response(
+    test_name, test, prompt, prompt_response, log_file_path
+):
+    test_function_result_obj = test["eval_function"](
+        prompt_response, *test["eval_function_args"]
+    )
+    result_obj = _generate_result_object(
+        test=test,
+        test_result=test_function_result_obj,
+        prompt=prompt,
+        prompt_response=prompt_response,
+    )
+
+    _log_results(
+        result_obj,
+        log_file_path=log_file_path,
+    )
+
+    return result_obj["test_result"]["result"]
+
+
+def _load_prompt(test_name):
+    test_file_path = f"{TEST_DIR}/{test_name}/prompt.txt"
+    absolute_path = os.path.abspath(test_file_path)
+    return read_from_file(absolute_path)
+
+
+def _load_tests(test_name):
+    test_file_path = f"{TEST_DIR}/{test_name}/assertions.py"
+    # Get the absolute path by resolving against the current working directory
+    absolute_path = os.path.abspath(test_file_path)
+
+    # Get the directory path and module name from the absolute path
+    directory, module_name = os.path.split(absolute_path)
+    module_name = os.path.splitext(module_name)[0]
+
+    # Load the module dynamically
+    spec = importlib.util.spec_from_file_location(module_name, absolute_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    # Access the `tests` array from the module
+    return getattr(module, "tests", [])
+
+
+def _generate_result_object(test, test_result, prompt, prompt_response):
+    did_test_pass = test_result["result"]
+    failure_labels = test["failure_labels"] if not did_test_pass else []
+    test_function_name = test["eval_function"].__name__
+    return {
+        "test": {
+            **test,
+            "eval_function": test_function_name,
+        },
+        "test_result": test_result,
+        "prompt": prompt,
+        "prompt_response": prompt_response,
+        "failure_labels": failure_labels,
+    }
+
+
+def _log_results(result_obj, log_file_path=None):
+    sys.stdout.flush()  # Flush the stdout buffer to ensure immediate printing to the console
+    if log_file_path is not None:
+        create_file(log_file_path)
+        with open(log_file_path, "a") as log_file:
+            _log_prompt_results(result_obj, log_file)
+            _log_test_results(result_obj, log_file)
+    else:
+        _log_prompt_results(result_obj)
+        _log_test_results(result_obj)
+
+
+def _log_to_file_and_console(output, log_file=None):
+    if log_file is not None:
+        log_file.write(output + "\n")
+        log_file.flush()  # Ensure immediate writing to the file
+    logger.info(output)
+
+
+def _log_prompt_results(result_obj, log_file=None):
+    prompt = result_obj["prompt"]
+    prompt_response = result_obj["prompt_response"]
+    _log_to_file_and_console(f"Prompt: {prompt}", log_file)
+    _log_to_file_and_console(f"Prompt Response: {prompt_response}", log_file)
+
+
+def _log_test_results(result_obj, log_file=None):
+    test_description = result_obj["test"]["description"]
+    test_function_result_bool = result_obj["test_result"]["result"]
+    test_result_str = "✅ Passed" if test_function_result_bool else "❌ Failed"
+    test_result_reason = result_obj["test_result"]["reason"]
+    failure_labels = result_obj["failure_labels"]
+
+    _log_to_file_and_console(f"Test: {test_description}", log_file)
+    _log_to_file_and_console(f"Test Result: {test_result_str}", log_file)
+    _log_to_file_and_console(f"Reason: {test_result_reason}", log_file)
+    _log_to_file_and_console(f"Failure Labels: {failure_labels}", log_file)
+    _log_to_file_and_console("\n", log_file)
+
+
+def _log_file_path(test_name):
+    current_timestamp = datetime.now()
+    formatted_timestamp = current_timestamp.strftime("%Y-%m-%d_%H-%M-%S")
+    log_file_path = f"{TESTRUNS_DIR}/{test_name}/{formatted_timestamp}.txt"
+    return log_file_path
