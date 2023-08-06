3c0fc1d0c824decad711f224329bc7d37d5a34ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:32 +0000

    Add package kubernetes-stubs-elephant-fork/27.2.0/kubernetes_stubs_elephant_fork-27.2.0-py2.py3-none-any.whl

---
+++ packages/kubernetes-stubs-elephant-fork/kubernetes_stubs_elephant_fork-27.2.0-py2.py3-none-any.whl/kubernetes-stubs/client/models/v1_validation_rule.pyi
@@ -0,0 +1,18 @@
+import datetime
+import typing
+
+import kubernetes.client
+
+class V1ValidationRule:
+    message: typing.Optional[str]
+    message_expression: typing.Optional[str]
+    rule: str
+    
+    def __init__(self, *, message: typing.Optional[str] = ..., message_expression: typing.Optional[str] = ..., rule: str) -> None:
+        ...
+    def to_dict(self) -> V1ValidationRuleDict:
+        ...
+class V1ValidationRuleDict(typing.TypedDict, total=False):
+    message: typing.Optional[str]
+    messageExpression: typing.Optional[str]
+    rule: str
