ea6e86add40f3027b8126100a6927b5446e2a308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:47 +0000

    Add package whttpcats/1.0/whttpcats-1.0.tar.gz

---
+++ packages/whttpcats/whttpcats-1.0.tar.gz/whttpcats-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="whttpcats",
+    version="1.0",
+    packages=find_packages(),
+    author="resweirdoo",
+    author_email="weirdoo145@gmail.com",
+    description="A minimal wrapper to https://http.cat"
+)
