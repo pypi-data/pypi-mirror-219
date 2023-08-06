d1bc7250ad4ac797647595daae3941545d509e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:19 +0000

    Add package Sockets-connection/0.0.1/Sockets_connection-0.0.1.tar.gz

---
+++ packages/Sockets-connection/Sockets_connection-0.0.1.tar.gz/Sockets_connection-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup (
+    name="Sockets_connection",
+    version='0.0.1',
+    author="Sridhar",
+    url="https://git.selfmade.ninja/SRIDHARDSCV/sockets-python",
+    author_email="sridhardscv@gmail.com",
+    description="Connect the Multiple devices using Python Sockets with simple ways",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+    install_requires = [
+        "sockets"
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            'Sockets_connection=main.main:Socket',
+        ],
+    }
+
+)
+
