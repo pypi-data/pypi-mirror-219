0a230fba2989db674aefe8ac13bbbe00417155a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:12 +0000

    Add package database-without-orm-local/0.0.10/database-without-orm-local-0.0.10.tar.gz

---
+++ packages/database-without-orm-local/database-without-orm-local-0.0.10.tar.gz/database-without-orm-local-0.0.10/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+# used by python -m build
+setuptools.setup(
+     name='database-without-orm-local',  
+     version='0.0.10', # https://pypi.org/project/database-without-orm-local/
+     author="Circles",
+     author_email="info@circles.life",
+     description="PyPI Package for Circles Local Database Python",
+     long_description="This is a package for sharing common Logger function used in different repositories",
+     long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
