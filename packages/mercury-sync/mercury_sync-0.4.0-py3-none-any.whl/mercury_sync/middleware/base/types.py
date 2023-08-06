271ed7c811f1efefa32e640b2e75200d6669e325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:47 +0000

    Add package mercury-sync/0.4.0/mercury_sync-0.4.0-py3-none-any.whl

---
+++ packages/mercury-sync/mercury_sync-0.4.0-py3-none-any.whl/mercury_sync/middleware/base/types.py
@@ -0,0 +1,92 @@
+from enum import Enum
+from mercury_sync.models.response import Response
+from mercury_sync.models.request import Request
+from pydantic import BaseModel
+from typing import (
+    Any,
+    Callable, 
+    Union, 
+    Tuple,
+    Coroutine
+)
+
+
+class MiddlewareType(Enum):
+    BIDIRECTIONAL='BIDIRECTIONAL'
+    UNIDIRECTIONAL_BEFORE='UNIDIRECTIONAL_BEFORE'
+    UNIDIRECTIONAL_AFTER='UNIDIRECTIONAL_AFTER'
+
+
+RequestHandler = Callable[
+    [Request],
+    Coroutine[
+        Any,
+        Any,
+        Tuple[
+            Union[
+                Response,
+                BaseModel,
+                str,
+                None
+            ],
+            int
+        ]
+    ]
+]
+
+WrappedHandler = Callable[
+    [
+        Request,
+        Response,
+        int
+    ],
+    Coroutine[
+        Any,
+        Any,
+        Tuple[
+            Response,
+            int
+        ]
+    ]
+]
+
+MiddlewareHandler = Callable[
+    [
+        Request,
+        Response,
+        int
+    ],
+    Coroutine[
+        Any, 
+        Any, 
+        Tuple[
+            Tuple[Response, int],
+            bool
+        ]
+    ]
+]
+
+
+BidirectionalMiddlewareHandler = Callable[
+    [
+        Request,
+        Response,
+        int
+    ],
+    Coroutine[
+        Any, 
+        Any, 
+        Tuple[
+            Tuple[
+                Request,
+                Response, 
+                int
+            ],
+            bool
+        ]
+    ]
+]
+
+
+
+Handler = Union[RequestHandler, WrappedHandler]
