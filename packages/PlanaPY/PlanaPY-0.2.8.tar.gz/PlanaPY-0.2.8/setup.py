900111642ed032a8d869c2352c0e436b5747a09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:46 +0000

    Add package PlanaPY/0.2.8/PlanaPY-0.2.8.tar.gz

---
+++ packages/PlanaPY/PlanaPY-0.2.8.tar.gz/PlanaPY-0.2.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.8'
+DESCRIPTION = 'Anaplan RESTful API Convenience Library'
+
+setup(
+    name='PlanaPY',
+    version=VERSION,
+    license='MIT',
+    author='Erik Schau',
+    author_email='hopfenspiel@gmail.com',
+    packages=find_packages(),
+    description=DESCRIPTION,
+    url='https://github.com/hopfenspiel/PlanaPY',
+    keywords='anaplan',
+    install_requires=[
+          'requests',
+          'pandas',
+      ],
+
+)
