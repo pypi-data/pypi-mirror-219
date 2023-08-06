bf6b94183031b320d555216b2a9eca7d0cd09c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:05 +0000

    Add package aliyundriveAutoCheckin/0.1.7.1/aliyundriveAutoCheckin-0.1.7.1.tar.gz

---
+++ packages/aliyundriveAutoCheckin/aliyundriveAutoCheckin-0.1.7.1.tar.gz/aliyundriveAutoCheckin-0.1.7.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aliyundriveAutoCheckin",
+    version="0.1.7.1",
+    author="xiaohan17",
+    author_email="2799153122@qq.com",
+    description="阿里云盘自动签到",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zzh0107/aliyundriveAutoCheckin",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
