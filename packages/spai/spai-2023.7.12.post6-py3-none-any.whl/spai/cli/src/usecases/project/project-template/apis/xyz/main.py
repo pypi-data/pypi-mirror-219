daffba531b3fa94265dc50c6b34cf967d673604d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:07 +0000

    Add package spai/2023.7.12.post6/spai-2023.7.12.post6-py3-none-any.whl

---
+++ packages/spai/spai-2023.7.12.post6-py3-none-any.whl/spai/cli/src/usecases/project/project-template/apis/xyz/main.py
@@ -0,0 +1,64 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from starlette.responses import StreamingResponse
+from fastapi import HTTPException, status
+import uvicorn
+
+from spai.image.xyz import get_image_data, get_tile_data, ready_image
+from spai.image.xyz.errors import ImageOutOfBounds
+from spai.storage import Storage
+
+# init api
+app = FastAPI(title="xyz")
+
+# configure CORS
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+storage = Storage("data")
+
+
+@app.get("/")
+def retrieve_images():
+    return storage.list("*.tif")
+
+
+@app.get("/{image}/{z}/{x}/{y}.png")
+def retrieve_image_tile(
+    image: str,
+    z: int,
+    x: int,
+    y: int,
+    bands: str = "1",
+    stretch: str = "0,1",
+    palette: str = "viridis",
+):
+    image_path = storage.get_path(image)
+    tile_size = (256, 256)
+    if len(bands) == 1:
+        bands = int(bands)
+    else:
+        bands = tuple([int(band) for band in bands.split(",")])
+    stretch = tuple([float(v) for v in stretch.split(",")])
+    try:
+        tile = get_tile_data(image_path, (x, y, z), bands, tile_size)
+        tile = get_image_data(tile, stretch, palette)
+        image = ready_image(tile)
+        return StreamingResponse(image, media_type="image/png")
+    except ImageOutOfBounds as error:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=error.message)
+
+
+# put ui here !
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
