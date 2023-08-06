427ae7759d88f40f9573471635bcff0f6c1983d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:35 +0000

    Add package summers/0.1.0/summers-0.1.0-py3.6.egg

---
+++ packages/summers/summers-0.1.0-py3.6.egg/summers/cc.py
@@ -0,0 +1,18 @@
+from summers import CleanArch
+
+
+class CleanArchImpl(CleanArch):
+
+    @staticmethod
+    def dependencies(*args, **kwargs):
+        """
+        获取依赖关系信息, JSON 格式 TODO
+        """
+        pass
+
+    @staticmethod
+    def validate(*args, **kwargs):
+        """
+        整洁架构框风格依赖关系校验 TODO
+        """
+        pass
