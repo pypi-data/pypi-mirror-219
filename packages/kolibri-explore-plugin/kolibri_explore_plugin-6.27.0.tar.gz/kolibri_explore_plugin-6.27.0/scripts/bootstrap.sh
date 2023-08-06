f6013bea67ca11a3f053c11257641f9f041f7c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:36 +0000

    Add package kolibri-explore-plugin/6.27.0/kolibri_explore_plugin-6.27.0.tar.gz

---
+++ packages/kolibri-explore-plugin/kolibri_explore_plugin-6.27.0.tar.gz/kolibri_explore_plugin-6.27.0/scripts/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+#
+# Setup Project
+
+pipenv install --dev --deploy
+pipenv run nodeenv -p --node=16.14.0
+pipenv run npm install -g yarn@1.22.19
+pipenv run yarn global add @vue/cli@v4.5.15
+pipenv run yarn install --ignore-engines
