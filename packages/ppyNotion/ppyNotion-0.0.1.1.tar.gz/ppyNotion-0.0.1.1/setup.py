1c8d02883766ff26d0841bc635a18727c3fef5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:16 +0000

    Add package ppyNotion/0.0.1.1/ppyNotion-0.0.1.1.tar.gz

---
+++ packages/ppyNotion/ppyNotion-0.0.1.1.tar.gz/ppyNotion-0.0.1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ppyNotion",
+    version="0.0.1.1",
+    author="jirayuwat b.",
+    author_email="jirayuwat.work@gmail.com",
+    description="Connect to your Notion database with ease.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jirayuwat12/ppyNotion",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
