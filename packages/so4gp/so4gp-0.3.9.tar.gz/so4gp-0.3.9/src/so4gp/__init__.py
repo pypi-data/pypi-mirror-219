d802af5ca74fa69c549e9742fdfab3242f941d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:25 +0000

    Add package so4gp/0.3.9/so4gp-0.3.9.tar.gz

---
+++ packages/so4gp/so4gp-0.3.9.tar.gz/so4gp-0.3.9/src/so4gp/__init__.py
@@ -0,0 +1,20 @@
+
+from .so4gp import DataGP
+from .so4gp import ExtGP
+from .so4gp import GI
+from .so4gp import GP
+from .so4gp import NumericSS
+from .so4gp import TimeLag
+
+from .so4gp import ClusterGP
+from .so4gp import GRAANK
+from .so4gp import AntGRAANK
+from .so4gp import GeneticGRAANK
+from .so4gp import HillClimbingGRAANK
+from .so4gp import ParticleGRAANK
+from .so4gp import RandomGRAANK
+
+from .so4gp import analyze_gps
+from .so4gp import get_num_cores
+from .so4gp import get_slurm_cores
+from .so4gp import write_file
