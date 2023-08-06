6178310d304221ed725ac40ee200783d0e3b5d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:09 +0000

    Add package dbt-semantic-interfaces/0.1.0.dev10/dbt_semantic_interfaces-0.1.0.dev10-py3-none-any.whl

---
+++ packages/dbt-semantic-interfaces/dbt_semantic_interfaces-0.1.0.dev10-py3-none-any.whl/dbt_semantic_interfaces/validations/metrics.py
@@ -0,0 +1,155 @@
+import traceback
+from typing import Generic, List, Sequence
+
+from dbt_semantic_interfaces.errors import ParsingException
+from dbt_semantic_interfaces.implementations.metric import PydanticMetricTimeWindow
+from dbt_semantic_interfaces.protocols import (
+    Metric,
+    SemanticManifest,
+    SemanticManifestT,
+)
+from dbt_semantic_interfaces.references import MetricModelReference
+from dbt_semantic_interfaces.type_enums import MetricType
+from dbt_semantic_interfaces.validations.unique_valid_name import UniqueAndValidNameRule
+from dbt_semantic_interfaces.validations.validator_helpers import (
+    FileContext,
+    MetricContext,
+    SemanticManifestValidationRule,
+    ValidationError,
+    ValidationIssue,
+    validate_safely,
+)
+
+
+class CumulativeMetricRule(SemanticManifestValidationRule[SemanticManifestT], Generic[SemanticManifestT]):
+    """Checks that cumulative sum metrics are configured properly."""
+
+    @staticmethod
+    @validate_safely(whats_being_done="checking that the params of metric are valid if it is a cumulative sum metric")
+    def _validate_cumulative_sum_metric_params(metric: Metric) -> List[ValidationIssue]:
+        issues: List[ValidationIssue] = []
+
+        if metric.type == MetricType.CUMULATIVE:
+            if metric.type_params.window and metric.type_params.grain_to_date:
+                issues.append(
+                    ValidationError(
+                        context=MetricContext(
+                            file_context=FileContext.from_metadata(metadata=metric.metadata),
+                            metric=MetricModelReference(metric_name=metric.name),
+                        ),
+                        message="Both window and grain_to_date set for cumulative metric. Please set one or the other",
+                    )
+                )
+
+            if metric.type_params.window:
+                try:
+                    window_str = f"{metric.type_params.window.count} {metric.type_params.window.granularity.value}"
+                    # TODO: Should not call an implementation class.
+                    PydanticMetricTimeWindow.parse(window_str)
+                except ParsingException as e:
+                    issues.append(
+                        ValidationError(
+                            context=MetricContext(
+                                file_context=FileContext.from_metadata(metadata=metric.metadata),
+                                metric=MetricModelReference(metric_name=metric.name),
+                            ),
+                            message="".join(traceback.format_exception_only(etype=type(e), value=e)),
+                            extra_detail="".join(traceback.format_tb(e.__traceback__)),
+                        )
+                    )
+
+        return issues
+
+    @staticmethod
+    @validate_safely(whats_being_done="running model validation ensuring cumulative sum metrics are valid")
+    def validate_manifest(semantic_manifest: SemanticManifestT) -> Sequence[ValidationIssue]:  # noqa: D
+        issues: List[ValidationIssue] = []
+
+        for metric in semantic_manifest.metrics or []:
+            issues += CumulativeMetricRule._validate_cumulative_sum_metric_params(metric=metric)
+
+        return issues
+
+
+class DerivedMetricRule(SemanticManifestValidationRule[SemanticManifestT], Generic[SemanticManifestT]):
+    """Checks that derived metrics are configured properly."""
+
+    @staticmethod
+    @validate_safely(whats_being_done="checking that the alias set are not unique and distinct")
+    def _validate_alias_collision(metric: Metric) -> List[ValidationIssue]:
+        issues: List[ValidationIssue] = []
+
+        if metric.type == MetricType.DERIVED:
+            metric_context = MetricContext(
+                file_context=FileContext.from_metadata(metadata=metric.metadata),
+                metric=MetricModelReference(metric_name=metric.name),
+            )
+            used_names = {input_metric.name for input_metric in metric.input_metrics}
+            for input_metric in metric.input_metrics:
+                if input_metric.alias:
+                    issues += UniqueAndValidNameRule.check_valid_name(input_metric.alias, metric_context)
+                    if input_metric.alias in used_names:
+                        issues.append(
+                            ValidationError(
+                                context=metric_context,
+                                message=f"Alias '{input_metric.alias}' for input metric: '{input_metric.name}' is "
+                                "already being used. Please choose another alias.",
+                            )
+                        )
+                        used_names.add(input_metric.alias)
+        return issues
+
+    @staticmethod
+    @validate_safely(whats_being_done="checking that the input metrics exist")
+    def _validate_input_metrics_exist(semantic_manifest: SemanticManifest) -> List[ValidationIssue]:
+        issues: List[ValidationIssue] = []
+
+        all_metrics = {m.name for m in semantic_manifest.metrics}
+        for metric in semantic_manifest.metrics:
+            if metric.type == MetricType.DERIVED:
+                for input_metric in metric.input_metrics:
+                    if input_metric.name not in all_metrics:
+                        issues.append(
+                            ValidationError(
+                                context=MetricContext(
+                                    file_context=FileContext.from_metadata(metadata=metric.metadata),
+                                    metric=MetricModelReference(metric_name=metric.name),
+                                ),
+                                message=f"For metric: {metric.name}, input metric: '{input_metric.name}' does not "
+                                "exist as a configured metric in the model.",
+                            )
+                        )
+        return issues
+
+    @staticmethod
+    @validate_safely(whats_being_done="checking that input metric time offset params are valid")
+    def _validate_time_offset_params(metric: Metric) -> List[ValidationIssue]:
+        issues: List[ValidationIssue] = []
+
+        for input_metric in metric.input_metrics or []:
+            if input_metric.offset_window and input_metric.offset_to_grain:
+                issues.append(
+                    ValidationError(
+                        context=MetricContext(
+                            file_context=FileContext.from_metadata(metadata=metric.metadata),
+                            metric=MetricModelReference(metric_name=metric.name),
+                        ),
+                        message=f"Both offset_window and offset_to_grain set for derived metric '{metric.name}' on "
+                        f"input metric '{input_metric.name}'. Please set one or the other.",
+                    )
+                )
+
+        return issues
+
+    @staticmethod
+    @validate_safely(
+        whats_being_done="running model validation ensuring derived metrics properties are configured properly"
+    )
+    def validate_manifest(semantic_manifest: SemanticManifestT) -> Sequence[ValidationIssue]:  # noqa: D
+        issues: List[ValidationIssue] = []
+
+        issues += DerivedMetricRule._validate_input_metrics_exist(semantic_manifest=semantic_manifest)
+        for metric in semantic_manifest.metrics or []:
+            issues += DerivedMetricRule._validate_alias_collision(metric=metric)
+            issues += DerivedMetricRule._validate_time_offset_params(metric=metric)
+        return issues
