573d29f9ad56ecf58e17bc40ce7a13694c1f3896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:12 +0000

    Add package connectors-to-databases/1.0.3/connectors_to_databases-1.0.3.tar.gz

---
+++ packages/connectors-to-databases/connectors_to_databases-1.0.3.tar.gz/connectors_to_databases-1.0.3/connectors_to_databases/class_use.py
@@ -0,0 +1,21 @@
+from PostgreSQL import PostgreSQL
+
+pg = PostgreSQL(port=1)
+
+# df = pg.execute_to_df('select 1 as one')
+
+# check = pg.check_connection_to_database()
+
+# print(check)
+
+# pg.generate_on_conflict_sql_query(
+#     # source_table_schema_name='stg',
+#     source_table_name='tmp_test_pk',
+#     # target_table_schema_name='dds',
+#     target_table_name='test_pk',
+#     list_columns=['id', 'value'],
+#     # pk=['id','username'],
+#     replace=True
+# )
+
+pg.check_connection_to_database()
