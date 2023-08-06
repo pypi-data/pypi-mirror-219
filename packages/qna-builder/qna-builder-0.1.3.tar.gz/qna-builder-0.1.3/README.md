c7fa37b6bf2be82852f5035edad353a978c2d6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:03 +0000

    Add package qna-builder/0.1.3/qna-builder-0.1.3.tar.gz

---
+++ packages/qna-builder/qna-builder-0.1.3.tar.gz/qna-builder-0.1.3/README.md
@@ -0,0 +1,92 @@
+# QnA Builder
+
+![](https://img.shields.io/badge/version-v0.1.3-green)
+![](https://img.shields.io/badge/python-3.7%20%7C%203.8%20%7C%203.9-blue)
+![](https://github.com/msamsami/qna-bot/actions/workflows/python-publish.yml/badge.svg)
+[![](https://img.shields.io/pypi/v/qna-builder)](https://pypi.org/project/qna-builder/)
+![](https://img.shields.io/pypi/dm/qna-builder)
+
+<p>
+<img src="https://raw.githubusercontent.com/msamsami/qna-builder/main/docs/logo.jpg" alt="QnA Builder logo" width="200"/>
+<br>
+</p>
+
+## Introduction
+**QnA Builder** is a simple, no-code way to build chatbots in Python. It provides a similarity-based conversational dialog
+engine, **QnA Bot**, which makes it easy to generate automated responses to input questions according to a set of known
+conversations, i.e., question-answer pairs, stored in a ***knowledge base***. QnA Bot relies on a collection of
+question-answer pairs to generate answers for new inputs.
+
+## Install
+The easiest way to install the qna-builder is by using `pip`:
+```shell
+pip install qna-builder
+```
+This library is shipped as an all-in-one module implementation with minimalistic dependencies and requirements.
+
+## Getting started
+A QnA Bot can be set up and used in four simple steps:
+
+1. Import `QnABot` class
+
+```python
+from qnabuilder import QnABot
+```
+
+2. Initialize a bot
+```python
+bot = QnABot()
+```
+
+3. Fit the bot engine to a knowledge base
+```python
+bot.fit(kb="knowledge_base.json")
+```
+
+4. Generate answers
+```python
+bot.answer("Hey. What's up?")
+```
+`"All good. What's up with you?"`
+
+## Algorithms
+Currently, QnA Bot engine supports the following algorithms for similarity-based answer generation:
+- TF-IDF Vectorization (`'tfidf'`)
+- Murmurhash3 Vectorization (`'murmurhash'`)
+- Count Vectorization (`'count'`)
+
+Supported similarity metrics are as follows:
+- Cosine similarity (`'cosine'`)
+- Euclidean distance (`'euclidean'`)
+- Manhattan distance (`'manhattan'`)
+
+## Knowledge base editor
+By calling `run_editor()` method of `QnAKnowledgeBase` class, the knowledge base editor window will open up in
+your web browser and allows you to edit your knowledge base by adding, removing, or modifying questions/answers.
+
+```python
+from qnabuilder import QnAKnowledgeBase
+
+kb = QnAKnowledgeBase('my_knowledge_base.json')
+kb.run_editor()
+```
+
+Here, you can see a screenshot of the knowledge base editor:
+
+<div style="text-align:center">
+<img src="https://raw.githubusercontent.com/msamsami/qna-builder/main/docs/kb_editor.png" alt="QnA Bot Knowledge Base Editor" width="450"/>
+</div>
+
+Note that you need to install the optional requirement [streamlit](https://streamlit.io/) to be able to use the
+knowledge base editor.
+
+## Tests
+To run the tests, install development requirements:
+```
+pip install -r requirements_dev.txt
+```
+
+Then, run pytest:
+```
+pytest
+```
---
+++ packages/qna-builder/qna-builder-0.1.3.tar.gz/qna-builder-0.1.3/qnabuilder/qna_bot.py
@@ -0,0 +1,220 @@
+from typing import Any, Tuple, Union
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+from sklearn.exceptions import NotFittedError
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_extraction.text import (
+    TfidfVectorizer,
+    HashingVectorizer,
+    CountVectorizer,
+)
+from sklearn.metrics.pairwise import (
+    cosine_similarity,
+    euclidean_distances,
+    manhattan_distances,
+    haversine_distances,
+)
+
+from .kb import QnAKnowledgeBase, FilePath, DEFAULT_KNOWLEDGE_BASE_FILE_PATH
+from ._enums import EmbeddingModel, SimilarityMetric
+from ._utils import value_error_message
+
+
+class QnABot:
+    _is_fitted: bool = False
+    _model_kwargs: dict = {}
+
+    _params = {"kb": None, "model": None, "ref_embeddings": None}
+
+    def __init__(
+        self,
+        model_name: Union[str, EmbeddingModel] = "tfidf",
+        similarity_metric: Union[str, SimilarityMetric] = "cosine",
+        min_score: float = 0.25,
+        cache: bool = False,
+        **kwargs
+    ) -> None:
+        """Initializes an instance of the QnABot class.
+
+        Args:
+            model_name (Union[str, EmbeddingModel]): Name of the model used for text embedding. Defaults to 'tfidf'.
+            similarity_metric (Union[str, SimilarityMetric]): Similarity metric used to find the most similar question.
+                                                              Defaults to 'cosine'.
+            min_score (float): Minimum similarity score below which an "I don't know" answer will be returned.
+                               Defaults to 0.25.
+            cache (bool): Whether to cache the entire knowledge base in memory. Defaults to False.
+            **kwargs: Other keyword arguments supported to initialize models.
+
+        """
+        self.model_name: str = model_name
+        self.similarity_metric: str = similarity_metric
+        self.min_score: float = min_score
+        self.cache: bool = cache
+
+        self._model_kwargs = kwargs
+
+    @staticmethod
+    def _initialize_model(model_name: str, **kwargs):
+        if model_name == "tfidf":
+            return TfidfVectorizer(**kwargs)
+        elif model_name == "murmurhash":
+            return HashingVectorizer(**kwargs)
+        elif model_name == "count":
+            return CountVectorizer(**kwargs)
+        else:
+            raise ValueError(
+                value_error_message(
+                    "model_name", model_name, [e.value for e in EmbeddingModel]
+                )
+            )
+
+    def fit(
+        self, kb: Union[FilePath, QnAKnowledgeBase] = DEFAULT_KNOWLEDGE_BASE_FILE_PATH
+    ):
+        """Fits QnA Bot to a given knowledge base.
+
+        Args:
+            kb (Union[FilePath, QnAKnowledgeBase]): Path to the knowledge base JSON file or buffer, or a
+                                                    QnAKnowledgeBase object. Defaults to the file path of the default
+                                                    QnA Bot knowledge base.
+
+        Returns:
+            self: The instance itself.
+        """
+        self._is_fitted = False
+        self._params["kb"] = (
+            kb if isinstance(kb, QnAKnowledgeBase) else QnAKnowledgeBase(kb, self.cache)
+        )
+        self._params["model"] = self._initialize_model(
+            model_name=self.model_name, **self._model_kwargs
+        )
+        self._params["model"].fit(self.knowledge_base_.ref_questions)
+        self._params["ref_embeddings"] = self.model_.transform(
+            self.knowledge_base_.ref_questions
+        )
+
+        self._is_fitted = True
+        return self
+
+    @property
+    def _similarity_function(self):
+        functions = {
+            "cosine": cosine_similarity,
+            "euclidean": euclidean_distances,
+            "manhattan": manhattan_distances,
+        }
+
+        if self.similarity_metric not in functions:
+            raise ValueError(
+                value_error_message(
+                    "similarity_metric",
+                    self.similarity_metric,
+                    [e.value for e in SimilarityMetric],
+                )
+            )
+        else:
+            return functions[self.similarity_metric]
+
+    def find_similarity(self, input: str) -> Tuple[int, float]:
+        """Returns the index and similarity score of the question in the knowledge base most similar to the input.
+
+        Args:
+            input (str): Input question.
+
+        Returns:
+            int: Index of the most similar question.
+            float: Similarity score of the most similar question.
+        """
+        if not self._is_fitted:
+            raise NotFittedError(
+                "The model is not fitted. Use fit() method before calling answer()"
+            )
+
+        # Retrieve questions' indices from knowledge base
+        q_idx = self.knowledge_base_.ref_questions_idx
+
+        # Extract input statement embedding
+        input_embeddings = self.model_.transform([input])
+
+        # Calculate the similarities between the input embedding and the reference embeddings
+        similarities = self._similarity_function(
+            input_embeddings, self.ref_embeddings_
+        ).flatten()
+
+        if self.similarity_metric != "cosine":
+            similarities = MinMaxScaler().fit_transform(similarities.reshape(-1, 1))
+            similarities = 1.0 - similarities.flatten()
+
+        # Find the score of the answer with the highest score
+        highest_id = int(np.argmax(similarities))
+        score = float(similarities[highest_id])
+
+        # Find the ID of the answer with the highest score
+        highest_qna_id = q_idx[highest_id]
+
+        return highest_qna_id, score
+
+    def answer(
+        self, input: str, return_score: bool = False
+    ) -> Union[str, Tuple[str, float]]:
+        """Returns the index and similarity score of a question in the knowledge base that is most similar to the input.
+
+        Args:
+            input (str): Input question.
+            return_score (bool): Whether to return the similarity score. Defaults to False.
+
+        Returns:
+            Union[str, Tuple[str, float]]: One of the answers of the most similar question in the knowledge base
+                                           if return_score=False, otherwise a tuple containing:
+                str: One of the answers of the most similar question in the knowledge base.
+                float: Similarity score.
+        """
+        highest_id, score = self.find_similarity(input)
+
+        # If score was lower than the minimum accepted value
+        if score < self.min_score:
+            # Return a random "I don't know" answer
+            answer_ = np.random.choice(self.knowledge_base_.idk_answers)
+
+        else:
+            # Pick a random answer among the answers of the most similar question
+            answer_ = np.random.choice(self.knowledge_base_.qna[highest_id]["a"])
+
+        if return_score:
+            return answer_, score
+        else:
+            return answer_
+
+    @property
+    def knowledge_base_(self) -> QnAKnowledgeBase:
+        """Returns the knowledge base on which the QnA Bot is fitted.
+
+        """
+        return self._params["kb"]
+
+    @property
+    def model_(self) -> Any:
+        """Returns the embedding model fitted on the knowledge base.
+
+        """
+        return self._params["model"]
+
+    @property
+    def ref_embeddings_(self) -> csr_matrix:
+        """Returns the embedding matrix extracted from reference questions in the knowledge base.
+
+        """
+        return self._params["ref_embeddings"]
+
+    def __repr__(self):
+        return (
+            "<QnABot(model_name='%s', similarity_metric='%s', min_score=%.2f, cache=%s)>"
+            % (
+                str(self.model_name),
+                str(self.similarity_metric),
+                self.min_score,
+                self.cache,
+            )
+        )
