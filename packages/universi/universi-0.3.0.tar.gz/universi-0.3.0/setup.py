9db205fdb5b9b04a50517143371ba0c572eb966a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:27 +0000

    Add package universi/0.3.0/universi-0.3.0.tar.gz

---
+++ packages/universi/universi-0.3.0.tar.gz/universi-0.3.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['universi', 'universi.structure']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.96.1',
+ 'type-inspector>=1.2.2,<2.0.0',
+ 'typer[all]',
+ 'typing-extensions']
+
+setup_kwargs = {
+    'name': 'universi',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': '# universi\n\nPackage description\n\n---\n\n<p align="center">\n<a href="https://github.com/ovsyanka83/universi/actions?query=workflow%3ATests+event%3Apush+branch%3Amain" target="_blank">\n    <img src="https://github.com/Ovsyanka83/universi/actions/workflows/test.yaml/badge.svg?branch=main&event=push" alt="Test">\n</a>\n<a href="https://codecov.io/gh/ovsyanka83/universi" target="_blank">\n    <img src="https://img.shields.io/codecov/c/github/ovsyanka83/universi?color=%2334D058" alt="Coverage">\n</a>\n<a href="https://pypi.org/project/universi/" target="_blank">\n    <img alt="PyPI" src="https://img.shields.io/pypi/v/universi?color=%2334D058&label=pypi%20package" alt="Package version">\n</a>\n<a href="https://pypi.org/project/universi/" target="_blank">\n    <img src="https://img.shields.io/pypi/pyversions/universi?color=%2334D058" alt="Supported Python versions">\n</a>\n</p>\n\n## Installation\n\n```bash\npip install universi\n```\n\n<!---\n# TODO: Note that we don\'t handle "from .schemas import Schema as OtherSchema" case\n# TODO: Need to validate that the user doesn\'t use versioned schemas instead of the latest ones\n-->',
+    'author': 'Stanislav Zmiev',
+    'author_email': 'szmiev2000@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/ovsyanka83/universi',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
