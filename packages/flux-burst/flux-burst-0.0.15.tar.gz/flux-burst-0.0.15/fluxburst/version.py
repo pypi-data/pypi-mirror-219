ef38cd7f636737460172c90c253860d58cf64bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:01 +0000

    Add package flux-burst/0.0.15/flux-burst-0.0.15.tar.gz

---
+++ packages/flux-burst/flux-burst-0.0.15.tar.gz/flux-burst-0.0.15/fluxburst/version.py
@@ -0,0 +1,31 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC and other
+# HPCIC DevTools Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (MIT)
+
+__version__ = "0.0.15"
+AUTHOR = "Vanessa Sochat"
+EMAIL = "vsoch@users.noreply.github.com"
+NAME = "flux-burst"
+PACKAGE_URL = "https://github.com/converged-computing/flux-burst"
+KEYWORDS = "cloud, flux-framework, flux, bursting"
+DESCRIPTION = "Flux module that enables burstable plugins."
+LICENSE = "LICENSE"
+
+################################################################################
+# Global requirements
+
+INSTALL_REQUIRES = (("pyyaml", {"min_version": None}),)
+
+INSTALL_REQUIRES_KUBERNETES = (
+    ("kubernetes", {"min_version": None}),
+    ("fluxoperator", {"min_version": None}),
+    ("requests", {"min_version": None}),
+)
+
+TESTS_REQUIRES = (("pytest", {"min_version": "4.6.2"}),)
+
+################################################################################
+# Submodule Requirements (versions that include database)
+
+INSTALL_REQUIRES_ALL = INSTALL_REQUIRES + INSTALL_REQUIRES_KUBERNETES + TESTS_REQUIRES
