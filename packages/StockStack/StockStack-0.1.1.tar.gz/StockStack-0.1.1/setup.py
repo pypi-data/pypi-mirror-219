483224e317de8a94a42555bd7323b1177e58d7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:49 +0000

    Add package StockStack/0.1.1/StockStack-0.1.1.tar.gz

---
+++ packages/StockStack/StockStack-0.1.1.tar.gz/StockStack-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='StockStack',
+    version='0.1.1',
+    author='Carlo Bortolan',
+    author_email='carlobortolan@gmail.com',
+    description='Look up and plot current stock prices from your terminal',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/carlobortolan/StockStack',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'stockstack=stockstack.caller:main'
+        ]
+    },
+    install_requires=[
+        'yfinance',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+)
