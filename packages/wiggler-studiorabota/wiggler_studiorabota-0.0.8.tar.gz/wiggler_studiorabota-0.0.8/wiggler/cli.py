580b362797ec52e0717bd3278b519316f527528a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:47 +0000

    Add package wiggler-studiorabota/0.0.8/wiggler_studiorabota-0.0.8.tar.gz

---
+++ packages/wiggler-studiorabota/wiggler_studiorabota-0.0.8.tar.gz/wiggler_studiorabota-0.0.8/wiggler/cli.py
@@ -0,0 +1,19 @@
+import argparse
+import os
+
+def main():
+  
+    parser = argparse.ArgumentParser(prog ='wiggler', description ='WiggleR API')
+
+    parser.add_argument('-s', '--server', action='store_true')
+    parser.add_argument('-b', '--boot', action='store_true')
+    
+    args = parser.parse_args()
+  
+    if args.server:
+        os.system(f"uvicorn wiggler.main:app --reload --host 0.0.0.0")
+
+    if args.boot:
+         os.system('cp ./start_wiggler.sh /usr/bin/start_wiggler.sh')
+         os.system('cp ./start_wiggler.service /etc/systemd/system/start_wiggler.service')
+         os.system('systemctl enable start_wiggler')
