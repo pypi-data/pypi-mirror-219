f28e639703bebf75171f3d2804e25e059c4663f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:43 +0000

    Add package opents/0.1.3.1/opents-0.1.3.1-py3-none-any.whl

---
+++ packages/opents/opents-0.1.3.1-py3-none-any.whl/opents/datasets/datasets.py
@@ -0,0 +1,240 @@
+# coding=utf-8
+
+import pandas as pd
+import torch
+import numpy as np
+from opents.utils.data_utils import data_file_type, get_dataset_root_path, split_train_test
+
+class Dataset:
+    def __init__(
+            self,
+            dataset_name,
+            dataset_root_path=None,
+            datasets_root_name=None,
+            split=False,
+            train_percentage=None,
+            open_percentage=None,
+            random_state=None
+            ):
+        """_summary_
+
+        Args:
+            dataset_name (_type_): _description_
+            dataset_root_path (_type_, optional): _description_. Defaults to None.
+            datasets_root_name (_type_, optional): _description_. Defaults to None.
+            split (bool, optional): _description_. Defaults to False.
+        """
+        self.dataset_name = dataset_name
+        self.dataset_root_path = get_dataset_root_path(dataset_root_path, dataset_name, datasets_root_name)
+        self.datasets_root_name = datasets_root_name
+        self.split = split
+        self.train_percentage = train_percentage
+        self.open_percentage = open_percentage
+        self.random_state = random_state
+    
+    def ts_dataset(self):
+        """ The path to read the UCR or UEA file, including the path of the training file and the path of the testing file.
+        Args:
+            dataset(string):Define a variable 'dataset_name' for the name of each file, which can be used to locate the training and testing file paths for each function.
+        Returns:
+            for UCR:add an extra dimension to the train data and test data, then return the train data, new label of train data, test data, and new label of test data.
+            for UEA:return the train data, new label of train data, test data, and new label of test data.
+        """
+        train_file_df, test_file_df = data_file_type(self.dataset_name, self.dataset_root_path, self.datasets_root_name)
+        
+        if self.datasets_root_name.lower() == 'ucr':
+            # Create a two-dimensional tensor file from the data in the training set and testing set files.
+            train_tensor = torch.tensor(train_file_df.values)
+            test_tensor = torch.tensor(test_file_df.values)
+
+            # Here, we extract the distinct labels from each dataset.
+            train_labels = torch.unique(train_tensor[:, 0])
+            test_labels = torch.unique(test_tensor[:, 0]) 
+            
+            # Convert the data into a list.
+            train_array = train_tensor.tolist()
+            train_array = np.array(train_array)
+            test_array = test_tensor.tolist()
+            test_array = np.array(test_array)
+
+            # Retrieve the time-series data, excluding the labels. 
+            x_train = train_array[:, 1:].astype(np.float64)
+            x_test = test_array[:, 1:].astype(np.float64)
+            # Store the labels in a dictionary, where the key is the original label and the value is the count of that label.
+            transform = {}
+
+            for i, l in enumerate(train_labels):
+                transform[l.item()] = i
+            # Retrieve the values of all new labels and store them in 'train_label'.
+            y_train = np.vectorize(transform.get)(train_array[:, 0])
+
+            # Retrieve all label values from the test set and store them in 'test_label'.
+            y_test = np.vectorize(transform.get)(test_array[:, 0])
+            # if dataset's dimention is 2, we change the dimention to 3
+            x_train = x_train[..., np.newaxis]
+            x_test = x_test[..., np.newaxis]           
+
+        elif self.datasets_root_name.lower() == 'uea':
+            train_dataset_name = self.dataset_name + "_TRAIN"
+            test_dataset_name = self.dataset_name + "_TRAIN"
+
+            # Store the train data and test data in list, and extend its dimention to 3.
+            train_array = np.array(train_file_df[train_dataset_name].tolist())
+            x_train = train_array.view((np.float64, len(train_array.dtype.names)))
+
+            test_array = np.array(test_file_df[test_dataset_name].tolist())
+            x_test = test_array.view((np.float64, len(test_array.dtype.names)))
+
+            # Here, we extract the distinct labels from each dataset.
+            train_labels = torch.tensor(pd.to_numeric(train_file_df['target'], errors='coerce'))
+            train_label = torch.unique(train_labels)
+            test_labels = torch.tensor(pd.to_numeric(test_file_df['target'], errors='coerce'))
+            test_label = torch.unique(test_labels)
+            
+            transform = {}
+
+            for i, l in enumerate(train_label):
+                transform[l.item()] = i
+            
+            # Retrieve the values of all new labels and store them in 'train_label'.
+            y_train = np.vectorize(transform.get)(train_labels)
+
+            # Retrieve all label values from the test set and store them in 'test_label'.
+            y_test = np.vectorize(transform.get)(test_labels) 
+
+
+        # Add an extra dimension to the train data and test data, then return the train data, new label of train data, test data, and new label of test data.
+        return x_train, y_train, x_test, y_test
+
+    def opents_dataset(self):
+        """ The path to read the UCR or UEA file, including the path of the training file and the path of the testing file.
+        Args:
+            dataset(string):Define a variable 'dataset_name' for the name of each file, which can be used to locate the training and testing file paths for each function.
+        Returns:
+            for UCR:add an extra dimension to the train data and test data, then return the train data, new label of train data, test data, and new label of test data.
+            for UEA:return the train data, new label of train data, test data, and new label of test data.
+        """
+        train_file_df, test_file_df = data_file_type(self.dataset_name, self.dataset_root_path, self.datasets_root_name) 
+
+        if self.datasets_root_name.lower() == 'ucr': 
+            # merge the train and test file into a full data
+            all_data_df = pd.concat([train_file_df, test_file_df], axis=0)
+        
+            # Create a two-dimensional tensor file from the data in the all files.
+            all_tensor = torch.tensor(all_data_df.values)
+
+            # Here, we extract the distinct labels from each dataset.
+            all_labels = torch.unique(all_tensor[:, 0])
+            
+            # Store the labels in a dictionary, where the key is the original label and the value is the count of that label.
+            transform = {}
+
+            for i, l in enumerate(all_labels):
+                transform[l.item()] = i
+
+            # Convert the data into a list.
+            all_array = all_tensor.tolist()
+            all_array = np.array(all_array)
+
+            # Retrieve the time-series data, excluding the labels. 
+            all = all_array[:, 1:].astype(np.float64)
+
+            # Retrieve the values of all new labels and store them in 'train_label'.
+            y_all = np.vectorize(transform.get)(all_array[:, 0])
+            
+            # Add an extra dimension to the train data and test data, then return the train data, new label of train data, test data, and new label of test data.
+            x_all = all[..., np.newaxis]
+
+            x_train, y_train, x_test, y_test = split_train_test(x_all=x_all, y_all=y_all, train_percentage=self.train_percentage, open_percentage=self.open_percentage)
+
+        elif self.datasets_root_name.lower() == 'uea':
+            
+            all_data_df = pd.concat([train_file_df, test_file_df], axis=0)
+
+            all_data_df = all_data_df.reset_index(drop=True)
+            all_dataset_name = self.dataset_name + "_TRAIN"
+
+            # Store the train data and test data in list, and extend its dimention to 3.
+            all_array = np.array(all_data_df[all_dataset_name].tolist())
+            x_all = all_array.view((np.float64, len(all_array.dtype.names)))
+            
+            # Here, we extract the distinct labels from each dataset.
+            all_labels = torch.tensor(pd.to_numeric(all_data_df['target'], errors='coerce'))
+            y_all = torch.unique(all_labels)
+            
+            transform = {}
+
+            for i, l in enumerate(y_all):
+                transform[l.item()] = i
+            
+            # Retrieve the values of all new labels and store them in 'train_label'.
+            y_all = np.vectorize(transform.get)(all_labels)
+
+            x_train, y_train, x_test, y_test = split_train_test(x_all=x_all, y_all=y_all, train_percentage=self.train_percentage, open_percentage=self.open_percentage)
+
+        return  x_train, y_train, x_test, y_test
+
+    
+
+class TSDataset(Dataset):
+    def __init__(self, dataset_name, dataset_root_path=None, datasets_name=None):
+        super().__init__(dataset_name, dataset_root_path, datasets_name)
+
+    
+class UCRDataset(TSDataset):
+    def __init__(self, dataset_name, dataset_root_path=None, datasets_name='UCR'):
+        super().__init__(dataset_name, dataset_root_path, datasets_name)
+        self.datasets_name = datasets_name
+        self.x_train, self.y_train, self.x_test, self.y_test = self.ts_dataset()
+    
+    def __iter__(self):
+        yield from (self.x_train, self.y_train, self.x_test, self.y_test)
+
+class UEADataset(TSDataset):
+    def __init__(self, dataset_name, dataset_root_path=None, datasets_name='UEA'):
+        super().__init__(dataset_name, dataset_root_path, datasets_name)
+        self.datasets_name = datasets_name
+        self.x_train, self.y_train, self.x_test, self.y_test = self.ts_dataset()
+    
+    def __iter__(self):
+        yield from (self.x_train, self.y_train, self.x_test, self.y_test)
+
+
+
+class OpenDataset(Dataset):
+    def __init__(self, dataset_name, dataset_root_path=None, datasets_name=None, split=True, train_percentage=None,open_percentage=None, random_state=None):
+        super().__init__(dataset_name, dataset_root_path, datasets_name, split, train_percentage, open_percentage, random_state)
+
+    
+class OpenUCRDataset(OpenDataset):
+    def __init__(self, dataset_name, dataset_root_path=None, datasets_name='UCR', split=True, train_percentage=None,open_percentage=None, random_state=None):
+        super().__init__(dataset_name, dataset_root_path, datasets_name, split, train_percentage, open_percentage, random_state)
+        self.datasets_name = datasets_name
+
+        self.x_train, self.y_train, self.x_test, self.y_test = self.opents_dataset()
+    
+    def __iter__(self):
+        yield from (self.x_train, self.y_train, self.x_test, self.y_test)
+
+class OpenUEADataset(OpenDataset):
+    def __init__(self, dataset_name, dataset_root_path=None, datasets_name='UEA', split=True, train_percentage=None,open_percentage=None, random_state=None):
+        super().__init__(dataset_name, dataset_root_path, datasets_name, split, train_percentage, open_percentage, random_state)
+        self.datasets_name = datasets_name
+
+        self.x_train, self.y_train, self.x_test, self.y_test = self.opents_dataset()
+    
+    def __iter__(self):
+        yield from (self.x_train, self.y_train, self.x_test, self.y_test)
+   
+class GenerateDataset(Dataset):
+    def __init__(self, data, targets):
+        self.data = data
+        self.targets = targets
+    
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.targets[index]
+        return x, y
+    
+    def __len__(self):
+        return len(self.data)
---
+++ packages/opents/opents-0.1.3.1-py3-none-any.whl/opents/utils/data_utils.py
@@ -0,0 +1,106 @@
+import os
+import requests
+import zipfile 
+import pandas as pd
+from scipy.io import arff
+import numpy as np
+import random
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
+
+DEFAULT_DATASETS_ROOT = "data"
+
+def download_file(url, datasets_name, datasets_root_path):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(datasets_root_path, 'wb') as file:
+            file.write(response.content)
+        print(datasets_name + "is successfully downloaded")
+    else:
+        print(datasets_name + "is not successfully downloaded")
+
+
+def get_dataset_root_path(dataset_root_path=None, dataset_name=None, datasets_name=None,
+                          datasets_root_path=DEFAULT_DATASETS_ROOT):
+
+    if dataset_name is None:
+        print("Please provide a valid dataset name.")
+        return None
+    
+    if dataset_root_path is None:
+        dataset_root_path = os.path.join(datasets_root_path, dataset_name)
+    dataset_root_path = os.path.abspath(dataset_root_path)
+    
+    ucr_url = 'https://www.timeseriesclassification.com/ClassificationDownloads/Archives/Univariate2018_ts.zip'
+    ucr_password = "someone"
+
+    uea_url = "https://www.timeseriesclassification.com/ClassificationDownloads/Archives/Multivariate2018_arff.zip"
+
+    if not os.path.isdir(dataset_root_path):
+        os.makedirs(dataset_root_path, exist_ok=True)
+        if datasets_name.lower() == "ucr":
+            download_file(ucr_url, "UCR", datasets_root_path)
+
+            with zipfile.ZipFile(datasets_root_path + "UCR_TS_Archive_2018.zip", 'r') as zf:
+                if zf.setpassword(bytes(ucr_password, 'utf-8')):
+                    zf.extractall(dataset_root_path)
+                    print("The ZIP file has been successfully extracted. UCR 2018 zip is successfully unzip.")
+
+        elif datasets_name.lower() == "uea":
+            download_file(uea_url, "UEA", datasets_root_path)
+
+            with zipfile.ZipFile(datasets_root_path + "Multivariate2018_ts.zip", 'r') as zf:
+                zf.extractall(dataset_root_path)
+                print("The ZIP file has been successfully extracted. UEA 2018 zip is successfully unzip.")
+
+        else:
+            print("the dataset is not contain in our contain, please use the Dataset command")
+    else:
+        print(dataset_name + "is available")
+    return dataset_root_path
+
+def data_file_type(dataset_name,dataset_root_path=None, datasets_root_name=None, all_file_path=None):
+
+    if datasets_root_name.lower() == "ucr":
+        # Define the paths for the training and testing sets.
+        train_file_path = os.path.join(dataset_root_path, dataset_name, dataset_name + "_TRAIN.tsv")
+        test_file_path = os.path.join(dataset_root_path, dataset_name, dataset_name + "_TEST.tsv")
+        # Read the data from each file in the training set and testing set using the pandas library, which is a library for data processing.
+        train_file_df = pd.read_csv(train_file_path, sep='\t', header=None)
+        test_file_df = pd.read_csv(test_file_path, sep='\t', header=None)
+
+    elif datasets_root_name.lower() == "uea":
+        # Define the paths for the training and testing sets.
+        train_file_path = os.path.join(dataset_root_path, dataset_name, dataset_name + "_TRAIN.arff")
+        test_file_path = os.path.join(dataset_root_path, dataset_name, dataset_name + "_TEST.arff")
+        train_data_arff = arff.loadarff(train_file_path)
+        train_file_df = pd.DataFrame(train_data_arff[0])
+        test_data_arff = arff.loadarff(test_file_path)
+        test_file_df = pd.DataFrame(test_data_arff[0])
+    return train_file_df, test_file_df
+
+
+def split_train_test(x_all, y_all, train_percentage=0.5, open_percentage=0.5, open_random=42, train_random_state=42):
+
+    # Get unique labels and calculate the number of labels to select for the test set
+    y_unique = np.unique(y_all)
+    y_open_nums = int(len(y_unique) * open_percentage)
+
+    # if the number of y_open is 0, it indicates that the dataset has not open data.
+    if y_open_nums == 0:
+        raise ValueError("The number of open label data is 0, please change the percentage.")
+
+    # fix the seed of random
+    random.seed(open_random)
+
+    # choose the y_open and y_not_open in y_all. the labels in the y_open and y_not_open are unique. 
+    y_open_select = random.sample(list(y_unique), y_open_nums)
+    y_not_open = [_ for _ in y_unique if _ not in y_open_select]
+
+    x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, train_size=train_percentage,random_state=train_random_state, stratify=y_all)
+
+    mask = np.isin(y_train, y_open_select, invert=True)
+    x_train = x_train[mask]
+    y_train = y_train[mask]
+
+    return x_train, y_train, x_test, y_test
