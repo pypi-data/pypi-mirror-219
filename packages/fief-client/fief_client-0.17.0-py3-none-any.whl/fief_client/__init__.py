45e4c9a4711eba36b7807cb3f3347296b6a6965d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:34 +0000

    Add package fief-client/0.17.0/fief_client-0.17.0-py3-none-any.whl

---
+++ packages/fief-client/fief_client-0.17.0-py3-none-any.whl/fief_client/__init__.py
@@ -0,0 +1,35 @@
+"Fief client for Python."
+from fief_client.client import (
+    Fief,
+    FiefAccessTokenExpired,
+    FiefAccessTokenInfo,
+    FiefAccessTokenInvalid,
+    FiefAccessTokenMissingPermission,
+    FiefAccessTokenMissingScope,
+    FiefAsync,
+    FiefError,
+    FiefIdTokenInvalid,
+    FiefRequestError,
+    FiefTokenResponse,
+    FiefUserInfo,
+)
+
+__version__ = "0.17.0"
+
+__all__ = [
+    "Fief",
+    "FiefAsync",
+    "FiefTokenResponse",
+    "FiefAccessTokenInfo",
+    "FiefUserInfo",
+    "FiefError",
+    "FiefAccessTokenExpired",
+    "FiefAccessTokenMissingPermission",
+    "FiefAccessTokenMissingScope",
+    "FiefAccessTokenInvalid",
+    "FiefIdTokenInvalid",
+    "FiefRequestError",
+    "crypto",
+    "pkce",
+    "integrations",
+]
