4b609e1d465fb197b3359f0b61c28e324f21a4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:58 +0000

    Add package pyfrobot/0.1.0/pyfrobot-0.1.0.tar.gz

---
+++ packages/pyfrobot/pyfrobot-0.1.0.tar.gz/pyfrobot-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyfrobot',
+    version='0.1.0',
+    author='alpeza',
+    author_email='alvaroperi.06@gmail.com',
+    description='Descripción de mi librería',
+    long_description='Texto largo con información detallada de la librería',
+    long_description_content_type='text/markdown',
+    url='https://github.com/tu_usuario/mi_libreria',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[],
+)
