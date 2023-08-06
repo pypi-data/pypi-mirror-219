e3d32e714ea415659f94a4c16cfd997d9b080293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:46 +0000

    Add package simple2encrypt/1.5/simple2encrypt-1.5-py3-none-any.whl

---
+++ packages/simple2encrypt/simple2encrypt-1.5-py3-none-any.whl/example_message.py
@@ -0,0 +1,31 @@
+from encryption_utils import custom_input, Encryption, read_binary
+
+
+def main():
+    """
+    The Main function for encrypting and decrypting a message.
+    """
+    # Get user input
+    message = custom_input('Enter your message: ')
+    convert = bytes(message.encode())
+    try:
+        # Read the encryption key from the file
+        key = read_binary('my.key')
+
+        # Encrypt the message
+        encryptor = Encryption(key, convert)
+        encrypted_message = encryptor.encrypt()
+        print(f'The encrypted message: {encrypted_message}')
+
+        # Decrypt the message
+        decryptor = Encryption(key, encrypted_message)
+        decrypted_message = decryptor.decrypt()
+        print(f'The decrypted message: {decrypted_message.decode()}')
+    except FileNotFoundError:
+        print('please run first\npython3 aes-key file_key your_password')
+    except (PermissionError, ValueError) as error:
+        print(f'Error: {error}')
+
+
+if __name__ == '__main__':
+    main()
