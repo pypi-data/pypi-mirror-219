ca07c1e8db28d8eb5b41f5c180b0f7517bc9ec8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:33 +0000

    Add package pyDecision/2.9.7/pyDecision-2.9.7.tar.gz

---
+++ packages/pyDecision/pyDecision-2.9.7.tar.gz/pyDecision-2.9.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name='pyDecision',
+    version='2.9.7',
+    license='GNU',
+    author='Valdecy Pereira',
+    author_email='valdecy.pereira@gmail.com',
+    url='https://github.com/Valdecy/pyDecisions',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scikit-learn',
+        'scipy'
+    ],
+    description='A MCDA Library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
