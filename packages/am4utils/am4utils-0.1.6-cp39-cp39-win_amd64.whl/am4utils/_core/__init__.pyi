a1a315ab577af7434eebdd2089c6e50283d0e323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:41 +0000

    Add package am4utils/0.1.6/am4utils-0.1.6-cp39-cp39-win_amd64.whl

---
+++ packages/am4utils/am4utils-0.1.6-cp39-cp39-win_amd64.whl/am4utils/_core/__init__.pyi
@@ -0,0 +1,16 @@
+from __future__ import annotations
+import am4utils._core
+import typing
+
+__all__ = [
+    "aircraft",
+    "airport",
+    "db",
+    "demand",
+    "game",
+    "route",
+    "ticket"
+]
+
+
+__version__ = '0.1.6'
