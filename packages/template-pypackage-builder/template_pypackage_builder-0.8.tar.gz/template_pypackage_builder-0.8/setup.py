45515d81ddeb581ce7bae2c2a9edb962a481a822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:31 +0000

    Add package template-pypackage-builder/0.8/template_pypackage_builder-0.8.tar.gz

---
+++ packages/template-pypackage-builder/template_pypackage_builder-0.8.tar.gz/template_pypackage_builder-0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+with open("README.md", "r") as f:
+    long_description = f.read()
+setup(
+    name="template_pypackage_builder",
+    version="0.8",
+    description="A simple tool for packaging python",
+    package_dir={"": "src"},
+    include_package_data=True,
+    url="https://github.com/dipson94/packagemaker",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Dipson",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['pyperclip'],
+    python_requires=">=3.10",
+    entry_points={
+        'console_scripts': [
+            'pysetup=template_pypackage_builder:main','gitpip=template_pypackage_builder:pip_code',
+        ],
+    },
+)
