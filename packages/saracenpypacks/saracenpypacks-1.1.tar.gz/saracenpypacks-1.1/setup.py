d52f1d9051dcd051a8197e7284d1506f850a64c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:27 +0000

    Add package saracenpypacks/1.1/saracenpypacks-1.1.tar.gz

---
+++ packages/saracenpypacks/saracenpypacks-1.1.tar.gz/saracenpypacks-1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import codecs
+import json
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+with open("packages.json", "r") as f:
+    packages = json.load(f)
+
+VERSION = '1.1'
+DESCRIPTION = ''
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="saracenpypacks",
+    version=VERSION,
+    author="Saracen Rhue",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=packages,
+    keywords=['python', 'db'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
