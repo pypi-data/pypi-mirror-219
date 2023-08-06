c7c551438e2d644d8b68de97cb6645ad9b3d4345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:55 +0000

    Add package korrel8-data-ingest/1.2.7/korrel8_data_ingest-1.2.7-py3-none-any.whl

---
+++ packages/korrel8-data-ingest/korrel8_data_ingest-1.2.7-py3-none-any.whl/test/test_uploader.py
@@ -0,0 +1,12 @@
+import unittest
+from korrel8 import uploader
+
+class TestReturnThree(unittest.TestCase):
+
+	def test_return_three(self):
+		pass
+
+import pytest,sys
+if __name__ == '__main__':
+  unittest.main() 
+  exit(0)
