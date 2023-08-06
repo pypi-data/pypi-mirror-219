e16fed189e71940b807e3fd69d9d457e9f72e7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:51 +0000

    Add package cernrequests/0.5.0/cernrequests-0.5.0-py3-none-any.whl

---
+++ packages/cernrequests/cernrequests-0.5.0-py3-none-any.whl/cernrequests/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# © Copyright 2023 CERN
+#
+# This software is distributed under the terms of the GNU Lesser General Public
+# Licence version 3 (LGPL Version 3), copied verbatim in the file “LICENSE”
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+__version__ = "0.5.0"
+
+from .core import get, get_with_token
+from .token import get_api_token
+from .cookies import get_sso_cookies
