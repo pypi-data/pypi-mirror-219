41c774ade6c92111e78e6c994669420476374c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:28 +0000

    Add package fennel-ai/0.16.8/fennel_ai-0.16.8.tar.gz

---
+++ packages/fennel-ai/fennel_ai-0.16.8.tar.gz/fennel_ai-0.16.8/fennel/lib/schema/__init__.py
@@ -0,0 +1,14 @@
+from fennel.lib.schema.schema import (
+    get_datatype,
+    between,
+    oneof,
+    dtype_to_string,
+    data_schema_check,
+    FENNEL_INPUTS,
+    FENNEL_OUTPUTS,
+    Embedding,
+    regex,
+    inputs,
+    outputs,
+    get_dtype,
+)
