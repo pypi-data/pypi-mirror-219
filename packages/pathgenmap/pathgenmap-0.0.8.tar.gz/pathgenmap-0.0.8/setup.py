70d98da8f2903231a4084a87d3c92962ab40ffb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:40 +0000

    Add package pathgenmap/0.0.8/pathgenmap-0.0.8.tar.gz

---
+++ packages/pathgenmap/pathgenmap-0.0.8.tar.gz/pathgenmap-0.0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pathgenmap",
+    version="0.0.8",
+    packages=find_packages(),
+    install_requires=["pandas>=1.1.0", "tqdm>=4.48.0"],
+    author="Marcos Paulo Alves de Sousa",
+    author_email="msousa@museu-goeldi.br",
+    description="PathGenMap is a comprehensive Python application designed to integrate pathway, annotation, and species abundance data.",
+    license="MIT",
+    keywords="pathway annotation species abundance",
+    url="http://github.com/marcos-de-sousa/pathgenmap",
+    entry_points={
+        'console_scripts': [
+            'pathgenmap=pathgenmap.PathGenMap:main',
+        ],
+    },
+)
