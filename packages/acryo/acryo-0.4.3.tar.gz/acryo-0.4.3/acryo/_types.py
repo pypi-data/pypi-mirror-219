fdb74f79c4e82ef4dc0578116de402165777e2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:57 +0000

    Add package acryo/0.4.3/acryo-0.4.3.tar.gz

---
+++ packages/acryo/acryo-0.4.3.tar.gz/acryo-0.4.3/acryo/_types.py
@@ -0,0 +1,10 @@
+from typing_extensions import TypeAlias
+
+RangeLike = tuple[float, float]
+Ranges = tuple[RangeLike, RangeLike, RangeLike]
+
+# type alias
+nm: TypeAlias = float
+degree: TypeAlias = float
+pixel: TypeAlias = int
+subpixel: TypeAlias = float
