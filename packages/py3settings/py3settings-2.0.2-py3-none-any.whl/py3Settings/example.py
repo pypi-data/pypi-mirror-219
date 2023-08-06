edc5fcefc60c3bd495910016f7740f995dfa77c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:17 +0000

    Add package py3settings/2.0.2/py3settings-2.0.2-py3-none-any.whl

---
+++ packages/py3settings/py3settings-2.0.2-py3-none-any.whl/py3Settings/example.py
@@ -0,0 +1,13 @@
+from main import AppSettings, Option, Attribute
+from typing import List
+import os
+options: List[Option] = list()
+content = Option("content","warn","Already with content in it!")
+content.append(Attribute("ask", bool))
+exists = Option("exists","warn","{x} already exists!")
+exists.append(Attribute("ask", bool))
+settingsProxy = AppSettings([content,exists])
+settingsProxy.loadFile("sure.json", os.path.abspath(os.path.join(__file__,'..','..','..','tests')))
+print(settingsProxy)
+settingsProxy.saveFile('algo.json', os.path.abspath(os.path.join(__file__,'..','..','..','tests')))
+settingsProxy.validateAll()
