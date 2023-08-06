754c373992e51e3493bdc366577efa7241d71393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:04 +0000

    Add package mqttapi/0.1.2/mqttapi-0.1.2.tar.gz

---
+++ packages/mqttapi/mqttapi-0.1.2.tar.gz/mqttapi-0.1.2/mqttapi/websocket_server.py
@@ -0,0 +1,71 @@
+import asyncio
+import json
+import threading
+import websockets
+import websocket
+
+
+
+class WebSocketServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.connected_clients = set()
+        self.server = None
+
+    async def handle_websocket(self, client, path):
+        # Adicionar o cliente à lista de clientes conectados
+        self.connected_clients.add(client)
+
+        try:
+            # Aguardar mensagens do cliente
+            async for message in client:
+                # Aqui você pode processar a mensagem recebida do cliente WebSocket, se necessário
+                print("Received message:", message)
+
+                # Enviar mensagem para todos os clientes conectados
+                for connected_client in self.connected_clients:
+                    await connected_client.send(message)
+        finally:
+            # Remover o cliente da lista de clientes conectados
+            self.connected_clients.remove(client)
+
+    def run_server(self):
+        # Iniciar o servidor WebSocket
+        self.server = websockets.serve(self.handle_websocket, self.host, self.port)
+
+        # Executar o servidor em um loop de eventos
+        asyncio.get_event_loop().run_until_complete(self.server)
+        asyncio.get_event_loop().run_forever()
+
+    def stop_server(self):
+        if self.server:
+            self.server.close()
+            
+    def send_message_to_websocket(topic, payload):
+        if websocket and websocket.sock and websocket.sock.connected:
+            message = {
+                'topic': topic,
+                'payload': payload
+            }
+            websocket.send(json.dumps(message))
+            
+    def connect_to_websocket(self):
+        global websocket
+        websocket_server = f"ws://{self.host}:{self.port}/"
+        websocket = websocket.WebSocketApp(websocket_server, on_message=self.on_message)
+        websocket.run_forever()
+        
+    def on_message(ws, message):
+        data = json.loads(message)
+        topic = data['topic']
+        payload = data['payload']
+
+    def start_websocket_thread(self):
+        websocket_thread = threading.Thread(target=self.connect_to_websocket)
+        websocket_thread.daemon = True
+        websocket_thread.start()
+
+
+
+            
