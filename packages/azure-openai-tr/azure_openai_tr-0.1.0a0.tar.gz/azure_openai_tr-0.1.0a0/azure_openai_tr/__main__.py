04cc6f32e14f9cd142e25193efbc9d10117d4355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:01 +0000

    Add package azure-openai-tr/0.1.0a0/azure_openai_tr-0.1.0a0.tar.gz

---
+++ packages/azure-openai-tr/azure_openai_tr-0.1.0a0.tar.gz/azure_openai_tr-0.1.0a0/azure_openai_tr/__main__.py
@@ -0,0 +1,45 @@
+"""Prep __main__.py."""
+# pylint: disable=invalid-name
+from pathlib import Path
+from typing import Optional
+
+import logzero
+import typer
+from logzero import logger
+from set_loglevel import set_loglevel
+
+from azure_openai_tr import __version__, azure_openai_tr
+
+del Path, logger, azure_openai_tr
+logzero.loglevel(set_loglevel())
+
+app = typer.Typer(
+    name="azure_openai_tr",
+    add_completion=False,
+    help="azure_openai_tr help",
+)
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{app.info.name} v.{__version__} -- ...")
+        raise typer.Exit()
+
+
+@app.command()
+def main(
+    version: Optional[bool] = typer.Option(  # pylint: disable=(unused-argument
+        None,
+        "--version",
+        "-v",
+        "-V",
+        help="Show version info and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    ),
+):
+    """Define."""
+
+
+if __name__ == "__main__":
+    app()
