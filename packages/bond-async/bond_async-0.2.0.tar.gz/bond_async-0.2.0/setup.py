1e6a28fcadc37d04f50f6f0c224974527164c5cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:50 +0000

    Add package bond-async/0.2.0/bond_async-0.2.0.tar.gz

---
+++ packages/bond-async/bond_async-0.2.0.tar.gz/bond_async-0.2.0/setup.py
@@ -0,0 +1,38 @@
+"""Setup script for bond-async."""
+
+import pathlib
+
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="bond_async",
+    version="0.2.0",
+    packages=find_packages(exclude=['tests']),
+
+    author="Olibra LLC",
+    author_email="mobiledev@olibra.io",
+    description="Asynchronous Python wrapper library over Bond Local API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    keywords="bond local api async",
+
+    install_requires=[
+        "aiohttp>=3.6.1"
+    ],
+
+    url="https://github.com/bondhome/bond-async",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Home Automation"
+    ]
+)
