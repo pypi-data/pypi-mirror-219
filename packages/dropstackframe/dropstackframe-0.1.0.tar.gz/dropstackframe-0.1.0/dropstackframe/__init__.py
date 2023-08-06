0a43bd921c1f782a641fce0b5a595786585b9255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:09 +0000

    Add package dropstackframe/0.1.0/dropstackframe-0.1.0.tar.gz

---
+++ packages/dropstackframe/dropstackframe-0.1.0.tar.gz/dropstackframe-0.1.0/dropstackframe/__init__.py
@@ -0,0 +1,13 @@
+from dropstackframe.dropstackframe import (
+    DROP_STACK_FRAME_SUPPORTED,
+    drop_stack_frame,
+    get_enable_drop_stack_frame,
+    set_enable_drop_stack_frame,
+)
+
+__all__ = [
+    "DROP_STACK_FRAME_SUPPORTED",
+    "drop_stack_frame",
+    "set_enable_drop_stack_frame",
+    "get_enable_drop_stack_frame",
+]
