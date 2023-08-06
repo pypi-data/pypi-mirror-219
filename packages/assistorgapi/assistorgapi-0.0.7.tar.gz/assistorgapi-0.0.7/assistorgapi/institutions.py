015a4e8a8a8bc5f0470ce7177510c0e2c59cfad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:47 +0000

    Add package assistorgapi/0.0.7/assistorgapi-0.0.7.tar.gz

---
+++ packages/assistorgapi/assistorgapi-0.0.7.tar.gz/assistorgapi-0.0.7/assistorgapi/institutions.py
@@ -0,0 +1,21 @@
+import requests
+
+def get_institutions():
+    url = "https://assist.org/api/institutions"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+def get_institutions_academic_years(sendingInstitutionId):
+    url = "https://assist.org/institutions"+sendingInstitutionId+"/transferability/availableAcademicYears"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+
