dffbc7cd195e44719a8c39e6efa5fd0a2f76f5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:16 +0000

    Add package sp-repo-review/2023.7.13/sp_repo_review-2023.7.13.tar.gz

---
+++ packages/sp-repo-review/sp_repo_review-2023.7.13.tar.gz/sp_repo_review-2023.7.13/{{cookiecutter.project_name}}/src/{{cookiecutter.__project_slug}}/_compat/__init__.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) {{ cookiecutter.__year }} {{ cookiecutter.full_name }}. All rights reserved.
+
+{{ cookiecutter.project_name }}: {{ cookiecutter.project_short_description }}
+"""
+
+
+from __future__ import annotations
