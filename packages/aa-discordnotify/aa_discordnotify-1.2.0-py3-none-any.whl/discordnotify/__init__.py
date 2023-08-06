18e4796144ba3bb379d74102622c2f07c8e232f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:28 +0000

    Add package aa-discordnotify/1.2.0/aa_discordnotify-1.2.0-py3-none-any.whl

---
+++ packages/aa-discordnotify/aa_discordnotify-1.2.0-py3-none-any.whl/discordnotify/__init__.py
@@ -0,0 +1,6 @@
+"""Forward Alliance Auth notifications to users on Discord."""
+
+default_app_config = "discordnotify.apps.DiscordnotifyConfig"
+
+__title__ = "Discord Notify"
+__version__ = "1.2.0"
