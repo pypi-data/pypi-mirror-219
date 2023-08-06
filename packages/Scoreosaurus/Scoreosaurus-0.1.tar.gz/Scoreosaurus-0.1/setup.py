9ae7257de47e88205a1a2e7dcb81f483a92f4ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:58 +0000

    Add package Scoreosaurus/0.1/Scoreosaurus-0.1.tar.gz

---
+++ packages/Scoreosaurus/Scoreosaurus-0.1.tar.gz/Scoreosaurus-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='Scoreosaurus',  # Nazwa twojej biblioteki
+    version='0.1',  # Wersja twojej biblioteki
+    scripts=['api_interaction.py']  # Skrypt pythona z twóją biblioteką
+)
