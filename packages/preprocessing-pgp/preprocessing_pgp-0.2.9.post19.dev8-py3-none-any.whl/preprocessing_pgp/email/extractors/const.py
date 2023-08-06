e7c949ba66fd8f6727d8cdf5215df7cb5ceb78ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:28 +0000

    Add package preprocessing-pgp/0.2.9.post19.dev8/preprocessing_pgp-0.2.9.post19.dev8-py3-none-any.whl

---
+++ packages/preprocessing-pgp/preprocessing_pgp-0.2.9.post19.dev8-py3-none-any.whl/preprocessing_pgp/email/extractors/const.py
@@ -0,0 +1,22 @@
+"""
+Module contains constants for email information extraction
+"""
+import pandas as pd
+from preprocessing_pgp.const import HDFS_BASE_PTH, hdfs
+
+# ? STUDENT FULLNAME
+fullname_path = f"{HDFS_BASE_PTH}/email_info/norm_name_trace_dict.parquet"
+
+FULLNAME_DICT = pd.read_parquet(fullname_path, filesystem=hdfs)
+
+# ? YOB
+dob_path = f"{HDFS_BASE_PTH}/email_info/dob_dict.parquet"
+DOB_DICT = pd.read_parquet(dob_path, filesystem=hdfs)
+
+DOB_REGEX_DICT = DOB_DICT.set_index("dob_type")["regex"].to_dict()
+DOB_NUM_DIGIT_DICT = DOB_DICT.set_index("dob_type")["num_digit"].to_dict()
+DOB_FORMAT_DICT = DOB_DICT.set_index("dob_type")["format"].to_dict()
+
+# ? PHONE
+
+VIETNAMESE_PHONE_REGEX = r"((02[0-9]|0|84)[3|5|7|8|9]*[0-9]{8})"
---
+++ packages/preprocessing-pgp/preprocessing_pgp-0.2.9.post19.dev8-py3-none-any.whl/preprocessing_pgp/name/type/const.py
@@ -0,0 +1,32 @@
+"""
+Constants for processing name type extraction
+"""
+
+import pandas as pd
+from preprocessing_pgp.const import HDFS_BASE_PTH, hdfs
+
+# ? IMPORTANT PATHS
+
+_NAME_TYPE_LV1_PATH = f"{HDFS_BASE_PTH}/name_type/customer_type_extra.parquet"
+
+_NAME_TYPE_LV2_PATH = f"{HDFS_BASE_PTH}/name_type/customer_type_lv2.parquet"
+
+# ? DATA BY LEVELS
+LV1_NAME_TYPE = pd.read_parquet(_NAME_TYPE_LV1_PATH, filesystem=hdfs)
+LV2_NAME_TYPE = pd.read_parquet(_NAME_TYPE_LV2_PATH, filesystem=hdfs)
+NAME_TYPE_DATA = {"lv1": LV1_NAME_TYPE, "lv2": LV2_NAME_TYPE}
+
+# ? NAME TYPE REGEX
+LV1_NAME_TYPE_WITHOUT_ACCENT_REGEX = (
+    LV1_NAME_TYPE.query("~is_accented").groupby("ctype")["term"].apply(list).to_dict()
+)
+LV1_NAME_TYPE_ACCENTED_REGEX = (
+    LV1_NAME_TYPE.query("is_accented").groupby("ctype")["term"].apply(list).to_dict()
+)
+LV2_NAME_TYPE_REGEX = LV2_NAME_TYPE.groupby("ctype")["term"].apply(list).to_dict()
+NAME_TYPE_REGEX_DATA = {
+    "lv1_accented": LV1_NAME_TYPE_ACCENTED_REGEX,
+    "lv1_without_accent": LV1_NAME_TYPE_WITHOUT_ACCENT_REGEX,
+    "lv2_accented": LV2_NAME_TYPE_REGEX,
+    "lv2_without_accent": LV2_NAME_TYPE_REGEX,
+}
