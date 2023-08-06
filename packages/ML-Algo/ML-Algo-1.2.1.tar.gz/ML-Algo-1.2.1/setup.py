71021b44b607c87b3b2a5561a9b7907222b0b333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:46 +0000

    Add package ML-Algo/1.2.1/ML-Algo-1.2.1.tar.gz

---
+++ packages/ML-Algo/ML-Algo-1.2.1.tar.gz/ML-Algo-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ML-Algo',
+    version='1.2.1',
+    author='Mr Raj',
+    author_email='arunraj14092002@gmail.com',
+    description='A package for calculating Series Time Data',
+    packages=['ML-Algo'],
+    install_requires=['pandas'],
+)
