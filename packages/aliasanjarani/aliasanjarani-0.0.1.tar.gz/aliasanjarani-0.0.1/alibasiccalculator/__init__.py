4d92adf75834c0f70acc083793e53c5d1e45c8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:29 +0000

    Add package aliasanjarani/0.0.1/aliasanjarani-0.0.1.tar.gz

---
+++ packages/aliasanjarani/aliasanjarani-0.0.1.tar.gz/aliasanjarani-0.0.1/alibasiccalculator/__init__.py
@@ -0,0 +1,13 @@
+class calculator:
+    
+    def add(num1,num2):
+        return num1 + num2
+    
+    def subtract(num1,num2):
+        return num1 - num2
+    
+    def multiply(num1,num2):
+        return num1 * num2
+    
+    def divide(num1,num2):
+        return num1 / num2
