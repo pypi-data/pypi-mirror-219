36c932172e1467f212f52c422b0a253cb3408f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:20 +0000

    Add package testlibraryorsomething/1.0.3/testlibraryorsomething-1.0.3.tar.gz

---
+++ packages/testlibraryorsomething/testlibraryorsomething-1.0.3.tar.gz/testlibraryorsomething-1.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='testlibraryorsomething',
+    version='1.0.3',
+    description='Test Library or Something',
+    author='Bamboooz',
+    author_email='bambusixmc@gmail.com',
+    packages=['testlibraryorsomething']
+)
