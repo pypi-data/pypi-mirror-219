f513f628a78249cb247bcfea94106928a3ae7484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:37 +0000

    Add package wiggler-studiorabota/0.0.17/wiggler_studiorabota-0.0.17-py3-none-any.whl

---
+++ packages/wiggler-studiorabota/wiggler_studiorabota-0.0.17-py3-none-any.whl/wiggler/cli.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+import shutil
+from pathlib import Path
+
+def main():
+  
+    parser = argparse.ArgumentParser(prog ='wiggler', description ='WiggleR API')
+
+    parser.add_argument('-s', '--server', action='store_true')
+    parser.add_argument('-b', '--boot', action='store_true')
+    
+    args = parser.parse_args()
+  
+    if args.server:
+        os.system(f"uvicorn wiggler.main:app --reload --host 0.0.0.0")
+
+    if args.boot:
+        scriptFile = Path(__file__).parent / f"start_wiggler.sh"
+        serviceFile = Path(__file__).parent / f"start_wiggler.service"
+        os.system(f'sudo cp {scriptFile} /usr/bin/start_wiggler.sh')
+        os.system(f'sudo cp {serviceFile} /etc/systemd/system/start_wiggler.service')
+        os.system('sudo systemctl enable start_wiggler')
