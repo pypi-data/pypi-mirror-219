e31116d2cd2b56c9d0fd7794c814115210c10bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:38 +0000

    Add package davidflmatematica/0.1/davidflmatematica-0.1-py3-none-any.whl

---
+++ packages/davidflmatematica/davidflmatematica-0.1-py3-none-any.whl/matematica/aritmetica.py
@@ -0,0 +1,16 @@
+# Módulo aritmética
+
+def sumar(a: float, b: float) -> float:
+    return a + b
+
+
+def restar(a: float, b: float) -> float:
+    return a - b
+
+
+def multi(a: float, b: float) -> float:
+    return a * b
+
+
+def div(a: float, b: float) -> float:
+    return a / b
