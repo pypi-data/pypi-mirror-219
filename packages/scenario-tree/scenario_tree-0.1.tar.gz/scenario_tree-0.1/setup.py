576858718974413a5cc9d8e0478e377cabe4bb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:47 +0000

    Add package scenario-tree/0.1/scenario_tree-0.1.tar.gz

---
+++ packages/scenario-tree/scenario_tree-0.1.tar.gz/scenario_tree-0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 15 11:21:27 2023
+
+@author: zhenchen
+
+@disp:  
+    
+    
+"""
+
+from setuptools import setup
+
+setup(
+    name = 'scenario_tree',
+    version = '0.1',
+    author='Zhen Chen',
+    author_email='15011074486@163.com',
+    description = 'Get the sceanrio tree stucture',
+    py_modules=['catslog'],
+    install_requires=[]
+)
