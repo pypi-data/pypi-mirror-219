b86ca93fa2b9f7321869735cd1434d140fc09329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:42 +0000

    Add package localstack-core/2.1.1.dev20230714062049/localstack-core-2.1.1.dev20230714062049.tar.gz

---
+++ packages/localstack-core/localstack-core-2.1.1.dev20230714062049.tar.gz/localstack-core-2.1.1.dev20230714062049/localstack/services/cloudformation/models/kinesisfirehose.py
@@ -0,0 +1,48 @@
+from localstack.services.cloudformation.deployment_utils import select_parameters
+from localstack.services.cloudformation.service_models import GenericBaseModel
+from localstack.utils.aws import aws_stack
+
+
+class FirehoseDeliveryStream(GenericBaseModel):
+    @staticmethod
+    def cloudformation_type():
+        return "AWS::KinesisFirehose::DeliveryStream"
+
+    def fetch_state(self, stack_name, resources):
+        stream_name = self.props.get("DeliveryStreamName") or self.logical_resource_id
+        return aws_stack.connect_to_service("firehose").describe_delivery_stream(
+            DeliveryStreamName=stream_name
+        )
+
+    @staticmethod
+    def get_deploy_templates():
+        def _handle_result(result, resource_id, resources, resource_type):
+            resources[resource_id]["Properties"]["Arn"] = result["DeliveryStreamARN"]
+            resources[resource_id]["PhysicalResourceId"] = resources[resource_id]["Properties"][
+                "DeliveryStreamName"
+            ]
+
+        return {
+            "create": {
+                "function": "create_delivery_stream",
+                "parameters": select_parameters(
+                    "DeliveryStreamName",
+                    "DeliveryStreamType",
+                    "S3DestinationConfiguration",
+                    "ElasticsearchDestinationConfiguration",
+                    "AmazonopensearchserviceDestinationConfiguration",
+                    "DeliveryStreamEncryptionConfigurationInput",
+                    "ExtendedS3DestinationConfiguration",
+                    "HttpEndpointDestinationConfiguration",
+                    "KinesisStreamSourceConfiguration",
+                    "RedshiftDestinationConfiguration",
+                    "SplunkDestinationConfiguration",
+                    "Tags",
+                ),
+                "result_handler": _handle_result,
+            },
+            "delete": {
+                "function": "delete_delivery_stream",
+                "parameters": {"DeliveryStreamName": "DeliveryStreamName"},
+            },
+        }
