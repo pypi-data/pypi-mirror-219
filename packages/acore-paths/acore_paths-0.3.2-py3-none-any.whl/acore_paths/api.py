bc59d23b9d8734f677bb453236e1027b56d4fdb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:42 +0000

    Add package acore-paths/0.3.2/acore_paths-0.3.2-py3-none-any.whl

---
+++ packages/acore-paths/acore_paths-0.3.2-py3-none-any.whl/acore_paths/api.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from .acore_paths import dir_home
+from .acore_paths import dir_azeroth_server
+from .acore_paths import dir_server_bin
+from .acore_paths import path_azeroth_server_authserver_conf_dist
+from .acore_paths import path_azeroth_server_worldserver_conf_dist
+from .acore_paths import path_azeroth_server_authserver_conf
+from .acore_paths import path_azeroth_server_worldserver_conf
+from .acore_paths import dir_azeroth_server_data
+from .acore_paths import dir_azeroth_server_data_dot_zip
+from .acore_paths import dir_azeroth_server_logs
+from .acore_paths import dir_modules
+from .acore_paths import path_mod_eluna_conf_dist
+from .acore_paths import path_mod_eluna_conf
+from .acore_paths import dir_server_lua_scripts
+from .acore_paths import dir_git_repos
+from .acore_paths import dir_acore_soap_app_project
+from .acore_paths import path_acore_soap_app_cli
+from .acore_paths import dir_acore_db_app_project
+from .acore_paths import path_acore_db_app_cli
+from .acore_paths import dir_acore_server_bootstrap_project
+from .acore_paths import path_acore_server_bootstrap_cli
