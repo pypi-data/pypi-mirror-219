094c2262876882ec04e2337ea1d7e08ab427065c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:42 +0000

    Add package pympipool/0.5.4/pympipool-0.5.4-py3-none-any.whl

---
+++ packages/pympipool/pympipool-0.5.4-py3-none-any.whl/pympipool/__init__.py
@@ -0,0 +1,15 @@
+from pympipool.external_interfaces.communication import (
+    SocketInterface,
+    connect_to_socket_interface,
+    send_result,
+    close_connection,
+    receive_instruction,
+)
+from pympipool.external_interfaces.executor import Executor, PoolExecutor
+from pympipool.external_interfaces.pool import Pool, MPISpawnPool
+from pympipool.shared_functions.external_interfaces import cancel_items_in_queue
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
