573afabf3547acc2aa4b0a662902dd752220d8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:41 +0000

    Add package PSIcheck/0.0.7/PSIcheck-0.0.7.tar.gz

---
+++ packages/PSIcheck/PSIcheck-0.0.7.tar.gz/PSIcheck-0.0.7/PSIcheck/get_parser.py
@@ -0,0 +1,24 @@
+def get_parser():
+    import argparse
+    CBOLD = '\33[1m'
+    CEND = '\33[0m'
+    CRED = '\33[31m'
+    CGREEN = '\33[32m'
+    CYELLOW = '\33[33m'
+    parser = argparse.ArgumentParser(
+             prog="PSIcheck", 
+             epilog=f"""
+{CBOLD}Usage{CEND}
+
+    {CRED}$ {CGREEN}PSIcheck{CEND} PsiResultPath
+
+Runjia Ji, 2023
+"""
+            )
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.7', help='show PSIcheck version number and exit')
+    parser.add_argument('PsiResultPath', type=str, help='directory stores PSI-Blast results')
+    parser.add_argument('-gbk','--GenbankFilesPath', type=str, help='directory stores Genbank files')
+
+    args = parser.parse_args()
+    return args
+
