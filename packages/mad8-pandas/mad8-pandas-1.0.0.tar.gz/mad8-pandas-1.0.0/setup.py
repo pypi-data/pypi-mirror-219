fad3c357251350c87e9745a63e234b3d7e8bb1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:16 +0000

    Add package mad8-pandas/1.0.0/mad8-pandas-1.0.0.tar.gz

---
+++ packages/mad8-pandas/mad8-pandas-1.0.0.tar.gz/mad8-pandas-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="mad8-pandas",
+    version="1.0.0",
+    description="load mad8 output with pandas",
+    packages=find_packages(include=["pand8"]),
+    install_requires=["pandas", "fortranformat", "numpy"],
+    url="https://github.com/st-walker/mad8-pandas",
+    license="MIT",
+    keywords="mad8 pandas twiss",
+    author="Stuart Walker",
+    author_email="stuart.derek.walker@gmail.com",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Typing :: Typed",
+    ]
+)
