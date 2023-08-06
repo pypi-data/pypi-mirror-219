43dfdf6f367b421fe461a3cc8bf8bb97b594667e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:28 +0000

    Add package trailblazer/15.0.7/trailblazer-15.0.7-py3-none-any.whl

---
+++ packages/trailblazer/trailblazer-15.0.7-py3-none-any.whl/trailblazer/store/crud/update.py
@@ -0,0 +1,17 @@
+from trailblazer.store.base import BaseHandler_2
+from trailblazer.store.models import User
+
+
+class UpdateHandler(BaseHandler_2):
+    """Class for updating items in the database."""
+
+    def add_user(self, name: str, email: str) -> User:
+        """Add a new user to the database."""
+        new_user: User = User(email=email, name=name)
+        self.add_commit(new_user)
+        return new_user
+
+    def update_user_is_archived(self, user: User, archive: bool = True) -> None:
+        """Update is archived fpr a user in the database."""
+        user.is_archived = archive
+        self.commit()
