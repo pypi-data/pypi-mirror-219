ec83df4489ed5cd423bbd27c4ad17e6844fd3a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:06 +0000

    Add package mutual/0.0.3/mutual-0.0.3.tar.gz

---
+++ packages/mutual/mutual-0.0.3.tar.gz/mutual-0.0.3/README.md
@@ -0,0 +1,56 @@
+# mutual
+
+A python package to interact with the Mutuai API.
+
+## Installation
+
+Run `pip install mutual` in the project root directory.
+
+## Usage
+
+```python
+import mutual
+
+# SIGNUP
+response = mutual.Auth.signup("username", "email")
+print(response) # after signing up the key is automatically set
+
+# to print the api_key from the response
+api_key = response.get("api_key", None)
+print(api_key)
+
+# to get the api_key
+print(mutual.api_key)
+# to set the api_key
+mutual.api_key = "your_api_key"
+
+# CHAT
+mutual.api_key = "your_api_key"
+for message in mutual.Chat.create("Hello", "bot_id", "username"):
+    print(message)
+
+# CHAT DEMO
+for message in mutual.Chat.create_demo("Hello"):
+    print(message)
+
+# BOT
+print(mutual.Bot.getBots())
+print(mutual.Bot.getBot("1"))
+print(mutual.Bot.createBot("2", "alex", "mutual")) # bot_id, bot_name, bot_org
+print(mutual.Bot.updateBot("2", bot_name="alex2"))
+print(mutual.Bot.updateBot("2", bot_name="alex2", bot_org="mutual"))
+print(mutual.Bot.updateBot("2", bot_org="mutual"))
+
+# you can also set the bot_id like this so you dont need to pass it in chat
+mutual.api_key = "your_api_key"
+mutual.bot_id = "bot_id"
+
+for message in mutual.Chat.create("Hello", username="username"):
+    print(message)
+
+# to print the bot_id
+print(mutual.bot_id)
+
+# you can import the functions directly like so
+from mutual import Bot, Auth, Chat
+```
---
+++ packages/mutual/mutual-0.0.3.tar.gz/mutual-0.0.3/mutual/Bot.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import mutual
+
+def getBot(limit=20, offset=0):
+    url = f"https://api-agent.mutuai.io/api/bots?limit={limit}&offset={offset}"
+    headers = {
+        "Authorization": f"Bearer {mutual.api_key}"
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # Raise an exception if the response contains an HTTP error status code
+    return response.json()
+
+def getBot(bot_id):
+    url = f"https://api-agent.mutuai.io/api/bots/{bot_id}"
+    headers = {
+        "Authorization": f"Bearer {mutual.api_key}"
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()  # Raise an exception if the response contains an HTTP error status code
+    return response.json()
+
+def createBot(bot_id, bot_name, bot_org):
+    url = "https://api-agent.mutuai.io/api/bots"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {mutual.api_key}"
+    }
+    data = {
+        "bot_id": bot_id,
+        "bot_name": bot_name,
+        "bot_org": bot_org
+    }
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    response.raise_for_status()  # Raise an exception if the response contains an HTTP error status code
+    return response.json()
+
+def updateBot(bot_id, bot_name=None, bot_org=None):
+    url = f"https://api-agent.mutuai.io/api/bots/{bot_id}"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {mutual.api_key}"
+    }
+    data = {
+        "bot_name": bot_name,
+        "bot_org": bot_org
+    }
+    # remove keys with None value
+    data = {k: v for k, v in data.items() if v is not None}
+    response = requests.patch(url, data=json.dumps(data), headers=headers)
+    response.raise_for_status()  # Raise an exception if the response contains an HTTP error status code
+    return response.json()
