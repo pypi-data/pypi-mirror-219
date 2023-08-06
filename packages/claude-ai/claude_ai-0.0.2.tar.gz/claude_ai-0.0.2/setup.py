734b76cd3d235a64d821dd999863a2a04f242ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:53 +0000

    Add package claude-ai/0.0.2/claude_ai-0.0.2.tar.gz

---
+++ packages/claude-ai/claude_ai-0.0.2.tar.gz/claude_ai-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='claude_ai',
+    version='0.0.2',
+    url='https://github.com/boyueluzhipeng/claude_ai',
+    py_modules=find_packages(),
+
+    install_requires=[
+        'requests',
+    ],
+)
