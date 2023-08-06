50867156294e42b4124ef6d110a1f36ce6b3aebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:31 +0000

    Add package hyve/0.0.1/hyve-0.0.1.tar.gz

---
+++ packages/hyve/hyve-0.0.1.tar.gz/hyve-0.0.1/src/hyve/transforms.py
@@ -0,0 +1,458 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Visualisation transforms
+~~~~~~~~~~~~~~~~~~~~~~~~
+Functions for transforming the input and output of visualisation functions.
+See also ``flows.py`` for functions that transform the control flow of
+visualisation functions.
+"""
+from itertools import chain
+from math import ceil
+from pkg_resources import resource_filename as pkgrf
+from typing import Literal, Mapping, Optional, Sequence, Tuple, Union
+import numpy as np
+import nibabel as nb
+import pandas as pd
+from lytemaps.transforms import mni152_to_fsaverage, mni152_to_fslr
+import matplotlib.pyplot as plt
+import pyvista as pv
+
+from conveyant import (
+    direct_compositor,
+    SanitisedPartialApplication as Partial,
+)
+from .const import Tensor
+from .prim import (
+    surf_from_archive_p,
+    scalars_from_cifti_p,
+    scalars_from_gifti_p,
+    resample_to_surface_p,
+    parcellate_colormap_p,
+    plot_to_image_p,
+    plot_to_html_p,
+)
+from .surf import CortexTriSurface
+from .util import (
+    auto_focus,
+    filter_adjacency_data,
+    filter_node_data,
+)
+
+
+def surf_from_archive(
+    allowed: Sequence[str] = ('templateflow', 'neuromaps')
+) -> callable:
+    """
+    Load a surface from a cloud-based data archive.
+
+    Parameters
+    ----------
+    allowed : sequence of str (default: ("templateflow", "neuromaps")
+        The archives to search for the surface.
+
+    Returns
+    -------
+    callable
+        A transform function. Transform functions accept a plotting
+        function and return a new plotting function with different input and
+        output arguments.
+        * The transformed plotter will no longer require a ``surf`` argument,
+          but will require a ``template`` argument.
+        * The ``template`` argument should be a string that identifies the
+          template space to load the surface from. The ``template`` argument
+          will be passed to the archive loader function.
+        * An optional ``load_mask`` argument can be passed to the transformed
+          plotter to indicate whether the surface mask should be loaded
+        (defaults to ``True``).
+        * An optional ``projections`` argument can be passed to the
+          transformed plotter to indicate which projections to load
+          (defaults to ``("veryinflated",)``).
+    """
+    archives = {
+        'templateflow': CortexTriSurface.from_tflow,
+        'neuromaps': CortexTriSurface.from_nmaps,
+    }
+    archives = {k: v for k, v in archives.items() if k in allowed}
+    def transform(
+        f: callable,
+        compositor: callable = direct_compositor,
+    ) -> callable:
+        transformer_f = Partial(surf_from_archive_p, archives=archives)
+
+        def f_transformed(
+            *,
+            template: str = 'fsLR',
+            load_mask: bool = True,
+            surf_projection: Optional[Sequence[str]] = ('veryinflated',),
+            **params: Mapping,
+        ):
+            return compositor(f, transformer_f)(**params)(
+                template=template,
+                load_mask=load_mask,
+                projections=surf_projection,
+            )
+
+        return f_transformed
+    return transform
+
+
+def scalars_from_cifti(
+    scalars: str,
+    is_masked: bool = True,
+    apply_mask: bool = False,
+    null_value: Optional[float] = 0.0,
+    plot: bool = False,
+) -> callable:
+    """
+    Load a scalar dataset from a CIFTI file onto a CortexTriSurface.
+
+    Parameters
+    ----------
+    scalars : str
+        The name that the scalar dataset loaded from the CIFTI file is given
+        on the surface.
+    is_masked : bool (default: True)
+        Indicates whether the CIFTI file contains a dataset that is already
+        masked.
+    apply_mask : bool (default: False)
+        Indicates whether the surface mask should be applied to the CIFTI
+        dataset.
+    null_value : float or None (default: 0.)
+        The value to use for masked-out vertices.
+    plot : bool (default: False)
+        Indicates whether the scalar dataset should be plotted.
+
+    Returns
+    -------
+    callable
+        A transform function. Transform functions accept a plotting
+        function and return a new plotting function with different input and
+        output arguments.
+        * The transformed plotter will now require a ``<scalars>_cifti``
+          argument, where ``<scalars>`` is the name of the scalar dataset
+          provided as an argument to this function. The value of this
+          argument should be either a ``Cifti2Image`` object or a path to a
+          CIFTI file. This is the CIFTI image whose data will be loaded onto
+          the surface.
+        * If ``plot`` is ``True``, the transformed function will automatically
+          add the scalar dataset obtained from the CIfTI image to the sequence
+          of scalars to plot.
+    """
+    def transform(
+        f: callable,
+        compositor: callable = direct_compositor,
+    ) -> callable:
+        transformer_f = Partial(
+            scalars_from_cifti_p,
+            scalars=scalars,
+            is_masked=is_masked,
+            apply_mask=apply_mask,
+            null_value=null_value,
+            plot=plot,
+        )
+
+        def f_transformed(
+            *,
+            surf: CortexTriSurface,
+            **params: Mapping,
+        ):
+            try:
+                cifti = params.pop(f'{scalars}_cifti')
+            except KeyError:
+                raise TypeError(
+                    'Transformed plot function missing one required '
+                    f'keyword-only argument: {scalars}_cifti'
+                )
+            scalars_to_plot = params.get('scalars', []) if plot else None
+            return compositor(f, transformer_f)(**params)(
+                cifti=cifti,
+                surf=surf,
+                scalars_to_plot=scalars_to_plot,
+            )
+
+        return f_transformed
+    return transform
+
+
+def scalars_from_gifti(
+    scalars: str,
+    is_masked: bool = True,
+    apply_mask: bool = False,
+    null_value: Optional[float] = 0.0,
+    select: Optional[Sequence[int]] = None,
+    exclude: Optional[Sequence[int]] = None,
+    plot: bool = False,
+) -> callable:
+    """
+    Load a scalar dataset from a GIfTI file onto a CortexTriSurface.
+
+    Parameters
+    ----------
+    scalars : str
+        The name that the scalar dataset loaded from the GIfTI file is given
+        on the surface.
+    is_masked : bool (default: True)
+        Indicates whether the GIfTI file contains a dataset that is already
+        masked.
+    apply_mask : bool (default: False)
+        Indicates whether the surface mask should be applied to the GIfTI
+        dataset.
+    null_value : float or None (default: 0.)
+        The value to use for masked-out vertices.
+    plot : bool (default: False)
+        Indicates whether the scalar dataset should be plotted.
+
+    Returns
+    -------
+    callable
+        A transform function. Transform functions accept a plotting
+        function and return a new plotting function with different input and
+        output arguments.
+        * The transformed plotter will now require a ``<scalars>_gifti``
+          argument, where ``<scalars>`` is the name of the scalar dataset
+          provided as an argument to this function. The value of this
+          argument should be either a ``GIfTIImage`` object or a path to a
+          GIfTI file. This is the GIfTI image whose data will be loaded onto
+          the surface.
+        * If ``plot`` is ``True``, the transformed function will automatically
+          add the scalar dataset obtained from the GIfTI image to the sequence
+          of scalars to plot.
+    """
+    def transform(
+        f: callable,
+        compositor: callable = direct_compositor,
+    ) -> callable:
+        transformer_f = Partial(
+            scalars_from_gifti_p,
+            scalars=scalars,
+            is_masked=is_masked,
+            apply_mask=apply_mask,
+            null_value=null_value,
+            select=select,
+            exclude=exclude,
+            plot=plot,
+        )
+
+        def f_transformed(
+            *,
+            surf: CortexTriSurface,
+            **params: Mapping,
+        ):
+            left_gifti = params.pop(f'{scalars}_gifti_left', None)
+            right_gifti = params.pop(f'{scalars}_gifti_right', None)
+            if left_gifti is None and right_gifti is None:
+                raise TypeError(
+                    'Transformed plot function missing a required '
+                    f'keyword-only argument: either {scalars}_gifti_left or '
+                    f'{scalars}_gifti_right'
+                )
+            scalars_to_plot = params.get('scalars', []) if plot else None
+            return compositor(f, transformer_f)(**params)(
+                left_gifti=left_gifti,
+                right_gifti=right_gifti,
+                surf=surf,
+                scalars_to_plot=scalars_to_plot,
+            )
+
+        return f_transformed
+    return transform
+
+
+def resample_to_surface(
+    scalars: str,
+    template: str = 'fsLR',
+    null_value: Optional[float] = 0.0,
+    select: Optional[Sequence[int]] = None,
+    exclude: Optional[Sequence[int]] = None,
+    plot: bool = False,
+) -> callable:
+    """
+    Resample a scalar dataset from volumetric MNI152 space to a surface.
+
+    Parameters
+    ----------
+    scalars : str
+        The name that the scalar dataset resampled from volumetric space is
+        given on the surface.
+    template : str (default: "fsLR")
+        The name of the template to which the scalar dataset should be
+        resampled. Currently, only "fsLR" and "fsaverage" are supported.
+    null_value : float or None (default: 0.)
+        The value to use for voxels that are outside the brain mask.
+    select : Sequence[int] or None (default: None)
+        If not None, the indices of the scalar maps to load from the
+        volumetric dataset. If None, all scalar maps are loaded.
+    exclude : Sequence[int] or None (default: None)
+        If not None, the indices of the scalar maps to exclude from the
+        volumetric dataset. If None, no scalar maps are excluded.
+    plot : bool (default: False)
+        Indicates whether the scalar dataset should be plotted.
+
+    Returns
+    -------
+    callable
+        A transform function. Transform functions accept a plotting
+        function and return a new plotting function with different input and
+        output arguments.
+        * The transformed plotter will now require a ``<scalars>_nifti``
+          argument, where ``<scalars>`` is the name of the scalar dataset
+          provided as an argument to this function. The value of this
+          argument should be an instance of a ``nibabel`` ``Nifti1Image``
+          or a path to a NIfTI file.
+        * If ``plot`` is ``True``, the transformed function will automatically
+          add the resampled scalar dataset to the sequence of scalars to plot.
+    """
+    templates = {
+        'fsLR': mni152_to_fslr,
+        'fsaverage': mni152_to_fsaverage,
+    }
+    f_resample = templates[template]
+    def transform(
+        f: callable,
+        compositor: callable = direct_compositor,
+    ) -> callable:
+        transformer_f = Partial(
+            resample_to_surface_p,
+            scalars=scalars,
+            f_resample=f_resample,
+            null_value=null_value,
+            select=select,
+            exclude=exclude,
+            plot=plot,
+        )
+
+        def f_transformed(
+            *,
+            surf: CortexTriSurface,
+            **params: Mapping,
+        ):
+            try:
+                nifti = params.pop(f'{scalars}_nifti')
+            except KeyError:
+                raise TypeError(
+                    'Transformed plot function missing one required '
+                    f'keyword-only argument: {scalars}_nifti'
+                )
+            scalars_to_plot = params.get('scalars', []) if plot else None
+            return compositor(f, transformer_f)(**params)(
+                nifti=nifti,
+                surf=surf,
+                scalars_to_plot=scalars_to_plot,
+            )
+
+        return f_transformed
+    return transform
+
+
+def parcellate_colormap(
+    cmap_name: str,
+    parcellation_name: str,
+) -> callable:
+    """
+    Add a colormap to a surface, and then parcellate it to obtain colours for
+    each parcel.
+
+    Parameters
+    ----------
+    cmap_name : str
+        The name of the colormap to add to the surface. Currently, only
+        "network" and "modal" are supported. The "network" colormap colours
+        each parcel based on its overlap with the 7 resting-state networks
+        defined by Yeo et al. (2011). The "modal" colormap colours each
+        parcel based on its affiliation with the 3 modal domains defined by
+        Glasser et al. (2016).
+    parcellation_name : str
+        The name of the parcellation to use to parcellate the colormap. The
+        parcellation must be present on the surface.
+
+    Returns
+    -------
+    callable
+        A transform function. Transform functions accept a plotting
+        function and return a new plotting function with different input and
+        output arguments.
+        * The transformed function will no longer accept the arguments
+          ``cmap``, ``clim``, ``node_cmap``, or ``node_cmap_range``.
+    """
+    cmaps = {
+        'network': 'data/cmap/cmap_network.nii',
+        'modal': 'data/cmap/cmap_modal.nii',
+    }
+    cmap = pkgrf(
+        'hyve',
+        cmaps[cmap_name],
+    )
+    def transform(
+        f: callable,
+        compositor: callable = direct_compositor,
+    ) -> callable:
+        transformer_f = Partial(
+            parcellate_colormap_p,
+            cmap_name=cmap_name,
+            parcellation_name=parcellation_name,
+            cmap=cmap,
+        )
+
+        def f_transformed(
+            *,
+            surf: CortexTriSurface,
+            **params: Mapping,
+        ):
+            return compositor(f, transformer_f)(**params)(surf=surf)
+
+        return f_transformed
+    return transform
+
+
+def plot_to_image():
+    def transform(
+        f: callable,
+        compositor: callable = direct_compositor,
+    ) -> callable:
+        transformer_f = plot_to_image_p
+
+        def f_transformed(
+            *,
+            basename: str = None,
+            views: Sequence = (
+                'medial',
+                'lateral',
+                'dorsal',
+                'ventral',
+                'anterior',
+                'posterior',
+            ),
+            window_size: Tuple[int, int] = (1300, 1000),
+            hemisphere: Optional[Literal['left', 'right', 'both']] = None,
+            **params,
+        ):
+            return compositor(transformer_f, f)(
+                basename=basename,
+                views=views,
+                window_size=window_size,
+                hemisphere=hemisphere,
+            )(hemisphere=hemisphere, **params)
+
+        return f_transformed
+    return transform
+
+
+def plot_to_html(
+    backend: Literal["panel", "pythreejs"] = "panel",
+) -> callable:
+    def transform(
+        f: callable,
+        compositor: callable = direct_compositor,
+    ) -> callable:
+        transformer_f = Partial(
+            plot_to_html_p,
+            backend=backend,
+        )
+
+        def f_transformed(*, filename: str = None, **params):
+            return compositor(transformer_f, f)(filename=filename)(**params)
+
+        return f_transformed
+    return transform
---
+++ packages/hyve/hyve-0.0.1.tar.gz/hyve-0.0.1/src/hyve/util.py
@@ -0,0 +1,320 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Plot and report utilities
+~~~~~~~~~~~~~~~~~~~~~~~~~
+Utilities for plotting and reporting.
+"""
+from typing import Any, Dict, Literal, Optional, Sequence, Tuple, Union
+
+import numpy as np
+import pandas as pd
+import pyvista as pv
+from pyvista.plotting.helpers import view_vectors
+
+
+def cortex_theme() -> Any:
+    """
+    Return a theme for the pyvista plotter for use with the cortex surface
+    plotter.
+    """
+    cortex_theme = pv.themes.DocumentTheme()
+    cortex_theme.transparent_background = True
+    return cortex_theme
+
+
+def half_width(
+    p: pv.Plotter,
+    slack: float = 1.05,
+) -> Tuple[float, float, float]:
+    """
+    Return the half-width of the plotter's bounds, multiplied by a slack
+    factor.
+
+    We use this to set the position of the camera when we're using
+    ``cortex_cameras`` and we receive a string corresponding to an anatomical
+    direction (e.g. "dorsal", "ventral", etc.) as the ``position`` argument.
+
+    The slack factor is used to ensure that the camera is not placed exactly
+    on the edge of the plotter bounds, which can cause clipping of the
+    surface.
+    """
+    return (
+        (p.bounds[1] - p.bounds[0]) / 2 * slack,
+        (p.bounds[3] - p.bounds[2]) / 2 * slack,
+        (p.bounds[5] - p.bounds[4]) / 2 * slack,
+    )
+
+
+def auto_focus(
+    vector: Sequence[float],
+    plotter: pv.Plotter,
+    slack: float = 1.05,
+    focal_point: Optional[Sequence[float]] = None,
+) -> Tuple[float, float, float]:
+    vector = np.asarray(vector)
+    if focal_point is None:
+        focal_point = plotter.center
+    hw = half_width(plotter, slack=slack)
+    scalar = np.nanmin(hw / np.abs(vector))
+    vector = vector * scalar + focal_point
+    return vector, focal_point
+
+
+def cortex_view_dict() -> Dict[str, Tuple[Sequence[float], Sequence[float]]]:
+    """
+    Return a dict containing tuples of (vector, viewup) pairs for each
+    hemisphere. The vector is the position of the camera, and the
+    viewup is the direction of the "up" vector in the camera frame.
+    """
+    common = {
+        'dorsal': ((0, 0, 1), (1, 0, 0)),
+        'ventral': ((0, 0, -1), (-1, 0, 0)),
+        'anterior': ((0, 1, 0), (0, 0, 1)),
+        'posterior': ((0, -1, 0), (0, 0, 1)),
+    }
+    return {
+        'left': {
+            'lateral': ((-1, 0, 0), (0, 0, 1)),
+            'medial': ((1, 0, 0), (0, 0, 1)),
+            **common,
+        },
+        'right': {
+            'lateral': ((1, 0, 0), (0, 0, 1)),
+            'medial': ((-1, 0, 0), (0, 0, 1)),
+            **common,
+        },
+        'both': {
+            'left': ((-1, 0, 0), (0, 0, 1)),
+            'right': ((1, 0, 0), (0, 0, 1)),
+            **common,
+        },
+    }
+
+
+def cortex_cameras(
+    position: Union[str, Sequence[Tuple[float, float, float]]],
+    plotter: pv.Plotter,
+    negative: bool = False,
+    hemisphere: Optional[Literal['left', 'right']] = None,
+) -> Tuple[
+    Tuple[float, float, float],
+    Tuple[float, float, float],
+    Tuple[float, float, float],
+]:
+    """
+    Return a tuple of (position, focal_point, view_up) for the camera. This
+    function accepts a string corresponding to an anatomical direction (e.g.
+    "dorsal", "ventral", etc.) as the ``position`` argument, and returns the
+    corresponding camera position, focal point, and view up vector.
+    """
+    if isinstance(position, str):
+        try:
+            # TODO: I'm a little bit concerned that ``view_vectors`` is not
+            #       part of the public API. We should probably find a better
+            #       way to do this.
+            position = view_vectors(view=position, negative=negative)
+        except ValueError as e:
+            if isinstance(hemisphere, str):
+                try:
+                    vector, view_up = cortex_view_dict()[hemisphere][position]
+                    vector, focal_point = auto_focus(vector, plotter)
+                    return (vector, focal_point, view_up)
+                except KeyError:
+                    raise e
+            else:
+                raise e
+    return position
+
+
+def robust_clim(
+    surf: pv.PolyData,
+    scalar: str,
+    percent: float = 5.0,
+    bgval: Optional[float] = 0.0,
+) -> Tuple[float, float]:
+    data = surf.point_data[scalar]
+    if bgval is not None:
+        data = data[~np.isclose(data, bgval)]
+    return (
+        np.nanpercentile(data, percent),
+        np.nanpercentile(data, 100 - percent),
+    )
+
+
+def plot_to_display(
+    p: pv.Plotter,
+    cpos: Optional[Sequence[Sequence[float]]] = 'yz',
+) -> None:
+    p.show(cpos=cpos)
+
+
+def format_position_as_string(
+    position: Union[str, Sequence[Tuple[float, float, float]]],
+    precision: int = 2,
+    delimiter: str = 'x',
+) -> str:
+    def _fmt_field(field: float) -> str:
+        return delimiter.join(
+            str(round(v, precision))
+            if v >= 0
+            else f'neg{abs(round(v, precision))}'
+            for v in field
+        )
+
+    if isinstance(position, str):
+        return f'view-{position}'
+    elif isinstance(position[0], float) or isinstance(position[0], int):
+        return f'vector-{_fmt_field(position)}'
+    else:
+        return (
+            f'vector-{_fmt_field(position[0])}_'
+            f'focus-{_fmt_field(position[1])}_'
+            f'viewup-{_fmt_field(position[2])}'
+        )
+
+
+def filter_node_data(
+    val: np.ndarray,
+    name: str = 'node',
+    threshold: Union[float, int] = 0.0,
+    percent_threshold: bool = False,
+    topk_threshold: bool = False,
+    absolute: bool = True,
+    node_selection: Optional[np.ndarray] = None,
+    incident_edge_selection: Optional[np.ndarray] = None,
+    removed_val: Optional[float] = None,
+    surviving_val: Optional[float] = 1.0,
+) -> pd.DataFrame:
+    node_incl = np.ones_like(val, dtype=bool)
+
+    sgn = np.sign(val)
+    if absolute:
+        val = np.abs(val)
+    if node_selection is not None:
+        node_incl[~node_selection] = 0
+    if incident_edge_selection is not None:
+        node_incl[~incident_edge_selection.any(axis=-1)] = 0
+    if topk_threshold:
+        indices = np.argpartition(-val, int(threshold))
+        node_incl[indices[int(threshold) :]] = 0
+    elif percent_threshold:
+        node_incl[val < np.percentile(val[node_incl], 100 * threshold)] = 0
+    else:
+        node_incl[val < threshold] = 0
+
+    if removed_val is not None:
+        val[~node_incl] = removed_val
+        if surviving_val is not None:
+            val[node_incl] = surviving_val
+        index = np.arange(val.shape[0])
+    else:
+        val = val[node_incl]
+        sgn = sgn[node_incl]
+        index = np.where(node_incl)[0]
+
+    return pd.DataFrame(
+        {
+            f'{name}_val': val,
+            f'{name}_sgn': sgn,
+        },
+        index=pd.Index(index, name='node'),
+    )
+
+
+def filter_adjacency_data(
+    adj: np.ndarray,
+    name: str = 'edge',
+    threshold: Union[float, int] = 0.0,
+    percent_threshold: bool = False,
+    topk_threshold_nodewise: bool = False,
+    absolute: bool = True,
+    incident_node_selection: Optional[np.ndarray] = None,
+    connected_node_selection: Optional[np.ndarray] = None,
+    edge_selection: Optional[np.ndarray] = None,
+    removed_val: Optional[float] = None,
+    surviving_val: Optional[float] = 1.0,
+    emit_degree: Union[bool, Literal['abs', '+', '-']] = False,
+    emit_incident_nodes: Union[bool, tuple] = False,
+) -> pd.DataFrame:
+    adj_incl = np.ones_like(adj, dtype=bool)
+
+    sgn = np.sign(adj)
+    if absolute:
+        adj = np.abs(adj)
+    if incident_node_selection is not None:
+        adj_incl[~incident_node_selection, :] = 0
+    if connected_node_selection is not None:
+        adj_incl[~connected_node_selection, :] = 0
+        adj_incl[:, ~connected_node_selection] = 0
+    if edge_selection is not None:
+        adj_incl[~edge_selection] = 0
+    if topk_threshold_nodewise:
+        indices = np.argpartition(-adj, int(threshold), axis=-1)
+        indices = indices[..., int(threshold) :]
+        adj_incl[
+            np.arange(adj.shape[0], dtype=int).reshape(-1, 1), indices
+        ] = 0
+    elif percent_threshold:
+        adj_incl[adj < np.percentile(adj[adj_incl], 100 * threshold)] = 0
+    else:
+        adj_incl[adj < threshold] = 0
+
+    degree = None
+    if emit_degree == 'abs':
+        degree = np.abs(adj).sum(axis=0)
+    elif emit_degree == '+':
+        degree = np.maximum(adj, 0).sum(axis=0)
+    elif emit_degree == '-':
+        degree = -np.minimum(adj, 0).sum(axis=0)
+    elif emit_degree:
+        degree = adj.sum(axis=0)
+
+    indices_incl = np.triu_indices(adj.shape[0], k=1)
+    adj_incl = adj_incl | adj_incl.T
+
+    incidents = None
+    if emit_incident_nodes:
+        incidents = adj_incl.any(axis=0)
+        if isinstance(emit_incident_nodes, tuple):
+            exc, inc = emit_incident_nodes
+            incidents = np.where(incidents, inc, exc)
+
+    if removed_val is not None:
+        adj[~adj_incl] = removed_val
+        if surviving_val is not None:
+            adj[adj_incl] = surviving_val
+    else:
+        adj_incl = adj_incl[indices_incl]
+        indices_incl = tuple(i[adj_incl] for i in indices_incl)
+    adj = adj[indices_incl]
+    sgn = sgn[indices_incl]
+
+    edge_values = pd.DataFrame(
+        {
+            f'{name}_val': adj,
+            f'{name}_sgn': sgn,
+        },
+        index=pd.MultiIndex.from_arrays(indices_incl, names=['src', 'dst']),
+    )
+
+    if degree is not None:
+        degree = pd.DataFrame(
+            degree,
+            index=range(1, degree.shape[0] + 1),
+            columns=(f'{name}_degree',),
+        )
+        if incidents is None:
+            return edge_values, degree
+    if incidents is not None:
+        incidents = pd.DataFrame(
+            incidents,
+            index=range(1, incidents.shape[0] + 1),
+            columns=(f'{name}_incidents',),
+        )
+        if degree is None:
+            return edge_values, incidents
+        df = degree.join(incidents, how='outer')
+        return edge_values, df
+    return edge_values
