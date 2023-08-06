3294ba5435fcb1f63c2a18c607a0e675e1bfc35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:22 +0000

    Add package ovos-core/0.0.8a30/ovos_core-0.0.8a30-py3-none-any.whl

---
+++ packages/ovos-core/ovos_core-0.0.8a30-py3-none-any.whl/mycroft/skills/intent_services/__init__.py
@@ -0,0 +1,8 @@
+from ovos_core.intent_services import AdaptService,\
+    PadatiousService, PadatiousMatcher, \
+    ConverseService,\
+    CommonQAService, \
+    FallbackService
+from ovos_core.intent_services import IntentMatch
+from mycroft.skills.intent_services.adapt_service import AdaptIntent, IntentBuilder, Intent
+
