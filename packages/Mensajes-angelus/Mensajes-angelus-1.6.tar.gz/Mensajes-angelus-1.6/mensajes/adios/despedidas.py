c4f14430a181247d7b1b21eaa1f17ed33ef779ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:46 +0000

    Add package Mensajes-angelus/1.6/Mensajes-angelus-1.6.tar.gz

---
+++ packages/Mensajes-angelus/Mensajes-angelus-1.6.tar.gz/Mensajes-angelus-1.6/mensajes/adios/despedidas.py
@@ -0,0 +1,6 @@
+def despedir():
+    print("Adios, desde despedidas.despedir()")
+
+class Despedida():
+    def __init__(self):
+        print("Bye desde Despedida.__init__()")
---
+++ packages/Mensajes-angelus/Mensajes-angelus-1.6.tar.gz/Mensajes-angelus-1.6/setup.py
@@ -0,0 +1,23 @@
+from struct import pack
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Mensajes-angelus',
+    version = '1.6',
+    description = 'Paquete para saludar y despedir',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author = 'angelus',
+    author_email = 'cloud.angelus@gmail.com',
+    url = 'https://github.com/angelus',
+    license_files=['LICENSE'],
+    packages = find_packages(),
+    scripts = [],
+    test_suite='tests',
+    install_requires=[paquete.strip()
+                      for paquete in open("requirements.txt").readlines()],
+    classifiers=[
+        'Environment :: Console',
+        'Topic :: Utilities'
+    ]
+)
