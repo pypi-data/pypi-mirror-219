0ed553b95030a3b15e9c9e629a0fac28d70a6273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:48 +0000

    Add package gpforecaster/0.3.84/gpforecaster-0.3.84.tar.gz

---
+++ packages/gpforecaster/gpforecaster-0.3.84.tar.gz/gpforecaster-0.3.84/gpforecaster/tests/test_model_results_police.py
@@ -0,0 +1,19 @@
+import unittest
+import tsaugmentation as tsag
+from gpforecaster.model.gpf import GPF
+import shutil
+
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.data = tsag.preprocessing.PreprocessDatasets('police', top=10, freq='D').apply_preprocess()
+        self.n = self.data['predict']['n']
+        self.s = self.data['train']['s']
+        shutil.rmtree("./data/original_datasets")
+        self.gpf = GPF('police', self.data, log_dir="..")
+
+    def test_predict_shape(self):
+        model, like = self.gpf.train(epochs=10)
+        preds, preds_scaled = self.gpf.predict(model, like)
+        self.assertTrue(preds[0].shape == (self.n, self.s))
