8eda2264633705f9696e15ddba7a1c387befa8e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:16 +0000

    Add package atwiki-python/0.0.11/atwiki-python-0.0.11.tar.gz

---
+++ packages/atwiki-python/atwiki-python-0.0.11.tar.gz/atwiki-python-0.0.11/README.rst
@@ -0,0 +1,87 @@
+|GitHubActions|_ |CodeCov|_ |PyPi|_
+
+.. |GitHubActions| image:: https://github.com/kmaehashi/atwiki-python/actions/workflows/test.yml/badge.svg?branch=main
+.. _GitHubActions: https://github.com/kmaehashi/atwiki-python/actions/workflows/test.yml?query=branch%3Amain
+
+.. |CodeCov| image:: https://codecov.io/gh/kmaehashi/atwiki-python/branch/main/graph/badge.svg
+.. _CodeCov: https://codecov.io/gh/kmaehashi/atwiki-python
+
+.. |PyPI| image:: https://badge.fury.io/py/atwiki-python.svg
+.. _PyPI: https://pypi.org/project/atwiki-python
+
+atwiki-python
+=============
+
+``atwiki-python`` is a client library to access `@wiki <https://atwiki.jp/>`_ from Python.
+
+This package also includes ``atwiki-dump`` command to dump source from @wiki wiki site.
+
+Install
+-------
+
+::
+
+  pip install atwiki-python
+
+Requirements
+------------
+
+* Python 2.7 / 3.7 / 3.8 / 3.9 / 3.10 / 3.11
+
+Usage
+-----
+
+``atwiki-dump`` Command
+~~~~~~~~~~~~~~~~~~~~~~~
+
+Dump source and page name for each page in the wiki site.
+
+::
+
+  atwiki-dump -o /tmp/dump_dir https://w.atwiki.jp/python-client/
+
+Python API
+~~~~~~~~~~
+
+Python API provides access to @wiki features.
+
+.. code:: python
+
+  from atwiki import *
+
+  api = AtWikiAPI(AtWikiURI('https://w.atwiki.jp/python-client/'))
+
+  # Show list of tags.
+  for tag in api.get_tags():
+      print(tag)
+
+  # Show list of pages.
+  for page in api.get_list():
+      print(page)
+
+  # Show list of pages tagged with 'tag01'.
+  for page in api.get_list('tag01'):
+      print(page)
+
+  # Show source of page ID 14.
+  print(api.get_source(14))
+
+  # Show results of wiki search.
+  for result in api.search('test'):
+      print(result)
+
+Hints
+-----
+
+* Always use an appropraite interval between requests, or your IP address may get banned.
+  Empirically, 10 seconds of sleep between API call is sufficient.
+  Each ``AtWikiAPI`` instance automatically injects a delay between requests.
+* Your application must expect that entries returned from APIs may be duplicate/missing when pages/tags are added/removed during API call.
+  This is because listing requests are internally pagerized and it is costly to guarantee consistency.
+* AtWiki's specification may change anytime.
+  If you are going to build an automated system, it is encouraged to run test suites included with the installation of this library (``python -m unittest discover atwiki``) everytime before running your application.
+
+License
+-------
+
+MIT License
---
+++ packages/atwiki-python/atwiki-python-0.0.11.tar.gz/atwiki-python-0.0.11/atwiki/test/test_core.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import math
+import os
+import re
+import unittest
+from unittest import TestCase
+
+from atwiki.core import AtWikiAPI
+from atwiki.uri import AtWikiURI
+
+from . import TEST_BASE_URI
+
+
+skip_search_tests = os.environ.get('ATWIKI_TEST_SEARCH_SKIP', '1') == '1'
+
+
+class AtWikiAPITest(TestCase):
+  def setUp(self):
+    self._api = AtWikiAPI(AtWikiURI(TEST_BASE_URI))
+
+  def test_get_list(self):
+    results = list(self._api.get_list())
+    self.assertEqual(len(results), 19)
+
+  def test_get_list_tag(self):
+    results = list(self._api.get_list('tag01'))
+    self.assertEqual(len(results), 1)
+    self.assertEqual(results[0]['id'], 18)
+    self.assertEqual(results[0]['name'], 'Test_atwiki.test.test_core:AtWikiAPITest:test_get_list_tag')
+
+  def test_get_tags(self):
+    results = list(self._api.get_tags())
+    self.assertEqual(len(results), 1)
+    self.assertEqual(results[0]['name'], 'tag01')
+    self.assertEqual(results[0]['weight'], 1)
+
+  def test_get_source(self):
+    self.assertEqual(self._api.get_source(14, 0),
+                     'テスト1\nテスト2\n\nテスト3\nテスト4\n\n\nテスト5')
+    self.assertEqual(self._api.get_source(14, 3),
+                     'テスト1\nテスト2\n\nテスト3\nテスト4')
+
+  def test_get_source_special(self):
+    self.assertEqual(self._api.get_source(15, 0),
+                     '"テスト1"<br>&\n"テスト2"<br>&')
+
+  def test_get_source_invalid(self):
+    self.assertRaises(IndexError, self._api.get_source, 15, 100000)
+
+  @unittest.skipIf(skip_search_tests, 'Skipping search tests')
+  def test_search(self):
+    results = list(self._api.search('SearchKeyword01 SearchKeyword02'))
+    self.assertEqual(len(results), 1)
+    self.assertEqual(results[0]['name'], 'Test_atwiki.test.test_core:AtWikiAPITest:test_search')
+    self.assertEqual(results[0]['snippet'], 'SearchKeyword01  SearchKeyword02')
+
+  @unittest.skipIf(skip_search_tests, 'Skipping search tests')
+  def test_search_or(self):
+    results = list(self._api.search('SearchKeyword01 SearchKeyword02', False))
+    self.assertEqual(len(results), 2)
+    self.assertEqual(results[0]['name'], 'Test_atwiki.test.test_core:AtWikiAPITest:test_search')
+    self.assertEqual(results[0]['snippet'], 'SearchKeyword01  SearchKeyword02')
+    self.assertEqual(results[1]['name'], 'Test_atwiki.test.test_core:AtWikiAPITest:test_search_or')
+    self.assertEqual(results[1]['snippet'], 'SearchKeyword02')
+
+  def test_search_none(self):
+    results = list(self._api.search('no_result_expected_for_this'))
+    self.assertEqual(len(results), 0)
+
+
+class PagerizeTest(TestCase):
+    def setUp(self):
+        self._uri = AtWikiURI('https://w.atwiki.jp/hmiku')
+        self._api = AtWikiAPI(self._uri)
+
+    def test_get_list(self):
+        # Get the first page from the first listing.
+        top_page = next(self._api.get_list())
+        assert top_page == {'id': 1, 'name': 'トップページ'}
+
+        # Detect number of all pages.
+        soup = self._api._request(self._uri.list(sort='create', index=1))
+        text = soup.find('div', class_='pagelist').text
+        m = re.search(r'計 (\d+) ページ / 1 から 100 を表示', text)
+        assert m is not None
+        total_count = int(m.group(1))  # Total number of pages
+        assert 45000 < total_count < 90000
+        last_index = math.ceil(total_count / 100)
+
+        # Ensure listing pagerize works. (retrieve 3 listing, 100 pages in each)
+        count = 0
+        for page in self._api.get_list():
+            count += 1
+            if 100*3 <= count:
+                break
+        else:
+            assert False, "unexpected number of pages"
+
+        # Get list from the last listing.
+        # N.B. The page counter is not updated immediately.
+        pages = list(self._api.get_list(_start=last_index))
+        expected = (total_count % 100)
+        assert max(0, (expected - 50)) < len(pages) < min(100, (expected + 50))
+
+        # Get out-of-bounds listing. (expected to wrap around)
+        top_page = next(self._api.get_list(_start=last_index + 1))
+        assert top_page == {'id': 1, 'name': 'トップページ'}
+
+    def test_get_list_tag(self):
+        # Detect number of listings.
+        soup = self._api._request(self._uri.tag('曲', index=1))
+        last_index = 1
+        for link in soup.find('div', class_='cmd_tag').find_all('a'):
+            if not link.attrs['href'].endswith('?p={}'.format(last_index + 1)):
+                break
+            last_index += 1
+        assert 750 <= last_index
+
+        # Ensure listing pagerize works. (retrieve 3 listing, 50 pages in each)
+        count = 0
+        for page in self._api.get_list('曲'):
+            count += 1
+            if 50*3 <= count:
+                break
+        else:
+            assert False, "unexpected number of pages for the tag"
+
+        # Get list from the last listing.
+        pages = list(self._api.get_list('曲', _start=last_index))
+        assert 1 <= len(pages) <= 50
+
+        # Get out-of-bounds listing.
+        pages = list(self._api.get_list('曲', _start=last_index + 1))
+        assert len(pages) == 0
+
+    def test_get_tags(self):
+        # Get the first tag from the first listing.
+        song = next(self._api.get_tags('num'))
+        assert song['name'] == '曲'
+        assert 35000 < song['weight'] < 70000
+
+        # Get the first tag from the second listing.
+        not_song = next(self._api.get_tags('num', _start=2))
+        assert not_song['name'] != '曲'
+
+        # Ensure listing pagerize works. (retrieve 3 listing, 500 pages in each)
+        count = 0
+        for page in self._api.get_tags():
+            count += 1
+            if 500*3 <= count:
+                break
+        else:
+            assert False, "unexpected number of tags"
