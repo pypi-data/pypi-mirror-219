8335b22a1ed866f056657d687acb37b4fcda21cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:37 +0000

    Add package examon-core/1.0.3/examon_core-1.0.3.tar.gz

---
+++ packages/examon-core/examon_core-1.0.3.tar.gz/examon_core-1.0.3/examon_core/examon_item.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from .examon_item_registry import ExamonItemRegistry
+from .question_factory import QuestionFactory
+
+
+def examon_item(choices=None, tags=None, hints=None,
+              generated_choices=None, param1=None):
+    def inner_function(function):
+        processed_question = QuestionFactory.build(
+            function=function, choice_list=choices,
+            tags=tags, hints=hints,
+            generated_choices=generated_choices,
+            param1=param1, metrics=True)
+        ExamonItemRegistry.add(processed_question)
+
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            function(*args, **kwargs)
+            return wrapper
+
+    return inner_function
