b1652b46019aeaa1b65c45a24a2450ca6510a951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:07 +0000

    Add package satori-playbook-validator/3.1.7/satori_playbook_validator-3.1.7.tar.gz

---
+++ packages/satori-playbook-validator/satori_playbook_validator-3.1.7.tar.gz/satori_playbook_validator-3.1.7/src/satorici/validator/exceptions.py
@@ -0,0 +1,12 @@
+class PlaybookValidationError(Exception):
+    pass
+
+
+class PlaybookVariableError(PlaybookValidationError):
+    def __init__(self, *args: object, parameter: str) -> None:
+        super().__init__(*args)
+        self.parameter = parameter
+
+
+class NoExecutionsError(PlaybookValidationError):
+    pass
