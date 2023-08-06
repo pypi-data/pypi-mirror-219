a2cd59fd461759b8484957e4fcf51382913486e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:22 +0000

    Add package streamlit-dash/0.6.1/streamlit_dash-0.6.1.tar.gz

---
+++ packages/streamlit-dash/streamlit_dash-0.6.1.tar.gz/streamlit_dash-0.6.1/streamlit_dash/frontend/build/precache-manifest.333968c81367d442b90c2c407970109b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57eb9d3d9b1b2a83c0f16389ec841d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8982e4f5ca4fad3b5b5",
+    "url": "./static/js/2.451b2942.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.451b2942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8972a333194e8ffe8afc",
+    "url": "./static/js/main.c396fd5a.chunk.js"
+  },
+  {
+    "revision": "18300b1ffba716d884c2",
+    "url": "./static/js/runtime-main.58369df8.js"
+  }
+]);
