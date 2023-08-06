24e9550c7509258245c941d9e96c3cfe73772ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:04 +0000

    Add package Aksarantara/1.0.0/Aksarantara-1.0.0-py3-none-any.whl

---
+++ packages/Aksarantara/Aksarantara-1.0.0-py3-none-any.whl/Roman/RomanKana.py
@@ -0,0 +1,120 @@
+VowelMap = [
+    "\u0061",
+    "aa",
+    "\u0069",
+    "ii",
+    "\u0075",
+    "uu",
+    "ri",
+    "rii",
+    "r\u309Ai",
+    "r\u309Aii",
+    "ee",
+    "ai",
+    "oo",
+    "au",
+]
+
+SouthVowelMap = [
+    "e",
+    "o",
+]
+
+ModernVowelMap = [
+    "a",
+    "o",
+]
+
+SinhalaVowelMap = ["e"]
+
+VowelSignMap = VowelMap[1:]
+
+SouthVowelSignMap = SouthVowelMap[:]
+
+ModernVowelSignMap = ModernVowelMap[:]
+
+SinhalaVowelSignMap = SinhalaVowelMap[:]
+
+AyogavahaMap = ["n", "n", "H"]
+
+ViramaMap = ["\u00D7"]
+
+ConsonantMap = [
+    "\u006B",
+    "\u006B",
+    "\u0067",
+    "\u0067",
+    "k\u309A",
+    "c",
+    "c",
+    "\u006A",
+    "\u006A",
+    "J",
+    "t",
+    "t",
+    "d",
+    "d",
+    "n",
+    "t",
+    "t",
+    "d",
+    "d",
+    "\u006E",
+    "\u0070",
+    "\u0070",
+    "\u0062",
+    "\u0062",
+    "\u006D",
+    "\u0079",
+    "r",
+    "r\u309A",
+    "v",
+    "sh",
+    "sh",
+    "\u0073",
+    "\u0068",
+]
+
+SouthConsonantMap = ["r\u309A", "r\u309A", "r", "n"]
+
+NuktaConsonantMap = ["k", "k", "g", "z", "r", "r", "f", "y"]
+
+SinhalaConsonantMap = [
+    "ng",
+    "nj",
+    "nd",
+    "nd",
+    "nb",
+]
+
+NuktaMap = ["\u02BD\u02BD"]
+
+OmMap = ["oon"]
+
+SignMap = ["'", "。", "。。"]
+
+Aytham = ["gu"]
+
+NumeralMap = [
+    "\u0030",
+    "\u0031",
+    "\u0032",
+    "\u0033",
+    "\u0034",
+    "\u0035",
+    "\u0036",
+    "\u0037",
+    "\u0038",
+    "\u0039",
+]
+
+import sys
+
+sys.path.append("..") 
+
+import Map as GM
+
+GM.add_additional_chars(
+    dict([(charlist, globals()[charlist]) for charlist in GM.CharmapLists]),
+    __file__.split(".")[0].split("\\")[-1],
+)
