05c6aaa29a54530bd0d97286a957b46bfe1fbb61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:05 +0000

    Add package dci-task-manager/0.1.0/dci_task_manager-0.1.0.tar.gz

---
+++ packages/dci-task-manager/dci_task_manager-0.1.0.tar.gz/dci_task_manager-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='dci_task_manager',
+    version='0.1.0',
+    description='Utilities of DCI Class', 
+    author='Fab', 
+    packages=['dci_task'], 
+    python_requires='>=3.6', 
+    install_requires=[], 
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        ],
+    )
