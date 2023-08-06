a9ec10bf412d5a1849092e77a4a5ec3b8783da8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:03 +0000

    Add package chatllm/2023.7.15.22.40.30/chatllm-2023.7.15.22.40.30-py3-none-any.whl

---
+++ packages/chatllm/chatllm-2023.7.15.22.40.30-py3-none-any.whl/chatllm/langchain/applications/chatbase.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : chatdoc
+# @Time         : 2023/7/15 20:53
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+import os
+
+from meutils.pipe import *
+
+from chatllm.langchain.utils import docs2dataframe
+from chatllm.langchain.decorators import llm_stream
+from chatllm.langchain.vectorstores import Milvus
+from chatllm.langchain.embeddings import OpenAIEmbeddings
+
+from chatllm.langchain.document_loaders import FilesLoader
+
+from langchain.text_splitter import *
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.embeddings.base import Embeddings
+
+
+class ChatBase(object):
+
+    def __init__(self, model: str = "gpt-3.5-turbo-0613",
+                 collection_name: str = 'TEST',
+                 embeddings: Embeddings = OpenAIEmbeddings(chunk_size=5),
+                 temperature=0
+                 ):
+        self.embeddings = embeddings
+        self.collection_name = collection_name
+        self.llm = ChatOpenAI(model=model, temperature=temperature, streaming=True)
+        self.chain = load_qa_chain(self.llm, chain_type="stuff")
+
+        _vdb_kwargs = self.vdb_kwargs.copy()
+        _vdb_kwargs['embedding_function'] = _vdb_kwargs.pop('embedding')
+        self.vdb: Optional[Milvus] = Milvus(**_vdb_kwargs)
+
+    @staticmethod
+    def load_file(file_paths):
+        loader = FilesLoader(file_paths)
+        docs = loader.load_and_split(
+            RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200, add_start_index=True)
+        )
+        return docs
+
+    @diskcache(location=os.getenv('INSERT_VECTOR_CACHE', '~/.cache/insert_vector_cache'))
+    def create_index(self, docs, **kwargs):
+        """初始化drop_old=True"""
+        self.vdb = Milvus.from_documents(docs, **{**self.vdb_kwargs, **kwargs})
+
+    def llm_qa(self, query: str, k: int = 4, threshold: float = 0.5, **kwargs: Any):
+        docs = self.vdb.similarity_search(query, k=max(k, 10), threshold=threshold, **kwargs)
+        docs = docs | xUnique_plus(lambda doc: doc.page_content.strip())  # 按内容去重
+        docs = docs[:k]
+
+        return llm_stream(self.chain.run)({"input_documents": docs, "question": query})
+
+    @cached_property
+    def vdb_kwargs(self):
+        connection_args = {
+            'uri': os.getenv('ZILLIZ_ENDPOINT'),
+            'token': os.getenv('ZILLIZ_TOKEN')
+        }
+        address = os.getenv('MILVUS_ADDRESS')
+        if address:
+            connection_args.pop('uri')
+            connection_args['address'] = address
+
+        index_params = {
+            "metric_type": "IP",
+            "index_type": "IVF_FLAT",
+            "params": {"nlist": 1024}
+        }
+
+        embedding_function = self.embeddings
+
+        vdb_kwargs = dict(
+            embedding=embedding_function,
+            connection_args=connection_args,
+            index_params=index_params,
+            search_params=None,
+            collection_name=self.collection_name,
+            drop_old=False,
+        )
+
+        return vdb_kwargs
