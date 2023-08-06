8b00998810089340f17203ddf030d0d2d95a97e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:36 +0000

    Add package arctix/0.0.1/arctix-0.0.1-py3-none-any.whl

---
+++ packages/arctix/arctix-0.0.1-py3-none-any.whl/arctix/testing.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+__all__ = ["numpy_available", "torch_available"]
+
+from pytest import mark
+
+from arctix.utils.imports import is_numpy_available, is_torch_available
+
+numpy_available = mark.skipif(not is_numpy_available(), reason="Requires NumPy")
+torch_available = mark.skipif(not is_torch_available(), reason="Requires PyTorch")
---
+++ packages/arctix/arctix-0.0.1-py3-none-any.whl/arctix/utils/imports.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+__all__ = [
+    "check_numpy",
+    "check_torch",
+    "is_numpy_available",
+    "is_torch_available",
+]
+
+from importlib.util import find_spec
+
+
+def is_numpy_available() -> bool:
+    r"""Indicates if the NumPy package is installed or not."""
+    return find_spec("numpy") is not None
+
+
+def check_numpy() -> None:
+    r"""Checks if the numpy package is installed.
+
+    Raises:
+        RuntimeError if the numpy package is not installed.
+    """
+    if not is_numpy_available():
+        raise RuntimeError(
+            "`numpy` package is required but not installed. "
+            "You can install `numpy` package with the command:\n\n"
+            "pip install numpy\n"
+        )
+
+
+def is_torch_available() -> bool:
+    r"""Indicates if the torch package is installed or not."""
+    return find_spec("torch") is not None
+
+
+def check_torch() -> None:
+    r"""Checks if the torch package is installed.
+
+    Raises:
+        RuntimeError if the torch package is not installed.
+    """
+    if not is_torch_available():
+        raise RuntimeError(
+            "`torch` package is required but not installed. "
+            "You can install `torch` package with the command:\n\n"
+            "pip install torch\n"
+        )
