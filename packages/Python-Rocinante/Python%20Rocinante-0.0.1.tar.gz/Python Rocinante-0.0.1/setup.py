ec7eef440b409bc498bdf3d4fd2b9bb7331de1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:34 +0000

    Add package Python-Rocinante/0.0.1/Python%20Rocinante-0.0.1.tar.gz

---
+++ packages/Python-Rocinante/Python%20Rocinante-0.0.1.tar.gz/Python Rocinante-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+requirements = [
+]
+
+setup(
+    name='Python Rocinante',
+    version='0.0.1',
+    description='Python library for happy in using.',
+    author='Chisheng Chen',
+    author_email='gndlwccw@163.com',
+    url='http://gitee.com/PyRocinante',
+    license='MIT-0',
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=requirements
+)
