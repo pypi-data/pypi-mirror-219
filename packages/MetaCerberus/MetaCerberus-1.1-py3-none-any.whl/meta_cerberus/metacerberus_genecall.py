fd8954bf3cea243edc588e2c60609efa947f5f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:31 +0000

    Add package MetaCerberus/1.1/MetaCerberus-1.1-py3-none-any.whl

---
+++ packages/MetaCerberus/MetaCerberus-1.1-py3-none-any.whl/meta_cerberus/metacerberus_genecall.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""metacerberus_genecall.py: Module for finding Open Reading Frames (ORF) in FASTA nucleotide files
+Uses prodigal
+Uses FGS+
+"""
+
+from pathlib import Path
+import subprocess
+import pkg_resources as pkg
+
+
+# Eukaryotic option
+def findORF_fgs(contig, config, subdir):
+    path = Path(config['DIR_OUT'], subdir)
+    done = path / "complete"
+
+    baseOut = path / "proteins"
+    faaOut  = path / "proteins.faa"
+
+    if not config['REPLACE'] and done.exists() and faaOut.exists():
+            return faaOut
+    done.unlink(missing_ok=True)
+    path.mkdir(exist_ok=True, parents=True)
+
+    command = f"{config['EXE_FGS']} -p {config['CPUS']} -s {contig} -o {baseOut} -w 1 -t complete"
+    try:
+        with Path(path,"stdout.txt").open('w') as fout, Path(path,"stderr.txt").open('w') as ferr:
+            subprocess.run(command, shell=True, check=True, stdout=fout, stderr=ferr)
+    except Exception as e:
+        print(e)
+        return None
+
+    done.touch()
+    return faaOut
+
+
+# Microbial option
+def findORF_prod(contig, config, subdir):
+    path = Path(config['DIR_OUT'], subdir)
+    done = path / "complete"
+
+    faaOut = path / "proteins.faa"
+
+    if not config['REPLACE'] and done.exists() and faaOut.exists():
+        return faaOut
+    done.unlink(missing_ok=True)
+    path.mkdir(exist_ok=True, parents=True)
+
+    command = f"{config['EXE_PRODIGAL']} -i {contig} -o {path}/genes.gff -a {faaOut} -f gff"
+    try:
+        with Path(path, 'stdout.txt').open('w') as fout, Path(path, 'stderr.txt').open('w') as ferr:
+            subprocess.run(command, shell=True, check=True, stdout=fout, stderr=ferr)
+    except Exception as e:
+        print(e)
+
+    done.touch()    
+    return faaOut
+
+
+# Metagenome option
+def findORF_meta(contig, config, subdir):
+    path = Path(config['DIR_OUT'], subdir)
+    done = path / "complete"
+
+    faaOut = path / "proteins.faa"
+
+    if not config['REPLACE'] and done.exists() and faaOut.exists():
+        return faaOut
+    done.unlink(missing_ok=True)
+    path.mkdir(exist_ok=True, parents=True)
+
+    command = f"{config['EXE_PRODIGAL']} -i {contig} -o {path}/genes.gff -a {faaOut} -f gff -p meta"
+    try:
+        with Path(path, "stdout.txt").open('w') as fout, Path(path, "stderr.txt").open('w') as ferr:
+            subprocess.run(command, shell=True, check=True, stdout=fout, stderr=ferr)
+    except Exception as e:
+        print(e)
+
+    done.touch()
+    return faaOut
