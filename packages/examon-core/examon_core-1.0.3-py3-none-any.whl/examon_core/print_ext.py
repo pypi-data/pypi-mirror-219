11229593169f54cbbc2271172e12854f7089f3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:37 +0000

    Add package examon-core/1.0.3/examon_core-1.0.3-py3-none-any.whl

---
+++ packages/examon-core/examon_core-1.0.3-py3-none-any.whl/examon_core/print_ext.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+import builtins
+from contextlib import redirect_stdout
+import io
+from inspect import getframeinfo, stack
+
+@dataclass
+class PrintLogItem:
+    output: str
+    line_no: int
+
+class PrintLog:
+    print_logs = []
+
+    @classmethod
+    def append(cls, value):
+        cls.print_logs.append(value)
+
+    @classmethod
+    def logs(cls):
+        return cls.print_logs
+
+    @classmethod
+    def apply_offset(cls, offset):
+        cls.print_logs = list(
+            map(lambda x: (x[0], x[1] - offset), cls.print_logs)
+        )
+
+    @classmethod
+    def reset(cls):
+        cls.print_logs = []
+
+
+def print(*args, **kwargs):
+    caller = getframeinfo(stack()[1][0])
+    f = io.StringIO()
+
+    with redirect_stdout(f):
+        builtins.print(*args, **kwargs)
+    s = f.getvalue().rstrip()
+    PrintLog.append(PrintLogItem    (s, caller.lineno))
