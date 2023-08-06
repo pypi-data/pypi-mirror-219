71805d6c85f8776ef7554f8fee76fa20b117f9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:21 +0000

    Add package PyRandomNameGenerator/0.1/PyRandomNameGenerator-0.1.tar.gz

---
+++ packages/PyRandomNameGenerator/PyRandomNameGenerator-0.1.tar.gz/PyRandomNameGenerator-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="PyRandomNameGenerator",
+    version="0.1",
+    description="This is a simple name generator package",
+    author="Md. Ismiel Hossen Abir",
+    packages=["PyRandomNameGenerator"],
+    install_requires=[]
+    
+)
