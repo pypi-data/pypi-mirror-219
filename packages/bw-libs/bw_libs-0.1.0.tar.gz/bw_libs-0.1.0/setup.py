35e1d7249e3bafbc7728ccd9cd5259ad52fc301b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:41 +0000

    Add package bw-libs/0.1.0/bw_libs-0.1.0.tar.gz

---
+++ packages/bw-libs/bw_libs-0.1.0.tar.gz/bw_libs-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.0'
+DESCRIPTION = 'DESCRIPTION'
+LONG_DESCRIPTION = 'LONG_DESCRIPTION'
+
+# Setting up
+setup(
+    name="bw_libs",
+    version=VERSION,
+    author="Sargis",
+    author_email="<sargis@bridgewise.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
