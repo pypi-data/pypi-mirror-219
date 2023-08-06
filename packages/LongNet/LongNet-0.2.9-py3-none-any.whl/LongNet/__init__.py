77440eb584628378f4460af01b09cc452dc359a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:12 +0000

    Add package LongNet/0.2.9/LongNet-0.2.9-py3-none-any.whl

---
+++ packages/LongNet/LongNet-0.2.9-py3-none-any.whl/LongNet/__init__.py
@@ -0,0 +1,2 @@
+from LongNet.attention import DilatedAttention, MultiModalDilationAttention, DilatedAttention2, DistributedDilatedAttention
+from LongNet.model import LongNetTokenizer, LongNet, DecoderConfig, Decoder
