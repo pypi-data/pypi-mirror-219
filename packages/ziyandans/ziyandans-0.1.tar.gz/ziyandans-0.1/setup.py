98b38e5dd7b2f9b9c1021c656c42840ed5a58332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:15 +0000

    Add package ziyandans/0.1/ziyandans-0.1.tar.gz

---
+++ packages/ziyandans/ziyandans-0.1.tar.gz/ziyandans-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ziyandans',
+    version = '0.1',
+    author = 'WJY',
+    author_email = '912092829@qq.com',
+    description = 'test',
+    packages = find_packages(),
+    install_requires = [],
+)
