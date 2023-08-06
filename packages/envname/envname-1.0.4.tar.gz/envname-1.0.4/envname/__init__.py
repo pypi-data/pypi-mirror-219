a83c59f54cd4e1308f10c856dd4c58f7eb247729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:36 +0000

    Add package envname/1.0.4/envname-1.0.4.tar.gz

---
+++ packages/envname/envname-1.0.4.tar.gz/envname-1.0.4/envname/__init__.py
@@ -0,0 +1,31 @@
+'''
+Copyright [2023] [lcctoor.com]
+
+license: Apache License, Version 2.0
+
+author: lcctoor.com
+domain-name: lcctoor.com
+email: lcctoor@outlook.com
+'''
+
+import sys
+from pathlib import Path
+
+from ._envname import envname
+
+
+def _parsecmd():
+    kws = sys.argv[1:]
+    if kws:
+        kw = kws[0].lower()
+        if kw == 'set' and len(kws) > 1:
+            py = Path(__file__).parent / '_envname.py'
+            py.write_text(f"envname = '{kws[1]}'", 'utf8')
+            print('创建成功!')
+        elif kw == 'read':
+            print(envname)
+    else:
+        print('''指令集:
+envname set <名称> | 创建环境名称
+envname read       | 查看环境名称
+''')
