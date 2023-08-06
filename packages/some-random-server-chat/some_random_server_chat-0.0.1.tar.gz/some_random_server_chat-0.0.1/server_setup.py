00684dc46c9bc745d28fedd3752bf4304ee4af27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:50 +0000

    Add package some-random-server-chat/0.0.1/some_random_server_chat-0.0.1.tar.gz

---
+++ packages/some-random-server-chat/some_random_server_chat-0.0.1.tar.gz/some_random_server_chat-0.0.1/server_setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="some_random_server_chat",
+      version="0.0.1",
+      description="some_random_server_chat",
+      author="Artur B.",
+      author_email="ex1le@hotbox.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
