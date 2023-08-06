bb345907af310f29de80b0fc7c40c4a2f2db9804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:51 +0000

    Add package d22d/1.1.78/d22d-1.1.78.tar.gz

---
+++ packages/d22d/d22d-1.1.78.tar.gz/d22d-1.1.78/d22d/version.py
@@ -0,0 +1,21 @@
+__VERSION__ = '1.1.78'
+
+__ALL__ = [
+    'ElasticSearchD',
+    'MySqlD',
+    'CsvD',
+    'TxtD',
+    'ZipD',
+    'JsonListD',
+    'XlsIbyFileD',
+    'XlsxIbyFileD',
+    'SqlFileD',
+    'ListD',
+    'ClickHouseD',
+    'MongoDBD',
+    'Migration',
+    'Migration2DB',
+    'secure_filename',
+    'get_line_num_fast',
+    'get_realpath',
+]
