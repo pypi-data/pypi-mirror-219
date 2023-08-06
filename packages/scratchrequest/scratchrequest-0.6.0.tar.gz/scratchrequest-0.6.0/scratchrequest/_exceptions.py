0b296e2879c6c3d54b4b0e4a3d91667254982473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:03 +0000

    Add package scratchrequest/0.6.0/scratchrequest-0.6.0.tar.gz

---
+++ packages/scratchrequest/scratchrequest-0.6.0.tar.gz/scratchrequest-0.6.0/scratchrequest/_exceptions.py
@@ -0,0 +1,20 @@
+class InvalidValueError(ValueError):
+    pass
+
+class QuickAccessDisabledError(Exception):
+    pass
+
+class LoginError(Exception):
+    pass
+
+class AuthError(Exception):
+    pass
+
+class UploadError(Exception):
+    pass
+
+class DownloadError(Exception):
+    pass
+
+class ProjectNotFound(Exception):
+    pass
