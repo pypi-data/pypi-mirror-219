ebf1f4f76bc04db39fa8a1c438668a7031e81dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:05 +0000

    Add package database-local/0.0.8/database-local-0.0.8.tar.gz

---
+++ packages/database-local/database-local-0.0.8.tar.gz/database-local-0.0.8/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+# used by python -m build
+setuptools.setup(
+     name='database-local',  
+     version='0.0.8',
+     author="Circles",
+     author_email="info@circles.life",
+     description="PyPI Package for Circles Local Database Python",
+     long_description="This is a package for sharing common Logger function used in different repositories",
+     long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
