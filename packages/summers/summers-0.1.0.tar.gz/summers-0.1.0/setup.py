8bfa7548dbc649deda048213d1fe11fe404f5ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:35 +0000

    Add package summers/0.1.0/summers-0.1.0.tar.gz

---
+++ packages/summers/summers-0.1.0.tar.gz/summers-0.1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name='summers',
+    version='0.1.0',
+    description='一个 python 依赖注入框架 <A dependency injection toolkit like Spring (java framework)>',
+    author='weiyi',
+    author_email='272654394@qq.com',
+    url='https://github.com/tuijs/summers',
+    packages=['summers'],
+)
