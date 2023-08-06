3e9be52e4964971bcea3f975c7bc667c906089e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:55 +0000

    Add package smitt/0.1.0/smitt-0.1.0.tar.gz

---
+++ packages/smitt/smitt-0.1.0.tar.gz/smitt-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['smitt']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'smitt',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'hukai916',
+    'author_email': '31452631+hukai916@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
