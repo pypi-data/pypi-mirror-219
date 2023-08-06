e2731ee24ab9bf53918b870920db9a4112e39747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:06 +0000

    Add package hotpot-zzy/0.3.0.10/hotpot-zzy-0.3.0.10.tar.gz

---
+++ packages/hotpot-zzy/hotpot-zzy-0.3.0.10.tar.gz/hotpot-zzy-0.3.0.10/test/test_cheminfo.py
@@ -0,0 +1,15 @@
+"""
+python v3.9.0
+@Project: hotpot
+@File   : test_cheminfo
+@Auther : Zhiyuan Zhang
+@Data   : 2023/7/16
+@Time   : 22:21
+Notes:
+    Test `hotpot/cheminfo` module
+"""
+import unittest as ut
+
+
+class TestMolecule(ut.TestCase):
+    """ Test `hotpot/cheminfo/Molecule` class """
