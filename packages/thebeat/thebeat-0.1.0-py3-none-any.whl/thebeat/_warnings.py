1c1b609b2f89416d76cc7d5dae4c3e30b054618a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:32 +0000

    Add package thebeat/0.1.0/thebeat-0.1.0-py3-none-any.whl

---
+++ packages/thebeat/thebeat-0.1.0-py3-none-any.whl/thebeat/_warnings.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2022-2023  Jelle van der Werff
+#
+# This file is part of thebeat.
+#
+# thebeat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# thebeat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with thebeat.  If not, see <https://www.gnu.org/licenses/>.
+
+framerounding_soundseq = (
+    "thebeat: For one or more of the used sounds, the exact start or end positions in frames (i.e. "
+    "samples) were rounded off to the neirest integer ceiling. This shouldn't be a problem. "
+    "To get rid of this warning, try rounding off the onsets in the passed Sequence object "
+    "by calling Sequence.round_onsets() before passing the object to the SoundSequence "
+    "constructor."
+)
+
+framerounding_melody = (
+    "thebeat: For one or more of the used sounds, the exact start or end positions in frames (i.e. "
+    "samples) were rounded off to the neirest integer ceiling. This shouldn't be a problem."
+    "To get rid of this warning, try using a sampling frequency of 48000 Hz, or a different"
+    "beat_ms."
+)
+
+framerounding_soundsynthesis = (
+    "thebeat: During sound synthesis, the number of frames was rounded off. This shouldn't be a problem. "
+    "To get rid of this warning, try using a combination of sound duration (in seconds) and sampling "
+    "frequency that results in integer values."
+)
+
+missing_values = "thebeat: There were missing values in the passed data."
+
+normalization = "thebeat: Sound was normalized to avoid distortion. If undesirable, change the amplitude of the sounds."
+
+phases_t_at_zero = (
+    "thebeat: The first onset of the test sequence was at t=0.\nThis would result in a phase difference "
+    "that is always 0, which is not very informative.\nTherefore, the first phase difference was discarded.\n"
+    "If you want the first onset at a different time than zero, use the Sequence.from_onsets() method to "
+    "create the Sequence object."
+)
