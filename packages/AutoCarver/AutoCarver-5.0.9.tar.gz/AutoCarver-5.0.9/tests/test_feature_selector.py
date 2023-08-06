c55a1782ea4243f7f1ebbb316dbcd667143d9b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:05 +0000

    Add package AutoCarver/5.0.9/AutoCarver-5.0.9.tar.gz

---
+++ packages/AutoCarver/AutoCarver-5.0.9.tar.gz/AutoCarver-5.0.9/tests/test_feature_selector.py
@@ -0,0 +1,17 @@
+"""Set of tests for feature_selector module."""
+
+from pytest import fixture
+
+from AutoCarver.feature_selector import *
+
+
+def test_feature_selector_quanti():
+    """TODO"""
+    pass
+    # assert False, "TODO"
+
+
+def test_feature_selector_quali():
+    """TODO"""
+    pass
+    # assert False, "TODO"
