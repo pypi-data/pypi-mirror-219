bf664069eede20fa48c5ee89489d840a533a2b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:49 +0000

    Add package pipdeptree/2.10.0/pipdeptree-2.10.0-py3-none-any.whl

---
+++ packages/pipdeptree/pipdeptree-2.10.0-py3-none-any.whl/pipdeptree/_render/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .graphviz import render_graphviz
+from .json import render_json
+from .json_tree import render_json_tree
+from .mermaid import render_mermaid
+from .text import render_text
+
+if TYPE_CHECKING:
+    from pipdeptree._cli import Options
+    from pipdeptree._models import PackageDAG
+
+
+def render(options: Options, tree: PackageDAG) -> None:
+    if options.json:
+        print(render_json(tree))  # noqa: T201
+    elif options.json_tree:
+        print(render_json_tree(tree))  # noqa: T201
+    elif options.mermaid:
+        print(render_mermaid(tree))  # noqa: T201
+    elif options.output_format:
+        assert options.graph_output is not None  # noqa: S101
+        render_graphviz(tree, output_format=options.graph_output, reverse=options.reverse)
+    else:
+        render_text(tree, options.depth, options.encoding_type, options.all, options.freeze)
+
+
+__all__ = [
+    "render",
+]
