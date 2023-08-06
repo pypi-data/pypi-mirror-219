b793c1d519242eef09f9df03bf2e5e447e809c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:32 +0000

    Add package monotonicnetworks/1.5.0/monotonicnetworks-1.5.0.tar.gz

---
+++ packages/monotonicnetworks/monotonicnetworks-1.5.0.tar.gz/monotonicnetworks-1.5.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="monotonicnetworks",
+    version="1.5.0",
+    packages=["monotonicnetworks"],
+    url="",
+    license="MIT",
+    author="Ouail Kitouni, Niklas Nolte",
+    author_email="kitouni@mit.edu, nolte@meta.com",
+    description=(
+        "Pytorch implementation of constrained weight operator norms for robustness "
+        + "and monotonicity in neural networks."
+    ),
+)
