c50a6e1b364fe7aeccfc2915cec9b6ca7028a8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:54 +0000

    Add package hat-drivers/0.7.2/hat_drivers-0.7.2-cp310.cp311-abi3-manylinux_2_24_aarch64.whl

---
+++ packages/hat-drivers/hat_drivers-0.7.2-cp310.cp311-abi3-manylinux_2_24_aarch64.whl/hat/drivers/iec60870/link/endpoint.py
@@ -0,0 +1,72 @@
+import logging
+
+from hat import aio
+
+from hat.drivers import serial
+from hat.drivers.iec60870.link import common
+from hat.drivers.iec60870.link import encoder
+
+
+mlog: logging.Logger = logging.getLogger(__name__)
+
+
+async def create(address_size: common.AddressSize,
+                 direction_valid: bool,
+                 port: str,
+                 baudrate: int = 9600,
+                 bytesize: serial.ByteSize = serial.ByteSize.EIGHTBITS,
+                 parity: serial.Parity = serial.Parity.NONE,
+                 stopbits: serial.StopBits = serial.StopBits.ONE,
+                 xonxoff: bool = False,
+                 rtscts: bool = False,
+                 dsrdtr: bool = False
+                 ) -> 'Endpoint':
+    endpoint = Endpoint()
+    endpoint._encoder = encoder.Encoder(address_size=address_size,
+                                        direction_valid=direction_valid)
+
+    endpoint._endpoint = await serial.create(port=port,
+                                             baudrate=baudrate,
+                                             bytesize=bytesize,
+                                             parity=parity,
+                                             stopbits=stopbits,
+                                             xonxoff=xonxoff,
+                                             rtscts=rtscts,
+                                             dsrdtr=dsrdtr)
+
+    return endpoint
+
+
+class Endpoint(aio.Resource):
+
+    @property
+    def async_group(self):
+        return self._endpoint.async_group
+
+    async def receive(self) -> common.Frame:
+        while True:
+            msg_bytes = bytearray()
+
+            while True:
+                try:
+                    size = self._encoder.get_next_frame_size(msg_bytes)
+
+                except Exception:
+                    msg_bytes = msg_bytes[1:]
+                    continue
+
+                if len(msg_bytes) >= size:
+                    break
+
+                data = await self._endpoint.read(size - len(msg_bytes))
+                msg_bytes.extend(data)
+
+            try:
+                return self._encoder.decode(memoryview(msg_bytes))
+
+            except Exception as e:
+                mlog.error("error decoding message: %s", e, exc_info=e)
+
+    async def send(self, msg: common.Frame):
+        data = self._encoder.encode(msg)
+        await self._endpoint.write(data)
