7c0aca7a1f6060d0524f4d484a2af92c050904a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:28 +0000

    Add package bw-libs/0.0.2/bw_libs-0.0.2.tar.gz

---
+++ packages/bw-libs/bw_libs-0.0.2.tar.gz/bw_libs-0.0.2/bw_libs/__init__.py
@@ -0,0 +1,6 @@
+from bw_libs.streaming import StreamingServer
+from bw_libs.streaming import CameraClient
+from bw_libs.streaming import VideoClient
+from bw_libs.streaming import ScreenShareClient
+from bw_libs.audio import AudioSender
+from bw_libs.audio import AudioReceiver
