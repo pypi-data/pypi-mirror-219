56178518551982364d74181132f855f2cd6ce2bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:04 +0000

    Add package noelgram1/0.0.2/noelgram1-0.0.2.tar.gz

---
+++ packages/noelgram1/noelgram1-0.0.2.tar.gz/noelgram1-0.0.2/noelgram/types/bots_and_keyboards/menu_button_commands.py
@@ -0,0 +1,32 @@
+#  Noelgram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Noelgram.
+#
+#  Noelgram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Noelgram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Noelgram.  If not, see <http://www.gnu.org/licenses/>.
+
+import noelgram
+from noelgram import raw
+from .menu_button import MenuButton
+
+
+class MenuButtonCommands(MenuButton):
+    """A menu button, which opens the bot's list of commands.
+    """
+
+    def __init__(self):
+        super().__init__("commands")
+
+    async def write(self, client: "noelgram.Client") -> "raw.types.BotMenuButtonCommands":
+        return raw.types.BotMenuButtonCommands()
