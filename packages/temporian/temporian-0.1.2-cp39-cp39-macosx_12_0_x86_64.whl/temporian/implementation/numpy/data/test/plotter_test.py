1f040c0f7c818d49a3f4b3f7b0700fdb2ad80642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:19 +0000

    Add package temporian/0.1.2/temporian-0.1.2-cp39-cp39-macosx_12_0_x86_64.whl

---
+++ packages/temporian/temporian-0.1.2-cp39-cp39-macosx_12_0_x86_64.whl/temporian/implementation/numpy/data/test/plotter_test.py
@@ -0,0 +1,81 @@
+from absl.testing import parameterized, absltest
+
+import matplotlib
+
+from temporian.implementation.numpy.data import plotter
+from temporian.implementation.numpy.data.io import event_set
+
+
+class PlotterTest(parameterized.TestCase):
+    def setUp(self):
+        # Make sure that the plot functions don't fail on command line
+        print("Setting matplotlib backend to: agg")
+        matplotlib.use("agg")
+
+    @parameterized.parameters((True,), (False,))
+    def test_plot(self, interactive):
+        try:
+            import IPython.display
+        except ImportError:
+            # IPython is not installed / supported
+            return
+
+        evset = event_set(
+            timestamps=[0.1, 0.2, 0.3, 0.4, 0.5],
+            features={
+                "a": [1, 2, 3, 7, 8],
+                "b": [4, 5, 6, 9, 10],
+                "c": ["X", "Y", "X", "X", "Z"],
+                "x": [1, 1, 1, 2, 2],
+            },
+            indexes=["x"],
+        )
+
+        _ = plotter.plot(
+            evset, indexes=None, interactive=interactive, return_fig=True
+        )
+        _ = plotter.plot(
+            evset, indexes=1, interactive=interactive, return_fig=True
+        )
+        _ = plotter.plot(
+            evset, indexes=[1, 2], interactive=interactive, return_fig=True
+        )
+        _ = plotter.plot(
+            evset,
+            indexes=[(1,), (2,)],
+            interactive=interactive,
+            return_fig=True,
+        )
+
+    def test_index_str(self):
+        try:
+            import IPython.display
+        except ImportError:
+            # IPython is not installed / supported
+            return
+
+        evset = event_set(
+            timestamps=[0.1, 0.2, 0.3],
+            features={
+                "a": [1, 2, 3],
+                "b": [4, 5, 6],
+                "c": ["X", "Y", "X"],
+                "x": [1, 1, 1],
+            },
+            indexes=["c"],
+        )
+
+        _ = plotter.plot(evset, indexes="X", return_fig=True)
+        _ = plotter.plot(evset, indexes=b"X", return_fig=True)
+        _ = plotter.plot(evset, indexes=["X", "Y"], return_fig=True)
+        _ = plotter.plot(evset, indexes=[("X",), ("Y",)], return_fig=True)
+
+    def test_is_uniform(self):
+        self.assertTrue(plotter.is_uniform([]))
+        self.assertTrue(plotter.is_uniform([1]))
+        self.assertTrue(plotter.is_uniform([1, 2, 3]))
+        self.assertFalse(plotter.is_uniform([1, 2, 2.5]))
+
+
+if __name__ == "__main__":
+    absltest.main()
