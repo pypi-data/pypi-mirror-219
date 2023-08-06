96510c54be11c5150b5df97681146596aef83c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:37 +0000

    Add package hsmm-mvpy/0.1.0b8/hsmm_mvpy-0.1.0b8.tar.gz

---
+++ packages/hsmm-mvpy/hsmm_mvpy-0.1.0b8.tar.gz/hsmm_mvpy-0.1.0b8/src/hsmm_mvpy/resample.py
@@ -0,0 +1,127 @@
+import numpy as np
+import xarray as xr
+import multiprocessing as mp
+import warnings
+from warnings import warn, filterwarnings
+from hsmm_mvpy.models import hmp
+from hsmm_mvpy.utils import transform_data, stack_data, save_eventprobs
+from hsmm_mvpy.visu import plot_topo_timecourse
+
+try:
+    __IPYTHON__
+    from tqdm.notebook import tqdm
+except NameError:
+    import tqdm.tqdm
+
+def _gen_idx(data, dim, iterations=1):
+    orig_index = data[dim].values
+    indexes = np.array([np.random.choice(orig_index, size=len(orig_index), replace=True) for x in range(iterations)])
+    sort_idx = orig_index.argsort()
+    corresponding_indexes = sort_idx[np.searchsorted(orig_index,indexes[0],sorter = sort_idx)]
+    return  indexes[0]#corresponding_indexes,
+
+def _gen_dataset(data, dim, n_iterations):   
+    original_dim_order = list(data.dims.keys())
+    original_dim_order_data = list(data.data.dims)
+    if isinstance(dim, list):
+        if len(dim) == 2:
+            data = data.stack({str(dim[0])+'_x_'+str(dim[1]):dim}).dropna(str(dim[0])+'_x_'+str(dim[1]), how='all')
+            dim = str(dim[0])+'_x_'+str(dim[1])
+        elif len(dim) > 2:
+            raise ValueError('Cannot stack more than two dimensions')
+        else: dim = dim[0]
+    named_index = []
+    for iteration in range(n_iterations):
+        named_index.append(_gen_idx(data, dim))
+    return named_index, data, dim, original_dim_order_data
+
+def _bootstrapped_run(data, dim, indexes, order, init, positions, sfreq, n_iter, tolerance, decimate, summarize, verbose, plots=True, cpus=1,
+                      trace=False, path='./'):
+    resampled_data = data.loc[{dim:list(indexes)}].unstack().transpose(*order)
+    if '_x_' in dim:
+        dim = dim.split('_x_')
+    if isinstance(dim, list):
+        resampled_data = resampled_data.assign_coords({dim[0]: np.arange(len(resampled_data[dim[0]]))})#Removes indices to avoid duplicates
+        resampled_data = resampled_data.assign_coords({dim[1]: np.arange(len(resampled_data[dim[1]]))})
+    else:
+        resampled_data = resampled_data.assign_coords({dim: np.arange(len(resampled_data[dim]))})
+    if 'channels' in resampled_data.dims:
+        hmp_data_boot = transform_data(resampled_data, n_comp=init.n_dims)
+    else:
+        hmp_data_boot = resampled_data
+    init_boot = hmp(hmp_data_boot, sfreq=sfreq, event_width=init.event_width, cpus=1)
+    estimates_boot = init_boot.fit(verbose=verbose, tolerance=tolerance, decimate=decimate)
+    if trace:
+        save_eventprobs(estimates_boot.eventprobs, path+str(n_iter)+'.nc')
+    if summarize:
+        times = init_boot.compute_times(init_boot, estimates_boot, mean=True)
+        channels = init_boot.compute_topologies(resampled_data, estimates_boot, 
+                                                init_boot.event_width_samples, mean=True)
+        boot_results = xr.combine_by_coords([estimates_boot.magnitudes.to_dataset(name='magnitudes'),
+                                         estimates_boot.parameters.to_dataset(name='parameters'), 
+                                         times.to_dataset(name='event_times'),
+                                         channels.to_dataset(name='channels_activity')])
+    else:
+        boot_results = xr.combine_by_coords([estimates_boot.magnitudes.to_dataset(name='magnitudes'),
+                                         estimates_boot.parameters.to_dataset(name='parameters'), 
+                                         estimates_boot.eventprobs.to_dataset(name='eventprobs')])
+    return boot_results
+
+def bootstrapping(data, dim, n_iterations, init, positions, sfreq, tolerance=1e-4,
+                  decimate=None, summarize=True, verbose=False,
+                  plots=True, cpus=1, trace=False, path='./'):
+    import itertools
+    if 'channels' in data.dims:
+        data_type = 'raw'
+    else:
+        data_type = 'transformed'
+    if verbose:
+        print(f'Bootstrapping {data_type} on {n_iterations} iterations')
+    data_views, data, dim, order = _gen_dataset(data, dim, n_iterations)
+    
+    #calculate estimates, returns lkhs, mags, times
+    inputs = zip(itertools.repeat(data), itertools.repeat(dim), data_views, itertools.repeat(order), 
+                itertools.repeat(init), 
+                itertools.repeat(positions), itertools.repeat(init.sfreq), np.arange(n_iterations),
+                itertools.repeat(tolerance), itertools.repeat(decimate), itertools.repeat(summarize), itertools.repeat(verbose),
+                itertools.repeat(plots),itertools.repeat(cpus), itertools.repeat(trace), itertools.repeat(path))
+    with mp.Pool(processes=cpus) as pool:
+            boot = list(tqdm(pool.imap(_boot_star, inputs), total=n_iterations))
+        
+    boot = xr.concat(boot, dim='iteration')
+    # if plots:
+    #     plot_topo_timecourse(boot.channels, boot.event_times, positions, init_boot, title='iteration = '+str(n_iter), skip_electodes_computation=True)
+    return boot
+
+def percent_event_occurence(iterations,  model_to_compare=None, count=False):
+    from scipy.spatial import distance_matrix
+    if model_to_compare is None:
+        max_n_bump_model_index = np.where(np.sum(np.isnan(iterations.magnitudes.values[:,:,0]), axis=(1)) == 0)[0][0]
+        max_mags = iterations.sel(iteration=max_n_bump_model_index).magnitudes.squeeze()
+        model_to_compare = iterations.sel(iteration=max_n_bump_model_index)
+    else:
+        max_mags = model_to_compare.magnitudes
+    all_diffs = []
+    all_n = np.zeros(len(iterations.iteration))
+    for iteration in iterations.iteration:
+        n_events_iter = int(np.sum(np.isfinite(iterations.sel(iteration=iteration).magnitudes.values[:,0])))
+        if iteration != max_n_bump_model_index:
+            diffs = distance_matrix(iterations.sel(iteration=iteration).magnitudes.squeeze(),
+                        max_mags)
+            index_event = diffs.argmin(axis=1)
+            index_event[n_events_iter:] = 9999
+
+            all_diffs.append(index_event)
+        all_n[iteration] = n_events_iter
+    labels, counts = np.unique(all_diffs, return_counts=True)
+    n_event_labels, n_event_count = np.unique(all_n, return_counts=True)
+    n_events_per_iter = [int(np.sum(np.isfinite(iterations.sel(iteration=i).magnitudes.values[:,0]))) for i in iterations.iteration]
+    if np.any(np.diff(n_events_per_iter) != 0):
+        labels, counts = labels[:-1], counts[:-1]
+    if not count:
+        labels, counts = labels, counts/iterations.iteration.max().values
+        n_event_count = n_event_count/iterations.iteration.max().values
+    return model_to_compare, labels, counts, n_event_count, n_event_labels
+
+def _boot_star(args): #for tqdm usage
+    return _bootstrapped_run(*args)
---
+++ packages/hsmm-mvpy/hsmm_mvpy-0.1.0b8.tar.gz/hsmm_mvpy-0.1.0b8/src/hsmm_mvpy/simulations.py
@@ -0,0 +1,298 @@
+'''
+
+'''
+
+import os.path as op
+import os
+import numpy as np
+import mne
+from mne.datasets import sample
+from warnings import warn
+
+
+def available_sources(subselection=True):
+    '''
+    list available sources for sample subject in MNE
+    '''
+    data_path = sample.data_path()
+    subjects_dir = op.join(data_path, 'subjects')
+    labels = mne.read_labels_from_annot('sample', subjects_dir=subjects_dir, verbose=False)
+    named_labels = []
+    for label in range(len(labels)):
+        named_labels.append(labels[label].name)
+    #Here we select sources with different topologies on HMP and relativ equal contribution on channels, not checked on MEG
+    named_labels = np.array(named_labels)
+    if subselection:
+        named_labels = named_labels[[0,1,3,5,7,15,17,22,30,33,37,40,42,46,50,54,57,59,65,67]]
+    return named_labels
+
+def simulation_sfreq():
+    data_path = sample.data_path()
+    subjects_dir = op.join(data_path, 'subjects')
+    evoked_fname = op.join(data_path, 'MEG', 'sample', 'sample_audvis-ave.fif')
+    info = mne.io.read_info(evoked_fname, verbose=False)
+    return info['sfreq']
+
+def simulation_positions():
+    data_path = sample.data_path()
+    subjects_dir = op.join(data_path, 'subjects')
+    subject = 'sample'
+    evoked_fname = op.join(data_path, 'MEG', subject, 'sample_audvis-ave.fif')
+    info = mne.io.read_info(evoked_fname, verbose=False)
+    positions = np.delete(mne.channels.layout._find_topomap_coords(info, 'eeg'),52,axis=0)#inferring channel location using MNE    
+    return positions
+
+def simulation_info():
+    data_path = sample.data_path()
+    subjects_dir = op.join(data_path, 'subjects')
+    subject = 'sample'
+    evoked_fname = op.join(data_path, 'MEG', subject, 'sample_audvis-ave.fif')
+    info = mne.io.read_info(evoked_fname, verbose=False)
+    
+    return info
+
+def event_shape(event_width, event_width_samples, steps):
+    '''
+    Computes the template of a half-sine (event) with given frequency f and sampling frequency
+    '''
+    event_idx = np.arange(event_width_samples)*steps+steps/2
+    event_frequency = 1000/(event_width*2)#gives event frequency given that events are defined as half-sines
+    template = np.sin(2*np.pi*event_idx/1000*event_frequency)#event morph based on a half sine with given event width and sampling frequency
+    template = template/np.sum(template**2)#Weight normalized
+    return template
+
+
+def simulate(sources, n_trials, n_jobs, file, data_type='eeg', n_subj=1, path='./', overwrite=False, verbose=False, noise=True, times=None, location=1, seed=None): 
+    '''
+    Simulates n_trials of EEG and/or MEG using MNE's tools based on the specified sources
+    
+    Parameters
+    ----------
+    sources : list
+        2D or 3D list with dimensions (n_subjects *) sources * source_parameters
+        Source parameters should contain :
+        - the name of the source (see the output of available_source())
+        - the duration of the event (in frequency, usually 10Hz)
+        - the amplitude or strength of the signal from the source, expressed in volt (e.g. 1e-8 V)
+        - the duration of the preceding stage as a scipy.stats distribution (e.g. scipy.stats.gamma(a, scale))
+    n_trials: int
+        Number of trials
+    n_jobs: int
+        Number of jobs to use with MNE's function (multithreading)
+    file: str
+        Name of the file to be saved (number of the subject will be added)
+    data_type: str
+        Whether to simulate "eeg" or "meg"
+    n_subj: int
+        How many subjects to simulate
+    path: str
+        path where to save the data
+    overwrite: bool
+        Whether to overwrite existing file
+    verbose: bool
+        Whether to display MNE's output
+    noise: bool
+        Adding noise to the simulated sources
+    times: ndarray
+        Deterministic simulation of event transitions times. Format is n_sources X n_trials
+    location: float
+        value in ms to add after a simulated event and before another one
+    
+    Returns
+    -------
+    generating_events: ndarray
+        Times of the simulated events used to test for accurate recovery compared to estimation
+    files: list
+        list of file names (file + number of subject)
+    '''
+    if seed is not None:
+        random_state = np.random.RandomState(seed)
+    else:
+        random_state = None
+    sources = np.array(sources, dtype=object)
+    if len(np.shape(sources)) == 2:
+        sources = [sources]#If only one subject
+    if np.shape(sources)[0] != n_subj:
+        raise ValueError('Number of subject is not coherent with the sources provided')
+    #Infer max duration of a trial from the specified sources
+    percentiles = np.empty(len(sources[0]))
+    for source in range(len(sources[0])):
+        if times is None:
+            stage_dur_fun = sources[0][source][-1]
+            percentiles[source] = np.percentile(stage_dur_fun.rvs(size=n_trials), q=99)
+        else:
+            percentiles[source] = np.max(times[:,source])
+    max_trial_length = np.sum(percentiles)+1000
+    # Following code and comments largely comes from MNE examples (e.g. \
+    # https://mne.tools/stable/auto_examples/simulation/simulated_raw_data_using_subject_anatomy.html)
+    # It loads the data, info structure and forward solution for one example subject,
+    # Note that all 'subject' will use this forward solution
+    data_path = sample.data_path()
+    subjects_dir = op.join(data_path, 'subjects')
+    subject = 'sample'
+    # First, we get an info structure from the test subject.
+    evoked_fname = op.join(data_path, 'MEG', subject, 'sample_audvis-ave.fif')
+    info = mne.io.read_info(evoked_fname, verbose=verbose)
+    if data_type == 'eeg':
+        picked_type = mne.pick_types(info, meg=False, eeg=True)
+    elif data_type == 'meg':
+        picked_type = mne.pick_types(info, meg=True, eeg=False)
+    elif data_type == 'eeg/meg':
+        picked_type = mne.pick_types(info, meg=True, eeg=False)
+    else:
+        raise ValueError(f'Invalid data type {data_type}, expected "eeg", "meg" or "eeg/meg"')
+    info = mne.pick_info(info, picked_type)
+    tstep = 1. / info['sfreq']
+    # To simulate sources, we also need a source space. It can be obtained from the
+    # forward solution of the sample subject.
+    fwd_fname = op.join(data_path, 'MEG', subject,'sample_audvis-meg-eeg-oct-6-fwd.fif')
+    fwd = mne.read_forward_solution(fwd_fname, verbose=verbose)
+    src = fwd['src']
+
+    
+    #For each subject, simulate associated sources
+    files = []
+    for subj in range(n_subj):
+        #Build simulator
+        files_subj = []
+        source_simulator = mne.simulation.SourceSimulator(src, tstep=tstep, first_samp=0, \
+                    duration=(2+1*n_trials+3)*max_trial_length*tstep)
+        if n_subj == 1: subj_file = file + f'_raw.fif'
+        else: subj_file = file + f'_{subj}_raw.fif'
+        if subj_file in os.listdir(path) and not overwrite:
+            subj_file = path+subj_file
+            warn(f'{subj_file} exists no new simulation performed', UserWarning)
+            files_subj.append(subj_file)
+            files_subj.append(subj_file.split('.fif')[0]+'_generating_events.npy')
+            files.append(files_subj)
+        else:
+            subj_file = path+subj_file
+            print(f'Simulating {subj_file}')
+            sources_subj = sources[subj]
+            # stim_onset occurs every x samples.
+            events = np.zeros((n_trials, 3), int)
+            stim_onsets =  2000+max_trial_length * np.arange(n_trials)#2000 = offset of first stim
+            events[:,0] = stim_onsets#last event 
+            events[:,2] = 1#trigger 1 = stimulus 
+
+            #Fake source, actually stimulus onset
+            selected_label = mne.read_labels_from_annot(
+                    subject, regexp=sources_subj[0][0], subjects_dir=subjects_dir, verbose=verbose)[0]
+            label = mne.label.select_sources(subject, selected_label, subjects_dir=subjects_dir, random_state=random_state)
+            source_time_series = np.array([1e-99])#stim trigger
+            source_simulator.add_data(label, source_time_series, events)
+            source_simulator.add_data(label, source_time_series, events)
+
+            trigger = 2
+            random_source_times = []
+            generating_events = events
+            for source in sources_subj:
+                selected_label = mne.read_labels_from_annot(
+                    subject, regexp=source[0], subjects_dir=subjects_dir, verbose=verbose)[0]
+                label = mne.label.select_sources(subject, selected_label, subjects_dir=subjects_dir, location=0, grow_outside=False, random_state=random_state)
+                #last two parameters ensure sources that are different enough
+                # Define the time course of the activity for each source of the region to
+                # activate
+                event_duration = int(((1/source[1])/2)*info['sfreq'])
+                source_time_series = event_shape(((1000/source[1])/2),event_duration,1000/info['sfreq'])*source[2]
+
+                #adding source event
+                events = events.copy()
+                if times is None:
+                    rand_i = np.round(source[-1].rvs(size=n_trials, random_state=random_state)/(tstep*1000),decimals=0)
+                else:
+                    rand_i = times[:,trigger-2]
+                if len(sources_subj)+1 > trigger > 2:
+                    rand_i += location/(tstep*1000)
+                else:
+                    rand_i += 1
+                if 0 in rand_i:
+                    warn("0 stage duration found, add a location parameter to avoid this case", UserWarning)
+                random_source_times.append(rand_i) #varying event 
+                events[:, 0] = events[:,0] + random_source_times[-1] # Events sample.
+                events[:, 2] = trigger  # All events have the sample id.
+                trigger += 1
+                generating_events = np.concatenate([generating_events, events])
+
+                #add these events
+                source_simulator.add_data(label, source_time_series, events)
+
+            generating_events = generating_events[generating_events[:, 0].argsort()]
+            # Project the source time series to sensor space and add some noise. The source
+            # simulator can be given directly to the simulate_raw function.
+            raw = mne.simulation.simulate_raw(info, source_simulator, forward=fwd, n_jobs=n_jobs,verbose=verbose)
+            if data_type == 'eeg':
+                raw = raw.pick_types(meg=False, eeg=True, stim=True)
+            elif data_type == 'meg':
+                raw = raw.pick_types(meg=True, eeg=False, stim=True)
+            elif data_type == 'eeg/meg':
+                raw = raw.pick_types(meg=True, eeg=True, stim=True)
+            if noise:
+                cov = mne.make_ad_hoc_cov(raw.info, verbose=verbose)
+                mne.simulation.add_noise(raw, cov, iir_filter=[0.2, -0.2, 0.04], verbose=verbose, random_state=random_state)
+            data = raw.get_data()
+            raw.save(subj_file, overwrite=True)
+            files_subj.append(subj_file)
+            np.save(subj_file.split('.fif')[0]+'_generating_events.npy', generating_events)
+            files_subj.append(subj_file.split('.fif')[0]+'_generating_events.npy')
+            files.append(files_subj)
+            print(f'{subj_file} simulated')
+    if n_subj == 1:
+        return files[0]
+    else:
+        return files
+    
+
+def demo(cpus, n_events, seed=667):#12
+    
+    ## Imports and code specific to the simulation (see tutorial 3 and 4 for real data)
+    from scipy.stats import gamma
+    import matplotlib.pyplot as plt 
+    from hsmm_mvpy.utils import read_mne_data
+    
+    random_gen =  np.random.default_rng(seed=seed)
+
+    ## Parameters for the simulations
+    frequency, amplitude = 10., .3e-6 #Frequency of the transition event and its amplitude in Volt
+    shape = 2#shape of the gamma distribution
+
+    #Storing electrode position, specific to the simulations
+    positions = simulation_info()#Electrode position
+    sfreq = simulation_sfreq()#sampling freqency of the simulated data
+    resample_freq = sfreq/np.round(sfreq / 100).astype(int)#Resampling at 100Hz to make processing faster and less demanding
+    all_source_names = available_sources()#all brain sources you can play with
+    n_trials = 50 #Number of trials to simulate
+    
+    # Randomly specify the transition events
+    name_sources = random_gen.choice(all_source_names,n_events+1, replace=False)#randomly pick source without replacement
+
+    times = random_gen.uniform(25,300,n_events+1)/shape#randomly pick average times in millisecond between the events
+
+    sources = []
+    for source in range(len(name_sources)):
+        sources.append([name_sources[source], frequency, amplitude, \
+                          gamma(shape, scale=times[source])])
+
+    file = 'dataset_tutorial2' #Name of the file to save
+
+    #Simulating and recover information on electrode location and true time onset of the simulated events
+    files = simulate(sources, n_trials, cpus,file, overwrite=True, location=25, seed=seed)
+    generating_events = np.load(files[1])
+    number_of_sources = len(np.unique(generating_events[:,2])[1:])#one trigger = one source
+    random_source_times = np.reshape(np.diff(generating_events[:,0], prepend=0),(n_trials,number_of_sources+1))[:,1:]/(sfreq/resample_freq)#By-trial generated event times
+
+    #Reading the necessary info to read the EEG data
+    resp_trigger = int(np.max(np.unique(generating_events[:,2])))#Resp trigger is the last source in each trial
+    event_id = {'stimulus':1}
+    resp_id = {'response':resp_trigger}
+    events = generating_events[(generating_events[:,2] == 1) | (generating_events[:,2] == resp_trigger)]#only retain stimulus and response triggers
+
+    # Reading the data
+    eeg_dat = read_mne_data(files[0], event_id, resp_id, sfreq=resample_freq, events_provided=events, verbose=False)
+    
+    all_other_chans = range(len(positions.ch_names[:-61]))#non-eeg
+    chan_list = list(np.arange(len(positions.ch_names)))
+    chan_list = [e for e in chan_list if e not in all_other_chans]
+    chan_list.pop(52)#Bad elec
+    positions = mne.pick_info(positions, sel=chan_list)
+    return eeg_dat, random_source_times, positions
