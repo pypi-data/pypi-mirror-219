9dc12d192bd590deb402d7828ffb63528f5be3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:21 +0000

    Add package siriuspy/2.78.0/siriuspy-2.78.0.tar.gz

---
+++ packages/siriuspy/siriuspy-2.78.0.tar.gz/siriuspy-2.78.0/siriuspy/devices/__init__.py
@@ -0,0 +1,44 @@
+"""Devices subpackage."""
+
+from .bbb import BunchbyBunch
+from .bpm import BPM, FamBPMs, BPMLogicalTrigger
+from .currinfo import CurrInfoTranspEff, CurrInfoLinear, \
+    CurrInfoBO, CurrInfoSI, CurrInfoAS
+from .dcct import DCCT
+from .device import Device, DeviceApp, Devices, DeviceNC
+from .egun import EGBias, EGFilament, EGHVPS, EGTriggerPS, EGPulsePS, EGun
+from .energy import Energy
+from .fofb import FOFBCtrlDCC, BPMDCC, FOFBCtrlRef, FamFOFBControllers, \
+    FamFastCorrs, HLFOFB
+from .ict import ICT, TranspEff
+from .ids import APU, WIG, PAPU, EPU
+from .idff import IDFF, WIGIDFF, PAPUIDFF, EPUIDFF, APUIDFF
+from .injctrl import InjCtrl
+from .injsys import PUMagsStandbyHandler, BOPSRampStandbyHandler, \
+    BORFRampStandbyHandler, InjSysStandbyHandler, LinacStandbyHandler, \
+    InjSysPUModeHandler
+from .lillrf import LILLRF, DevLILLRF
+from .machshift import MachShift
+from .modltr import LIModltr
+from .orbit_interlock import BPMOrbitIntlk, BaseOrbitIntlk, OrbitInterlock
+from .posang import PosAng
+from .psconv import PSProperty, StrengthConv
+from .pssofb import PSCorrSOFB, PSApplySOFB
+from .pwrsupply import PowerSupply, PowerSupplyPU, PowerSupplyFC, \
+    PowerSupplyFBP
+from .rf import RFGen, ASLLRF, BORFCavMonitor, SIRFCavMonitor, RFCav, \
+    RFKillBeam
+from .screen import Screen
+from .sofb import SOFB
+from .syncd import DevicesSync
+from .timing import EVG, Event, Trigger, HLTiming
+from .tune import TuneFrac, TuneProc, Tune, TuneCorr
+from .dvf import DVF, DVFImgProc
+from .lienergy import LIEnergy
+from .blctrl import BLPPSCtrl
+
+
+del device, bpm, dcct, egun, ict, lillrf, modltr
+del pwrsupply, posang, psconv, pssofb, rf, injsys, injctrl
+del screen, tune, sofb, timing, syncd, energy
+del ids, currinfo, bbb, machshift, dvf, lienergy, blctrl
