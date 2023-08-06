c7442a82e6b039c0cb8070ea4c41b318781ef410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:21 +0000

    Add package graphing-gd32/0.0.1/graphing-gd32-0.0.1.tar.gz

---
+++ packages/graphing-gd32/graphing-gd32-0.0.1.tar.gz/graphing-gd32-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+setup(name='graphing-gd32',
+      version='0.0.1',
+      description='Several no-fuss methods for creating plots with plotly by microsoft',
+      author='GeorgeDong32',
+      author_email='GeorgeDong32@outlook.com',
+      requires= ['numpy','typing','pandas','plotly',], # 定义依赖哪些模块
+      packages=find_packages(),  # 系统自动从当前目录开始找包
+      )
