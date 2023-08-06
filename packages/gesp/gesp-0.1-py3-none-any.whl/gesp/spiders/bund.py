d572f3cd06a30cdf4a69b6d3e8be016ac2e2b988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:27 +0000

    Add package gesp/0.1/gesp-0.1-py3-none-any.whl

---
+++ packages/gesp/gesp-0.1-py3-none-any.whl/gesp/spiders/bund.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import re
+import scrapy
+from ..pipelines.formatters import AZsPipeline, CourtsPipeline
+from ..pipelines.exporters import ExportAsHtmlPipeline, FingerprintExportPipeline, RawExporter
+
+class SpdrBund(scrapy.Spider):
+    name = "spider_bund"
+    start_urls = ["http://www.rechtsprechung-im-internet.de/jportal/docs/bsjrs/"]
+    custom_settings = {
+        "ITEM_PIPELINES": { 
+            AZsPipeline: 100,
+            CourtsPipeline: 200,
+            ExportAsHtmlPipeline: 300,
+            FingerprintExportPipeline: 400,
+            RawExporter: 900
+        }
+    }
+
+    def __init__(self, path, courts="", states="", fp=False, domains="", store_docId=False, postprocess=False, **kwargs):
+        self.path = path
+        self.courts = courts
+        self.states = states
+        self.fp = fp
+        self.domains = domains
+        self.store_docId = store_docId
+        self.postprocess = postprocess
+        if ("zivil" in domains and not any(court in courts for court in ["bgh", "bpatg", "bag"])):
+            courts.extend(["bgh", "bpatg", "bag"])
+        if ("oeff" in domains and not any(court in courts for court in ["bfh", "bsg", "bverfg", "bverwg"])):
+            courts.extend(["bfh", "bsg", "bverfg", "bverwg"])
+        if ("straf" in domains and not "bgh" in courts):
+            courts.append("bgh")
+        super().__init__(**kwargs)
+    
+    def parse(self, response):
+        for item in response.xpath("//item"):
+            link = item.xpath("link/text()").get()
+            y = {
+                "court": item.xpath("gericht/text()").get(),
+                "date": item.xpath("entsch-datum/text()").get(),
+                "az": item.xpath("aktenzeichen/text()").get(),
+                "link": link,
+                "docId": re.fullmatch(r'.+/jb\-([0-9A-Z]+)\.zip', link)[1],
+                "postprocess": self.postprocess
+            }
+            if self.courts:
+                for court in self.courts:
+                    if y["court"][0:len(court)].lower() == court:
+                        if court == "bgh" and self.domains:
+                            for domain in self.domains:
+                                if domain in y["court"].lower(): yield y
+                        else: yield y
+            else: yield y
---
+++ packages/gesp/gesp-0.1-py3-none-any.whl/gesp/src/create_file.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from fileinput import filename
+import os
+import requests
+from io import BytesIO
+from .output import output
+from zipfile import ZipFile
+
+def info(item):
+    if "link" in item:
+        output("downloading " + item["link"] + "...")
+    return item
+
+def save_as_html(item, spider_name, spider_path, store_docId): # spider.name, spider.path
+    info(item)
+    if (spider_name == "bund") or (spider_name == "by"): # Sonderfall Bund und Bayern: *.zip mit *.xml
+        filename = item["court"] + "_" + item["date"] + "_" + item["az"]
+        if store_docId and item.get('docId'):
+            filename += "_" + item['docId']
+        filename += ".xml"
+        try:
+            with ZipFile(BytesIO((requests.get(item["link"]).content))) as zip_ref: # Im RAM entpacken
+                for zipinfo in zip_ref.infolist(): # Teilweise auch Bilder in .zip enthalten
+                    if (zipinfo.filename.endswith(".xml") and ("manifest" not in zipinfo.filename)):
+                        zipinfo.filename = filename
+                        #bayportalrsp
+                        item["xmlfilename"] = os.path.join(spider_path, spider_name, zipinfo.filename)
+                        zip_ref.extract(zipinfo, os.path.join(spider_path, spider_name))
+        except:
+            output(f"could not create file {filename}", "err")
+        else:
+            return item
+    else:
+        if "text" in item and "court" in item and "date" in item and "az" in item and "filetype" in item:
+            filename = item["court"] + "_" + item["date"] + "_" + item["az"]
+            if store_docId and item.get('docId'):
+                filename += "_" + item['docId']
+            filename += "." + item["filetype"]
+            filepath = os.path.join(spider_path, spider_name, filename)
+            enc = "utf-8" if spider_name != "by" else "ascii"
+            try:
+                with open(filepath, "w", encoding=enc) as f:
+                    f.write(item["text"])
+            except:
+                output(f"could not create file {filepath}", "err")
+            else:
+                return item
+        else:
+            output("could not retrieve " + item["link"], "err")
+
+def save_as_pdf(item, spider_name, spider_path): # spider.name, spider.path
+    info(item)
+    content = ""
+    if "link" in item and not "body" in item: # Bremen / Sachsen (OVG)
+        try:
+            content = requests.get(url=item["link"]).content
+        except:
+            output("could not retrieve " + item["link"], "err")
+    elif "body" in item: # Sachsen (AG/LG/OLG)
+        content = item["body"]
+    if content and "court" in item and "date" in item and "az" in item:
+        filename = item["court"] + "_" + item["date"] + "_" + item["az"] + ".pdf"
+        filepath = os.path.join(spider_path, spider_name, filename)
+        try:
+            with open(filepath, "wb") as f:
+                f.write(content)
+        except:
+            output(f"could not create file {filepath}", "err")
+        else:
+            return item
+    else:
+        output("missing information " + item["link"], "err")
