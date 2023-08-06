cc70eeb45280357f56f3fa8349cdaa20ac5b7f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:17 +0000

    Add package pysdbots/2.1/pysdbots-2.1.tar.gz

---
+++ packages/pysdbots/pysdbots-2.1.tar.gz/pysdbots-2.1/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", encoding="utf8") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="pysdbots",
+    packages=setuptools.find_packages(),
+    version="2.1",
+    license="MIT",
+    description="A Project Made To Centralize Various APIs 📖 No Authorization Needed :)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="DamanthaJasinghe",
+    author_email="damanthaja@gmail.com",
+    url="https://github.com/Damantha126/pysdbots",
+    keywords=["sdbots", "python-pakage", "sd-api", "api", "damanthaja", "mritzme", "damantha126", "pysdbots", "jasinghe", "damantha-jasinghe"],
+    install_requires=["requests>=2.28.1"],
+    project_urls={
+        "Tracker": "https://github.com/Damantha126/pysdbots/issues",
+        "Community": "https://t.me/SDBOTs_inifinity",
+        "Source": "https://github.com/Damantha126/pysdbots",
+        "Documentation": "https://docs.sdbots.tech",
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
