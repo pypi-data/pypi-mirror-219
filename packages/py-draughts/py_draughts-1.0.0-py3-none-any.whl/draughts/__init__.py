3c8be48259e50c1668e71890948219855f402406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:05 +0000

    Add package py-draughts/1.0.0/py_draughts-1.0.0-py3-none-any.whl

---
+++ packages/py-draughts/py_draughts-1.0.0-py3-none-any.whl/draughts/__init__.py
@@ -0,0 +1,23 @@
+# This file is part of the py-draughts library.
+# Copyright (C) 2023-2023 Michał Skibiński
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+A draughts library for Python, with move generation and validation,
+PDN parsing and writing. Supprots multiple variants of game.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Michał Skibiński"
