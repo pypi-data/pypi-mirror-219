66bc7e319ad25e4fa66796eea19d257b2599965d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:05 +0000

    Add package dragonk8s/1.0.3/dragonk8s-1.0.3.tar.gz

---
+++ packages/dragonk8s/dragonk8s-1.0.3.tar.gz/dragonk8s-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='dragonk8s',
+    version='1.0.3',
+    description="A k8s implement of Python3",
+    author='ma',
+    author_email='591867837@qq.com',
+    url='https://dragonk8s.com',
+    packages=find_packages(),
+)
