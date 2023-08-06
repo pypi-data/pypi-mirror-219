c386eac04639736b5a25d61cffc3dde46fb9fffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:47 +0000

    Add package nonblocking-logging/0.1.0/nonblocking_logging-0.1.0-py3-none-any.whl

---
+++ packages/nonblocking-logging/nonblocking_logging-0.1.0-py3-none-any.whl/nonblocking_logging/handlers/telegram.py
@@ -0,0 +1,50 @@
+import logging
+from datetime import datetime
+
+import requests
+
+
+class PseudoFile:
+    def __init__(self, name, content):
+        self.name = name
+        self.content = content
+
+    def read(self):
+        return self.content
+
+
+class TelegramHandler(logging.Handler):
+    def __init__(self, bot_token, chat_id, level=logging.NOTSET):
+        super().__init__(level=level)
+        self._token = bot_token
+        self._chat_id = chat_id
+
+    def emit(self, record: logging.LogRecord):
+        if record.levelno == logging.ERROR:
+            self.send_error_message(record)
+        else:
+            self.send_notification_message(record)
+
+    def send_error_message(self, record: logging.LogRecord):
+        raise NotImplementedError
+
+    def send_notification_message(self, record: logging.LogRecord):
+        raise NotImplementedError
+
+    def _send_message(self, message):
+        requests.post(
+            f'https://api.telegram.org/bot{self._token}/sendMessage',
+            data={'chat_id': self._chat_id, 'text': message},
+        )
+
+    def _send_document(self, subject, message):
+        document_name = '%s.%s.html' % (
+            datetime.now().strftime('%Y-%m-%dT%H-%M'),
+            logging.getLevelName(self.level)
+        )
+        # TODO: handle 429, e.g. add rate limiter
+        requests.post(
+            f'https://api.telegram.org/bot{self._token}/sendDocument',
+            data={'chat_id': self._chat_id, 'caption': subject},
+            files={'document': PseudoFile(message, document_name)},
+        )
---
+++ packages/nonblocking-logging/nonblocking_logging-0.1.0-py3-none-any.whl/nonblocking_logging/integrations/django/apps.py
@@ -0,0 +1,13 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+from .startup import start_queue_listening
+
+
+class NonblockingLoggingConfig(AppConfig):
+    name = "nonblocking_logging.django"
+    verbose_name = _("Nonblocking Logging")
+    default_auto_field = "django.db.models.BigAutoField"
+
+    def ready(self):
+        start_queue_listening()
