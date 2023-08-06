96a5868f352c5b96e45dd5b277e81a7dc539a4b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:56 +0000

    Add package ork.build.tools/0.0.4/ork.build.tools-0.0.4.tar.gz

---
+++ packages/ork.build.tools/ork.build.tools-0.0.4.tar.gz/ork.build.tools-0.0.4/scripts/ork/make.py
@@ -0,0 +1,30 @@
+###############################################################################
+# Orkid Build System
+# Copyright 2010-2020, Michael T. Mayers
+# email: michael@tweakoz.com
+# The Orkid Build System is published under the GPL 2.0 license
+# see http://www.gnu.org/licenses/gpl-2.0.html
+###############################################################################
+
+from ork.command import Command
+import ork.host
+import ork.math
+
+def exec(target=None,parallelism=1.0):
+  """
+  Execute make with target and specific parallelism.
+   Assumes Makefile present in current directory.
+  Keyword Arguments:
+  target - makefile target (eg all, default, install, etc..)
+  parallelism - numjobs normalized to numcores/2 (0.0: numjobs=1, 1.0: numjobs=numcores/2)
+  """
+  cmd = ["make"]
+  print("make with parallel<%g>"%parallelism)
+  if parallelism!=0.0:
+    p = ork.math.clamp(parallelism,0.0,1.0)
+    numcores = int(ork.host.NumCores*p)
+    numcores = ork.math.clamp(numcores,1,ork.host.NumCores)
+    cmd += ["-j",numcores]
+  if target!=None:
+    cmd += [target]
+  return Command(cmd).exec()
