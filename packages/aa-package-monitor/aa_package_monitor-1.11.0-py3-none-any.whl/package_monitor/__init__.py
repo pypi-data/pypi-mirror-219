0db66ea1fdbbff2f4999951fb2469b1d9a038814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:19 +0000

    Add package aa-package-monitor/1.11.0/aa_package_monitor-1.11.0-py3-none-any.whl

---
+++ packages/aa-package-monitor/aa_package_monitor-1.11.0-py3-none-any.whl/package_monitor/__init__.py
@@ -0,0 +1,8 @@
+"""An app that helps keep track of installed packages
+and outstanding updates for Alliance Auth.
+"""
+
+default_app_config = "package_monitor.apps.PackageMonitorConfig"
+
+__version__ = "1.11.0"
+__title__ = "Package Monitor"
