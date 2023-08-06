9fb82319492c08aae15d14cdbe1a2ba592374503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:03 +0000

    Add package dock-py-selenium/1.0.3/dock-py-selenium-1.0.3.tar.gz

---
+++ packages/dock-py-selenium/dock-py-selenium-1.0.3.tar.gz/dock-py-selenium-1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="dock-py-selenium",
+    version="1.0.3",
+    author="Bishal Shrestha",
+    author_email="iambstha@gmail.com",
+    description="A startup / mockup folder for a selenium project with custom methods.",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/iambstha/dockSelenium",
+    packages=["dock_py_selenium.dock"],
+    install_requires=[
+        "selenium",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
