783ebf22f5ff90b3f6d7df16b9af0bb18e26d430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:25 +0000

    Add package toodaloo/0.0.3/toodaloo-0.0.3.tar.gz

---
+++ packages/toodaloo/toodaloo-0.0.3.tar.gz/toodaloo-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='toodaloo',
+    version='0.0.3',
+    author='Thibault Jaigu',
+    author_email='thibault.jaigu@gmail.com',
+    description='requesty Library',
+    py_modules=["toodaloo"],
+    package_dir={'': 'src'},
+    long_description='The best boefies library in the world',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.6',
+)
