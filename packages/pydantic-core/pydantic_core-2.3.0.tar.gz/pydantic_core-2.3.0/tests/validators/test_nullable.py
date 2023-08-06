447f1c8e4934c44712e7caa66af4722ba7d48ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:29 +0000

    Add package pydantic-core/2.3.0/pydantic_core-2.3.0.tar.gz

---
+++ packages/pydantic-core/pydantic_core-2.3.0.tar.gz/pydantic_core-2.3.0/tests/validators/test_nullable.py
@@ -0,0 +1,35 @@
+import pytest
+
+from pydantic_core import SchemaValidator, ValidationError
+
+
+def test_nullable():
+    v = SchemaValidator({'type': 'nullable', 'schema': {'type': 'int'}})
+    assert v.validate_python(None) is None
+    assert v.validate_python(1) == 1
+    assert v.validate_python('123') == 123
+    with pytest.raises(ValidationError) as exc_info:
+        v.validate_python('hello')
+    assert exc_info.value.errors(include_url=False) == [
+        {
+            'type': 'int_parsing',
+            'loc': (),
+            'msg': 'Input should be a valid integer, unable to parse string as an integer',
+            'input': 'hello',
+        }
+    ]
+
+
+def test_union_nullable_bool_int():
+    v = SchemaValidator(
+        {
+            'type': 'union',
+            'choices': [
+                {'type': 'nullable', 'schema': {'type': 'bool'}},
+                {'type': 'nullable', 'schema': {'type': 'int'}},
+            ],
+        }
+    )
+    assert v.validate_python(None) is None
+    assert v.validate_python(True) is True
+    assert v.validate_python(1) == 1
