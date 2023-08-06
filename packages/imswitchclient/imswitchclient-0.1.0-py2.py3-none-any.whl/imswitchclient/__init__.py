0e7a3bc8da09bff34551714c2d26a171bb8a6886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:53 +0000

    Add package imswitchclient/0.1.0/imswitchclient-0.1.0-py2.py3-none-any.whl

---
+++ packages/imswitchclient/imswitchclient-0.1.0-py2.py3-none-any.whl/imswitchclient/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for ImSwitchClient."""
+from imswitchclient import ImSwitchClient
+
+__author__ = """Benedict Diederich"""
+__email__ = 'benedictdied@gmail.com'
+__version__ = "v0.0.0"#'0.1.0'
