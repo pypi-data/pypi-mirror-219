30ccefdca915c632252bc7a079ffa0d5b39fc460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:27 +0000

    Add package connectors-to-databases/1.0.5/connectors_to_databases-1.0.5-py3-none-any.whl

---
+++ packages/connectors-to-databases/connectors_to_databases-1.0.5-py3-none-any.whl/connectors_to_databases/__init__.py
@@ -0,0 +1,10 @@
+"""
+:authors: k0rsakov
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2022 k0rsakov
+"""
+
+
+# __author__ = 'k0rsakov'
+# __version__ = '1.0.0'
+# __email__ = 'korsakov.iyu@gmail.com'
