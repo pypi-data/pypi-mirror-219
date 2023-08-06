f7753cdfbbab7869e2905785c3b172fde9c18bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:08 +0000

    Add package devchat/0.1.15/devchat-0.1.15.tar.gz

---
+++ packages/devchat/devchat-0.1.15.tar.gz/devchat-0.1.15/devchat/message.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+
+@dataclass
+class Message(ABC):
+    """
+    The basic unit of information in a prompt.
+    """
+    content: str = ""
+
+    INSTRUCT = "instruct"
+    CONTEXT = "context"
+    FUNCTION = "function"
+    CHAT = "chat"
+
+    @abstractmethod
+    def to_dict(self) -> dict:
+        """
+        Convert the message to a dictionary.
+        """
+
+    @abstractmethod
+    def stream_from_dict(self, message_data: dict) -> str:
+        """
+        Append to the message from a dictionary returned from a streaming chat API.
+        """
