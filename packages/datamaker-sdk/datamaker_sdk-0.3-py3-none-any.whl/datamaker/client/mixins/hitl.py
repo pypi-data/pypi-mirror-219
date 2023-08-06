e2b92c4abe048812aa36b431acde3976643e3c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:54 +0000

    Add package datamaker-sdk/0.3/datamaker_sdk-0.3-py3-none-any.whl

---
+++ packages/datamaker-sdk/datamaker_sdk-0.3-py3-none-any.whl/datamaker/client/mixins/hitl.py
@@ -0,0 +1,9 @@
+class HITLClientMixin:
+
+    def get_assignment(self, pk):
+        path = f'assignments/{pk}/'
+        return self._get(path)
+
+    def list_assignments(self, payload=None):
+        path = 'assignments/'
+        return self._list(path, payload)
