fcfbd9af179582174015166924e0a9d29ab2eed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:23 +0000

    Add package django-jet-new/2.3.3/django-jet-new-2.3.3.tar.gz

---
+++ packages/django-jet-new/django-jet-new-2.3.3.tar.gz/django-jet-new-2.3.3/setup.py
@@ -0,0 +1,66 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    path = os.path.join(os.path.dirname(__file__), fname)
+    try:
+        file = open(path, encoding='utf-8')
+    except TypeError:
+        file = open(path)
+    return file.read()
+
+
+def get_install_requires():
+    install_requires = ['Django']
+
+    try:
+        import importlib
+    except ImportError:
+        install_requires.append('importlib')
+
+    try:
+        from collections import OrderedDict
+    except ImportError:
+        install_requires.append('ordereddict')
+
+    return install_requires
+
+setup(
+    name='django-jet-new',
+    version=__import__('jet').VERSION,
+    description='Modern template for Django admin interface with improved functionality',
+    long_description=read('README.rst'),
+    author='Denis Kildishev',
+    author_email='support@jet.geex-arts.com',
+    url='https://github.com/geex-arts/django-jet',
+    packages=find_packages(),
+    license='AGPLv3',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django :: 4.2',
+        'License :: Free for non-commercial use',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Environment :: Web Environment',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: User Interfaces',
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=get_install_requires()
+)
