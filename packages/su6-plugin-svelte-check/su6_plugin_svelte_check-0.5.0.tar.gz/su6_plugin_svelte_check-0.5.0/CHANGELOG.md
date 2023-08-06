2805bc76572b4a884f52eeadf5a5a63c1716c802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:27 +0000

    Add package su6-plugin-svelte-check/0.5.0/su6_plugin_svelte_check-0.5.0.tar.gz

---
+++ packages/su6-plugin-svelte-check/su6_plugin_svelte_check-0.5.0.tar.gz/su6_plugin_svelte_check-0.5.0/CHANGELOG.md
@@ -0,0 +1,40 @@
+# Changelog
+
+<!--next-version-placeholder-->
+
+## v0.5.0 (2023-07-17)
+### Feature
+* Allow defining custom target ([`bcac570`](https://github.com/robinvandernoord/su6-plugin-svelte-check/commit/bcac57077b53d7c1b8a79d2100309b3448739fc3))
+
+## v0.4.0 (2023-07-17)
+### Feature
+* Add install-svelte-check command ([`84630d2`](https://github.com/robinvandernoord/su6-plugin-svelte-check/commit/84630d254fd1946d5789f50a427d29024f41b227))
+
+## v0.3.0 (2023-07-17)
+### Feature
+* Allow specifying a custom --node-modules directory which should have `svelte-check` ([`db1dc42`](https://github.com/robinvandernoord/su6-plugin-svelte-check/commit/db1dc42720e7f94020249b6859097b3cb541cda0))
+
+## v0.2.2 (2023-07-17)
+### Fix
+* Don't include node modules in git!! ([`360325c`](https://github.com/robinvandernoord/su6-plugin-svelte-check/commit/360325c02ed99fb34519941061e9b78679601d61))
+
+## v0.2.1 (2023-07-17)
+
+### Fix
+
+* Require the latest version of su6 ([`6300e97`](https://github.com/robinvandernoord/su6-plugin-svelte-check/commit/6300e974e732b9b5089490665c80b0d54d0071e9))
+
+## v0.2.0 (2023-07-17)
+
+### Feature
+
+* Add svelte-check to `su6 all` ([`b54e102`](https://github.com/robinvandernoord/su6-plugin-svelte-check/commit/b54e102cdfcbc4e8e1ec9f134cc9c559464ee1f7))
+
+## v0.1.0 (2023-07-15)
+### Feature
+* Initial version of plugin ([`7a171b8`](https://github.com/robinvandernoord/su6-plugin-svelte-check/commit/7a171b8327653ab366e13388471c342ab1c38873))
+
+## v0.1.0 (2023-06-05)
+### Feature
+
+* Initial version of the demo plugin ([`dc02818`](https://github.com/robinvandernoord/su6-plugin-demo/commit/dc02818b5d361469fa0ca480eee7394628faad89))
---
+++ packages/su6-plugin-svelte-check/su6_plugin_svelte_check-0.5.0.tar.gz/su6_plugin_svelte_check-0.5.0/tests/test_plugin.py
@@ -0,0 +1,83 @@
+import contextlib
+import os
+import shutil
+from pathlib import Path
+
+from su6 import app
+from typer.testing import CliRunner
+from plumbum import local
+
+try:
+    chdir = contextlib.chdir
+except AttributeError:
+    from contextlib_chdir import chdir
+
+runner = CliRunner(mix_stderr=False)
+
+
+def prepare_env():
+    local["npm"]("install", "svelte-check")
+
+
+def test_install():
+    # install_svelte_check
+
+    target_dir = Path("pytest_examples/node_modules")
+
+    if target_dir.exists():
+        shutil.rmtree(target_dir)
+
+    result = runner.invoke(app, ["install-svelte-check", "--target-dir", "./pytest_examples"])
+
+    assert not Path("node_modules").exists()
+    assert Path("pytest_examples/node_modules/svelte-check/bin/svelte-check").exists()
+
+    assert result.exit_code == 0
+
+    result = runner.invoke(app, ["install-svelte-check", "--target-dir", "./non/existent/folder"])
+
+    assert result.exit_code == 1
+
+
+def test_no_issues():
+    with chdir("./pytest_examples"):
+        prepare_env()
+
+        result = runner.invoke(app, ["svelte-check", "--target", "./no_issues"])
+        print(result.stdout, result.stderr)
+        assert result.exit_code == 0
+
+
+def test_issues():
+    with chdir("./pytest_examples"):
+        prepare_env()
+
+        result = runner.invoke(app, ["svelte-check", "--target", "./issues"])
+        print(result.stdout, result.stderr)
+        assert result.exit_code == 1
+
+
+def test_warnings():
+    with chdir("./pytest_examples"):
+        prepare_env()
+
+        result = runner.invoke(app, ["svelte-check", "--target", "./warnings"])
+        print(result.stdout, result.stderr)
+        assert result.exit_code == 0
+
+        result = runner.invoke(app, ["svelte-check", "--target", "./warnings", "--strict"])
+        print(result.stdout, result.stderr)
+        assert result.exit_code == 1
+
+
+def test_custom_tsconfig():
+    with chdir("./pytest_examples"):
+        prepare_env()
+
+        result = runner.invoke(app, ["svelte-check", "--target", ".", "--tsconfig", "custom_tsconfig.json", "--no-strict"])
+        print(result.stdout, result.stderr)
+        assert result.exit_code == 0
+
+        result = runner.invoke(app, ["svelte-check", "--tsconfig", "custom_tsconfig.json", "--strict"])
+        print(result.stdout, result.stderr)
+        assert result.exit_code == 1
