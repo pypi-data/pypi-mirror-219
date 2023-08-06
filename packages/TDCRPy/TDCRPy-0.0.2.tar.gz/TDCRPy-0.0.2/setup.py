63b688b904d2d0bda6283fa3a41b98fc7a43fc21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:07 +0000

    Add package TDCRPy/0.0.2/TDCRPy-0.0.2.tar.gz

---
+++ packages/TDCRPy/TDCRPy-0.0.2.tar.gz/TDCRPy-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = "0.0.2"
+DESCRIPTION = "TDCR model"
+
+setup(
+    name = "TDCRPy",
+    version = VERSION,
+    author = "RomainCoulon (Romain Coulon)",
+    author_email = "<romain.coulon@bipm.org>",
+    description = DESCRIPTION,
+    packages = find_packages(),
+    install_requires = ["numpy"],
+    keywords = ["python","TDCR","Monte-Carlo","radionuclide","scintillation","counting"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+		"License :: Freeware",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
