84f40810535c62bd6d318aed0dba43b8a5b1e731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:30 +0000

    Add package ravityemaillib/0.1.1.3/ravityemaillib-0.1.1.3.tar.gz

---
+++ packages/ravityemaillib/ravityemaillib-0.1.1.3.tar.gz/ravityemaillib-0.1.1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='ravityemaillib',
+    packages=find_packages(),
+    version='0.1.1.3',
+    description='Ravity Custom Libraries',
+    author='Me',
+    license='MIT',
+)
