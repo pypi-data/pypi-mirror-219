29b8d2badb44506642e87684523804accd8348ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:01 +0000

    Add package llm-base/0.0.2/llm_base-0.0.2-py3-none-any.whl

---
+++ packages/llm-base/llm_base-0.0.2-py3-none-any.whl/gpt/__init__.py
@@ -0,0 +1,17 @@
+from .openai.chat_completion import (
+    AssistantMessage,
+    ChatCompletion,
+    ChatCompletionRequest,
+    ChatMessage,
+    SystemMessage,
+    UserMessage,
+)
+
+__all__ = [
+    "ChatCompletion",
+    "ChatCompletionRequest",
+    "ChatMessage",
+    "UserMessage",
+    "AssistantMessage",
+    "SystemMessage",
+]
