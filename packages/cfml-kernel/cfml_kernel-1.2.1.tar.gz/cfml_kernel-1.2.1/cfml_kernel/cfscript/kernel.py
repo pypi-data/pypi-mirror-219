818a98876ad67b30d48deb750c0314348733d1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:08 +0000

    Add package cfml-kernel/1.2.1/cfml_kernel-1.2.1.tar.gz

---
+++ packages/cfml-kernel/cfml_kernel-1.2.1.tar.gz/cfml_kernel-1.2.1/cfml_kernel/cfscript/kernel.py
@@ -0,0 +1,43 @@
+from ipykernel.kernelbase import Kernel
+from cfml_kernel.box_wrapper import CommandBoxWrapper
+
+class CFScriptKernel(Kernel):
+    implementation = 'CFScript'
+    implementation_version = '1.0'
+    language = 'CFML'
+    language_version = '1.0'
+    language_info = {
+        'name': 'CFScript',
+        'mimetype': 'text/x-javascript',
+        'file_extension': '.cfm'
+    }
+    banner = "CFScript kernel"
+
+    def __init__(self, **kwargs):
+        Kernel.__init__(self, **kwargs)
+        self.box_wrapper = CommandBoxWrapper(repl_type="cfscript")
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None,
+                   allow_stdin=False):
+        if not silent:
+
+            responses = self.box_wrapper.do_execute(code)
+
+            for content in responses:
+                if isinstance( content, str ):
+                    stream_content = {'name': 'stdout', 'text': content}
+                    self.send_response(self.iopub_socket, 'stream', stream_content)
+                else:
+                    self.send_response(self.iopub_socket, 'display_data', content)
+
+        return {'status': 'ok',
+                # The base class increments the execution count
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=CFScriptKernel)
+    
