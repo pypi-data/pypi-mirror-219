6377072feb11266d43f9fb0374c0221fdba5c1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:59 +0000

    Add package tegridy/0.0.0/tegridy-0.0.0.tar.gz

---
+++ packages/tegridy/tegridy-0.0.0.tar.gz/tegridy-0.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# Third party modules
+from setuptools import setup
+
+setup(name='tegridy',
+      version='0.0.0',
+      description='placeholder',
+      url='http://github.com/SK/tegridy',
+      author='Stephen King',
+      author_email='sking.github@gmail.com',
+      license='MIT',
+      packages=['tegridy'],
+      zip_safe=False)
