477cdc4355e3b0b447f5fbb00c4dcda97275a9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:51 +0000

    Add package myeasygui/0.0.0.2/myeasygui-0.0.0.2.tar.gz

---
+++ packages/myeasygui/myeasygui-0.0.0.2.tar.gz/myeasygui-0.0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='myeasygui',
+    version='0.0.0.2',
+    packages=['myeasygui', 'myeasygui.boxes'],
+    url='',
+    license='MIT',
+    author='Yvan',
+    author_email='yvan_chen2022@163.com',
+    description=
+    """
+    this is a test project
+    you can use the boxes to do something
+    such as getting a password and user name to making a system
+    and so on.
+    this is a lot of guis in the boxes directory.
+    you can try to use they to make something.
+    """,
+)
