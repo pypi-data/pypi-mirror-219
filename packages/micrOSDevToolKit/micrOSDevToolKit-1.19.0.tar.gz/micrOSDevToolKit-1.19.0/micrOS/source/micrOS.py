97be236e7e42d32c5adb2c48af647d5c1fe93d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:06 +0000

    Add package micrOSDevToolKit/1.19.0/micrOSDevToolKit-1.19.0.tar.gz

---
+++ packages/micrOSDevToolKit/micrOSDevToolKit-1.19.0.tar.gz/micrOSDevToolKit-1.19.0/micrOS/source/micrOS.py
@@ -0,0 +1,89 @@
+"""
+Module is responsible for high level function invocations
+dedicated to micrOS framework.
+
+Designed by Marcell Ban aka BxNxM
+"""
+#################################################################
+#                           IMPORTS                             #
+#################################################################
+from TaskManager import Manager
+from SocketServer import SocketServer
+from Network import auto_network_configuration
+from Hooks import bootup_hook, profiling_info
+from InterruptHandler import enableInterrupt, enableCron
+from InterruptHandler import initEventIRQs
+from Debug import errlog_add
+from Time import ntp_time, suntime
+
+
+#################################################################
+#            INTERRUPT HANDLER INTERFACES / WRAPPERS            #
+#################################################################
+
+
+def safe_boot_hook():
+    try:
+        bootup_hook()
+    except Exception as e:
+        print(f"[micrOS main] Hooks.bootup_hook() error: {e}")
+        errlog_add(f"[ERR] safe_boot_hook error: {e}")
+
+
+def interrupt_handler():
+    try:
+        enableInterrupt()
+        enableCron()
+    except Exception as e:
+        print(f"[micrOS main] InterruptHandler.enableInterrupt/CronInterrupt error: {e}")
+        errlog_add(f"[ERR] interrupt_handler error: {e}")
+
+
+def external_interrupt_handler():
+    try:
+        initEventIRQs()
+    except Exception as e:
+        print(f"[micrOS main] InterruptHandler.initEventIRQs error: {e}")
+        errlog_add(f"[ERR] external_interrupt_handler error: {e}")
+
+
+#################################################################
+#                      MAIN FUNCTION CALLS                      #
+#################################################################
+
+
+def micrOS():
+    profiling_info(label='[memUsage] MAIN LOAD')
+
+    # CREATE ASYNC TASK MANAGER
+    aio = Manager()
+
+    # BOOT HOOK: Initial LM executions
+    safe_boot_hook()
+
+    # SET external interrupt with extirqcbf from nodeconfig
+    external_interrupt_handler()
+
+    # NETWORK setup
+    nwmd = auto_network_configuration()
+    if nwmd == 'STA':
+        # Set UTC + SUN TIMES FROM API ENDPOINTS
+        suntime()
+        # Set NTP - RTC + UTC shift + update uptime (boot time)
+        ntp_time()
+    else:
+        # AP mode - no ntp sync set uptime anyway
+        from Time import uptime
+        uptime(update=True)
+
+    # SET interrupt with timirqcbf from nodeconfig
+    interrupt_handler()
+    profiling_info(label='[memUsage] SYSTEM IS UP')
+
+    # [SocketServer] as async task
+    aio.create_task(SocketServer().run_server(), tag='server')
+    # [EVENT LOOP] Start async event loop
+    aio.run_forever()
+
+    # UNEXPECTED RESTART ???
+    errlog_add("[ERR] !!! Unexpected micrOS restart")
