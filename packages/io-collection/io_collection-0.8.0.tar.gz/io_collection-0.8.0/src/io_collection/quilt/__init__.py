b16b688b68e90dd509d02ab3fa3b33d0aa39d071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:47 +0000

    Add package io-collection/0.8.0/io_collection-0.8.0.tar.gz

---
+++ packages/io-collection/io_collection-0.8.0.tar.gz/io_collection-0.8.0/src/io_collection/quilt/__init__.py
@@ -0,0 +1,7 @@
+from prefect import task
+
+from .load_quilt_package import load_quilt_package
+from .save_quilt_item import save_quilt_item
+
+load_quilt_package = task(load_quilt_package)
+save_quilt_item = task(save_quilt_item)
