7ff7cb9317ac405df1c96e7a0d3f7d148a64128e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:10 +0000

    Add package LjxMathTest/1.0.1/LjxMathTest-1.0.1.tar.gz

---
+++ packages/LjxMathTest/LjxMathTest-1.0.1.tar.gz/LjxMathTest-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LjxMathTest",
+    version="1.0.1",
+    author="crazydone",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here (if any)
+    ],
+)
