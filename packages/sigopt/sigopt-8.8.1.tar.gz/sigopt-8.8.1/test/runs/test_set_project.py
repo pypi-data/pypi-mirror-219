a8d098f9eb4f8f95eecaf141ad02ae01f71ea9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:56 +0000

    Add package sigopt/8.8.1/sigopt-8.8.1.tar.gz

---
+++ packages/sigopt/sigopt-8.8.1.tar.gz/sigopt-8.8.1/test/runs/test_set_project.py
@@ -0,0 +1,22 @@
+# Copyright © 2022 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+import mock
+import pytest
+
+from sigopt import _global_factory, set_project
+
+
+def test_set_project_with_global_run():
+  with mock.patch("sigopt.get_run_id", mock.Mock(return_value="1")):
+    with pytest.warns(UserWarning):
+      set_project("test-123")
+  assert _global_factory.project == "test-123"
+
+
+def test_set_project_without_run():
+  with mock.patch("sigopt.get_run_id", mock.Mock(return_value=None)):
+    with pytest.warns(None) as warnings:
+      set_project("test-123")
+  assert len(warnings) == 0
+  assert _global_factory.project == "test-123"
