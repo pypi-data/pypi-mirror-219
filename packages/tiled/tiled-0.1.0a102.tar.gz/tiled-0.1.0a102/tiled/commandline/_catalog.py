265b8b827a5d5fe8da6436a552c2e49701c4fb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:05 +0000

    Add package tiled/0.1.0a102/tiled-0.1.0a102.tar.gz

---
+++ packages/tiled/tiled-0.1.0a102.tar.gz/tiled-0.1.0a102/tiled/commandline/_catalog.py
@@ -0,0 +1,75 @@
+import asyncio
+import re
+from pathlib import Path
+
+import typer
+
+from ._serve import serve_catalog
+
+catalog_app = typer.Typer()
+# Support both `tiled serve catalog` and `tiled catalog serve` as synonyms
+# because I cannot decide which is right.
+catalog_app.command("serve")(serve_catalog)
+DEFAULT_SQLITE_CATALOG_FILENAME = "catalog.db"
+
+
+@catalog_app.command("init")
+def init(
+    uri: str = typer.Argument(
+        Path.cwd() / DEFAULT_SQLITE_CATALOG_FILENAME, help="A filepath or database URI"
+    ),
+    if_not_exists: bool = typer.Option(
+        False,
+        help=(
+            "By default, it is an error if a database is already initialized."
+            "Set this flag to be permissive and return without an error."
+        ),
+    ),
+):
+    """
+    Initialize a database as a Tiled Catalog.
+
+    Examples:
+
+    # Using a simple local file as an embedded "database" (SQLite)
+    tiled init catalog.db
+    tiled init path/to/catalog.db
+    tiled init sqlite+aiosqlite:////path/to/catalog.db
+
+    # Using a client/serve database engine (PostgreSQL)
+    tiled init postgresql+asyncpg://uesrname:password@localhost/database_name:5432
+    """
+    from sqlalchemy.ext.asyncio import create_async_engine
+
+    from ..alembic_utils import UninitializedDatabase, check_database, stamp_head
+    from ..catalog.alembic_constants import ALEMBIC_DIR, ALEMBIC_INI_TEMPLATE_PATH
+    from ..catalog.core import ALL_REVISIONS, REQUIRED_REVISION, initialize_database
+
+    if not re.compile("^[a-z0-1+]+.*$").match(uri):
+        # Interpret URI as filepath.
+        uri = f"sqlite+aiosqlite:///{uri}"
+
+    async def do_setup():
+        engine = create_async_engine(uri)
+        redacted_url = engine.url._replace(password="[redacted]")
+        try:
+            await check_database(engine, REQUIRED_REVISION, ALL_REVISIONS)
+        except UninitializedDatabase:
+            # Create tables and stamp (alembic) revision.
+            typer.echo(
+                f"Database {redacted_url} is new. Creating tables.",
+                err=True,
+            )
+            await initialize_database(engine)
+            typer.echo("Database initialized.", err=True)
+        else:
+            if not if_not_exists:
+                typer.echo(
+                    f"Database at {redacted_url} is already initialized.", err=True
+                )
+                raise typer.Abort()
+        finally:
+            await engine.dispose()
+
+    asyncio.run(do_setup())
+    stamp_head(ALEMBIC_INI_TEMPLATE_PATH, ALEMBIC_DIR, uri)
