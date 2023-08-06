325cd60c64dd662fded868aef57d516ac945e9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:35 +0000

    Add package nectarchain/0.1.4/nectarchain-0.1.4.tar.gz

---
+++ packages/nectarchain/nectarchain-0.1.4.tar.gz/nectarchain-0.1.4/src/nectarchain/dqm/dqm_summary_processor.py
@@ -0,0 +1,104 @@
+import pickle
+
+import os
+import sys
+
+
+#from ctapipe.io import event_source
+import sys
+ 
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.stats import norm
+from traitlets.config.loader import Config 
+from astropy.io import fits
+from astropy.table import Table
+
+# ctapipe modules
+from ctapipe import utils
+from ctapipe.visualization import CameraDisplay
+#from ctapipe.plotting.camera import CameraPlotter
+from ctapipe.image.extractor import *
+from ctapipe.io import EventSeeker 
+from ctapipe.instrument import CameraGeometry
+from ctapipe.coordinates import EngineeringCameraFrame
+
+from ctapipe.io.hdf5tableio import HDF5TableWriter, HDF5TableReader
+
+from ctapipe.io import EventSource
+import ctapipe.instrument.camera.readout
+from ctapipe_io_nectarcam.constants import LOW_GAIN, HIGH_GAIN
+
+from astropy import time as astropytime
+
+class dqm_summary:
+    def __init__(self):
+        print("Processor 0")
+
+    def DefineForRun(self, reader1):
+        for i, evt1 in enumerate(reader1):
+            self.FirstReader = reader1
+            self.Samp = len(evt1.r0.tel[0].waveform[0][0])
+            self.Chan = len(evt1.r0.tel[0].waveform[0])
+
+        return self.Chan, self.Samp
+
+    def ConfigureForRun(self):
+        print("Processor 1")
+
+    def ProcessEvent(self, evt, noped):
+        print('Processor 2')
+
+    def FinishRun(self, M, M_ped, counter_evt, counter_ped):
+        print("Processor 3")
+
+    def GetResults(self):
+        print("Processor 4")
+
+    def PlotResults(
+        self, name, FigPath, k, M, M_ped, Mean_M_overChan, Mean_M_ped_overChan
+    ):
+        print("Processor 5")
+
+
+    def WriteAllResults(self,path, DICT):
+        data2 = Table()
+        data1 = Table()
+        data = Table()
+        hdulist = fits.HDUList()
+        for i, j in DICT.items():
+            if i == "Results_TriggerStatistics":
+                for n2, m2 in j.items():
+                    data2[n2] = m2
+                hdu2 = fits.BinTableHDU(data2)
+                hdu2.name = "Trigger"
+            
+
+            elif ((i == "Results_MeanWaveForms_HighGain") or (i == "Results_MeanWaveForms_LowGain")): 
+                for n1, m1 in j.items():
+                    data1[n1] = m1 
+                hdu1 = fits.BinTableHDU(data1)
+                hdu1.name = "MWF"
+
+
+            else:
+                for n, m in j.items():
+                    data[n] = m
+                hdu = fits.BinTableHDU(data)
+                hdu.name = "Camera"
+        try:          
+            hdulist.append(hdu2)
+        except:
+            print("No trigger statistics requests")
+        try:
+            hdulist.append(hdu1) 
+        except:
+            print("No MWF studies requests")
+        try:
+            hdulist.append(hdu)
+        except:
+            print("No Camera studies requests")
+        FileName = path + '_Results.fits'
+        print(FileName)
+        hdulist.writeto(FileName, overwrite=True)
+        return None
---
+++ packages/nectarchain/nectarchain-0.1.4.tar.gz/nectarchain-0.1.4/src/nectarchain/user_scripts/ggrolleron/test.py
@@ -0,0 +1,82 @@
+import numpy as np
+#import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from pathlib import Path
+import sys,os
+import logging
+logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s %(message)s',level=logging.INFO)
+log = logging.getLogger(__name__)
+##tips to add message to stdout
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
+
+
+from nectarchain.calibration.container import ChargeContainer,WaveformsContainer,WaveformsContainers,ChargeContainers
+from nectarchain.calibration.container.utils import DataManagement
+
+def test_multicontainers() :
+    run_number = [3731]
+    #waveforms = WaveformsContainers(run_number[0],max_events=20000)
+    #log.info("waveforms created")
+    #waveforms.load_wfs()
+    #log.info("waveforms loaded")
+    #
+    #waveforms.write(f"{os.environ['NECTARCAMDATA']}/waveforms/",overwrite = True)
+    #log.info('waveforms written')
+
+    #waveforms = WaveformsContainers.load(f"{os.environ['NECTARCAMDATA']}/waveforms/waveforms_run{run_number[0]}")
+    #log.info("waveforms loaded")
+
+    #charge = ChargeContainers.from_waveforms(waveforms,method = "LocalPeakWindowSum", window_width = 16, window_shift = 4)
+    #log.info("charge computed")
+    #
+    path = "LocalPeakWindowSum_4-12"
+    #charge.write(f"{os.environ['NECTARCAMDATA']}/charges/{path}/",overwrite = True)
+    #log.info("charge written")
+
+    charge = ChargeContainers.from_file(f"{os.environ['NECTARCAMDATA']}/charges/{path}/",run_number = run_number[0])
+    log.info("charge loaded")
+
+    charge_merged = charge.merge()
+    log.info('charge merged')
+
+
+
+
+
+
+"""
+def test_simplecontainer() :
+    run_number = [2633]
+    ped_run_number = [2630]
+    FF_run_number = [2609]
+
+    spe_run_1000V = WaveformsContainer(run_number[0],max_events = 1000)
+
+    spe_run_1000V.load_wfs()
+
+    spe_run_1000V.write(f"{os.environ['NECTARCAMDATA']}/waveforms/",overwrite = True)
+
+    spe_run_1000V.load(f"{os.environ['NECTARCAMDATA']}/waveforms/waveforms_run{run_number[0]}.fits")
+
+    charge = ChargeContainer.compute_charge(spe_run_1000V,1,method = "gradient_extractor")
+
+
+
+    charge = ChargeContainer.from_waveform(spe_run_1000V)
+
+
+    charge.write(f"{os.environ['NECTARCAMDATA']}/charges/std/",overwrite = True)
+
+"""
+
+if __name__ == "__main__" : 
+    test_multicontainers()
+
+    print("work completed")
