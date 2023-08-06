484eebcf5e96fba68dd99a2f35cda53ba6b2cf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:11 +0000

    Add package regulAS/2023.7/regulAS-2023.7.tar.gz

---
+++ packages/regulAS/regulAS-2023.7.tar.gz/regulAS-2023.7/setup.py
@@ -0,0 +1,38 @@
+import os
+
+import regulAS
+
+from setuptools import setup
+
+
+setup(
+    name='regulAS',
+    version=regulAS.__version__,
+    packages=['regulAS', 'regulAS.core', 'regulAS.utils', 'regulAS.reports', 'regulAS.persistence'],
+    package_data={'': [os.path.join('conf', '*.yaml')]},
+    url='https://github.com/slipnitskaya/regulAS',
+    author='Sofya Lipnitskaya',
+    author_email='lipnitskaya.sofya@gmail.com',
+    description='regulAS is a bioinformatics package designed to assist researchers in identifying prospective '
+                'regulators of alternative splicing events using large-scale RNA-Seq datasets based on multiple TCGA '
+                'and GTEx omics data sources. Discover the versatility of regulAS for your computational biology and '
+                'bioinformatics research.',
+    python_requires='>=3.7',
+    install_requires=[
+        'networkx==2.5',
+        'numpy==1.19.2',
+        'scipy==1.7.1',
+        'scikit-learn==0.24.2',
+        'pandas==1.2.2',
+        'matplotlib==3.1.3',
+        'omegaconf==2.1.0',
+        'hydra-core==1.1.0',
+        'hydra-colorlog==1.1.0',
+        'sqlalchemy==1.4.17',
+        'msgpack==1.0.2',
+        'msgpack-numpy==0.4.6.1',
+        'requests==2.26.0',
+        'pyensembl==1.9.1',
+        'tqdm==4.46.0'
+    ]
+)
