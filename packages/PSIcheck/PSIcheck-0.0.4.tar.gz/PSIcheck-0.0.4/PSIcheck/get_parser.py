ee35e418a3cca0e9544c09e03ef58679420c8052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:40 +0000

    Add package PSIcheck/0.0.4/PSIcheck-0.0.4.tar.gz

---
+++ packages/PSIcheck/PSIcheck-0.0.4.tar.gz/PSIcheck-0.0.4/PSIcheck/get_parser.py
@@ -0,0 +1,13 @@
+def get_parser():
+    import argparse
+    
+    parser = argparse.ArgumentParser(
+             prog="PSIcheck", 
+            )
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.4', help='show PSIcheck version number and exit')
+    parser.add_argument('PsiResultPath', type=str, help='directory stores PSI-Blast results')
+    parser.add_argument('-gbk','--GenbankFilesPath', type=str, help='directory stores Genbank files')
+
+    args = parser.parse_args()
+    return args
+
