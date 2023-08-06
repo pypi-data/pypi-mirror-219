0074145acf3bb0638046e3ce0929637cb31af23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:46 +0000

    Add package adafri/0.0.19/adafri-0.0.19.tar.gz

---
+++ packages/adafri/adafri-0.0.19.tar.gz/adafri-0.0.19/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.19' 
+DESCRIPTION = 'Adafri python module'
+LONG_DESCRIPTION = 'Adafri python module helper'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="adafri", 
+        version=VERSION,
+        author="Ibrahima Touré",
+        author_email="ibrahima.toure.dev@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
