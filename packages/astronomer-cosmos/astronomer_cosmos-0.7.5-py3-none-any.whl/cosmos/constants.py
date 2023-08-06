2ed7cd38678ac7d7a7cfaf001533c0c56dbf41ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:55 +0000

    Add package astronomer-cosmos/0.7.5/astronomer_cosmos-0.7.5-py3-none-any.whl

---
+++ packages/astronomer-cosmos/astronomer_cosmos-0.7.5-py3-none-any.whl/cosmos/constants.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+
+DBT_PROFILE_PATH = Path(os.path.expanduser("~")).joinpath(".dbt/profiles.yml")
+DEFAULT_DBT_PROFILE_NAME = "cosmos_profile"
+DEFAULT_DBT_TARGET_NAME = "cosmos_target"
