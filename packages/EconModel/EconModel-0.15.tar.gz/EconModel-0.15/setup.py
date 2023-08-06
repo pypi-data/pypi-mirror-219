943645fa712c941fe7a62a1c03e326515c446700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:50 +0000

    Add package EconModel/0.15/EconModel-0.15.tar.gz

---
+++ packages/EconModel/EconModel-0.15.tar.gz/EconModel-0.15/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='EconModel',
+    url='https://github.com/NumEconCopenhagen/EconModel',
+    download_url='https://github.com/NumEconCopenhagen/EconModel/archive/refs/tags/v0.15.tar.gz',
+    version='0.15',
+    author='Jeppe Druedahl',
+    packages=setuptools.find_packages(),
+)
