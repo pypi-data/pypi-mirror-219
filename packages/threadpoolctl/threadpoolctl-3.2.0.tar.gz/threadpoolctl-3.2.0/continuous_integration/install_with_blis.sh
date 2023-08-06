5d58f81065a8f82ffe7c353798fda4720c4486ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:36 +0000

    Add package threadpoolctl/3.2.0/threadpoolctl-3.2.0.tar.gz

---
+++ packages/threadpoolctl/threadpoolctl-3.2.0.tar.gz/threadpoolctl-3.2.0/continuous_integration/install_with_blis.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+pushd ..
+ABS_PATH=$(pwd)
+popd
+
+# Install a recent version of clang and libomp
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 10
+sudo apt-get install libomp-dev
+
+# create conda env
+conda create -n $VIRTUALENV -q --yes -c conda-forge python=$VERSION_PYTHON pip cython
+source activate $VIRTUALENV
+
+if [[ "$BLIS_CC" == "gcc-8" ]]; then
+    sudo apt install gcc-8
+fi
+
+pushd ..
+
+# build & install blis
+mkdir BLIS_install
+git clone https://github.com/flame/blis.git
+pushd blis
+
+./configure --prefix=$ABS_PATH/BLIS_install --enable-cblas --enable-threading=$BLIS_ENABLE_THREADING CC=$BLIS_CC auto
+make -j4
+make install
+popd
+
+# build & install numpy
+git clone https://github.com/numpy/numpy.git
+pushd numpy
+git submodule update --init
+echo "[blis]
+libraries = blis
+library_dirs = $ABS_PATH/BLIS_install/lib
+include_dirs = $ABS_PATH/BLIS_install/include/blis
+runtime_library_dirs = $ABS_PATH/BLIS_install/lib" > site.cfg
+python setup.py develop
+popd
+
+popd
+
+python -m pip install -q -r dev-requirements.txt
+CFLAGS=-I$ABS_PATH/BLIS_install/include/blis LDFLAGS=-L$ABS_PATH/BLIS_install/lib \
+    bash ./continuous_integration/build_test_ext.sh
+
+python --version
+python -c "import numpy; print(f'numpy {numpy.__version__}')" || echo "no numpy"
+python -c "import scipy; print(f'scipy {scipy.__version__}')" || echo "no scipy"
+
+python -m flit install --symlink
