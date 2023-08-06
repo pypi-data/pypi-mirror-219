0d92626f58f253efdc9a11018a9594f19e70b5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:02 +0000

    Add package bbot/1.1.0.1905rc0/bbot-1.1.0.1905rc0.tar.gz

---
+++ packages/bbot/bbot-1.1.0.1905rc0.tar.gz/bbot-1.1.0.1905rc0/bbot/modules/smuggler.py
@@ -0,0 +1,57 @@
+import sys
+
+from bbot.modules.base import BaseModule
+
+
+"""
+wrapper for https://github.com/defparam/smuggler.git
+"""
+
+
+class smuggler(BaseModule):
+    watched_events = ["URL"]
+    produced_events = ["FINDING"]
+    flags = ["active", "aggressive", "slow", "web-thorough"]
+    meta = {"description": "Check for HTTP smuggling"}
+
+    in_scope_only = True
+
+    deps_ansible = [
+        {
+            "name": "Get smuggler repo",
+            "git": {"repo": "https://github.com/defparam/smuggler.git", "dest": "#{BBOT_TOOLS}/smuggler"},
+        }
+    ]
+
+    async def setup(self):
+        self.scanned_hosts = set()
+        return True
+
+    async def handle_event(self, event):
+        host = f"{event.parsed.scheme}://{event.parsed.netloc}/"
+        host_hash = hash(host)
+        if host_hash in self.scanned_hosts:
+            self.debug(f"Host {host} was already scanned, exiting")
+            return
+        else:
+            self.scanned_hosts.add(host_hash)
+
+        command = [
+            sys.executable,
+            f"{self.scan.helpers.tools_dir}/smuggler/smuggler.py",
+            "--no-color",
+            "-q",
+            "-u",
+            event.data,
+        ]
+        async for line in self.helpers.run_live(command):
+            for f in line.split("\r"):
+                if "Issue Found" in f:
+                    technique = f.split(":")[0].rstrip()
+                    text = f.split(":")[1].split("-")[0].strip()
+                    description = f"[HTTP SMUGGLER] [{text}] Technique: {technique}"
+                    self.emit_event(
+                        {"host": str(event.host), "url": event.data, "description": description},
+                        "FINDING",
+                        source=event,
+                    )
