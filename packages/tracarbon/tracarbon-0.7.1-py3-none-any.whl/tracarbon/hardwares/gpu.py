6358da0b07e52f3971a713fc881a7fc4b1d306c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:02 +0000

    Add package tracarbon/0.7.1/tracarbon-0.7.1-py3-none-any.whl

---
+++ packages/tracarbon/tracarbon-0.7.1-py3-none-any.whl/tracarbon/hardwares/gpu.py
@@ -0,0 +1,55 @@
+import subprocess
+from abc import ABC
+from typing import Tuple
+
+from pydantic import BaseModel
+
+from tracarbon.exceptions import HardwareNoGPUDetectedException
+
+
+class GPUInfo(ABC, BaseModel):
+    """
+    GPU information.
+    """
+
+    @classmethod
+    def get_gpu_power_usage(cls) -> float:
+        """
+        Get the GPU power usage in watts.
+
+        :return: the gpu power usage in W
+        """
+        return NvidiaGPU.get_gpu_power_usage()
+
+
+class NvidiaGPU(GPUInfo):
+    """
+    Nvidia GPU information.
+    """
+
+    @classmethod
+    def launch_shell_command(cls) -> Tuple[bytes, int]:
+        """
+        Launch a shell command.
+
+        :return: result of the shell command and returncode
+        """
+        process = subprocess.Popen(
+            "nvidia-smi --query-gpu=power.draw --format=csv,noheader",
+            stdout=subprocess.PIPE,
+            shell=True,
+        )
+        stdout, _ = process.communicate()
+        return stdout, process.returncode
+
+    @classmethod
+    def get_gpu_power_usage(cls) -> float:
+        """
+        Get the GPU power usage in watts.
+
+        :return: the gpu power usage in W
+        """
+        gpu_utilization, return_code = cls.launch_shell_command()
+        if return_code == 0:
+            return float(gpu_utilization.split()[0])
+        raise HardwareNoGPUDetectedException(f"No Nvidia GPU detected.")
