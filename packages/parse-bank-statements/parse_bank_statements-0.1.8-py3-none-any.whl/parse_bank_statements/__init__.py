b35d7cc80482e51307d9aead243875a8cb478abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:19 +0000

    Add package parse-bank-statements/0.1.8/parse_bank_statements-0.1.8-py3-none-any.whl

---
+++ packages/parse-bank-statements/parse_bank_statements-0.1.8-py3-none-any.whl/parse_bank_statements/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""Parse bank statements
+"""
+
+import os, logging
+
+__author__  = 'Giovanni Tardini'
+__version__ = '0.1.8'
+__date__    = '13.07.2023'
+
+
+fmt = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s: %(message)s', '%Y-%m-%d %H:%M:%S')
+hnd = logging.StreamHandler()
+hnd.setFormatter(fmt)
+hnd.setLevel(level=logging.INFO)
+logger = logging.getLogger('PBS')
+logger.addHandler(hnd)
+logger.setLevel(logging.INFO)
+logger.propagate = False
+
+pbs_home = os.path.dirname(os.path.realpath(__file__))
+
+logger.info('Using version %s', __version__)
+logger.info('PBS home %s', pbs_home)
+
+from .pbs_gui import *
+
+import encodings.utf_8
