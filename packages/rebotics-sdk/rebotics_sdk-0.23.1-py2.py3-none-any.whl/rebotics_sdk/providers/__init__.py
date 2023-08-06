f12222476f4ad79a86da16e2fbc391ff0770a183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:15 +0000

    Add package rebotics-sdk/0.23.1/rebotics_sdk-0.23.1-py2.py3-none-any.whl

---
+++ packages/rebotics-sdk/rebotics_sdk-0.23.1-py2.py3-none-any.whl/rebotics_sdk/providers/__init__.py
@@ -0,0 +1,7 @@
+from .base import *  # NOQA
+from .admin import AdminProvider
+from .dataset import DatasetProvider
+from .retailer import RetailerProvider
+from .fvm import FVMProvider
+from .hawkeye import HawkeyeProvider
+from .cvat import CvatProvider
