b232440a0cfd1a54474d1e272692fae57ba85428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:10 +0000

    Add package NEMO-CE/1.5.9/NEMO_CE-1.5.9-py3-none-any.whl

---
+++ packages/NEMO-CE/NEMO_CE-1.5.9-py3-none-any.whl/NEMO/views/constants.py
@@ -0,0 +1,7 @@
+# Maximum length for additional information of reservations
+ADDITIONAL_INFORMATION_MAXIMUM_LENGTH = 3000
+
+# Maximum length for feedback emails
+FEEDBACK_MAXIMUM_LENGTH = 5000
+
+SYSTEM_USER_DISPLAY = "System"
