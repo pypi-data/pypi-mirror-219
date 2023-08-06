cb1907087cc96c5e4b1c50946095a1a37518c844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:43 +0000

    Add package pympipool/0.5.3/pympipool-0.5.3-py3-none-any.whl

---
+++ packages/pympipool/pympipool-0.5.3-py3-none-any.whl/pympipool/share/communication.py
@@ -0,0 +1,99 @@
+import subprocess
+
+import cloudpickle
+import zmq
+
+
+class SocketInterface(object):
+    def __init__(self, queue_adapter=None, queue_adapter_kwargs=None):
+        self._context = zmq.Context()
+        self._socket = self._context.socket(zmq.PAIR)
+        self._process = None
+        self._queue_adapter = queue_adapter
+        self._queue_adapter_kwargs = queue_adapter_kwargs
+
+    def send_dict(self, input_dict):
+        self._socket.send(cloudpickle.dumps(input_dict))
+
+    def receive_dict(self):
+        output = cloudpickle.loads(self._socket.recv())
+        if "result" in output.keys():
+            return output["result"]
+        else:
+            error_type = output["error_type"].split("'")[1]
+            raise eval(error_type)(output["error"])
+
+    def send_and_receive_dict(self, input_dict):
+        self.send_dict(input_dict=input_dict)
+        return self.receive_dict()
+
+    def bind_to_random_port(self):
+        return self._socket.bind_to_random_port("tcp://*")
+
+    def bootup(self, command_lst, cwd=None, cores=None):
+        if self._queue_adapter is not None:
+            self._queue_adapter.submit_job(
+                working_directory=cwd,
+                cores=cores,
+                command=" ".join(command_lst),
+                **self._queue_adapter_kwargs
+            )
+        else:
+            self._process = subprocess.Popen(
+                args=command_lst,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                stdin=subprocess.PIPE,
+                cwd=cwd,
+            )
+
+    def shutdown(self, wait=True):
+        result = None
+        if self._process is not None and self._process.poll() is None:
+            result = self.send_and_receive_dict(
+                input_dict={"shutdown": True, "wait": wait}
+            )
+            self._process_close(wait=wait)
+        elif self._queue_adapter is not None and self._socket is not None:
+            result = self.send_and_receive_dict(
+                input_dict={"shutdown": True, "wait": wait}
+            )
+        if self._socket is not None:
+            self._socket.close()
+        if self._context is not None:
+            self._context.term()
+        self._process = None
+        self._socket = None
+        self._context = None
+        return result
+
+    def _process_close(self, wait=True):
+        self._process.terminate()
+        self._process.stdout.close()
+        self._process.stdin.close()
+        self._process.stderr.close()
+        if wait:
+            self._process.wait()
+
+    def __del__(self):
+        self.shutdown(wait=True)
+
+
+def connect_to_socket_interface(host, port):
+    context = zmq.Context()
+    socket = context.socket(zmq.PAIR)
+    socket.connect("tcp://" + host + ":" + port)
+    return context, socket
+
+
+def send_result(socket, result_dict):
+    socket.send(cloudpickle.dumps(result_dict))
+
+
+def receive_instruction(socket):
+    return cloudpickle.loads(socket.recv())
+
+
+def close_connection(socket, context):
+    socket.close()
+    context.term()
