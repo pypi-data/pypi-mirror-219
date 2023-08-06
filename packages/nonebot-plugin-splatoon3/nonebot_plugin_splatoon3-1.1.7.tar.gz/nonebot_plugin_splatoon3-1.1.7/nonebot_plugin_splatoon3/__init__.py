1b340412b50d6bc40d751f23c2948fd8bbe990fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:12 +0000

    Add package nonebot-plugin-splatoon3/1.1.7/nonebot_plugin_splatoon3-1.1.7.tar.gz

---
+++ packages/nonebot-plugin-splatoon3/nonebot_plugin_splatoon3-1.1.7.tar.gz/nonebot_plugin_splatoon3-1.1.7/nonebot_plugin_splatoon3/__init__.py
@@ -0,0 +1,17 @@
+from nonebot.log import logger
+
+try:
+    from .onebot_matcher import *
+except ImportError as e:
+    logger.error(
+        "No OneBot adapter found, you can try 'nb adapter install nonebot-adapter-onebot' to install. \n"
+        "If not, you can still use the plugin, but it will not respond to OneBot messages."
+    )
+
+try:
+    from .telegram_matcher import *
+except ImportError as e:
+    logger.error(
+        "No Telegram adapter found, you can try 'nb adapter install nonebot-adapter-telegram' to install. \n"
+        "If not, you can still use the plugin, but it will not respond to Telegram messages."
+    )
