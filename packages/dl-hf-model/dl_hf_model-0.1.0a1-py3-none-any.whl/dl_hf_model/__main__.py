ae9578af21f6c7c6a97965d47ba3f26ea70337db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:45 +0000

    Add package dl-hf-model/0.1.0a1/dl_hf_model-0.1.0a1-py3-none-any.whl

---
+++ packages/dl-hf-model/dl_hf_model-0.1.0a1-py3-none-any.whl/dl_hf_model/__main__.py
@@ -0,0 +1,45 @@
+"""Prep __main__.py."""
+# pylint: disable=invalid-name
+from pathlib import Path
+from typing import Optional
+
+import logzero
+import typer
+from logzero import logger
+from set_loglevel import set_loglevel
+
+from dl_hf_model import __version__, dl_hf_model
+
+del Path, logger, dl_hf_model
+logzero.loglevel(set_loglevel())
+
+app = typer.Typer(
+    name="dl_hf_model",
+    add_completion=False,
+    help="dl_hf_model help",
+)
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{app.info.name} v.{__version__} -- ...")
+        raise typer.Exit()
+
+
+@app.command()
+def main(
+    version: Optional[bool] = typer.Option(  # pylint: disable=(unused-argument
+        None,
+        "--version",
+        "-v",
+        "-V",
+        help="Show version info and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    ),
+):
+    """Define."""
+
+
+if __name__ == "__main__":
+    app()
