17e01a7fab877e8a047ef8079ccc418100f1d9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:47 +0000

    Add package jaime38130/0.4/jaime38130-0.4.tar.gz

---
+++ packages/jaime38130/jaime38130-0.4.tar.gz/jaime38130-0.4/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'jaime38130',
+  packages = ['jaime38130'],
+  version = '0.4',
+  license='-',
+  description = 'Colors!',
+  author = 'Jaime Silva',
+  author_email = 'jaimedcsilva@hotmail.com',
+  url = 'https://github.com/JaimeSilva/jaime38130.git',
+  download_url = 'https://github.com/JaimeSilva/jaime38130/archive/refs/tags/v_04.tar.gz',
+  keywords = ['SOME', 'MEANINGFULL', 'COLORS'],
+  install_requires=[
+          'colorama',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
