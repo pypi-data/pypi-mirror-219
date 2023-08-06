3f8cb74b9c71b0a56070d347c8f782c7ee44396d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:58 +0000

    Add package wiggler-studiorabota/0.0.3/wiggler_studiorabota-0.0.3-py3-none-any.whl

---
+++ packages/wiggler-studiorabota/wiggler_studiorabota-0.0.3-py3-none-any.whl/wiggler/main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from datetime import datetime
+import os
+
+app = FastAPI()
+
+app.mount("/image", StaticFiles(directory="./images"), name="image")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Worm"}
+
+@app.get("/images")
+def images():
+    out = []
+    for filename in sorted(os.listdir("./images")):
+        name, ext = os.path.splitext(filename)
+        if ext == '.jpg':
+            out.append({
+                "name": name,
+                "path": "/image/" + filename
+            })
+    return out
+
+@app.get("/picture")
+def images():
+    now = datetime.now()
+    filename = now.strftime("%Y-%m-%d-%H-%M")
+    path = f"./images/{filename}.jpg"
+    os.system(f"libcamera-jpeg --width 1024 --height 768 --nopreview -t 1 -o {path}")
+    return {"picture": path}
