c0c1b21e89eb4eade10e2c6e33a6170fde306b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:05 +0000

    Add package python2verilog/0.0.2/python2verilog-0.0.2.tar.gz

---
+++ packages/python2verilog/python2verilog-0.0.2.tar.gz/python2verilog-0.0.2/python2verilog/__init__.py
@@ -0,0 +1,7 @@
+"""Python 2 Verilog"""
+
+# flake8: noqa
+from . import frontend
+from . import backend
+from . import optimizer
+from . import utils
---
+++ packages/python2verilog/python2verilog-0.0.2.tar.gz/python2verilog-0.0.2/python2verilog/backend/__init__.py
@@ -0,0 +1,16 @@
+"""Backend"""
+
+# flake8: noqa
+from .statements import (
+    NonBlockingSubsitution,
+    BlockingSubsitution,
+    Subsitution,
+    Declaration,
+    Case,
+    CaseItem,
+    IfElse,
+    While,
+    Statement,
+)
+
+from .expressions import Expression
