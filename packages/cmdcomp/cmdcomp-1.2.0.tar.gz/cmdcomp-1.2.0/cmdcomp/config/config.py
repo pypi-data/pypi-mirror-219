24d635cbc4e354c95df6041b9d1386a0e8dbbeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:03 +0000

    Add package cmdcomp/1.2.0/cmdcomp-1.2.0.tar.gz

---
+++ packages/cmdcomp/cmdcomp-1.2.0.tar.gz/cmdcomp-1.2.0/cmdcomp/config/config.py
@@ -0,0 +1,37 @@
+import json
+import os
+import tomllib
+from typing import BinaryIO
+
+import yaml
+from pydantic import ConfigDict
+
+from cmdcomp.config.app_info import AppInfo
+from cmdcomp.config.cmdcomp_info import CmdCompInfo
+from cmdcomp.config.command.command import Command
+from cmdcomp.config.model import Model
+
+
+class Config(Model):
+    """cmdcomp config."""
+
+    model_config = ConfigDict(arbitrary_types_allowed=True, extra="forbid")
+
+    cmdcomp: CmdCompInfo
+    app: AppInfo
+    root: Command
+
+
+def load(file: BinaryIO) -> Config:
+    _, extention = os.path.splitext(file.name)
+    match extention:
+        case ".json":
+            data = json.load(file)
+        case ".yaml" | ".yml":
+            data = yaml.full_load(file)
+        case ".toml":
+            data = tomllib.load(file)
+        case _:
+            raise Exception(f"Unsupported config file type `{extention}`.")
+
+    return Config.model_validate(data)
