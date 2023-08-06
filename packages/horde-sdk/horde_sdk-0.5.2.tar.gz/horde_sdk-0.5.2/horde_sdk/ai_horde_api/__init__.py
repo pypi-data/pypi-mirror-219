3557416d0ad80e9ba8fd196f0b55cb9c3473cb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:24 +0000

    Add package horde-sdk/0.5.2/horde_sdk-0.5.2.tar.gz

---
+++ packages/horde-sdk/horde_sdk-0.5.2.tar.gz/horde_sdk-0.5.2/horde_sdk/ai_horde_api/__init__.py
@@ -0,0 +1,25 @@
+from horde_sdk.ai_horde_api.ai_horde_clients import (
+    AIHordeAPIManualClient,
+)
+from horde_sdk.ai_horde_api.consts import (
+    ALCHEMY_FORMS,
+    GENERATION_STATE,
+    KNOWN_SAMPLERS,
+    KNOWN_SOURCE_PROCESSING,
+    WORKER_TYPE,
+)
+from horde_sdk.ai_horde_api.endpoints import (
+    AI_HORDE_BASE_URL,
+    AI_HORDE_API_URL_Literals,
+)
+
+__all__ = [
+    "AIHordeAPIManualClient",
+    "AI_HORDE_BASE_URL",
+    "AI_HORDE_API_URL_Literals",
+    "ALCHEMY_FORMS",
+    "GENERATION_STATE",
+    "KNOWN_SAMPLERS",
+    "KNOWN_SOURCE_PROCESSING",
+    "WORKER_TYPE",
+]
---
+++ packages/horde-sdk/horde_sdk-0.5.2.tar.gz/horde_sdk-0.5.2/tests/ai_horde_api/test_swagger.py
@@ -0,0 +1,45 @@
+from horde_sdk.ai_horde_api.endpoints import get_ai_horde_swagger_url
+from horde_sdk.generic_api.utils.swagger import SwaggerDoc, SwaggerParser
+
+
+def test_swagger_parser_init() -> None:
+    SwaggerParser(swagger_doc_url=get_ai_horde_swagger_url())
+
+
+def test_get_swagger_doc() -> None:
+    parser = SwaggerParser(swagger_doc_url=get_ai_horde_swagger_url())
+    doc = parser.get_swagger_doc()
+    assert isinstance(doc, SwaggerDoc)
+    assert doc.swagger == "2.0"
+    assert doc.basePath == "/api"
+    assert doc.info.title == "AI Horde"
+
+    assert doc.produces
+    assert "application/json" in doc.produces
+    assert doc.consumes
+    assert "application/json" in doc.consumes
+
+    assert doc.paths
+    assert len(doc.paths) > 0
+    assert "/v2/generate/async" in doc.paths
+
+    assert doc.definitions
+    assert len(doc.definitions) > 0
+    assert "ModelGenerationInputStable" in doc.definitions
+
+    assert doc.responses
+    assert len(doc.responses) > 0
+
+
+def test_extract_all_payload_examples() -> None:
+    swagger_doc = SwaggerParser(swagger_doc_url=get_ai_horde_swagger_url()).get_swagger_doc()
+
+    all_request_examples = swagger_doc.get_all_payload_examples()
+    assert len(all_request_examples) > 0, "Failed to extract any examples from the swagger doc"
+
+
+def test_extract_all_response_examples() -> None:
+    swagger_doc = SwaggerParser(swagger_doc_url=get_ai_horde_swagger_url()).get_swagger_doc()
+
+    all_response_examples = swagger_doc.get_all_response_examples()
+    assert len(all_response_examples) > 0, "Failed to extract any examples from the swagger doc"
