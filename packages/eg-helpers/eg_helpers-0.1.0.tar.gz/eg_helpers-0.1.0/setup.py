97a9e395f06c284889e377938ebf83af720a499e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:38 +0000

    Add package eg-helpers/0.1.0/eg_helpers-0.1.0.tar.gz

---
+++ packages/eg-helpers/eg_helpers-0.1.0.tar.gz/eg_helpers-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='eg_helpers',
+    version='0.1.0',    
+    description='Just some helper functions I use in my projects',
+    url='https://github.com/Talla/eg_helpers',  
+    author='Eduard Germis',
+    author_email='edugermis@gmail.com',
+    license='MIT',
+    packages=['eg_helpers'],
+    install_requires=[
+        'pandas>=2.0.1',
+        'pytz>=2023.3',
+        'regex>=2022.10.31',
+        'tiktoken>=0.4.0'
+    ],
+)
