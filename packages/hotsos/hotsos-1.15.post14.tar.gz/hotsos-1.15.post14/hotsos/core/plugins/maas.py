3e4f17b499fa178a3792d89a7ad0a72b4f2fa346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:01 +0000

    Add package hotsos/1.15.post14/hotsos-1.15.post14.tar.gz

---
+++ packages/hotsos/hotsos-1.15.post14.tar.gz/hotsos-1.15.post14/hotsos/core/plugins/maas.py
@@ -0,0 +1,34 @@
+from hotsos.core.host_helpers import (
+    APTPackageHelper,
+    SnapPackageHelper,
+    SystemdHelper,
+)
+from hotsos.core.plugintools import PluginPartBase
+from hotsos.core.utils import cached_property
+
+CORE_APT = ['maas', 'postgres']
+APT_DEPS = ['isc-dhcp', 'bind9']
+CORE_SNAPS = CORE_APT
+
+SERVICE_EXPRS = [s + '[A-Za-z0-9-]*' for s in CORE_APT + APT_DEPS]
+
+
+class MAASChecksBase(PluginPartBase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.snaps = SnapPackageHelper(core_snaps=CORE_SNAPS)
+        self.apt = APTPackageHelper(core_pkgs=CORE_APT, other_pkgs=APT_DEPS)
+        self.systemd = SystemdHelper(service_exprs=SERVICE_EXPRS,
+                                     ps_allow_relative=False)
+
+    @cached_property
+    def maas_installed(self):
+        if self.apt.core or self.snaps.core:
+            return True
+
+        return False
+
+    @property
+    def plugin_runnable(self):
+        return self.maas_installed
