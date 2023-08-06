55824fcae89e86b0d15292cecc659c25f95f7696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:48 +0000

    Add package arctix/0.0.0a13/arctix-0.0.0a13-py3-none-any.whl

---
+++ packages/arctix/arctix-0.0.0a13-py3-none-any.whl/arctix/__init__.py
@@ -0,0 +1,17 @@
+__all__ = [
+    "BaseFormatter",
+    "BaseSummarizer",
+    "Summarizer",
+    "set_summarizer_options",
+    "summarizer_options",
+    "summary",
+]
+
+from arctix.formatter import BaseFormatter
+from arctix.summarizer import (
+    BaseSummarizer,
+    Summarizer,
+    set_summarizer_options,
+    summarizer_options,
+    summary,
+)
