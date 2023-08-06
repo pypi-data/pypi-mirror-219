89fed45b4c798abd1b20550230b3f190c8b7f1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:14 +0000

    Add package protomodel/0.0.8/protomodel-0.0.8-py3-none-any.whl

---
+++ packages/protomodel/protomodel-0.0.8-py3-none-any.whl/protomodel/example/data.py
@@ -0,0 +1,30 @@
+from protomodel import message, service, rpc
+
+@message
+class Product:
+    id: int
+    name: str
+    price: float
+    brand_id: int
+    category_id: int
+    enabled: bool
+
+
+@message
+class GetProductRequest:
+    product_id: int
+
+@message
+class GetProductResponse:
+    product: Product
+
+@message
+class GetProductsResponse:
+    products: list[Product]
+
+@service
+class ProductService:
+
+    @rpc
+    def get_product(get_product_request: GetProductRequest, other_param: int) -> GetProductResponse: 
+        ...
