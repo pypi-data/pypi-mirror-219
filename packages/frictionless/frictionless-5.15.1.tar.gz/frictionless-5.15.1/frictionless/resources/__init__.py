09aa9ebf4a7c873bf45b1506526abe5df7bf64f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:18 +0000

    Add package frictionless/5.15.1/frictionless-5.15.1.tar.gz

---
+++ packages/frictionless/frictionless-5.15.1.tar.gz/frictionless-5.15.1/frictionless/resources/__init__.py
@@ -0,0 +1,38 @@
+from .file import *
+from .inquiry import *
+from .json import *
+from .metadata import *
+from .package import *
+from .resource import *
+from .table import *
+from .text import *
+from .types import *
+
+__all__ = [
+    "ArticleResource",
+    "CatalogResource",
+    "ChartResource",
+    "ChecklistResource",
+    "DialectResource",
+    "DocumentResource",
+    "FileResource",
+    "ImageResource",
+    "InquiryResource",
+    "JsonResource",
+    "JsonschemaResource",
+    "MapResource",
+    "MetadataResource",
+    "PackageResource",
+    "PipelineResource",
+    "ReportResource",
+    "ResourceResource",
+    "SchemaResource",
+    "ScriptResource",
+    "TableResource",
+    "TextResource",
+    "ViewResource",
+    "Convertible",
+    "Extractable",
+    "Indexable",
+    "Transformable",
+]
