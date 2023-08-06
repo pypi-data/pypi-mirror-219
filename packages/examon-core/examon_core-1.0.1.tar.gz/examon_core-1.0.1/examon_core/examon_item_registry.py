97e6e3a7d0320030db02977b94d10498c4241a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:28 +0000

    Add package examon-core/1.0.1/examon_core-1.0.1.tar.gz

---
+++ packages/examon-core/examon_core-1.0.1.tar.gz/examon_core-1.0.1/examon_core/examon_item_registry.py
@@ -0,0 +1,21 @@
+import logging
+
+class ExamonItemRegistry:
+    __registry = []
+
+    @classmethod
+    def add(cls, quiz_item):
+        logging.debug(f'Adding {quiz_item} to registry')
+        cls.__registry.append(quiz_item)
+
+    @classmethod
+    def reset(cls):
+        cls.__registry = []
+
+    @classmethod
+    def registry(cls, tag=None):
+        return [
+            py_quiz_data
+            for py_quiz_data in cls.__registry
+            if tag in py_quiz_data.tags or tag is None
+        ]
