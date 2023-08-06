fde2c5f12bcdff3700c1de6c4de5ae18e48d9e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:57 +0000

    Add package motti/0.0.5/motti-0.0.5.tar.gz

---
+++ packages/motti/motti-0.0.5.tar.gz/motti-0.0.5/README.md
@@ -0,0 +1,13 @@
+### Changelog
+`0.0.5`:
+```python
+from motti.tools import (
+    pil2str,
+    str2pil,
+    get_datetime,
+    project_dir_with,
+    is_abs_path,
+)
+```
+
+`0.0.4`: init
---
+++ packages/motti/motti-0.0.5.tar.gz/motti-0.0.5/motti/tools.py
@@ -0,0 +1,36 @@
+from io import BytesIO
+import base64
+from PIL import Image
+from datetime import datetime
+import os
+from typing import Optiona, Union
+from pathlib import Path
+
+
+def pil2str(x):
+    buffer = BytesIO()
+    x.save(buffer, format='PNG')
+    b64 = base64.b64encode(buffer.getvalue())
+    res = str(b64, 'utf-8')
+    return res
+
+def str2pil(s):
+    b64 = base64.b64decode(s.encode('utf-8'))
+    return Image.open(BytesIO(b64))
+
+def get_datetime():
+    return datetime.now().strftime("%Y%m%d-%H%M%S")
+
+def project_dir_with(project_dir="."):
+    def f(*args, **kwargs):
+        return os.path.join(project_dir, *args, **kwargs)
+    return f
+
+def is_abs_path(path: Union[Path, str]):
+    if isinstance(path, Path):
+        return path.is_absolute()
+    elif isinstance(path, str):
+        return os.path.isabs(path)
+    else:
+        raise NotImplementedError 
+        return False
