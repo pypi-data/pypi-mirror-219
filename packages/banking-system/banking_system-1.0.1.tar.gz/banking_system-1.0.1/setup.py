a2f9cdf09a0eb3ec723e6b0d81f5ea9f8002cdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:18 +0000

    Add package banking-system/1.0.1/banking_system-1.0.1.tar.gz

---
+++ packages/banking-system/banking_system-1.0.1.tar.gz/banking_system-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='banking_system',
+    version='1.0.1',
+    author='Kunal Kumar',
+    description='A simple banking management system package',
+    packages=['banking_system'],
+    install_requires=[],  # Add dependencies if any
+)
