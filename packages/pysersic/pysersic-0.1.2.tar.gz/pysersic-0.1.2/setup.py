620ceba4313ec85a81453eb911675edee82ace4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:05 +0000

    Add package pysersic/0.1.2/pysersic-0.1.2.tar.gz

---
+++ packages/pysersic/pysersic-0.1.2.tar.gz/pysersic-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="pysersic",
+    version="0.1.2",
+    author="Imad Pasha & Tim Miller",
+    author_email="imad.pasha@yale.edu",
+    description="A Tool for fitting sersic profiles in python",
+    download_url="https://github.com/pysersic/pysersic/archive/refs/tags/v0.1.2.tar.gz",
+    packages=["pysersic",],
+    install_requires=['numpy',
+        'scipy',
+        'matplotlib',
+        'pandas',
+        'astropy',
+        'corner',
+        'photutils>=0.16',
+        'jax',
+        'numpyro',
+        'arviz',
+        'tqdm',
+        'asdf',
+        ]
+ )
