3974d63b8fc60a4acb54fbfb6911bd4393e93570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:09 +0000

    Add package drupal-scout/1.0.0/drupal-scout-1.0.0.tar.gz

---
+++ packages/drupal-scout/drupal-scout-1.0.0.tar.gz/drupal-scout-1.0.0/drupal_scout/formatters/jsonformatter.py
@@ -0,0 +1,38 @@
+import json
+
+from .formatter import Formatter
+from drupal_scout.module import Module
+
+
+class JSONFormatter(Formatter):
+    """
+    Formats the output as JSON.
+    """
+
+    def format(self, modules: [Module]) -> str:
+        """
+        Format the output as JSON.
+        :param modules:     the list of modules
+        :type modules:      list
+        :return:            the formatted output
+        :rtype:             str
+        """
+        output = []
+        for module in modules:
+            output.append({
+                'name': module.name,
+                'version': module.version,
+                'suitable_entries': []
+            })
+
+            # omit modules that are not active
+            if module.active is False:
+                continue
+
+            for entry in module.suitable_entries:
+                output[-1]['suitable_entries'].append({
+                    'version': entry['version'],
+                    'requirement': entry['requirement']
+                })
+        return json.dumps(output, indent=4)
+
