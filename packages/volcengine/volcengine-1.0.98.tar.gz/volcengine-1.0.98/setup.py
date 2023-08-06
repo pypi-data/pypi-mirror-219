16efa211600aad1ad47e909e73480077d5c98572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:56 +0000

    Add package volcengine/1.0.98/volcengine-1.0.98.tar.gz

---
+++ packages/volcengine/volcengine-1.0.98.tar.gz/volcengine-1.0.98/setup.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+import sys
+
+from setuptools import setup, find_packages
+from volcengine import VERSION
+
+install_requires = ["requests", "retry", "pytz", "pycryptodome", "protobuf", "google", "six"]
+
+setup(
+    name="volcengine",
+    version=VERSION,
+    keywords=("pip", "volcengine", "volc-sdk-python"),
+    description="The Volcengine SDK for Python",
+    license="MIT Licence",
+
+    url="https://github.com/Volcengine/volc-sdk-python",
+    author="Volcengine SDK",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=install_requires
+)
