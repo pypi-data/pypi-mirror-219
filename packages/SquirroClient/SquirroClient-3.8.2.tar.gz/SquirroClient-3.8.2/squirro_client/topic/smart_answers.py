a72359d5fadcfd163cfbda6011b5c794c0664821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:34 +0000

    Add package SquirroClient/3.8.2/SquirroClient-3.8.2.tar.gz

---
+++ packages/SquirroClient/SquirroClient-3.8.2.tar.gz/SquirroClient-3.8.2/squirro_client/topic/smart_answers.py
@@ -0,0 +1,24 @@
+import json
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class SmartAnswersMixin:
+    def get_similar_searches(
+        self, project_id: str, query: str, options: dict = None
+    ) -> dict:
+        """Find similar searches in popular queries given the input query
+
+        :param project_id: Id of the Squirro project.
+        :param query: The query that is used to find similar searches.
+        :param options: An optional dictionary to override default options
+        """
+        url = (
+            f"{self.topic_api_url}/v0/{self.tenant}/projects/{project_id}"
+            "/smart_answers/similar_searches"
+        )
+        headers = {"Content-Type": "application/json"}
+        params = {"query": query, "options": json.dumps(options)}
+        res = self._perform_request("get", url, headers=headers, params=params)
+        return self._process_response(res)["result"]
