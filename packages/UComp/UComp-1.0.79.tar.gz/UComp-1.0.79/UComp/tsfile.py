115ba6772cb76328cfce26decbf53f27543b292f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:20 +0000

    Add package UComp/1.0.79/UComp-1.0.79.tar.gz

---
+++ packages/UComp/UComp-1.0.79.tar.gz/UComp-1.0.79/UComp/tsfile.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+
+def ts(y, start='1990', freq='A'):
+    """
+    Converts a numpy vector or matrix into a pandas time series
+    """
+    if isinstance(y, list):
+        y = np.array(y)
+    elif isinstance(y, pd.Series) or isinstance(y, pd.DataFrame):
+        if start == '1990':
+            return y
+        else:
+            y = y.values
+    if len(y.shape) > 1 and y.shape[0] < y.shape[1]:
+        y = y.T
+    time = pd.date_range(start=start, periods=y.shape[0], freq=freq)
+    y = np.array(y, dtype=float)
+    if len(y.shape) > 1:
+        return pd.DataFrame(y, time)
+    else:
+        return pd.Series(y, time)
+
+
