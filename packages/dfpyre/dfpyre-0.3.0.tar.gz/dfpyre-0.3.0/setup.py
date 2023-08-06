735fbf6e4ebb3a007b5cffe1e6acd86eb72069f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:26 +0000

    Add package dfpyre/0.3.0/dfpyre-0.3.0.tar.gz

---
+++ packages/dfpyre/dfpyre-0.3.0.tar.gz/dfpyre-0.3.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dfpyre',
+    version='0.3.0',
+    description='A package for externally creating code templates for the DiamondFire Minecraft server.',
+    author='Amp',
+    url='https://github.com/Amp63/pyre',
+    license='MIT',
+    keywords=['diamondfire', 'minecraft'],
+    packages=find_packages(),
+)
