6e48e4643314b16c00f2fccd2118337a4b181538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:49 +0000

    Add package aestream/0.6.0/aestream-0.6.0.tar.gz

---
+++ packages/aestream/aestream-0.6.0.tar.gz/aestream-0.6.0/src/pybind/types.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <nanobind/nanobind.h>
+#include <nanobind/ndarray.h>
+
+#include <nanobind/stl/vector.h>
+
+namespace nb = nanobind;
+
+using py_size_t = std::vector<size_t>;
