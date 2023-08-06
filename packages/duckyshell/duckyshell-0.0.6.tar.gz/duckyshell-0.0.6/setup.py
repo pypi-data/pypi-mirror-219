5b133d4ede0f905e23204126db6f6814631dc8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:45 +0000

    Add package duckyshell/0.0.6/duckyshell-0.0.6.tar.gz

---
+++ packages/duckyshell/duckyshell-0.0.6.tar.gz/duckyshell-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='duckyshell',
+    version='0.0.6',
+    description='Hak5 USB Rubber Ducky CLI',
+    author='Daniel Boye',
+    author_email='danielboye888@gmail.com',
+    url='https://github.com/DanielBoye/duckyshell',
+    package_dir={'': 'duckyshell'},
+)
