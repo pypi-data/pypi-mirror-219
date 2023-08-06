8f8205ed23e47a5b64863b027c76ded47256a52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:00 +0000

    Add package kaiju-db/2.1.7/kaiju_db-2.1.7-py3-none-any.whl

---
+++ packages/kaiju-db/kaiju_db-2.1.7-py3-none-any.whl/kaiju_db/__init__.py
@@ -0,0 +1,8 @@
+from .types import *
+from .functions import *
+from .services import *
+
+__version__ = '2.1.7'
+__python_version__ = '3.8'
+__author__ = 'antonnidhoggr@me.com'
+__service_package__ = True
