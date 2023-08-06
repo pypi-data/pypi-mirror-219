75526a6eb38a1deaf6ae2ba3fe4afebd490e0fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:08 +0000

    Add package demisto-sdk/1.17.2/demisto_sdk-1.17.2-py3-none-any.whl

---
+++ packages/demisto-sdk/demisto_sdk-1.17.2-py3-none-any.whl/demisto_sdk/commands/generate_unit_tests/common.py
@@ -0,0 +1,23 @@
+import ast as ast_mod
+from typing import Union
+
+from demisto_sdk.commands.common.handlers import JSON_Handler
+
+json = JSON_Handler()
+
+
+def ast_name(
+    id: str, ctx: Union[ast_mod.Load, ast_mod.Store, ast_mod.Del] = ast_mod.Load()
+):
+    """
+    Creates an ast Name node.
+    """
+    return ast_mod.Name(id=id, ctx=ctx)
+
+
+def extract_outputs_from_command_run(context_example, output_prefix):
+    splited_prefix = output_prefix.split(".")
+    context_example = json.loads(context_example)
+    for prefix in splited_prefix:
+        context_example = context_example.get(prefix)
+    return context_example
