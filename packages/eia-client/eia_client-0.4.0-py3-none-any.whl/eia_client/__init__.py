4cd5bf4cd7dd08df6c803ed0046a684592173279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:28 +0000

    Add package eia-client/0.4.0/eia_client-0.4.0-py3-none-any.whl

---
+++ packages/eia-client/eia_client-0.4.0-py3-none-any.whl/eia_client/__init__.py
@@ -0,0 +1,9 @@
+"""EIA Open Data API Client Package."""
+
+__version__ = "0.4.0"
+
+
+from eia_client import api_key
+from eia_client import endpoint
+from eia_client import parse
+from eia_client.client import Client
---
+++ packages/eia-client/eia_client-0.4.0-py3-none-any.whl/eia_client/client.py
@@ -0,0 +1,29 @@
+"""EIA client module."""
+
+from dataclasses import asdict
+import json
+
+from requests import Session
+
+from eia_client import api_key as ak
+from eia_client.endpoint.builder import Endpoint, EndpointParams, join_api_key
+
+
+def _headers(params: EndpointParams) -> dict:
+    return {"X-Params": json.dumps(asdict(params))}
+
+
+class Client:
+    """
+    EIA Client class.
+    """
+
+    def __init__(self, session: Session = None, api_key: ak.ApiKey = None):
+        self._session = Session() if session is None else session
+        self._api_key = ak.load() if api_key is None else api_key
+
+    def get(self, endpoint: Endpoint):
+        """EIA Client get endpoint."""
+        endpoint = join_api_key(endpoint, self._api_key)
+        headers = _headers(endpoint.params)
+        return self._session.get(endpoint.endpoint, headers=headers)
