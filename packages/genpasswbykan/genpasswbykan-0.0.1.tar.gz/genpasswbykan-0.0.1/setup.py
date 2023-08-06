8c3a5a9ca6a08b2fe474d2665e2157cc891dea60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:55 +0000

    Add package genpasswbykan/0.0.1/genpasswbykan-0.0.1.tar.gz

---
+++ packages/genpasswbykan/genpasswbykan-0.0.1.tar.gz/genpasswbykan-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="genpasswbykan",                     # This is the name of the package
+    version="0.0.1",                        # The initial release version
+    author="kanchan",                     # Full name of the author
+    description="Quicksample Test Package for generating password",
+    
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["genpass"],             # Name of the python package
+    install_requires=[]                     # Install other dependencies if any
+)
