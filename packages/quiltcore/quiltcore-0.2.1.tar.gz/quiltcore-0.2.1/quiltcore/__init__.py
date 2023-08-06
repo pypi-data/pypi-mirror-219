4fae125ea2c0d8659213feb43cdaf8c8bbe62bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:47 +0000

    Add package quiltcore/0.2.1/quiltcore-0.2.1.tar.gz

---
+++ packages/quiltcore/quiltcore-0.2.1.tar.gz/quiltcore-0.2.1/quiltcore/__init__.py
@@ -0,0 +1,35 @@
+"""
+
+This Python package is the very first implementation of QuiltCore,
+a next-generation architecture and primitives ("plumbing")
+for reading and writing Quilt packages:
+
+QuiltCore is makes Quilt easy to:
+
+1. Implement in other languages
+2. Extend to other storage backends (blobs, filesystems, etc.)
+3. Support alternate manifest serializations (and additional semantics)
+4. Wrap in application-specific "porcelain" (convenience APIs)
+
+The core Resource classes are, in order of increasing specialization:
+
+- Resource
+- Registry
+- Namespace
+- Manifest
+- Entry
+
+These make use of the 'Config' class which loads and
+manages the 'quiltcore.yaml' configuration file.
+
+"""
+
+from .changes import Changes  # noqa: F401
+from .delta import Delta  # noqa: F401
+from .entry import Entry  # noqa: F401
+from .manifest import Manifest  # noqa: F401
+from .namespace import Namespace  # noqa: F401
+from .registry import Registry  # noqa: F401
+from .resource import Resource  # noqa: F401
+from .volume import Volume  # noqa: F401
+from .yaml.config import Config  # noqa: F401
---
+++ packages/quiltcore/quiltcore-0.2.1.tar.gz/quiltcore-0.2.1/quiltcore/registry.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+
+from .manifest import Manifest
+from .resource import Resource
+from .resource_path import ResourcePath
+
+
+class Registry(ResourcePath):
+    """
+    Top-level Resource reperesenting a Quilt Registry.
+    Defines core paths containing Namespaces and Manifests.
+    `list` and `get` return Namespace objects
+    """
+
+    def __init__(self, path: Path, **kwargs):
+        super().__init__(path, **kwargs)
+        self.root = path
+        self.base = self.setup_dir(path, "quilt3/dirs/config")
+        self.path = self.setup_dir(self.base, "quilt3/dirs/names")
+        self.manifests = self.setup_dir(self.base, "quilt3/dirs/manifests")
+
+    def setup_dir(self, path: Path, key: str) -> Path:
+        """Form dir and create if it does not exist."""
+        dir = path / self.cf.get_path(key)
+        if not dir.exists():
+            dir.mkdir(parents=True, exist_ok=True)
+        return dir
+
+    def _child_args(self, key: str) -> dict:
+        return {"manifests": self.manifests}
+
+    def put(self, res: Resource, **kwargs) -> "Resource":
+        """Link manifest into namespace"""
+        if not isinstance(res, Manifest):
+            raise TypeError(f"Expected Manifest, got {type(res)}")
+        hash = res.hash()
+        name = kwargs[self.KEY_NAME]
+        name_dir = self.path / name
+        name_dir.mkdir(parents=True, exist_ok=True)
+
+        tag = self.Timestamp()
+        tag_file = name_dir / tag
+        tag_file.write_text(hash)
+
+        latest_file = name_dir / self.TAG_DEFAULT
+        latest_file.unlink(missing_ok=True)
+        latest_file.write_text(hash)
+
+        return res
