dd50d2b9d041f9d7d9b055672206a5642e73a701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:43 +0000

    Add package tja2fumen/0.5.2/tja2fumen-0.5.2.tar.gz

---
+++ packages/tja2fumen/tja2fumen-0.5.2.tar.gz/tja2fumen-0.5.2/README.md
@@ -0,0 +1,48 @@
+# tja2fumen
+
+`tja2fumen` is a tool for Taiko no Tatsujin games that allows you to convert TJA files (`.tja`) to fumen files (`.bin`).
+
+This project attempts to replace/complement the existing closed-source [tja2bin.exe](https://github.com/Fluto/TakoTako/blob/c269bcab60530877a16c2a473c84796b94d0a5ce/README.md?plain=1#L181) converter packaged alongside TakoTako. 
+
+## Goals
+
+- Act as a drop-in replacement for `tja2bin.exe` in TakoTako.
+- Fix https://github.com/Fluto/TakoTako/issues/16. (The original `tjabin.exe` doesn't properly handle `#BPMCHANGE` commands.)
+- Better handle commands that `tjabin.exe` fails for (`#SECTION`, `#NEXTSONG`, `#LYRIC`, etc.)
+- Provide open source code to act as a reference for parsing and writing both the TJA and Fumen file formats.
+- Stick to the Python stdlib, i.e. no external dependencies if possible.
+
+## Usage
+
+### Option 1: Standalone Python installation
+
+If you're familiar with Python, you can install `tja2fumen` by running:
+
+```
+pip install tja2fumen
+```
+
+Then, you can convert a TJA file by running:
+
+```
+tja2fumen file.tja
+```
+
+### Option 2: Using with TakoTako
+
+> **Note**: Before adding `tja2fumen` to TakoTako, you may want to back up the original `tja2bin.exe` file, to make sure you can switch back to the old converter if necessary. The easiest way to do this is by renaming the existing file to `tja2bin.exe.bak`.
+
+To use this converter with TakoTako, head to the [Releases](https://github.com/vivaria/tja2fumen/releases) page, and download the `tja2fumen.exe` file attached to the release. Then, rename `tja2fumen.exe` to `tja2bin.exe`, and place the file in the TakoTako plugin folder.
+
+TakoTako's plugin folder is inside of the BepInEx folder, which will typically look something like:
+
+```
+C:\XboxGames\T Tablet\Content\BepInEx\plugins\com.fluto.takotako
+```
+
+## Attribution
+
+- The fumen-parsing code in this project is based off of a modified copy of the [`readFumen()`](https://github.com/KatieFrogs/fumen-tools/blob/6ff3a2f7f53687f3dd49c5c57fcfc5ccbe3e5a10/fumen2osu/fumen2osu.py#L7-L152) function from the [`fumen2osu.py`](https://github.com/KatieFrogs/fumen-tools/blob/main/fumen2osu/fumen2osu.py) found in @KatieFrogs' [`fumen-tools`](https://github.com/KatieFrogs/fumen-tools) project.
+- The TJA-parsing code in this project is a Python translation of the [`parseTJA.js`](https://github.com/WHMHammer/tja-tools/blob/master/src/js/parseTJA.js) file from @WHMHammer's [`tja-tools`](https://github.com/WHMHammer/tja-tools).
+
+> **Note**: To be explicily clear, neither @KatieFrogs nor @WHMHammer have endorsed this project, are affiliated with this project, or have made any direct contributions to this project. I have just modified their existing work.
---
+++ packages/tja2fumen/tja2fumen-0.5.2.tar.gz/tja2fumen-0.5.2/src/tja2fumen/constants.py
@@ -0,0 +1,130 @@
+# Note types for TJA files
+TJA_NOTE_TYPES = {
+    '1': 'Don',
+    '2': 'Ka',
+    '3': 'DON',
+    '4': 'KA',
+    '5': 'Drumroll',
+    '6': 'DRUMROLL',
+    '7': 'Balloon',
+    '8': 'EndDRB',
+    '9': 'Kusudama',
+    'A': 'DON',  # hands
+    'B': 'KA',   # hands
+}
+
+# Note types for fumen files
+noteTypes = {
+    0x1: "Don",   # ドン
+    0x2: "Don2",  # ド
+    0x3: "Don3",  # コ
+    0x4: "Ka",    # カッ
+    0x5: "Ka2",   # カ
+    0x6: "Drumroll",
+    0x7: "DON",
+    0x8: "KA",
+    0x9: "DRUMROLL",
+    0xa: "Balloon",
+    0xb: "DON2",        # hands
+    0xc: "Kusudama",
+    0xd: "KA2",         # hands
+    0xe: "Unknown1",    # ? (Present in some Wii1 songs)
+    0xf: "Unknown2",    # ? (Present in some PS4 songs)
+    0x10: "Unknown3",   # ? (Present in some Wii1 songs)
+    0x11: "Unknown4",   # ? (Present in some Wii1 songs)
+    0x12: "Unknown5",   # ? (Present in some Wii4 songs)
+    0x13: "Unknown6",   # ? (Present in some Wii1 songs)
+    0x14: "Unknown7",   # ? (Present in some PS4 songs)
+    0x15: "Unknown8",   # ? (Present in some Wii1 songs)
+    0x16: "Unknown9",   # ? (Present in some Wii1 songs)
+    0x17: "Unknown10",  # ? (Present in some Wii4 songs)
+    0x18: "Unknown11",  # ? (Present in some PS4 songs)
+    0x19: "Unknown12",  # ? (Present in some PS4 songs)
+    0x22: "Unknown13",  # ? (Present in some Wii1 songs)
+    0x62: "Drumroll2"   # ?
+}
+typeNotes = {v: k for k, v in noteTypes.items()}
+
+branchNames = ("normal", "advanced", "master")
+
+# The first 432 bytes of fumen headers are made up of combinations of byte substrings
+# Commonly repeated byte substrings are listed below:
+byte_strings = {
+    # 6-byte substring
+    'x00': b'\x00\x00\x00\x00\x00\x00',
+    # 12-byte substrings
+    '431': b'43\xc8Ag&\x96B"\xe2\xd8B',
+    '432': b'43\xc8Ag&\x96BD\x84\xb7B',
+    '433': b'43\xc8A"\xe2\xd8B\x00@\xfaB',
+    '434': b'43\xc8AD\x84\xb7B"\xe2\xd8B',
+    'g1': b'g&\x96B4\xa3\x89Cxw\x05A',
+    'V1': b'V\xd5&B\x00@\xfaB\x00@\xfaB',
+    'V2': b'V\xd5&B"\xe2\xd8B\x00@\xfaB',
+    'V3': b'V\xd5&B\x00@\xfaB\xf0\xce\rC',
+}
+# The simplest 432-byte headers are just 36 repeated copies of the specific 12-byte substrings below
+simpleHeaders = [b * 36 for b in [byte_strings['431'], byte_strings['V1'], byte_strings['V2']]]
+
+# The next 80 bytes of fumen headers contain mostly 0s, with a few non-zero bytes thrown in
+sampleHeaderMetadata = [0] * 80
+# The following bytes are hardcoded by tja2bin.exe (implying they have little/no effect on how the song is parsed)
+sampleHeaderMetadata[4] = 16
+sampleHeaderMetadata[5] = 39
+sampleHeaderMetadata[12] = 10
+sampleHeaderMetadata[16] = 8
+sampleHeaderMetadata[21] = 255
+sampleHeaderMetadata[22] = 255
+sampleHeaderMetadata[23] = 255
+sampleHeaderMetadata[26] = 1
+sampleHeaderMetadata[30] = 1
+sampleHeaderMetadata[34] = 1
+sampleHeaderMetadata[36] = 20
+sampleHeaderMetadata[40] = 10
+sampleHeaderMetadata[48] = 1
+sampleHeaderMetadata[52] = 20
+sampleHeaderMetadata[56] = 10
+sampleHeaderMetadata[60] = 1
+sampleHeaderMetadata[64] = 30
+sampleHeaderMetadata[68] = 30
+sampleHeaderMetadata[72] = 20
+sampleHeaderMetadata[76] = 78
+sampleHeaderMetadata[77] = 97
+sampleHeaderMetadata[78] = 188
+# Certain other bytes (8+9, 20) will need to be filled in on a song-by-song basis
+
+TJA_COURSE_NAMES = []
+for difficulty in ['Ura', 'Oni', 'Hard', 'Normal', 'Easy']:
+    for player in ['', 'P1', 'P2']:
+        TJA_COURSE_NAMES.append(difficulty+player)
+
+NORMALIZE_COURSE = {
+    '0': 'Easy',
+    'Easy': 'Easy',
+    '1': 'Normal',
+    'Normal': 'Normal',
+    '2': 'Hard',
+    'Hard': 'Hard',
+    '3': 'Oni',
+    'Oni': 'Oni',
+    '4': 'Ura',
+    'Ura': 'Ura',
+    'Edit': 'Ura'
+}
+
+DIFFICULTY_BYTES = {
+    'Easy': [112, 23],
+    'Normal': [88, 27],
+    'Hard': [88, 27],
+    'Oni': [64, 31],
+    'Ura': [64, 31],
+    'Edit': [64, 31]
+}
+
+COURSE_IDS = {
+    'Easy': 'e',
+    'Normal': 'n',
+    'Hard': 'h',
+    'Oni': 'm',
+    'Ura': 'x',
+    'Edit': 'x'
+}
