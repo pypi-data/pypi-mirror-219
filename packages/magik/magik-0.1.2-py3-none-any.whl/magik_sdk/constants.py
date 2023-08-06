95c3cf51ff0adf9dddc32f259eac1f1852560f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:16 +0000

    Add package magik/0.1.2/magik-0.1.2-py3-none-any.whl

---
+++ packages/magik/magik-0.1.2-py3-none-any.whl/magik_sdk/constants.py
@@ -0,0 +1,15 @@
+# API URLs
+API_BASE_URL = "http://localhost:9000"
+DEPLOY_URL = f"{API_BASE_URL}/api/v1/testDeploy"
+RUN_URL = f"{API_BASE_URL}/api/v1/testRun/group"
+
+# Directory paths
+TEST_DIR = "./magik/tests"
+TESTRUNS_DIR = "./magik/test_runs"
+CONFIG_FILE_PATH = f"./magik/magik_config.json"
+MAGIK_SDK_DIR = "/Users/shivsakhuja/dev/magik/magik-prompt-sdk-python/magik_sdk"
+EXAMPLES_DIR = f"{MAGIK_SDK_DIR}/examples"
+EXAMPLE_CONFIG_PATH = f"{EXAMPLES_DIR}/magik_config.example.json"
+
+# Open AI defaults
+OPEN_AI_DEFAULT_MODEL = "gpt-3.5-turbo"
