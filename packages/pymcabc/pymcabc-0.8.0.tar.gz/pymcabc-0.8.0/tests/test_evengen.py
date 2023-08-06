482436a1db2eb1dec685e8a21f957275b7ea8560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:49 +0000

    Add package pymcabc/0.8.0/pymcabc-0.8.0.tar.gz

---
+++ packages/pymcabc/pymcabc-0.8.0.tar.gz/pymcabc-0.8.0/tests/test_evengen.py
@@ -0,0 +1,43 @@
+import pymcabc
+import os
+
+
+# test if the file is prepared
+def test_eventGen_detector_decay():
+    pymcabc.DefineProcess("A A > B B", mA=4, mB=10, mC=1, pi=30)
+    pymcabc.CrossSection().calc_xsection()
+    pymcabc.SaveEvent(100, boolDecay=True, boolDetector=True).to_root(
+        "test_eventGen_detector_decay.root"
+    )
+    # pymcabc.PlotData.file('test_eventGen_detector_decay.root')
+    assert "test_eventGen_detector_decay.root" in os.listdir(), "file not created"
+
+
+def test_eventGen_decay():
+    pymcabc.DefineProcess("A A > B B", mA=4, mB=10, mC=1, pi=30)
+    pymcabc.CrossSection().calc_xsection()
+    pymcabc.SaveEvent(100, boolDecay=True, boolDetector=False).to_root(
+        "test_eventGen_decay.root"
+    )
+    # pymcabc.PlotData.file('test_eventGen_decay.root')
+    assert "test_eventGen_decay.root" in os.listdir(), "file not created"
+
+
+def test_eventGen_detector():
+    pymcabc.DefineProcess("A A > B B", mA=4, mB=10, mC=1, pi=30)
+    pymcabc.CrossSection().calc_xsection()
+    pymcabc.SaveEvent(100, boolDecay=False, boolDetector=True).to_root(
+        "test_eventGen_detector.root"
+    )
+    # pymcabc.PlotData.file('test_eventGen_detector.root')
+    assert "test_eventGen_detector.root" in os.listdir(), "file not created"
+
+
+def test_eventGen():
+    pymcabc.DefineProcess("A A > B B", mA=4, mB=10, mC=1, pi=30)
+    pymcabc.CrossSection().calc_xsection()
+    pymcabc.SaveEvent(100, boolDecay=False, boolDetector=False).to_root(
+        "test_eventGen.root"
+    )
+    # pymcabc.PlotData.file('test_eventGen.root')
+    assert "test_eventGen.root" in os.listdir(), "file not created"
