ff7b0998e6c77830eaac9b60206d64284933bed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:32 +0000

    Add package increase/0.10.1/increase-0.10.1.tar.gz

---
+++ packages/increase/increase-0.10.1.tar.gz/increase-0.10.1/src/increase/types/check_transfer_create_params.py
@@ -0,0 +1,82 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from __future__ import annotations
+
+from typing_extensions import Required, TypedDict
+
+__all__ = ["CheckTransferCreateParams", "ReturnAddress"]
+
+
+class CheckTransferCreateParams(TypedDict, total=False):
+    account_id: Required[str]
+    """The identifier for the account that will send the transfer."""
+
+    address_city: Required[str]
+    """The city of the check's destination."""
+
+    address_line1: Required[str]
+    """The street address of the check's destination."""
+
+    address_state: Required[str]
+    """The state of the check's destination."""
+
+    address_zip: Required[str]
+    """The postal code of the check's destination."""
+
+    amount: Required[int]
+    """The transfer amount in cents."""
+
+    message: Required[str]
+    """The descriptor that will be printed on the memo field on the check."""
+
+    recipient_name: Required[str]
+    """The name that will be printed on the check."""
+
+    address_line2: str
+    """The second line of the address of the check's destination."""
+
+    note: str
+    """The descriptor that will be printed on the letter included with the check."""
+
+    require_approval: bool
+    """Whether the transfer requires explicit approval via the dashboard or API."""
+
+    return_address: ReturnAddress
+    """The return address to be printed on the check.
+
+    If omitted this will default to the address of the Entity of the Account used to
+    make the Check Transfer.
+    """
+
+    source_account_number_id: str
+    """
+    The identifier of the Account Number from which to send the transfer and print
+    on the check.
+    """
+
+    unique_identifier: str
+    """A unique identifier you choose for the transfer.
+
+    Reusing this identifer for another transfer will result in an error. You can
+    query for the transfer associated with this identifier using the List endpoint.
+    """
+
+
+class ReturnAddress(TypedDict, total=False):
+    city: Required[str]
+    """The city of the return address."""
+
+    line1: Required[str]
+    """The first line of the return address."""
+
+    name: Required[str]
+    """The name of the return address."""
+
+    state: Required[str]
+    """The US state of the return address."""
+
+    zip: Required[str]
+    """The postal code of the return address."""
+
+    line2: str
+    """The second line of the return address."""
