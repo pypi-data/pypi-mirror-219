bd6b45fbdb60a54c0cc9b20d4d4e6d0487c4a9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:08 +0000

    Add package x11-automation/0.3.6/x11_automation-0.3.6-py3-none-any.whl

---
+++ packages/x11-automation/x11_automation-0.3.6-py3-none-any.whl/x11_automation/__init__.py
@@ -0,0 +1,8 @@
+from .win_exists import win_exists, get_win_by_name
+from .win_activate import win_activate
+from .win_wait_active import win_wait_active
+from .send import send
+from .win_get_process import win_get_process
+from .move_mouse_to_new_window import move_mouse_to_new_window
+from .get_win_geometry import get_win_geometry
+from .x11_automation import X11AutomationError
