5a84a99b9db439911e5623929005b1e1a8433cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:10 +0000

    Add package DataProperty/1.0.1/DataProperty-1.0.1.tar.gz

---
+++ packages/DataProperty/DataProperty-1.0.1.tar.gz/DataProperty-1.0.1/setup.py
@@ -0,0 +1,92 @@
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+import os.path
+from typing import Dict, Type
+
+import setuptools
+
+
+MODULE_NAME = "DataProperty"
+REPOSITORY_URL = f"https://github.com/thombashi/{MODULE_NAME:s}"
+MISC_DIR = "misc"
+REQUIREMENT_DIR = "requirements"
+
+pkg_info: Dict[str, str] = {}
+
+
+def get_release_command_class() -> Dict[str, Type[setuptools.Command]]:
+    try:
+        from releasecmd import ReleaseCommand
+    except ImportError:
+        return {}
+
+    return {"release": ReleaseCommand}
+
+
+with open(os.path.join(MODULE_NAME.lower(), "__version__.py")) as f:
+    exec(f.read(), pkg_info)
+
+with open("README.rst", encoding="utf8") as f:
+    long_description = f.read()
+
+with open(os.path.join(MISC_DIR, "summary.txt"), encoding="utf8") as f:
+    summary = f.read().strip()
+
+with open(os.path.join(REQUIREMENT_DIR, "requirements.txt")) as f:
+    install_requires = [line.strip() for line in f if line.strip()]
+
+with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
+    tests_requires = [line.strip() for line in f if line.strip()]
+
+setuptools.setup(
+    name=MODULE_NAME,
+    version=pkg_info["__version__"],
+    url=REPOSITORY_URL,
+    author=pkg_info["__author__"],
+    author_email=pkg_info["__email__"],
+    description=summary,
+    include_package_data=True,
+    keywords=[
+        "data",
+        "library",
+        "property",
+    ],
+    license=pkg_info["__license__"],
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    maintainer=pkg_info["__author__"],
+    maintainer_email=pkg_info["__email__"],
+    packages=setuptools.find_packages(exclude=["test*"]),
+    package_data={MODULE_NAME: ["py.typed"]},
+    project_urls={
+        "Source": REPOSITORY_URL,
+        "Tracker": f"{REPOSITORY_URL:s}/issues",
+    },
+    python_requires=">=3.7",
+    install_requires=install_requires,
+    extras_require={
+        "logging": ["loguru>=0.4.1,<1"],
+        "test": tests_requires,
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    cmdclass=get_release_command_class(),
+    zip_safe=False,
+)
