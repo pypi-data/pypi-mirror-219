1e97c394b3fb50988a38e1d3b737acc5af57618b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:24 +0000

    Add package nautobot-floor-plan/1.0.0/nautobot_floor_plan-1.0.0-py3-none-any.whl

---
+++ packages/nautobot-floor-plan/nautobot_floor_plan-1.0.0-py3-none-any.whl/nautobot_floor_plan/__init__.py
@@ -0,0 +1,30 @@
+"""Plugin declaration for nautobot_floor_plan."""
+# Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
+try:
+    from importlib import metadata
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.version(__name__)
+
+from nautobot.extras.plugins import PluginConfig
+
+
+class FloorPlanConfig(PluginConfig):
+    """Configuration for the nautobot_floor_plan app."""
+
+    name = "nautobot_floor_plan"
+    verbose_name = "Floor Plans"
+    version = __version__
+    author = "Network to Code, LLC"
+    description = "Nautobot App for representing rack positions on per-location floor plan grids."
+    base_url = "floor-plan"
+    required_settings = []
+    min_version = "1.4.0"
+    max_version = "1.9999"
+    default_settings = {}
+    caching_config = {}
+
+
+config = FloorPlanConfig  # pylint:disable=invalid-name
