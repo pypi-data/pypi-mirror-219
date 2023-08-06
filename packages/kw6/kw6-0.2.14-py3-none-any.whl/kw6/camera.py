b1c66cf3a35418c6fa4df924c8036a0c506c5544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:58 +0000

    Add package kw6/0.2.14/kw6-0.2.14-py3-none-any.whl

---
+++ packages/kw6/kw6-0.2.14-py3-none-any.whl/kw6/camera.py
@@ -0,0 +1,98 @@
+import io
+from os import stat
+import struct
+import array
+import numpy as np
+from PIL import Image
+from pydantic import BaseModel
+
+from kw6 import settings, types
+
+
+class CameraHeader(BaseModel):
+    camera_version: str
+    camera_index: types.CAMERA_INDEX
+    scale_height: str
+    scale_length: str
+    xMM: str
+    yMM: str
+    xPixC: str
+    yPixC: str
+    sub_sample: str
+    expoMS: str
+    x0: str
+    y0: str
+    width: int
+    height: int
+    _10xReserved: str
+    _5xWearLeft: str
+    _5xWearRight: str
+
+    class Config:
+        allow_mutation = False
+
+    @staticmethod
+    def from_stream_(stream):
+        names = CameraHeader.__fields__.keys()
+        return CameraHeader(**dict(zip(
+            names,
+            array.array("d", stream.read(CameraHeader.byte_size()))
+        )))
+
+    @staticmethod
+    def from_bytes(bytes):
+        names = CameraHeader.__fields__.keys()
+        return CameraHeader(**dict(zip(
+            names,
+            array.array("d", bytes[:CameraHeader.byte_size()])
+        )))
+
+    @staticmethod
+    def byte_size():
+        names = CameraHeader.__fields__.keys()
+        return settings.N_BYTES_DOUBLE * len(names)
+
+
+class Camera(BaseModel):
+    header: CameraHeader
+    image: Image.Image
+
+    class Config:
+        arbitrary_types_allowed = True
+        allow_mutation = False
+
+    @staticmethod
+    def from_stream_(stream):
+        header = CameraHeader.from_stream_(stream)
+        return Camera(
+            header=header,
+            image=Camera.image_(stream, header),
+        )
+
+    @staticmethod
+    def image_(stream, header):
+        n_rows = header.height
+        n_cols = header.width
+        stream.read(settings.N_BYTES_DOUBLE * (settings.IM_HDR_SIZE - len(header.dict())))
+
+        image_data = np.array(
+            array.array("B", stream.read(n_rows * n_cols))
+        ).reshape(n_rows, n_cols)
+
+        return Image.fromarray(image_data)
+
+    @staticmethod
+    def skip_(stream):
+        header = CameraHeader.from_stream_(stream)
+        stream.seek(
+            Camera.byte_size(header) - CameraHeader.byte_size(), io.SEEK_CUR
+        )
+        return header
+
+    @staticmethod
+    def byte_size(header):
+        return (
+            CameraHeader.byte_size()
+            + settings.N_BYTES_DOUBLE * (settings.IM_HDR_SIZE - len(header.dict()))
+            + header.height * header.width
+        )
---
+++ packages/kw6/kw6-0.2.14-py3-none-any.whl/kw6/position.py
@@ -0,0 +1,66 @@
+import array
+from pydantic import BaseModel
+from typing import Tuple
+
+from kw6.camera import Camera
+from kw6 import settings, types
+
+
+class PositionHeader(BaseModel):
+    n_frame_bytes: int
+    camera_version: str
+    frame_index: types.FRAME_INDEX
+    time: str
+    pulses: str
+    n_active_cameras: int
+
+    class Config:
+        allow_mutation = False
+
+    @staticmethod
+    def peek_from_stream(stream):
+        byte_size = PositionHeader.byte_size()
+        return PositionHeader.from_bytes(stream.peek(byte_size)[:byte_size])
+
+    @staticmethod
+    def from_stream_(stream):
+        return PositionHeader.from_bytes(
+            stream.read(PositionHeader.byte_size())
+        )
+
+    @staticmethod
+    def from_bytes(bytes):
+        return PositionHeader(**dict(zip(
+            PositionHeader.__fields__.keys(),
+            array.array("d", bytes)
+        )))
+
+    @staticmethod
+    def byte_size():
+        names = PositionHeader.__fields__.keys()
+        return settings.N_BYTES_DOUBLE * len(names)
+
+
+class Position(BaseModel):
+    header: PositionHeader
+    cameras: Tuple[Camera, ...]
+
+    class Config:
+        allow_mutation = False
+
+    @staticmethod
+    def from_stream_(stream):
+        header = PositionHeader.from_stream_(stream)
+        return Position(
+            header=header,
+            cameras=tuple(
+                Camera.from_stream_(stream)
+                for _ in range(header.n_active_cameras)
+            )
+        )
+
+    @staticmethod
+    def skip_(stream):
+        header = PositionHeader.peek_from_stream(stream)
+        stream.seek(stream.tell() + header.n_frame_bytes)
+        return header
