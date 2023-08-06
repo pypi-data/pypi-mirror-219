3ff9cf4c0a2c043180b7aa3faa3a4bab936b7e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:58 +0000

    Add package confattr/0.15.0/confattr-0.15.0.tar.gz

---
+++ packages/confattr/confattr-0.15.0.tar.gz/confattr-0.15.0/conftest.py
@@ -0,0 +1,17 @@
+#!venv/bin/pytest
+
+from confattr import ConfigFile, Config, MultiConfig, ConfigFileCommand, Message
+
+import pytest
+import pathlib
+
+@pytest.fixture(autouse=True)
+def reset_config_path(tmp_path: pathlib.Path, monkeypatch: pytest.MonkeyPatch) -> None:
+	assert ConfigFile.config_path is None
+	assert ConfigFile.config_directory is None
+	monkeypatch.setattr(ConfigFile, 'config_directory', str(tmp_path))
+	monkeypatch.setattr(ConfigFileCommand, '_subclasses', ConfigFileCommand._subclasses.copy())
+	monkeypatch.setattr(ConfigFileCommand, '_used_names', ConfigFileCommand._used_names.copy())
+	Config.instances.clear()
+	MultiConfig.reset()
+	Message.reset()
---
+++ packages/confattr/confattr-0.15.0.tar.gz/confattr-0.15.0/tests/test_message.py
@@ -0,0 +1,84 @@
+#!../venv/bin/pytest -s
+
+from confattr import Message, NotificationLevel
+
+
+def test__show_file_name_on_first() -> None:
+	m = Message(NotificationLevel.ERROR, 'error', file_name='foo')
+
+	assert str(m) == '''\
+While loading foo:
+error'''
+
+def test__dont_repeat_file_name() -> None:
+	messages = [
+		Message(NotificationLevel.ERROR, 'info', file_name='/tmp/foo'),
+		Message(NotificationLevel.ERROR, 'error', file_name='/tmp/foo'),
+	]
+
+	assert '\n'.join(str(m) for m in messages) == '''\
+While loading /tmp/foo:
+info
+error'''
+
+def test__different_file_names() -> None:
+	messages = [
+		Message(NotificationLevel.ERROR, 'info', file_name='/tmp/foo'),
+		Message(NotificationLevel.ERROR, 'error', file_name='/tmp/foo'),
+		Message(NotificationLevel.ERROR, 'another error', file_name='/tmp/bar'),
+	]
+
+	assert '\n'.join(str(m) for m in messages) == '''\
+While loading /tmp/foo:
+info
+error
+
+While loading /tmp/bar:
+another error'''
+
+
+
+def test__no_file_name() -> None:
+	messages = [
+		Message(NotificationLevel.ERROR, 'info'),
+		Message(NotificationLevel.ERROR, 'error'),
+		Message(NotificationLevel.ERROR, 'another error'),
+	]
+
+	assert '\n'.join(str(m) for m in messages) == '''\
+info
+error
+another error'''
+
+def test__no_file_name_after_file_name() -> None:
+	messages = [
+		Message(NotificationLevel.ERROR, 'info', file_name='foo'),
+		Message(NotificationLevel.ERROR, 'error', file_name='foo'),
+		Message(NotificationLevel.ERROR, 'another error'),
+	]
+
+	assert '\n'.join(str(m) for m in messages) == '''\
+While loading foo:
+info
+error
+
+another error'''
+
+
+def test__reset() -> None:
+	m = Message(NotificationLevel.ERROR, 'error', file_name='foo')
+
+	assert str(m) == '''\
+While loading foo:
+error'''
+
+	Message.reset()
+	assert str(m) == '''\
+While loading foo:
+error'''
+
+
+
+def test__repr() -> None:
+	m = Message(NotificationLevel.ERROR, 'some message')
+	assert repr(m) == "Message(notification_level=ERROR, message='some message', file_name=None, line_number=None, line='', no_context=False)"
