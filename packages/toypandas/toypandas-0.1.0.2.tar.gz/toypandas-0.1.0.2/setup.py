f369000fdafb44428b2dac34ba3297b8a89a06b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:15 +0000

    Add package toypandas/0.1.0.2/toypandas-0.1.0.2.tar.gz

---
+++ packages/toypandas/toypandas-0.1.0.2.tar.gz/toypandas-0.1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="toypandas",
+    version="0.1.0.2",
+    author="Daniele Traversaro",
+    author_email="daniele.traversaro@dibris.unige.it",
+    description="An educational library based on Pandas for introductory data science.",
+    packages=["toypandas"],
+    install_requires=["pandas", "numpy", "matplotlib", "re", "itertools"],
+)
