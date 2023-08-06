7d5f0509ee33a74e506f8987222528c31035e4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:38 +0000

    Add package minet/1.0.0a43/minet-1.0.0a43.tar.gz

---
+++ packages/minet/minet-1.0.0a43.tar.gz/minet-1.0.0a43/minet/crawl/__init__.py
@@ -0,0 +1,13 @@
+from minet.crawl.focus import FocusCrawlInfo, FocusSpider
+from minet.crawl.types import (
+    CrawlResult,
+    AnyCrawlResult,
+    SuccessfulCrawlResult,
+    ErroredCrawlResult,
+    CrawlJob,
+    CrawlTarget,
+    UrlOrCrawlTarget,
+)
+from minet.crawl.state import CrawlerState
+from minet.crawl.spiders import Spider, SpiderResult, SpiderNextTargets, FunctionSpider
+from minet.crawl.crawler import Crawler, AnySpider, SpiderDeclaration
---
+++ packages/minet/minet-1.0.0a43.tar.gz/minet-1.0.0a43/minet/crawl/spiders.py
@@ -0,0 +1,123 @@
+# =============================================================================
+# Minet Spiders
+# =============================================================================
+#
+#
+from typing import (
+    TYPE_CHECKING,
+    Optional,
+    Tuple,
+    Union,
+    Callable,
+    Iterable,
+    Iterator,
+    Generic,
+)
+
+if TYPE_CHECKING:
+    from minet.crawl.crawler import Crawler
+
+from casanova.types import AnyWritableCSVRowPart
+
+from minet.crawl.types import (
+    UrlOrCrawlTarget,
+    CrawlTarget,
+    CrawlJob,
+    CrawlJobDataType,
+    CrawlResultDataType,
+    SuccessfulCrawlResult,
+)
+from minet.web import Response
+from minet.utils import PseudoFStringFormatter
+
+FORMATTER = PseudoFStringFormatter()
+
+SpiderNextTargets = Optional[Iterable[UrlOrCrawlTarget[CrawlJobDataType]]]
+SpiderResult = Optional[Tuple[CrawlResultDataType, SpiderNextTargets[CrawlJobDataType]]]
+
+
+class Spider(Generic[CrawlJobDataType, CrawlResultDataType]):
+    START_URL: Optional[str] = None
+    START_URLS: Optional[Iterable[str]] = None
+    START_TARGET: Optional[CrawlTarget[CrawlJobDataType]] = None
+    START_TARGETS: Optional[Iterable[CrawlTarget[CrawlJobDataType]]] = None
+
+    @property
+    def crawler(self) -> "Crawler":
+        c = getattr(self, "_crawler", None)
+
+        if c is None:
+            raise RuntimeError("spider has no attached crawler")
+
+        return c
+
+    def attach(self, crawler: "Crawler"):
+        if getattr(self, "_crawler", None) is not None:
+            raise TypeError("spider is already attached to a crawler")
+
+        self._crawler = crawler
+
+    def detach(self):
+        if getattr(self, "_crawler", None) is None:
+            raise TypeError("spider is not attached to a crawler")
+
+    def start(self) -> Optional[Iterable[UrlOrCrawlTarget[CrawlJobDataType]]]:
+        return None
+
+    def process(
+        self, job: CrawlJob[CrawlJobDataType], response: Response
+    ) -> SpiderResult[CrawlResultDataType, CrawlJobDataType]:
+        if callable(self):
+            return self(job, response)
+
+        raise NotImplementedError
+
+    def __repr__(self):
+        class_name = self.__class__.__name__
+
+        return "<%(class_name)s>" % {"class_name": class_name}
+
+    def write(
+        self,
+        filename: str,
+        contents: Union[str, bytes],
+        compress: bool = False,
+        relative: bool = False,
+    ) -> str:
+        return self.crawler.write(
+            filename, contents, compress=compress, relative=relative
+        )
+
+    def submit(self, fn, *args, **kwargs):
+        return self.crawler.submit(fn, *args, **kwargs)
+
+    def tabulate(
+        self, result: SuccessfulCrawlResult[CrawlJobDataType, CrawlResultDataType]
+    ) -> Iterator[AnyWritableCSVRowPart]:
+        yield result.data  # type: ignore
+
+
+FunctionSpiderCallable = Callable[
+    [CrawlJob[CrawlJobDataType], Response],
+    SpiderResult[CrawlResultDataType, CrawlJobDataType],
+]
+
+
+class FunctionSpider(Spider[CrawlJobDataType, CrawlResultDataType]):
+    fn: FunctionSpiderCallable[CrawlJobDataType, CrawlResultDataType]
+
+    def __init__(
+        self,
+        fn: FunctionSpiderCallable[CrawlJobDataType, CrawlResultDataType],
+        start: Optional[Iterable[UrlOrCrawlTarget[CrawlJobDataType]]] = None,
+    ):
+        self.fn = fn
+        self.__start_targets = start
+
+    def start(self) -> Optional[Iterable[UrlOrCrawlTarget[CrawlJobDataType]]]:
+        return self.__start_targets
+
+    def process(
+        self, job: CrawlJob[CrawlJobDataType], response: Response
+    ) -> SpiderResult[CrawlResultDataType, CrawlJobDataType]:
+        return self.fn(job, response)
