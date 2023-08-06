060b40fc2ab2607aeb265f9efefc131fe0efba1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:55 +0000

    Add package collective.restapi.facetedsearch/1.0.3/collective.restapi.facetedsearch-1.0.3.tar.gz

---
+++ packages/collective.restapi.facetedsearch/collective.restapi.facetedsearch-1.0.3.tar.gz/collective.restapi.facetedsearch-1.0.3/CHANGES.rst
@@ -0,0 +1,27 @@
+Changelog
+=========
+
+1.0.3 (2023-07-17)
+------------------
+
+- moved git repository to https://github.com/collective/collective.restapi.facetedsearch; modified readme.rst and setup.py
+
+
+1.0.2 (2023-07-05)
+------------------
+
+- added some project description
+
+
+1.0.1 (2022-06-02)
+------------------
+
+- bugfix for empty query and empty resultset in post handler
+- removing limit in post handler
+
+
+1.0.0 (2022-05-09)
+------------------
+
+- Initial release.
+  [muellers]
---
+++ packages/collective.restapi.facetedsearch/collective.restapi.facetedsearch-1.0.3.tar.gz/collective.restapi.facetedsearch-1.0.3/src/collective/restapi/facetedsearch/api/services/faceted_search/groupByModifiers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from collective.collectionfilter.interfaces import IGroupByCriteria
+from collective.collectionfilter.interfaces import IGroupByModifier
+from zope.component import adapter
+from zope.interface import implementer
+from zope.component import queryUtility
+from zope.schema.interfaces import IVocabularyFactory
+from collective.taxonomy.interfaces import ITaxonomy
+from plone.api import portal
+
+# see also collective.collectionfilter 'Overloading GroupByCriteria' how to set display_modifier function for indexes or assign a metadata column name different to the index name
+@implementer(IGroupByModifier)
+@adapter(IGroupByCriteria)
+def groupby_modifier(groupby):
+    pass
