ef4cfe68cdf0e541d2a19bb596e32675ddcf5d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:23 +0000

    Add package cupcake/0.1.5/cupcake-0.1.5-py3-none-any.whl

---
+++ packages/cupcake/cupcake-0.1.5-py3-none-any.whl/cupcake/data/new/include/{{name}}/{{name}}.hpp
@@ -0,0 +1,12 @@
+#ifndef {{ name | upper }}_{{ name | upper }}_HPP
+#define {{ name | upper }}_{{ name | upper }}_HPP
+
+#include <{{ name }}/export.hpp>
+
+namespace {{ name }} {
+
+{{ name | upper }}_EXPORT void {{ name }}();
+
+}
+
+#endif
