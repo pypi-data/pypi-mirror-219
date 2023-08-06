77c47b31ec564968c32f7050e9fd7aa2099d377d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:56 +0000

    Add package magentic/0.1.1/magentic-0.1.1-py3-none-any.whl

---
+++ packages/magentic/magentic-0.1.1-py3-none-any.whl/magentic/function_call.py
@@ -0,0 +1,34 @@
+import inspect
+from typing import Any, Callable, Generic, ParamSpec, TypeVar
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+class FunctionCall(Generic[T]):
+    def __init__(self, function: Callable[P, T], *args: P.args, **kwargs: P.kwargs):
+        self._function = function
+        self._args = args
+        self._kwargs = kwargs
+
+    def __call__(self) -> T:
+        return self._function(*self._args, **self._kwargs)
+
+    def __repr__(self) -> str:
+        args_kwargs_repr = ", ".join(
+            [
+                *(repr(arg) for arg in self._args),
+                *(f"{key}={value!r}" for key, value in self._kwargs.items()),
+            ]
+        )
+        return f"{type(self).__name__}({self._function!r}, {args_kwargs_repr})"
+
+    @property
+    def function(self):
+        return self._function
+
+    @property
+    def arguments(self) -> dict[str, Any]:
+        signature = inspect.signature(self._function)
+        bound_args = signature.bind(*self._args, **self._kwargs)
+        return bound_args.arguments.copy()
