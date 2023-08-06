1c36689b579f090df1aec7295abb5feb155e837d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:41 +0000

    Add package appgallery_spy/0.0.2/appgallery_spy-0.0.2-py3-none-any.whl

---
+++ packages/appgallery_spy/appgallery_spy-0.0.2-py3-none-any.whl/appgallery_spy/crud.py
@@ -0,0 +1,28 @@
+import pymongo
+from pymongo import MongoClient
+
+from appgallery_spy.models import Review
+
+
+def get_db():
+    client = MongoClient("mongodb://db:27017")
+    db = client["AppGallery"]
+    return db
+
+
+def get_recent_reviews(reviews_collection, count: int):
+    recent_reviews = reviews_collection.find().sort("date", pymongo.DESCENDING).limit(count)
+    reviews = [
+        Review(
+            username=review["username"], date=review["date"], review_text=review["review_text"], rating=review["rating"]
+        )
+        for review in recent_reviews
+    ]
+
+    return reviews
+
+
+def insert_data(data: list[dict], reviews_collection) -> None:
+    print("Saving data...")
+    reviews_collection.insert_many(data)
+    print("Data is saved.")
