b90be791b7dbbcef62ea40774a62920aa5a65619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:35 +0000

    Add package OneOnOne/0.1/OneOnOne-0.1.tar.gz

---
+++ packages/OneOnOne/OneOnOne-0.1.tar.gz/OneOnOne-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+VERSION=0.1
+
+DESCRIPTION="A package for pre-trained image classification and context-decider for question-answering chatbots."
+LONG_DESCRIPTION=""
+CLASSIFIERS=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+setup(name="OneOnOne",version=VERSION,author="Sohini Bhattacharya",author_email="mail.sohinibhattacharya@gmail.com",License="MIT",packages=find_packages(),keywords=["python","image-classification","active-learning-sampling","question-answering","pre-trained models","tiny-image-net","cifar10"],classifiers=CLASSIFIERS)
