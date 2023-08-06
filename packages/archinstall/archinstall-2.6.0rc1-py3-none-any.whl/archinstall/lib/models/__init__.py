75b096bb06a60c30bcaba8c685f59933d51d327c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:52 +0000

    Add package archinstall/2.6.0rc1/archinstall-2.6.0rc1-py3-none-any.whl

---
+++ packages/archinstall/archinstall-2.6.0rc1-py3-none-any.whl/archinstall/lib/models/__init__.py
@@ -0,0 +1,8 @@
+from .network_configuration import (
+	NetworkConfiguration,
+	NicType,
+	Nic
+)
+from .bootloader import Bootloader
+from .gen import VersionDef, PackageSearchResult, PackageSearch, LocalPackage
+from .users import PasswordStrength, User
