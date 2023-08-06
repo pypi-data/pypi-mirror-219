446d536651045625cefbbdae9c1ed6380a575843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:57 +0000

    Add package gispandas/0.1.9/gispandas-0.1.9.tar.gz

---
+++ packages/gispandas/gispandas-0.1.9.tar.gz/gispandas-0.1.9/gispandas/__init__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2023/07/11 21:06:55
+@Author  :   HMX
+@Version :   1.0
+@Contact :   kzdhb8023@163.com
+'''
+
+# here put the import lib
+from .mxclf import *
+from .mxgdal import *
+from .mxgee import *
+from .mxgpd import *
+
+
+__version__ = "0.1.9"
