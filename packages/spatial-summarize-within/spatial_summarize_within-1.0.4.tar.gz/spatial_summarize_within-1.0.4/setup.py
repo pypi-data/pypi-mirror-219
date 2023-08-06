df82610c15fbe3433fe7dc77e9dc86d8513bc969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package spatial-summarize-within/1.0.4/spatial_summarize_within-1.0.4.tar.gz

---
+++ packages/spatial-summarize-within/spatial_summarize_within-1.0.4.tar.gz/spatial_summarize_within-1.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spatial_summarize_within",
+    version="1.0.4",
+    Author = "Kenneth Landon Wall",
+    Email = "Landon@AlloyAnalytics.org",
+    packages=find_packages(),
+    install_requires=[
+        "geopandas",
+        "pandas",
+        "shapely",
+        "mapclassify",
+    ],
+)
