0f30c1370c23d09345d656f177606ced04a02b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:28 +0000

    Add package PyGraphica/0.0.7/PyGraphica-0.0.7.tar.gz

---
+++ packages/PyGraphica/PyGraphica-0.0.7.tar.gz/PyGraphica-0.0.7/src/PyGraphica/fonts.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+root = os.path.join(os.path.join(sys.executable,".."),"Lib/site-packages/PyGraphica")
+
+Arial = os.path.join(root,"arial.ttf")
+#Arial font is owned by Monotype imaging
+Baskerville = os.path.join(root,"baskerville.ttf")
+#Baskerville font was designed by JohnBaskerville in the 1750s
+BrushScript = os.path.join(root,"brushscript.otf")
+#BrushScript font is owned by Adobe Originals
+Calibri = os.path.join(root,"calibri.ttf")
+#Calibri is owned by Microsoft
+Courier = os.path.join(root,"courier.ttf")
+#Courier is owned  by IBM
+Garamond = os.path.join(root,"garamond.ttf")
+#Garamond is owned by Monotype imaging
+Helvetica  = os.path.join(root,"helvetica.ttf")
+#Helvetica is owned by Monotype imaging
+Impact = os.path.join(root,"impact.ttf")
+#Impact is owned by Monotype imaging
+OpenDyslexic = os.path.join(root,"opendyslexic.otf")
+#OpenDyslexic is owned by Abelardo Gonzales
+TimesNewRoman= os.path.join(root,"timesnewroman.ttf")
+#TimesNewRoman is owned by Monotype Imagine
+Trebuchet = os.path.join(root,"timesnewroman.ttf")
+#Trebuchet is owned by Microsoft
