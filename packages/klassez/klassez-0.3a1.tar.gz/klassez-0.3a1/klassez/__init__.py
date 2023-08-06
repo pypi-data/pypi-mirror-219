aefb4b27144d8d1ed207da10eba415ea9310616a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:47 +0000

    Add package klassez/0.3a1/klassez-0.3a1.tar.gz

---
+++ packages/klassez/klassez-0.3a1.tar.gz/klassez-0.3a1/klassez/__init__.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+import os
+import sys
+import numpy as np
+from numpy import linalg
+from scipy import stats
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import seaborn as sns
+from pprint import pprint
+
+from . import fit, misc, sim, figures, processing
+from .Spectra import Spectrum_1D, pSpectrum_1D, Spectrum_2D, pSpectrum_2D, Pseudo_2D
+
+__version__ = '0.3a.1'
+
+# Use seaborn's colormaps and save it to a dictionary
+from .config import CM, COLORS, cron
+
+def open_doc():
+    """ Open the documentation .pdf file in the browser. """
+    import webbrowser
+    webbrowser.open_new(__doc__)
+
+__doc__ = f'{__path__[0]}/docs/klassez.pdf'
