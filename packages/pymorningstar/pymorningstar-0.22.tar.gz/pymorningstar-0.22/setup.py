109c66e38522b60bd3dd398155f11d0338752826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:24 +0000

    Add package pymorningstar/0.22/pymorningstar-0.22.tar.gz

---
+++ packages/pymorningstar/pymorningstar-0.22.tar.gz/pymorningstar-0.22/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pymorningstar', 
+    version='0.22',
+    description = "Automating the download of information from Morningstar's Excel Add-In API",
+    author = 'Pablo Vilas',
+    author_email='pablo.vilas.naval@gmail.com',
+    packages = ['pymorningstar'],
+    install_requires=['xlwings','pandas','pyautogui', 'opencv-python', 'Pillow'],
+    url='https://github.com/VilasPablo/pymorningstar',
+    classifiers=[ 
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Programming Language :: Python :: 3',  ],
+    include_package_data=True,
+    zip_safe = False)
+
