a9da1ec1d4b55e7fe966c7e41c4990d450ceeb21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:42 +0000

    Add package x11-automation/0.3.5.1/x11_automation-0.3.5.1-py3-none-any.whl

---
+++ packages/x11-automation/x11_automation-0.3.5.1-py3-none-any.whl/x11_automation/get_win_geometry.py
@@ -0,0 +1,25 @@
+from .win_exists import get_win_by_name
+from ewmh import EWMH
+
+
+def get_win_geometry(name):
+    ewmh = EWMH()
+
+    win = get_win_by_name(name)['win']
+    if win:
+        win = win['win']
+    else:
+        print(f'Cannot find {name} window')
+    geom = win.get_geometry()
+    # print(f'{geom=}')
+    (x, y, w, h) = (geom.x, geom.y, geom.width, geom.height)
+    while True:
+        parent = win.query_tree().parent
+        pgeom = parent.get_geometry()
+        x += pgeom.x
+        y += pgeom.y
+        if parent.id == ewmh.root.id:
+            break
+        win = parent
+    # print(f'{x=}, {y=}')
+    return x, y, w, h
