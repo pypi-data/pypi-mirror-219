24199aa00b4b61541e869fe8e20f44a79bb4a9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:14 +0000

    Add package quickfix-ch/0.0.13/quickfix-ch-0.0.13.tar.gz

---
+++ packages/quickfix-ch/quickfix-ch-0.0.13.tar.gz/quickfix-ch-0.0.13/setup.py
@@ -0,0 +1,70 @@
+from setuptools import setup, Extension
+from distutils.command.build_ext import build_ext
+import glob
+import sys
+
+class build_ext_subclass( build_ext ):
+    def build_extensions(self):
+        self.compiler.define_macro("PYTHON_MAJOR_VERSION", sys.version_info[0])
+        print("Testing for std::tr1::shared_ptr...")
+        try:
+            self.compiler.compile(["test_std_tr1_shared_ptr.cpp"])
+            self.compiler.define_macro("HAVE_STD_TR1_SHARED_PTR")
+            print("...found")
+        except:
+            print(" ...not found")
+
+        print("Testing for std::shared_ptr...")
+        try:
+            self.compiler.compile(["test_std_shared_ptr.cpp"], extra_preargs=["-std=c++0x"]),
+            self.compiler.define_macro("HAVE_STD_SHARED_PTR")
+            print("...found")
+        except:
+            print("...not found")
+
+        print("Testing for std::unique_ptr...")
+        try:
+            self.compiler.compile(["test_std_unique_ptr.cpp"], extra_preargs=["-std=c++0x"]),
+            self.compiler.define_macro("HAVE_STD_UNIQUE_PTR")
+            print("...found")
+        except:
+            print("...not found")
+    
+        build_ext.build_extensions(self)
+
+# Remove the "-Wstrict-prototypes" compiler option, which isn"t valid for C++.
+import distutils.sysconfig
+cfg_vars = distutils.sysconfig.get_config_vars()
+for key, value in cfg_vars.items():
+    if type(value) == str:
+        cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
+        
+long_description=""
+with open("LICENSE") as file:
+    license = file.read();
+
+setup(
+    name="quickfix-ch",
+    version="0.0.13",
+    py_modules=["quickfix", "quickfixt11", "quickfix40", "quickfix41", "quickfix42", "quickfix43", "quickfix44", "quickfix50", "quickfix50sp1", "quickfix50sp2"],
+    data_files=[("share/quickfix", glob.glob("spec/FIX*.xml"))],
+    include_package_data=True,
+    author="Javier (coinhako)",
+    author_email="javier.phon@coinhako.com",
+    maintainer="Javier (coinhako)",
+    maintainer_email="javier.phon@coinhako.com",
+    description="FIX (Financial Information eXchange) protocol implementation",
+    url="http://www.quickfixengine.org",
+    download_url="http://www.quickfixengine.org",
+    license=license,
+    include_dirs=["C++"],
+    cmdclass = {"build_ext": build_ext_subclass },
+    ext_modules=[
+        Extension(
+            "_quickfix", glob.glob("C++/*.cpp"),
+            extra_compile_args=[
+                "-std=c++0x", "-Wno-deprecated", "-Wno-unused-variable", "-Wno-deprecated-declarations", "-Wno-uninitialized"
+            ]
+        )
+    ],
+)
