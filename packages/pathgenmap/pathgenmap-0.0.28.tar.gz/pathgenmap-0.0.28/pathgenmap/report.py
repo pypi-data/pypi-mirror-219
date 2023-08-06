fe237a290f937bcbffd45b1e4165da4de84a5066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:46 +0000

    Add package pathgenmap/0.0.28/pathgenmap-0.0.28.tar.gz

---
+++ packages/pathgenmap/pathgenmap-0.0.28.tar.gz/pathgenmap-0.0.28/pathgenmap/report.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class ReportGenerator:
+    """Class to generate reports from the integrated data."""
+
+    def __init__(self, species_abundance_file, top_filo, absolute):
+        """
+        Constructor method.
+
+        Args:
+            species_abundance_file (str): Path to the .csv file containing species abundance.
+            top_filo (int): Top N filos to be considered for the bar chart.
+            absolute (bool): Use absolute abundance for the bar chart. If False, relative abundance is used.
+        """
+        self.species_abundance_file = species_abundance_file
+        self.top_filo = top_filo
+        self.absolute = absolute
+
+    def generate_report(self):
+        """
+        Method to generate the report.
+        """
+        self.generate_stacked_bar_chart()
+
+    def generate_stacked_bar_chart(self):
+        """Method to generate a stacked bar chart."""
+        df = pd.read_csv(self.species_abundance_file)
+        df = df[df['reads'] > 0]  # Filter out rows with reads <= 0
+        df_grouped = df.groupby('phylum').agg({'reads': 'sum'}).reset_index()
+        df_grouped = df_grouped.sort_values(by='reads', ascending=False)
+
+        if not self.absolute:
+            df_grouped['reads'] = df_grouped['reads'] / df_grouped['reads'].sum() * 100
+
+        # Add a 'Others Phyla' group for those not in top N
+        top_filos = df_grouped.iloc[:self.top_filo].copy()
+        others = df_grouped.iloc[self.top_filo:].copy()
+        others_sum = others['reads'].sum()
+        others = pd.DataFrame([{'phylum': 'Other Phyla', 'reads': others_sum}])
+        df_final = pd.concat([top_filos, others])
+
+        # Pivot the DataFrame for stacked bar chart
+        df_final_pivot = df_final.pivot(columns='phylum', values='reads').fillna(0)
+
+        # Generate the chart
+        fig, ax = plt.subplots()
+        df_final_pivot.loc[:, df_final['phylum']].plot.bar(stacked=True, ax=ax)
+        ax.set_ylabel('Reads (%)' if not self.absolute else 'Reads')
+        ax.set_xlabel('Phylum')
+        ax.set_title('Phylum Abundance')
+
+        # Save the figure
+        fig.tight_layout()
+        fig.savefig('PhylumAbundance.png', bbox_inches='tight', edgecolor='black')
+
