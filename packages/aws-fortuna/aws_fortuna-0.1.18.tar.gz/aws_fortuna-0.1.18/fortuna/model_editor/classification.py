8e23a7b3753794e4b29b8ba87a23bc9ca96f0468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:07 +0000

    Add package aws-fortuna/0.1.18/aws_fortuna-0.1.18.tar.gz

---
+++ packages/aws-fortuna/aws_fortuna-0.1.18.tar.gz/aws_fortuna-0.1.18/fortuna/model_editor/classification.py
@@ -0,0 +1,47 @@
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Optional,
+    Tuple,
+    Union,
+)
+
+import flax.linen as nn
+import jax.numpy as jnp
+
+from fortuna.model_editor.base import ModelEditor
+from fortuna.typing import (
+    AnyKey,
+    Array,
+    InputData,
+    Params,
+)
+from fortuna.utils.probit import probit_scaling
+
+
+class ProbitClassificationModelEditor(ModelEditor):
+    freeze_fun: Optional[Callable[[Tuple[AnyKey, ...], Array], str]] = None
+    top_k: Optional[int] = None
+
+    @nn.compact
+    def __call__(
+        self,
+        apply_fn: Callable[
+            [Params, InputData], Union[jnp.ndarray, Tuple[jnp.ndarray, Dict]]
+        ],
+        model_params: Params,
+        x: Any,
+        has_aux: bool,
+    ) -> Union[jnp.ndarray, Tuple[jnp.ndarray, Dict]]:
+        log_var = self.param("log_var", nn.initializers.zeros, (1,))
+        outputs = probit_scaling(
+            apply_fn,
+            model_params,
+            x,
+            log_var=log_var,
+            has_aux=has_aux,
+            freeze_fun=self.freeze_fun,
+            top_k=self.top_k,
+        )
+        return outputs
