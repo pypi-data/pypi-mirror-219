6bba4db045a41edd40243cffe72cdedda09d33f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:54 +0000

    Add package gocept.logging/1.0/gocept.logging-1.0.tar.gz

---
+++ packages/gocept.logging/gocept.logging-1.0.tar.gz/gocept.logging-1.0/setup.py
@@ -0,0 +1,60 @@
+# This should be only one line. If it must be multi-line, indent the second
+# line onwards to keep the PKG-INFO file format intact.
+"""Infrastructure for semi-structured log messages.
+"""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='gocept.logging',
+    version='1.0',
+    python_requires='>=3.7',
+    install_requires=[
+        'setuptools',
+    ],
+
+    extras_require={
+        'test': [
+            'mock',
+        ],
+    },
+
+    entry_points={
+        'console_scripts': [
+            # 'binary-name = gocept.logging.module:function'
+        ],
+    },
+
+    author='gocept <mail@gocept.com>',
+    author_email='mail@gocept.com',
+    license='ZPL 2.1',
+    url='https://github.com/gocept/gocept.logging',
+
+    keywords='',
+    classifiers=[
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    description=__doc__.strip(),
+    long_description='\n\n'.join(open(name).read() for name in (
+        'README.rst',
+        'HACKING.rst',
+        'CHANGES.rst',
+    )),
+
+    namespace_packages=['gocept'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+)
