1b9e00f9e1aea5eac5a946762942d0ca88105eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:20 +0000

    Add package gfx-perp-python-sdk/0.0.3/gfx_perp_python_sdk-0.0.3.tar.gz

---
+++ packages/gfx-perp-python-sdk/gfx_perp_python_sdk-0.0.3.tar.gz/gfx_perp_python_sdk-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gfx_perp_python_sdk',
+    version='0.0.3',
+    url='https://github.com/GooseFX1/gfx-perp-python-sdk',
+    author='Shashank Shekhar',
+    author_email='shashank@goosefx.io',
+    description='Perp python sdk',
+    py_modules=['my_project'],
+    packages=find_packages(),    
+    install_requires=[
+        'pytest'
+    ],
+)
