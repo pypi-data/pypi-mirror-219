15ffcf8fbf6b29f603b0edb543eb36c0c82dc1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:21 +0000

    Add package mohaliyet/0.1/mohaliyet-0.1.tar.gz

---
+++ packages/mohaliyet/mohaliyet-0.1.tar.gz/mohaliyet-0.1/README.md
@@ -0,0 +1,11 @@
+# addnumbers
+
+A simple Python package to add two numbers.
+
+## Installation
+
+You can install `addnumbers` via pip:
+
+```shell
+pip install addnumbers
+```
---
+++ packages/mohaliyet/mohaliyet-0.1.tar.gz/mohaliyet-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mohaliyet',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple package to add two numbers',
+    author='Your Name',
+    author_email='your.email@example.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='sample setuptools development',
+    python_requires='>=3.6, <4',
+)
+
