e37474d7efb0ea960f3e85381f40ed729ad5fc13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:02 +0000

    Add package mccalcy/0.0.1/mccalcy-0.0.1.tar.gz

---
+++ packages/mccalcy/mccalcy-0.0.1.tar.gz/mccalcy-0.0.1/tests/test_calc.py
@@ -0,0 +1,65 @@
+import pytest
+from mccalcy import calculator
+
+
+def test_add() -> None:
+    """
+    Test the add() method.
+    """
+    calc = calculator.Calculator()
+    calc.add(10)
+    assert calc.memory == 10
+
+
+def test_subtract() -> None:
+    """
+    Test the subtract() method.
+    """
+    calc = calculator.Calculator()
+    calc.add(10)
+    calc.subtract(5)
+    assert calc.memory == 5
+
+
+def test_multiply() -> None:
+    """
+    Test the multiply() method.
+    """
+    calc = calculator.Calculator()
+    calc.add(10)
+    calc.multiply(2)
+    assert calc.memory == 20
+
+
+def test_divide() -> None:
+    """
+    Test the divide() method.
+    """
+    calc = calculator.Calculator()
+    calc.add(10)
+    calc.divide(2)
+    assert calc.memory == 5
+
+
+def test_root() -> None:
+    """
+    Test the root() method.
+    """
+    calc = calculator.Calculator()
+    calc.add(16)
+    calc.root(2)
+    assert calc.memory == 4
+
+
+def test_reset_memory() -> None:
+    """
+    Test the reset_memory() method.
+    """
+    calc = calculator.Calculator()
+    calc.add(10)
+    calc.reset_memory()
+    assert calc.memory == 0
+
+
+if __name__ == "__main__":
+    pytest.main()
