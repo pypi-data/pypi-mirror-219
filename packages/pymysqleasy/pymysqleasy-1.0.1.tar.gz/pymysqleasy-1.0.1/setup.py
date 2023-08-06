cdcea8ad2a6d2a95d77cd05b99368fa6a094d9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:46 +0000

    Add package pymysqleasy/1.0.1/pymysqleasy-1.0.1.tar.gz

---
+++ packages/pymysqleasy/pymysqleasy-1.0.1.tar.gz/pymysqleasy-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from time import time
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="pymysqleasy",
+    version="1.0.1",
+    author="duer",
+    author_email="1186969412@qq.com",
+    description="mysql crud exends",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/duerhong/py-mysql-easy",
+    project_urls={
+        "Bug Tracker": "https://github.com/duerhong/py-mysql-easy/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
