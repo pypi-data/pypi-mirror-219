c40ef3fe74ab8f8cd32639ec5995c9f9458ad122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:23 +0000

    Add package noelmode/1.0.6/noelmode-1.0.6.tar.gz

---
+++ packages/noelmode/noelmode-1.0.6.tar.gz/noelmode-1.0.6/noelmode/__init__.py
@@ -0,0 +1,27 @@
+"""
+noelmode - A monkeypatched add-on for Noelgram
+Copyright (C) 2020 Cezar H. <https://github.com/usernein>
+
+This file is part of noelmode.
+
+noelmode is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+noelmode is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with noelmode.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from noelmode.listen.listen import Client, Chat, MessageHandler, User
+from noelmode.utils import NoelmodConfig
+
+
+__copyright__ = "Copyright (C) 2022-present Noel <https://github.com/jokokendi>"
+__license__ = "GNU General Public License v3.0 (GPL-3.0)"
+__version__ = "1.0.6"
