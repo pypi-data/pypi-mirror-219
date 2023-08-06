0d26aac41527f3c1a2baf78ee9a64dda39647100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:48 +0000

    Add package py-go-goose/1.0/py_go_goose-1.0.tar.gz

---
+++ packages/py-go-goose/py_go_goose-1.0.tar.gz/py_go_goose-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_go_goose',
+    author='wangjianzhou',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+)
