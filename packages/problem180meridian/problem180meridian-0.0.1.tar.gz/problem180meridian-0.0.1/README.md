25310891ec61de6ee2fe6fc930a92c0f89ba74a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:26 +0000

    Add package problem180meridian/0.0.1/problem180meridian-0.0.1.tar.gz

---
+++ packages/problem180meridian/problem180meridian-0.0.1.tar.gz/problem180meridian-0.0.1/README.md
@@ -0,0 +1,2 @@
+# problem180
+Python module to fix 180 degree crossing issue for vector geodata
---
+++ packages/problem180meridian/problem180meridian-0.0.1.tar.gz/problem180meridian-0.0.1/problem180meridian/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: sergsadkov
+:license: None
+
+:copyright: (c) 2023 sergsadkov
+"""
+
+from .problem180meridian import (check180lon, cross180lon, split180lon,
+                                 split180lonMultipolygon)
+
+__author__ = 'sergsadkov'
+__version__ = '0.0.1'
+__email__ = 'sergsadkov@gmail.com'
