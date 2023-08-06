8761f31652f7b31cc8048ed3c884891dbf823411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:51 +0000

    Add package dbt-fal/1.5.5/dbt_fal-1.5.5.tar.gz

---
+++ packages/dbt-fal/dbt_fal-1.5.5.tar.gz/dbt_fal-1.5.5/src/fal/dbt/cli/cli.py
@@ -0,0 +1,54 @@
+from typing import List
+import sys
+
+from click.exceptions import ClickException
+
+from fal.dbt.cli.flow_runner import fal_flow_run
+from .args import parse_args
+from .fal_runner import fal_run
+from fal.dbt.telemetry import telemetry
+
+from fal.dbt.integration.logger import log_manager
+
+
+DBT_FAL_COMMAND_NOTICE = \
+"""The dbt tool `fal` and `dbt-fal` adapter have been merged into a single tool.
+Please use the new `dbt-fal` command line tool instead.
+Running `pip install dbt-fal` will install the new tool and the adapter alongside.
+Then run your command like
+
+    dbt-fal <command>
+"""
+
+def fal_cli(argv: List[str] = sys.argv):
+    print(DBT_FAL_COMMAND_NOTICE)
+    cli(argv)
+
+def cli(argv: List[str] = sys.argv):
+    # Wrapper to be able to shutdown telemetry afterwards
+    try:
+        _cli(argv)
+    finally:
+        telemetry.shutdown()
+
+
+@telemetry.log_call("cli")
+def _cli(argv: List[str]):
+    parsed = parse_args(argv[1:])
+
+    # TODO: do we still need this?
+    with log_manager.applicationbound():
+        if parsed.debug:
+            log_manager.set_debug()
+
+        if parsed.command == "flow":
+            if parsed.flow_command == "run":
+                exit_code = fal_flow_run(parsed)
+                if exit_code:
+                    raise SystemExit(exit_code)
+
+        elif parsed.command == "run":
+            fal_run(parsed)
+
+        else:
+            raise ClickException(f"Unknown command {parsed.command}")
