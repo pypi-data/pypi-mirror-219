422f4eee7a9964e214277587b6a706acecb45d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:26 +0000

    Add package big-query-testing-xyz/0.3/big_query_testing_xyz-0.3.tar.gz

---
+++ packages/big-query-testing-xyz/big_query_testing_xyz-0.3.tar.gz/big_query_testing_xyz-0.3/code_query/main.py
@@ -0,0 +1,26 @@
+from google.cloud import bigquery
+import pandas as pd
+
+credentials_path = '/Users/vedantrathi/Desktop/big_query_testing_xyz/irsdata-pypi-4955991a9ae8.json'
+
+
+project_id = 'irsdata-pypi'
+dataset_id = 'dataset001'
+table_id = 'vw_irs_filing_test02'
+# table_id = 'irsdata-pypi.dataset001.irs_filing'
+
+
+client = bigquery.Client.from_service_account_json(credentials_path)
+
+sql_query = f"SELECT * FROM `{project_id}.{dataset_id}.{table_id}`"
+# view_ref = client.dataset(dataset_id, project=project_id).table(table_id)
+view_data = client.query(sql_query).to_dataframe()
+
+# first_row = next(view_data)
+
+# for field, value in first_row.items():
+#     print(f"{field}: {value}")
+
+
+def testing(index):
+    return view_data.iloc[index]
