18ccddf4bc463a33523041e02607830ce4debb1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:35 +0000

    Add package qvsed/1.6.1/qvsed-1.6.1-py3-none-any.whl

---
+++ packages/qvsed/qvsed-1.6.1-py3-none-any.whl/qvsed/config_default.py
@@ -0,0 +1,38 @@
+# This is QVSED's config file template and should not be modified directly; please make any necessary changes in config.py.
+# pylint: disable=all
+
+# Font
+font_family = ["JetBrains Mono", "Cascadia Code", "Consolas", "Menlo", "monospace"]
+font_size = 11
+
+# Options
+tab_stop_width = 4
+echo_area_timeout = 3000
+
+# Colours
+colors = {
+    "window": {
+        "text": "#FFFFFF",
+        "background": "#282C34"
+    },
+    "button": {
+        "text": "#FFFFFF",
+        "background": "#393F4A",
+        "hover": "#31353F",
+        "pressed": "#282C34"
+    },
+    "text_area": {
+        "text": "#FFFFFF",
+        "background": "#31353F"
+    },
+    "echo_area": {
+        "text": "#FFFFFF",
+        "background": "#393F4A"
+    },
+    "scroll_bar": {
+        "text": "#363D49",
+        "background": "#282F3B",
+        "hover": "#4F5664",
+        "pressed": "#262B34"
+    }
+}
