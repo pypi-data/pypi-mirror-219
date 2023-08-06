f1038a855b904a43531209ba2626247ecfec616f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:44 +0000

    Add package glassmath/0.0.0/glassmath-0.0.0.tar.gz

---
+++ packages/glassmath/glassmath-0.0.0.tar.gz/glassmath-0.0.0/README.md
@@ -0,0 +1,7 @@
+Glassmath is a improved version of the built-in math module.
+
+Glassmath is created by the PyPi user @pymoduledev, or GitHub user zPointless.
+
+**UPDATES**
+
+July 14, 2023: Beta release
---
+++ packages/glassmath/glassmath-0.0.0.tar.gz/glassmath-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glassmath',
+    author='PyModuleDev',
+    description='[IN BETA TESTING]More advanced version of the math module. Protected by Apache 2.0 License.',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies required by your module
+    ],
+)
