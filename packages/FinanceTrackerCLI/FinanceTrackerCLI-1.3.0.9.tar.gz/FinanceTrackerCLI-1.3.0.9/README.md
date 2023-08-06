9e950587e4e719b494ed870d84727fcde4e0207b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:22 +0000

    Add package FinanceTrackerCLI/1.3.0.9/FinanceTrackerCLI-1.3.0.9.tar.gz

---
+++ packages/FinanceTrackerCLI/FinanceTrackerCLI-1.3.0.9.tar.gz/FinanceTrackerCLI-1.3.0.9/README.md
@@ -0,0 +1,11 @@
+# Finance tracker python CLI app<br>
+
+### Version 1.3.0.0<br>
+
+--added currencies conversion from Foreign Exchange Market.<br>
+--added convertor for entered amount.<br>
+--added input checks.<br>
+--added program name title
+--added delete string functionality 
+--overhaul, fixes
+--packed to pip
---
+++ packages/FinanceTrackerCLI/FinanceTrackerCLI-1.3.0.9.tar.gz/FinanceTrackerCLI-1.3.0.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FinanceTrackerCLI',
+    version='1.3.0.9',
+    author='Vladimir Kobranov',
+    author_email='josephclturok@gmail.com',
+    description='Finance tracker CLI app',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/VladimirKobranov/FinanceTrackerCLI',
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.11',
+    install_requires=[
+        'forex-python==1.8',
+    ],
+)
