46abc9974e646bb77751c6ae7586a66f664ff7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:52 +0000

    Add package multimcts/0.1.3/multimcts-0.1.3.tar.gz

---
+++ packages/multimcts/multimcts-0.1.3.tar.gz/multimcts-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension("multimcts.mcts", ["multimcts/mcts.pyx"]),
+    Extension("multimcts.gamestate", ["multimcts/gamestate.pyx"]),
+]
+
+setup(
+    name="multimcts",
+    version="0.1.3",
+    author="Taylor Vance",
+    author_email="mirrors.cities0w@icloud.com",
+    # url="https://github.com/taylorvance/multimcts",
+    description="Monte Carlo Tree Search for multiple teams",
+    license="MIT",
+    packages=["multimcts"],
+    ext_modules=cythonize(extensions),
+    zip_safe=False,
+)
