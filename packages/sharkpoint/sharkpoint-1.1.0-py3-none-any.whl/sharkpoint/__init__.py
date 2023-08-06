95130ad1d57fece679d82086986d414b87187f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:41 +0000

    Add package sharkpoint/1.1.0/sharkpoint-1.1.0-py3-none-any.whl

---
+++ packages/sharkpoint/sharkpoint-1.1.0-py3-none-any.whl/sharkpoint/__init__.py
@@ -0,0 +1,22 @@
+from sharkpoint import sharepoint
+from sharkpoint import sharepoint_file
+from sharkpoint import sharepoint_site
+
+from sharkpoint.sharepoint import (
+    SharePoint,
+)
+from sharkpoint.sharepoint_file import (
+    SharepointFile,
+)
+from sharkpoint.sharepoint_site import (
+    SharepointSite,
+)
+
+__all__ = [
+    "SharePoint",
+    "SharepointFile",
+    "SharepointSite",
+    "sharepoint",
+    "sharepoint_file",
+    "sharepoint_site",
+]
