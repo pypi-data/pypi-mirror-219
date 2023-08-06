28c46edcb216bf7eb57d8c55f52d65b72fd94096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:49 +0000

    Add package noelmode/1.0.2/noelmode-1.0.2.tar.gz

---
+++ packages/noelmode/noelmode-1.0.2.tar.gz/noelmode-1.0.2/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = '1.0.2'
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='noelmode',
+    version=version,
+
+    author='jokokendi',
+
+    author_email='ajual7832@gmail.com',
+    url='https://github.com/jokokendi/xmode',
+
+    description='pyromod custom',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    download_url='https://github.com/jokokendi/xmode/archive/v{}.zip'.format(
+        version
+    ),
+    license='MIT',
+
+    packages=['noelmode'],
+    install_requires=['requests'],
+    
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ]
+)
