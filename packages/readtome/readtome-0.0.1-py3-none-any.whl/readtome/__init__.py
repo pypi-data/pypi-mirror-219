fdea2e316bc08d30808334e6357730cadd5d2ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:25 +0000

    Add package readtome/0.0.1/readtome-0.0.1-py3-none-any.whl

---
+++ packages/readtome/readtome-0.0.1-py3-none-any.whl/readtome/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+CLI to read code via text-to-speech
+
+.. currentmodule:: readtome
+.. moduleauthor:: Josh Winters <josh@winters.codes>
+"""
+
+from .version import __version__, __release__  # noqa
