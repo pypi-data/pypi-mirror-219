d78e5f68cd13b53592abf02a999d8cce46ee4010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:33 +0000

    Add package django-restit/4.1.4/django_restit-4.1.4-py3-none-any.whl

---
+++ packages/django-restit/django_restit-4.1.4-py3-none-any.whl/auditlog/tq.py
@@ -0,0 +1,62 @@
+"""
+This is where you can put handlers for running async background tasks
+
+Task.Publish("myapp", "on_tq_test")
+"""
+from datetime import datetime, timedelta
+from auditlog.models import PersistentLog
+from rest import settings
+
+AUDITLOG_PRUNE_DAYS = settings.get("AUDITLOG_PRUNE_DAYS", 360)
+REST_LOG_PRUNE_DAYS = settings.get("REST_LOG_PRUNE_DAYS", 90)
+ACCOUNT_LOG_PRUNE_DAYS = settings.get("ACCOUNT_LOG_PRUNE_DAYS", 360)
+ASYNC_LOG_PRUNE_DAYS = settings.get("ASYNC_LOG_PRUNE_DAYS", 14)
+
+
+def on_cleanup(task):
+    # cleanup log files in the var directory
+    count = cleanupAuditLogs()
+    if count > 0:
+        task.log(f"deleted {count} audit logs")
+
+    count = cleanupAsyncLogs()
+    if count > 0:
+        task.log(f"deleted {count} async logs")
+
+    count = cleanupRestLogs()
+    if count > 0:
+        task.log(f"deleted {count} rest logs")
+
+    count = cleanupAccountLogs()
+    if count > 0:
+        task.log(f"deleted {count} account logs")
+
+
+def cleanupAuditLogs():
+    if AUDITLOG_PRUNE_DAYS:
+        before = datetime.now() - timedelta(days=AUDITLOG_PRUNE_DAYS)
+        return PersistentLog.objects.filter(when__lte=before).exclude(component__in=["account.Member", "account.Group"]).delete()[0]
+    return 0
+
+
+def cleanupRestLogs():
+    if REST_LOG_PRUNE_DAYS:
+        before = datetime.now() - timedelta(days=REST_LOG_PRUNE_DAYS)
+        return PersistentLog.objects.filter(when__lte=before, component__in=["rest", "action"]).exclude(component__in=["account.Member", "account.Group"]).delete()[0]
+    return 0
+
+
+def cleanupAccountLogs():
+    if ACCOUNT_LOG_PRUNE_DAYS:
+        before = datetime.now() - timedelta(days=ACCOUNT_LOG_PRUNE_DAYS)
+        return PersistentLog.objects.filter(when__lte=before, component__in=["account.Member", "account.Group"]).delete()[0]
+    return 0
+
+
+def cleanupAsyncLogs():
+    if ASYNC_LOG_PRUNE_DAYS:
+        before = datetime.now() - timedelta(days=ASYNC_LOG_PRUNE_DAYS)
+        return PersistentLog.objects.filter(when__lte=before, action="async").delete()[0]
+    return 0
+
+
