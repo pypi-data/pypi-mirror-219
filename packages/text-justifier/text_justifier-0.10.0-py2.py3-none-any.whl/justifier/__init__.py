39cedfe649af401bc845f051452cf0f82d708df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:46 +0000

    Add package text-justifier/0.10.0/text_justifier-0.10.0-py2.py3-none-any.whl

---
+++ packages/text-justifier/text_justifier-0.10.0-py2.py3-none-any.whl/justifier/__init__.py
@@ -0,0 +1,23 @@
+"""Top-level package for Text justifier."""
+
+import logging
+
+
+
+# *** DEFINITIONS ***
+__author__ = """Alastair Irvine"""
+__email__ = 'alastair@plug.org.au'
+__version__ = '0.10.0'
+
+root_logger = None  # logging.Logger
+params = {}
+
+
+# *** FUNCTIONS ***
+def init_logging(loglevel: int) -> logging.Logger:
+    global root_logger
+
+    root_logger = logging.getLogger()
+    logging.basicConfig(level=loglevel)
+
+    return root_logger
