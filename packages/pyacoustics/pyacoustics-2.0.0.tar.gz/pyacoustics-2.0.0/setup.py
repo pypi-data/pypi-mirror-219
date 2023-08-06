30e2bf52c6321f48870bf2dacb703b9b6e9b7c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:47 +0000

    Add package pyacoustics/2.0.0/pyacoustics-2.0.0.tar.gz

---
+++ packages/pyacoustics/pyacoustics-2.0.0.tar.gz/pyacoustics-2.0.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Created on Oct 15, 2014
+
+@author: tmahrt
+"""
+from setuptools import setup
+import io
+
+setup(
+    name="pyacoustics",
+    python_requires=">3.6.0",
+    version="2.0.0",
+    author="Tim Mahrt",
+    author_email="timmahrt@gmail.com",
+    url="https://github.com/timmahrt/pyAcoustics",
+    package_dir={"pyacoustics": "pyacoustics"},
+    packages=[
+        "pyacoustics",
+        "pyacoustics.intensity_and_pitch",
+        "pyacoustics.signals",
+        "pyacoustics.speech_detection",
+        "pyacoustics.speech_rate",
+        "pyacoustics.text",
+        "pyacoustics.textgrids",
+        "pyacoustics.utilities",
+    ],
+    package_data={
+        "pyacoustics": [
+            "matlabScripts/detect_syllable_nuclei.m",
+        ]
+    },
+    license="LICENSE",
+    install_requires=[
+        "praatio ~= 6.0",
+        "typing_extensions",
+    ],
+    description="A collection of python scripts for extracting and analyzing acoustics from audio files.",
+    long_description=io.open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
