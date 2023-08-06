37f5bb94bde436f20577dd7cb31f862a6bbb1b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:59 +0000

    Add package antidevinfo/1.0/antidevinfo-1.0.tar.gz

---
+++ packages/antidevinfo/antidevinfo-1.0.tar.gz/antidevinfo-1.0/code/__init__.py
@@ -0,0 +1,8 @@
+def s1337() :
+    print("1337 hiya ahssn madrassa w li kaykhraj manha kaykhdam f Google")
+
+
+def tatabiz(data) :
+    print (str(data) + " dkhlat l tatabiz")
+
+
---
+++ packages/antidevinfo/antidevinfo-1.0.tar.gz/antidevinfo-1.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name= 'antidevinfo',
+    version= '1.0',
+    author= 'Anti-Dev-Info',
+    description= 'Library just for fun',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
