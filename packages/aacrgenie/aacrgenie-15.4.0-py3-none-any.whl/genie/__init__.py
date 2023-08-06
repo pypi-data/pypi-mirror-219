1de25183ae1dab401e7bc69965e3c9949e562f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:33 +0000

    Add package aacrgenie/15.4.0/aacrgenie-15.4.0-py3-none-any.whl

---
+++ packages/aacrgenie/aacrgenie-15.4.0-py3-none-any.whl/genie/__init__.py
@@ -0,0 +1,13 @@
+# Import logging last to not take in synapseclient logging
+import logging
+import os
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logging.getLogger("keyring").setLevel(logging.WARNING)
+
+# create version in __init__.py
+# https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+__version__ = "15.4.0"
+
+__all__ = ["__version__"]
