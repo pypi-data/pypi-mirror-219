ea076ed07b95bd49b63229f6c49b0fa866df8da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:35 +0000

    Add package physicX/0.0.117/physicX-0.0.117-py3-none-any.whl

---
+++ packages/physicX/physicX-0.0.117-py3-none-any.whl/physicX/constants.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+c = 299792458
+G = 6.674e-11
+N_A = 6.02214076E23
+e = 1.602176634E-19
+m_e = 9.1093837015E-31
+h = 6.62607015E-34
+R = 8.31446261815324
+k_B = R/N_A
+rh = h/(2 * np.pi)
+H_0 = 69.32
+u0 = 4 * np.pi * 1E-7
+epsilon0 = 1/((c**2) * u0)
+k = 1/(4 * np.pi * epsilon0)
+f = e * N_A
+K_J = (2 * e)/h
+kaka = (8 * np.pi * G)/c ** 4
+R_K = h/e**2
+alpha = e**2/(2 * epsilon0 * (h * c))
