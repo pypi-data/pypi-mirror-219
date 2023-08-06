edd81de89f61de140c22b291db4c6a5630594150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:16 +0000

    Add package revealjs/0.0.3/revealjs-0.0.3.tar.gz

---
+++ packages/revealjs/revealjs-0.0.3.tar.gz/revealjs-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from yaml import safe_load
+
+with open("config.yaml") as fp:
+    data=safe_load(fp)
+
+with open("README.md", "r") as fd:
+    long_description = fd.read()
+
+setup(
+    **data,
+    long_description=long_description,
+    packages=["revealjs"],
+    py_modules=["revealjs.reveal_js"],
+    entry_points={
+        "console_scripts": ["reveal_js = revealjs.reveal_js:main"],
+    },
+    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+)
