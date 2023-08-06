5911930fcb160bd2a054f2674a1ad1b83ff7d8f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:48 +0000

    Add package streamlit-binary-tree/0.2.5/streamlit-binary-tree-0.2.5.tar.gz

---
+++ packages/streamlit-binary-tree/streamlit-binary-tree-0.2.5.tar.gz/streamlit-binary-tree-0.2.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-binary-tree",
+    version="0.2.5",
+    author="Abhishek Sharma",
+    author_email="abhishek1995sharma@gmail.com",
+    description="Interactive Binary Tree as a Streamlit component",
+    long_description="Interactive Binary Tree as a Streamlit component",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+        "scikit-learn >= 1.3.0",
+        "numpy",
+    ],
+)
