6b97e0d0268fe65c3a4b79625b4f022689df7dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:30 +0000

    Add package mercury-sync/0.3.34/mercury_sync-0.3.34-py3-none-any.whl

---
+++ packages/mercury-sync/mercury_sync-0.3.34-py3-none-any.whl/mercury_sync/middleware/__init__.py
@@ -0,0 +1,7 @@
+from .cors import Cors
+from .crsf import CRSF
+from .compressor import (
+    BidirectionalGZipCompressor,
+    GZipCompressor,
+    ZStandardCompressor
+)
---
+++ packages/mercury-sync/mercury_sync-0.3.34-py3-none-any.whl/mercury_sync/middleware/base/middleware.py
@@ -0,0 +1,125 @@
+from __future__ import annotations
+from mercury_sync.models.response import Response
+from mercury_sync.models.request import Request
+from pydantic import BaseModel
+from typing import (
+    Any,
+    Callable, 
+    Union, 
+    Dict, 
+    Optional,
+    List,
+    Literal,
+    Tuple
+)
+from .bidirectional_wrapper import BidirectionalWrapper
+from .unidirectional_wrapper import UnidirectionalWrapper
+from .types import MiddlewareType
+
+
+class Middleware:
+
+    def __init__(
+        self,
+        name: str,
+        middleware_type: MiddlewareType=MiddlewareType.UNIDIRECTIONAL_BEFORE,
+        methods: Optional[
+            List[
+                Literal[
+                    "GET",
+                    "HEAD",
+                    "OPTIONS",
+                    "POST",
+                    "PUT",
+                    "PATCH",
+                    "DELETE",
+                    "TRACE"
+                ]
+            ]
+        ]=None,
+        response_headers: Dict[str, str]={}
+    ) -> None:
+        
+        self.name = name
+        self.methods = methods
+        self.response_headers = response_headers
+        self.middleware_type = middleware_type
+
+        self._wrapper_types = {
+            MiddlewareType.BIDIRECTIONAL: BidirectionalWrapper,
+            MiddlewareType.UNIDIRECTIONAL_BEFORE: UnidirectionalWrapper,
+            MiddlewareType.UNIDIRECTIONAL_AFTER: UnidirectionalWrapper,
+        }
+
+    def __call__(self, request: Request) -> Tuple[
+        Tuple[Response, int],
+        bool
+    ]:
+        raise NotImplementedError('Err. __call__() should not be called on base Middleware class.')
+
+    def wrap(
+        self,
+        handler: Callable[
+            [Request],
+            Union[
+                BaseModel,
+                str,
+                None
+            ]
+        ]
+    ):
+        
+        wrapper = self._wrapper_types.get(
+            self.middleware_type,
+            BidirectionalWrapper(
+                self.name,
+                handler,
+                methods=self.methods,
+                response_headers=self.response_headers,
+                middleware_type=self.middleware_type 
+            )
+        )(
+            self.name,
+            handler,
+            methods=self.methods,
+            response_headers=self.response_headers,
+            middleware_type=self.middleware_type 
+        )
+
+        if isinstance(wrapper, BidirectionalWrapper):
+            wrapper.pre = self.__pre__
+            wrapper.post = self.__post__
+
+        elif isinstance(wrapper, UnidirectionalWrapper):
+
+            wrapper.run = self.__run__
+        
+            self.response_headers.update(wrapper.response_headers)
+        
+        return wrapper
+    
+    async def __pre__(self, request: Request):
+        raise NotImplementedError('Err. - __pre__() is not implemented for base Middleware class.')
+    
+    async def __post__(
+        self, 
+        request: Request,
+        response: Response,
+        status: int
+    ):
+        raise NotImplementedError('Err. - __post__() is not implemented for base Middleware class.')
+    
+    async def __run__(
+        self, 
+        request: Request,
+        response: Response,
+        status: int
+    ):
+        raise NotImplementedError('Err. - __post__() is not implemented for base Middleware class.')
+    
+    async def run(
+        self,
+        request: Request
+    ):
+        raise NotImplementedError('Err. - middleware() is not implemented for base Middleware class.')
+    
