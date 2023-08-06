4a16e1b3c226321aa7d939f8fdd90e0a96568eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:39 +0000

    Add package micrOSDevToolKit/1.18.3/micrOSDevToolKit-1.18.3-py3-none-any.whl

---
+++ packages/micrOSDevToolKit/micrOSDevToolKit-1.18.3-py3-none-any.whl/micrOS/source/InterruptHandler.py
@@ -0,0 +1,174 @@
+"""
+Module is responsible for hardware interrupt
+handling dedicated to micrOS framework.
+- Setting up interrupt memory buffer from config
+- Configure time based and external interrupts
+
+- Time based IRQ:
+    - Simple (timer0) with fix period callback
+    - Advanced (timer1) - time stump ! LM function;
+            0-6:0-24:0-59:0-59!system heartbeat; etc.
+
+Designed by Marcell Ban aka BxNxM
+
+Reference: https://docs.micropython.org/en/latest/library/machine.Pin.html
+"""
+#################################################################
+#                            IMPORTS                            #
+#################################################################
+from machine import Pin
+from utime import ticks_ms, ticks_diff
+from ConfigHandler import cfgget
+from Debug import console_write, errlog_add
+from TaskManager import exec_lm_pipe_schedule
+from LogicalPins import physical_pin
+if cfgget('cron'):
+    # Only import when enabled - memory usage optimization
+    from Scheduler import scheduler
+
+#################################################################
+#            CONFIGURE INTERRUPT MEMORY BUFFER                  #
+#################################################################
+
+
+def emergency_mbuff():
+    emergency_buff_kb = 1000
+    if cfgget('cron') or cfgget('extirq') or cfgget("timirq"):
+        from micropython import alloc_emergency_exception_buf
+        console_write("[IRQ] Interrupts was enabled, alloc_emergency_exception_buf={}".format(emergency_buff_kb))
+        alloc_emergency_exception_buf(emergency_buff_kb)
+    else:
+        console_write("[IRQ] Interrupts disabled, skip alloc_emergency_exception_buf configuration.")
+
+#################################################################
+#                       TIMER INTERRUPT(S)                      #
+#################################################################
+
+#############################################
+#    [TIMER 0] TIMIRQ CBFs - LM executor    #
+#############################################
+
+
+def enableInterrupt():
+    """
+    Set task pool executor in interrupt timer0
+    Input: timirq(bool), timirqseq(ms), timirqcbf(str)
+    """
+    console_write("[IRQ] TIMIRQ SETUP: {} SEQ: {}".format(cfgget("timirq"), cfgget("timirqseq")))
+    console_write("|- [IRQ] TIMIRQ CBF:{}".format(cfgget('timirqcbf')))
+    if cfgget("timirq"):
+        from machine import Timer
+        # INIT TIMER IRQ with callback function wrapper
+        lm_str = cfgget('timirqcbf')
+        timer = Timer(0)
+        timer.init(period=int(cfgget("timirqseq")), mode=Timer.PERIODIC,
+                   callback=lambda timer: exec_lm_pipe_schedule(lm_str))
+
+
+#############################################
+#    [TIMER 1] TIMIRQ CRON - LM executor    #
+#############################################
+
+def enableCron():
+    """
+    Set time stump based scheduler aka cron in timer1
+    Input: cron(bool), crontasks(str)
+    """
+    timer_period = 3000         # Timer period ms
+    console_write("[IRQ] CRON IRQ SETUP: {} SEQ: {}".format(cfgget('cron'), timer_period))
+    console_write("|- [IRQ] CRON CBF:{}".format(cfgget('crontasks')))
+    if cfgget("cron") and cfgget('crontasks').lower() != 'n/a':
+        from machine import Timer
+        # INIT TIMER 1 IRQ with callback function wrapper
+        lm_str = cfgget('crontasks')
+        sample = int(timer_period/1000)
+        timer = Timer(1)
+        timer.init(period=timer_period, mode=Timer.PERIODIC,
+                   callback=lambda timer: scheduler(lm_str, sample))
+
+
+#################################################################
+#                  EVENT/EXTERNAL INTERRUPT(S)                  #
+#################################################################
+# trigger=Pin.IRQ_FALLING   signal HIGH to LOW
+# trigger=Pin.IRQ_RISING    signal LOW to HIGH
+#################################################################
+
+def initEventIRQs():
+    """
+    EVENT INTERRUPT CONFIGURATION - multiple
+    """
+
+    def __edge_exec(_pin_obj, resolver):
+        """
+        Prell filter / edge detection and execution
+        :param _pin_obj: pin obj name
+        :param resolver: callback resolver dict,  LM_cbf obj by pins {PinKey: [LM_cbf, prellTimer]}
+        :return: None
+        """
+        _pin = str(_pin_obj)
+        # Get stored tick by pin - last successful trigger
+        last = resolver.get(_pin)[1]
+        # Calculate trigger diff in ms (from now)
+        diff = ticks_diff(ticks_ms(), last)
+        #console_write(f"[IRQ] Event {_pin} - tick diff: {diff}")
+        # Threshold between ext. irq evens
+        if abs(diff) > resolver.get('prell_ms'):
+            # Save now tick - last trigger action
+            resolver[_pin][1] = ticks_ms()
+            # [!] Execute User Load module by pin number (with micropython.schedule wrapper)
+            #console_write(f"---> action")
+            exec_lm_pipe_schedule(resolver.get(_pin)[0])
+
+    # Load External IRQ execution data set from node config
+    # ((irq, trig, lm_cbf), (irq, trig, lm_cbf), (irq, trig, lm_cbf), ...)
+    _irqdata = ((cfgget("irq1"), cfgget("irq1_trig"), cfgget("irq1_cbf")),
+                (cfgget("irq2"), cfgget("irq2_trig"), cfgget("irq2_cbf")),
+                (cfgget("irq3"), cfgget("irq3_trig"), cfgget("irq3_cbf")),
+                (cfgget("irq4"), cfgget("irq4_trig"), cfgget("irq4_cbf")))
+
+    # [*] hardcopy parameters to be able to resolve cbf-s
+    # cbf_resolver = {'Pin(1)': 'cbf;cbf', 'Pin(2)': 'cbf', ... + 'prell_ms': for example 300ms}
+    cbf_resolver = {'prell_ms': cfgget("irq_prell_ms")}
+    for i, data in enumerate(_irqdata):
+        irq, trig, lm_cbf = data
+        console_write("[IRQ] EXTIRQ SETUP - EXT IRQ{}: {} TRIG: {}".format(i+1, irq, trig))
+        console_write("|- [IRQ] EXTIRQ CBF: {}".format(lm_cbf))
+        if irq:
+            try:
+                pin = physical_pin('irq{}'.format(i + 1))  # irq1, irq2, etc.
+            except Exception as e:
+                msg = '[ERR] EVENT IRQ{} IO error: {}'.format(i+1, e)
+                pin = None
+                console_write("|-- [!] {}".format(msg))
+                errlog_add(msg)
+            if pin:
+                # [*] update resolver dict by pin number (available in irq callback):
+                # PinKey: [CallbackFunction, PrellTimer]  (prell: contact recurrence - fake event filtering... :D)
+                cbf_resolver['Pin({})'.format(pin)] = [lm_cbf, 0]
+                trig = trig.strip().lower()
+                # Init event irq with callback function wrapper
+                # pin_obj = Pin(pin, Pin.IN, Pin.PULL_UP)            #TODO: expose built in resistor parameter ?
+                pin_obj = Pin(pin, Pin.IN, Pin.PULL_DOWN)
+                # [IRQ] - event type setup
+                if trig == 'down':
+                    # pin_obj.irq(trigger=Pin.IRQ_FALLING, handler=lambda pin: print("[down] {}:{}".format(pin, cbf_resolver[str(pin)])))
+                    pin_obj.irq(trigger=Pin.IRQ_FALLING,
+                                handler=lambda pin: __edge_exec(pin, cbf_resolver))
+                    continue
+                if trig == 'both':
+                    # pin_obj.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=lambda pin: print("[both] {}:{}".format(pin, cbf_resolver[str(pin)])))
+                    pin_obj.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,
+                                handler=lambda pin: __edge_exec(pin, cbf_resolver))
+                    continue
+                # Default - 'up'
+                # pin_obj.irq(trigger=Pin.IRQ_RISING, handler=lambda pin: print("[up] {}:{}".format(pin, cbf_resolver[str(pin)])))
+                pin_obj.irq(trigger=Pin.IRQ_RISING,
+                            handler=lambda pin: __edge_exec(pin, cbf_resolver))
+
+#################################################################
+#                         INIT MODULE                           #
+#################################################################
+
+
+emergency_mbuff()
