4ab4313a9ffe3ff1bfd5c7ac57c32dc617751bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:07 +0000

    Add package cachetory/2.6.0/cachetory-2.6.0-py3-none-any.whl

---
+++ packages/cachetory/cachetory-2.6.0-py3-none-any.whl/cachetory/serializers/noop.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import Generic
+
+from cachetory.interfaces.serializers import Serializer, ValueT
+
+
+class NoopSerializer(Serializer[ValueT, ValueT], Generic[ValueT]):
+    """No-operation serializer: just forwards the value to and from backend."""
+
+    @classmethod
+    def from_url(cls, url: str) -> NoopSerializer[ValueT]:
+        return NoopSerializer[ValueT]()
+
+    def serialize(self, value: ValueT) -> ValueT:
+        return value
+
+    def deserialize(self, data: ValueT) -> ValueT:
+        return data
