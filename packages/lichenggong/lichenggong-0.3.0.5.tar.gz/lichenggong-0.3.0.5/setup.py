df5a2727f1c282ed488ed0abea6b7e0534206e0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:25 +0000

    Add package lichenggong/0.3.0.5/lichenggong-0.3.0.5.tar.gz

---
+++ packages/lichenggong/lichenggong-0.3.0.5.tar.gz/lichenggong-0.3.0.5/setup.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+from setuptools import setup
+with open(".\\ver\\Noodows\\version\\version.txt", 'r') as f1:
+    version = f1.readline()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+setup(
+    packages=['ver','ver.Noodows','ver.Noodows.version','ver.Program_Files.System','ver.Program_Files.User'],
+    name="lichenggong",
+    version=version,
+    description="python 3   windows 10 (best)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='',
+    package_data={'':['*.py','*.txt','*.log']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3")
