f7ff3f62ee9e85cc5bc275c2f248e2060d9ffa7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:50 +0000

    Add package gfx-perp-python-sdk/0.0.1/gfx_perp_python_sdk-0.0.1-py3-none-any.whl

---
+++ packages/gfx-perp-python-sdk/gfx_perp_python_sdk-0.0.1-py3-none-any.whl/my_project/product.py
@@ -0,0 +1,21 @@
+class Product:
+    def __init__(self):
+        super()
+
+    def init_by_index():
+        print('init_by_index')
+    
+    def init_by_name():
+        print('init_by_name')
+
+    def get_orderbook_L2():
+        print('ordebook_L2')
+
+    def get_orderbook_L3():
+        print('ordebook_L3')
+
+    def get_trades():
+        print('get_trades')
+
+    def subscribe_to_orderbook():
+        print('subscribe_to_orderbook')
