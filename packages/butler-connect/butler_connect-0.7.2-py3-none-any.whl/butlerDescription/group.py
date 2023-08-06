aa2fa240587cfd9b37057e5071f6c663a1277fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:01 +0000

    Add package butler-connect/0.7.2/butler_connect-0.7.2-py3-none-any.whl

---
+++ packages/butler-connect/butler_connect-0.7.2-py3-none-any.whl/butlerDescription/group.py
@@ -0,0 +1,8 @@
+class GroupType():
+    unDef   = 'undef'
+    groupClimate     = 'group_climate'
+    groupAnalytics   = 'group_analytics'
+    
+class GroupOptionType():
+    unDef = 'undef'
+    forwardingMQTT = 'forwarding_mqtt'
