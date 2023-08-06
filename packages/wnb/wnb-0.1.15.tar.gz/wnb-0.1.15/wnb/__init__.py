96e01249c58a884d1e8bf9671e17887175659f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:29 +0000

    Add package wnb/0.1.15/wnb-0.1.15.tar.gz

---
+++ packages/wnb/wnb-0.1.15.tar.gz/wnb-0.1.15/wnb/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.1.15"
+__author__ = "Mehdi Samsami"
+
+__all__ = [
+    "GeneralNB",
+    "GaussianWNB",
+    "Distribution",
+    "ContinuousDistMixin",
+    "DiscreteDistMixin",
+]
+
+
+from ._base import ContinuousDistMixin, DiscreteDistMixin
+from ._enums import Distribution
+from .gnb import GeneralNB
+from .gwnb import GaussianWNB
---
+++ packages/wnb/wnb-0.1.15.tar.gz/wnb-0.1.15/wnb/_enums.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+__all__ = ["Distribution"]
+
+
+class Distribution(str, Enum):
+    """
+    Names of probability distributions.
+    """
+
+    NORMAL = "Normal"
+    LOGNORMAL = "Lognormal"
+    EXPONENTIAL = "Exponential"
+    UNIFORM = "Uniform"
+    PARETO = "Pareto"
+    GAMMA = "Gamma"
+    BERNOULLI = "Bernoulli"
+    CATEGORICAL = "Categorical"
+    # MULTINOMIAL = "Multinomial"
+    GEOMETRIC = "Geometric"
+    POISSON = "Poisson"
