9f327e7eebdd3a3f6f6118deff190e4a306d9d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:27 +0000

    Add package gocept.country/3.0/gocept.country-3.0-py2.py3-none-any.whl

---
+++ packages/gocept.country/gocept.country-3.0-py2.py3-none-any.whl/gocept/country/tests/test_doctest.py
@@ -0,0 +1,15 @@
+import doctest
+import gocept.country
+import zope.app.wsgi.testlayer
+import zope.testing
+
+
+layer = zope.app.wsgi.testlayer.BrowserLayer(
+    gocept.country, allowTearDown=True)
+
+
+def test_suite():
+    suite = doctest.DocFileSuite(
+        '../README.txt', optionflags=doctest.ELLIPSIS)
+    suite.layer = layer
+    return suite
