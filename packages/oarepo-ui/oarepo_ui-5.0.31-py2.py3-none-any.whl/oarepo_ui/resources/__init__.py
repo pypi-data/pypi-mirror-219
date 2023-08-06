afd4b4b3129673931939c62f939cd0971e852cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:15:06 +0000

    Add package oarepo-ui/5.0.31/oarepo_ui-5.0.31-py2.py3-none-any.whl

---
+++ packages/oarepo-ui/oarepo_ui-5.0.31-py2.py3-none-any.whl/oarepo_ui/resources/__init__.py
@@ -0,0 +1,7 @@
+from .resource import UIResource, RecordsUIResource
+from .config import UIResourceConfig, RecordsUIResourceConfig
+
+__all__ = (
+"UIResource", "RecordsUIResource",
+"UIResourceConfig", "RecordsUIResourceConfig"
+)
