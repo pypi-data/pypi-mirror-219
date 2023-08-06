f03e1a7affe5ef19eb4b99a01fd6452d2a91e256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:40 +0000

    Add package keepercommander/16.9.10/keepercommander-16.9.10-py3-none-any.whl

---
+++ packages/keepercommander/keepercommander-16.9.10-py3-none-any.whl/keepercommander/yubikey/__init__.py
@@ -0,0 +1,14 @@
+#  _  __
+# | |/ /___ ___ _ __  ___ _ _ ®
+# | ' </ -_) -_) '_ \/ -_) '_|
+# |_|\_\___\___| .__/\___|_|
+#              |_|
+#
+# Keeper Commander
+# Copyright 2019 Keeper Security Inc.
+# Contact: ops@keepersecurity.com
+#
+
+from .yubikey import yubikey_authenticate
+
+__all__ = ['yubikey_authenticate']
