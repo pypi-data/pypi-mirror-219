7855f4f420c08131eabc3bb8c40e41dea553b34a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:03 +0000

    Add package Colorthon/2.9.3/colorthon-2.9.3.tar.gz

---
+++ packages/Colorthon/colorthon-2.9.3.tar.gz/colorthon-2.9.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="colorthon",
+    version="2.9.3",
+    license="MIT",
+    author="MOHAMMADREZA FEKRI",
+    author_email="Pymmdrza@Gmail.Com",
+    packages=find_packages('src'),
+    url="https://github.com/colorthon/colorthon",
+    keywords=['colorthon', 'color', 'text color', 'python color', 'text color', 'python colors', 'print colors']
+
+)
