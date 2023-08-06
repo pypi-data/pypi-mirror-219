de1130b5e1ce1aff0e9b1a62a07249907bfca705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:58 +0000

    Add package abstra/1.4.0/abstra-1.4.0-py3-none-any.whl

---
+++ packages/abstra/abstra-1.4.0-py3-none-any.whl/abstra_server/apps/__init__.py
@@ -0,0 +1,30 @@
+import os, flask, flask_cors
+
+from ..api import API
+from ..overloads import overloads
+from .editor import get_editor_bp
+from .player import get_player_bp
+
+HOST = os.getenv("ABSTRA_HOST", "localhost")
+
+
+def create_app(api: API):
+    app = flask.Flask(__name__)
+    flask_cors.CORS(app)
+
+    editor = get_editor_bp(api)
+    app.register_blueprint(editor, url_prefix="/_editor")
+
+    player = get_player_bp(api)
+    app.register_blueprint(player)
+
+    return app
+
+
+def serve(workspace_root: str, port: int, debug, use_reloader):
+    os.environ["ABSTRA_SERVER"] = "true"
+    overloads()
+
+    api = API(workspace_root)
+    app = create_app(api)
+    app.run(host=HOST, port=port, debug=debug, use_reloader=use_reloader)
---
+++ packages/abstra/abstra-1.4.0-py3-none-any.whl/abstra_server/runtimes/jobs/__init__.py
@@ -0,0 +1,10 @@
+import traceback
+from ...session import StaticSession
+
+
+def run_job(code: str, session: StaticSession):
+    try:
+        namespace: dict = {}
+        exec(code, namespace, namespace)
+    except:
+        traceback.print_exc()
