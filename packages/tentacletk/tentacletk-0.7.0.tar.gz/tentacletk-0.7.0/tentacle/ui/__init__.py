53e70cf3190dba118934f620e5e9c9b340457354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:42 +0000

    Add package tentacletk/0.7.0/tentacletk-0.7.0.tar.gz

---
+++ packages/tentacletk/tentacletk-0.7.0.tar.gz/tentacletk-0.7.0/tentacle/ui/__init__.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python
+# coding=utf-8
+import inspect
+import pythontk as ptk
+
+
+module = inspect.getmodule(inspect.currentframe())  # this module.
+path = ptk.get_filepath(module)  # this modules directory.
+
+# --------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
