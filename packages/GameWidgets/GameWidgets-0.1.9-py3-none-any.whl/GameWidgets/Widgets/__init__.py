84bd571057546b70040b11f284c2e2c70f5f16ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:39 +0000

    Add package GameWidgets/0.1.9/GameWidgets-0.1.9-py3-none-any.whl

---
+++ packages/GameWidgets/GameWidgets-0.1.9-py3-none-any.whl/GameWidgets/Widgets/__init__.py
@@ -0,0 +1,6 @@
+try: 
+    from GameWidgets.Widgets import Btn,Clock, Cursor,Sound,DialogeBox,TextInp,ImgDecoder,ToggleBtn
+    from GameWidgets import Constants
+except:
+    print('Widgets Failed to Import!')
+    quit()
