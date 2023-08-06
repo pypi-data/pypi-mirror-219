4930ad140c04ac93b378e3b2d7df3e0693b4b5c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:55 +0000

    Add package quick-menu/0.0.1/quick_menu-0.0.1-py3-none-any.whl

---
+++ packages/quick-menu/quick_menu-0.0.1-py3-none-any.whl/quick_menu/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Stephan Poole (e122785) <stephan.poole@honeywell.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ packages/quick-menu/quick_menu-0.0.1-py3-none-any.whl/quick_menu/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present Stephan Poole (e122785) <stephan.poole@honeywell.com>
+#
+# SPDX-License-Identifier: MIT
