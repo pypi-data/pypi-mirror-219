baaa49fd034b7aa8344b521f6d546bf0c77c6508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:40 +0000

    Add package banking-logging/0.2.1/banking_logging-0.2.1.tar.gz

---
+++ packages/banking-logging/banking_logging-0.2.1.tar.gz/banking_logging-0.2.1/setup.py
@@ -0,0 +1,20 @@
+#
+import setuptools
+from setuptools import setup
+
+
+metadata = {'name': 'banking_logging',
+            'maintainer': 'Edward Azizov',
+            'maintainer_email': 'edazizovv@gmail.com',
+            'description': 'Shared loggers for banking_api project',
+            'license': 'Proprietary',
+            'url': '',
+            'download_url': '',
+            'packages': setuptools.find_packages(),
+            'include_package_data': True,
+            'version': '0.2.1',
+            'long_description': '',
+            'python_requires': '>=3.10',
+            'install_requires': []}
+
+setup(**metadata)
