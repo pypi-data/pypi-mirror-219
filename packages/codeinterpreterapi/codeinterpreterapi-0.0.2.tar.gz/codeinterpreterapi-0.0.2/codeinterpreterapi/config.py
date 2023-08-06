704162de9dc68e4cbc69bb3872d9ee2f338f0fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:36 +0000

    Add package codeinterpreterapi/0.0.2/codeinterpreterapi-0.0.2.tar.gz

---
+++ packages/codeinterpreterapi/codeinterpreterapi-0.0.2.tar.gz/codeinterpreterapi-0.0.2/codeinterpreterapi/config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+
+# .env file
+load_dotenv()
+
+
+class CodeInterpreterAPISettings(BaseSettings):
+    """
+    CodeInterpreter API Config
+    """
+
+    VERBOSE: bool = False
+
+    CODEBOX_API_KEY: str | None = None
+    OPENAI_API_KEY: str | None = None
+
+
+settings = CodeInterpreterAPISettings()
