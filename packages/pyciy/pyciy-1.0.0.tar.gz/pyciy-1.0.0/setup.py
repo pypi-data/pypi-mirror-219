e3d907dddbd5ab0b43370b1163d50740922aa1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:04 +0000

    Add package pyciy/1.0.0/pyciy-1.0.0.tar.gz

---
+++ packages/pyciy/pyciy-1.0.0.tar.gz/pyciy-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+import setuptools
+setup(name='pyciy',
+    version='1.0.0',
+    author='boiciy',
+    packages=['pyciy'], 
+    package_dir={'requests': 'requests'},
+    )
