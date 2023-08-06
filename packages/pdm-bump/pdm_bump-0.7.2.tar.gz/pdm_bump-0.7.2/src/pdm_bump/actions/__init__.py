4760b0cb5839a02e754eeef1f2dec7f50ab666d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:14 +0000

    Add package pdm-bump/0.7.2/pdm_bump-0.7.2.tar.gz

---
+++ packages/pdm-bump/pdm_bump-0.7.2.tar.gz/pdm_bump-0.7.2/src/pdm_bump/actions/__init__.py
@@ -0,0 +1,24 @@
+#
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021-2023 Carsten Igel.
+#
+# This file is part of pdm-bump
+# (see https://github.com/carstencodes/pdm-bump).
+#
+# This file is published using the MIT license.
+# Refer to LICENSE for more information
+#
+""""""
+
+from ..core.loader import loader as _loader
+from .base import ActionBase, VersionConsumer, VersionModifier, actions
+
+_loader.load_modules_of_package(__file__, __name__, "__init__", "base")
+
+__all__ = [
+    "actions",
+    "ActionBase",
+    "VersionConsumer",
+    "VersionModifier",
+]
