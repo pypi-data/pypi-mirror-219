7c8a6eeccfede1ad561a33093f01c9aa8c97ae5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:59 +0000

    Add package connectors-to-databases/1.0.1/connectors_to_databases-1.0.1-py3-none-any.whl

---
+++ packages/connectors-to-databases/connectors_to_databases-1.0.1-py3-none-any.whl/connectors_to_databases/__init__.py
@@ -0,0 +1,13 @@
+"""
+:authors: k0rsakov
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2022 k0rsakov
+"""
+
+# from .ClickHouse import ClickHouse
+# from .PostgreSQL import PostgreSQL
+# from .TypeHinting import SQLQuery
+
+# __author__ = 'k0rsakov'
+# __version__ = '1.0.0'
+# __email__ = 'korsakov.iyu@gmail.com'
