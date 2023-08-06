d48b053126d28eb21848491eba018e26306e6a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:16 +0000

    Add package meteo-hr/1.0.2/meteo_hr-1.0.2.tar.gz

---
+++ packages/meteo-hr/meteo_hr-1.0.2.tar.gz/meteo_hr-1.0.2/meteo_hr/places.py
@@ -0,0 +1,25 @@
+import locale
+from typing import List
+from . import Place3D, Place7D, cache, api
+
+
+# Required for locale-aware sorting
+locale.setlocale(locale.LC_ALL, "hr_HR.utf8")
+
+
+def list_3d() -> List[Place3D]:
+    places = cache.load_3d()
+    if not places:
+        places = api.fetch_places_3d()
+        places = sorted(places, key=lambda p: locale.strxfrm(p.name))
+        cache.save_3d(places)
+    return places
+
+
+def list_7d() -> List[Place7D]:
+    places = cache.load_7d()
+    if not places:
+        places = api.fetch_places_7d()
+        places = sorted(places, key=lambda p: locale.strxfrm(p.name))
+        cache.save_7d(places)
+    return places
