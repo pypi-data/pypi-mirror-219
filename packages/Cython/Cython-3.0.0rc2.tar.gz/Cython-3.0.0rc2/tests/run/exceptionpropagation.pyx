9d29444b7c8aca0179ad6a5ea3cfc73917b6ccbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:01 +0000

    Add package Cython/3.0.0rc2/Cython-3.0.0rc2.tar.gz

---
+++ packages/Cython/Cython-3.0.0rc2.tar.gz/Cython-3.0.0rc2/tests/run/exceptionpropagation.pyx
@@ -0,0 +1,81 @@
+cdef int CHKERR(int ierr) except -1:
+    if ierr==0: return 0
+    raise RuntimeError
+
+cdef int obj2int(object ob) except *:
+    return ob
+
+def foo(a):
+    """
+    >>> foo(0)
+    >>> foo(1)
+    Traceback (most recent call last):
+    RuntimeError
+    """
+    cdef int i = obj2int(a)
+    CHKERR(i)
+
+cdef int* except_expr(bint fire) except <int*>-1:
+    if fire:
+        raise RuntimeError
+
+def test_except_expr(bint fire):
+    """
+    >>> test_except_expr(False)
+    >>> test_except_expr(True)
+    Traceback (most recent call last):
+    ...
+    RuntimeError
+    """
+    except_expr(fire)
+
+cdef double except_big_result(bint fire) except 100000000000000000000000000000000:
+    if fire:
+        raise RuntimeError
+
+def test_except_big_result(bint fire):
+    """
+    >>> test_except_big_result(False)
+    >>> test_except_big_result(True)
+    Traceback (most recent call last):
+    ...
+    RuntimeError
+    """
+    except_big_result(fire)
+
+
+cdef unsigned short except_promotion_compare(bint fire) except *:
+    if fire:
+        raise RuntimeError
+
+def test_except_promotion_compare(bint fire):
+    """
+    >>> test_except_promotion_compare(False)
+    >>> test_except_promotion_compare(True)
+    Traceback (most recent call last):
+    ...
+    RuntimeError
+    """
+    except_promotion_compare(fire)
+
+
+cdef int cdef_function_that_raises():
+    raise RuntimeError
+
+cdef int cdef_noexcept_function_that_raises() noexcept:
+    raise RuntimeError
+
+def test_except_raise_by_default():
+    """
+    >>> test_except_raise_by_default()
+    Traceback (most recent call last):
+    ...
+    RuntimeError
+    """
+    cdef_function_that_raises()
+
+def test_noexcept():
+    """
+    >>> test_noexcept()
+    """
+    cdef_noexcept_function_that_raises()
